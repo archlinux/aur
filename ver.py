f445eeef7bdad9b5897377ef812dd0798dba14e7
Yen Chi Hsuan <yan12125@gmail.com>
2014-09-21 22:35:09 +0800

    Script for checking versions of multiple packages

--- ver.py
+++ ver.py
@@ -1,6 +1,8 @@
 import requests
 import pyquery
+import re
 
 r = requests.get('http://flex.apache.org/download-binaries.html')
 d = pyquery.PyQuery(r.text)
-print(d('.headline h4').eq(0).text())
+ver_text = d('.headline h4').eq(0).text()
+print(re.match('^Current Release \((.*)\)$', ver_text).group(1))
