80bd1de2e8bdae1aac9f49f50ed39e1b93f0f990
Max <silverhammermba@gmail.com>
2014-10-17 10:43:15 -0400

    Arch no longer sets JAVA_HOME

--- gcs.sh
+++ gcs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if ! "$JAVA_HOME/bin/java" -version 2>&1 | grep 'version "1.8' >/dev/null
+if ! /usr/bin/java -version 2>&1 | grep 'version "1.8' >/dev/null
 then
 	echo "GURPS character sheet requires Java 8" >&2
 	exit 1
 fi
-GCS_HOME=/usr/share/gcs exec "$JAVA_HOME/bin/java" -jar '/usr/share/java/gcs/gcs.jar' "$@"
+GCS_HOME=/usr/share/gcs exec /usr/bin/java -jar '/usr/share/java/gcs/gcs.jar' "$@"
