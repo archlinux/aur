7c0df40082525c9afda6766ba4481ac912b97c38
Moritz Kaspar Rudert (mortzu) <me@mortzu.de>
2015-06-08 10:31:38 +0200

    stuff

--- PKGBUILD
+++ PKGBUILD
@@ -14,15 +14,18 @@ provides=('gitlab-ci-runner')
 install=gitlab-ci-runner.install
 source=("$pkgname-$pkgver.tar.gz::https://gitlab.com/gitlab-org/gitlab-ci-runner/repository/archive.tar.gz?ref=v${pkgver}"
         gitlab-ci-runner.service
-        gitlab-ci-runner.install)
+        gitlab-ci-runner.install
+        gitlab-ci-runner.sh)
 md5sums=('6cd87adb07405a2fa0acbefe31fef5c1'
          'af4748081599d3379ab63fb2f6e4e94f'
-         '6c24857d02eba34082ba0437daa6e91d')
+         '6c24857d02eba34082ba0437daa6e91d'
+         'c8fcc9f0193d9cc80a74f9e4cf213977')
 
 package() {
+  mkdir -p "$pkgdir/usr/lib"
   cp -r "$srcdir/gitlab-ci-runner.git" "$pkgdir/usr/lib/${pkgname}"
 
   # Create systemd service
   install -Dm644 "$srcdir/gitlab-ci-runner.service" "$pkgdir/usr/lib/systemd/system/gitlab-ci-runner.service"
-    echo "Install the gems for the runner: bundle install --deployment"
+  install -Dm777 "$srcdir/gitlab-ci-runner.sh" "$pkgdir/usr/bin/gitlab-ci-runner"
 }
--- gitlab-ci-runner.sh
+++ gitlab-ci-runner.sh
@@ -1,25 +1,50 @@
 #! /usr/bin/env bash
 
+# Switch to GitLab user if necessary
 if [ "$(id -nu)" != 'gitlab_ci_runner' ]; then
   sudo -u gitlab_ci_runner -H $0 $*
 fi
 
-cd /usr/lib/gitlab-ci-runner
+# Change to install directory
+cd /usr/lib/gitlab-ci-runner/
 
-if ! bundle install --deployment >/dev/null 2>/var/log/gitlab-ci-runner/bundler.log; then
-  echo 'foo' >&2
+# Install a newer JSON gem
+sed -e "s#gem 'json', '~> 1.7.7'#gem 'json', '~> 1.8.2'#" -i /usr/lib/gitlab-ci-runner/Gemfile
+
+# Install ruby dependencies, 'cause of changed Gemfile
+if ! bundle install --path vendor/bundle --no-deployment >/dev/null 2>/var/log/gitlab-ci-runner/bundler.log; then
+  echo 'Installing ruby dependency failed!' >&2
   exit 1
 fi
 
-case $1 in
+# Parse commandline arguments
+case "$1" in
   register)
-    CI_SERVER_URL=https://ci.example.com REGISTRATION_TOKEN=replaceme bundle exec ./bin/setup
+    # Configure GitLab CI runner
+    bundle exec ./bin/setup
   ;;
   start)
+    # Start GitLab CI runner
+    bundle exec ./bin/runner >>/var/log/gitlab-ci-runner/gitlog.log 2>&1 </dev/null
   ;;
   stop)
+    # Check if PID file exists
+    if [ ! -e /run/gitlab-ci-runner.pid ]; then
+      echo 'PID file does not exists!' >&2
+      exit 1
+    fi
+
+    # Try to kill runner
+    if kill $(</run/gitlab-ci-runner.pid) >/dev/null 2>&1; then
+      echo 'Gitlab CI runner stopped.'
+    else
+      echo 'Stopping failed!' >&2
+      exit 1
+    fi
   ;;
   restart)
+    $0 stop
+    $0 start
   ;;
   *)
     echo "Usage: $0 {start|stop|restart|register}"
