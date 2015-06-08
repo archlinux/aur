kyrias/pkgbuilds

Removed unnecesary references to $srcdir

PKGBUILD
@@ -22,17 +22,17 @@ source=('git://github.com/sampsyo/beets.git')
 md5sums=('SKIP')
 
 pkgver() {
-	cd "$srcdir"/beets
+	cd beets
 	printf "%s" "$(git describe --tags --long | sed 's/^v//;s/-/-r/' | tr - .)" 
 }
 
 build() {
-	cd "$srcdir"/beets
+	cd beets
 	python2 setup.py build
 }
 
 package() {
-	cd "$srcdir"/beets
+	cd beets
 	python2 setup.py install --root="$pkgdir" --optimize=1
 
 	install -D -m644 LICENSE "$pkgdir/usr/share/licenses/$pkgname/LICENSE"

archlinux/aur

PKGBUILD
@@ -19,13 +19,13 @@ source=('git://pcmanfm.git.sourceforge.net/gitroot/pcmanfm/pcmanfm')
 md5sums=('SKIP')
 
 pkgver() {
-	cd "$srcdir"/pcmanfm
+	cd pcmanfm
 
 	printf "%s" "$(git describe --always --long | sed 's/-/-r/' | tr - .)"
 }
 
 build() {
-	cd "$srcdir"/pcmanfm
+	cd pcmanfm
 
 	msg "Starting make..."
 	./autogen.sh
@@ -34,7 +34,7 @@ build() {
 }
 
 package() {
-	cd "$srcdir"/pcmanfm
+	cd pcmanfm
 	make DESTDIR="$pkgdir" install
 }
 