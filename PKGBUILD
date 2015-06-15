f5b0ed6504f740873fd59b8a472bf03d51950e5e
petres <peter.reschenhofer@gmail.com>
2015-06-15 13:11:14 +0200

    updating to version 26

--- PKGBUILD
+++ PKGBUILD
@@ -1,8 +1,8 @@
-# Maintainer: Lev Lybin <lev.lybin@gmail.com>
+# Maintainer: Peter Reschenhofer  <peter.reschenhofer@gmail.com>
 # Contributor: Lev Lybin <lev.lybin@gmail.com>
 
 pkgname=gnome-shell-extension-icontopbar
-pkgver=3.6.3
+pkgver=26
 pkgrel=1
 pkgdesc="A gnome shell extension that places the tray icons of apps on the top bar."
 url="https://extensions.gnome.org/extension/495/topicons"
@@ -10,13 +10,14 @@ license=('GPL2')
 arch=('any')
 conflicts=('gnome-shell-extension-gnome2-notifications-git')
 source=('metadata.json' 'extension.js')
-md5sums=('352116d9fb76c8e81d9bba3224ddddcc' '2875ed67b0ea16b151c8c4c046d8606d')
 
-_extpath="$pkgdir/usr/share/gnome-shell/extensions"
+_extpath="usr/share/gnome-shell/extensions"
 _extname="topIcons@adel.gadllah@gmail.com"
 
 package() {
+  cd $pkgdir
   mkdir -p "$_extpath/$_extname"
-  cd "$srcdir"
-  cp extension.js metadata.json "$_extpath/$_extname"
+  cp "$srcdir"/* "$_extpath/$_extname"
 }
+
+md5sums=('6f1e54a373f116d66e63bb7e8c4ae065' '6d7acd94cb86e49904b9928c20ed0de0')
--- extension.js
+++ extension.js
@@ -1,35 +1,40 @@
 // -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
 
+const Clutter = imports.gi.Clutter;
 const Shell = imports.gi.Shell;
+const St = imports.gi.St;
 const Main = imports.ui.main;
+const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Panel = imports.ui.panel;
 const PanelMenu = imports.ui.panelMenu;
+const Meta = imports.gi.Meta;
+const Mainloop = imports.mainloop;
 const NotificationDaemon = imports.ui.notificationDaemon;
 
 let trayAddedId = 0;
 let trayRemovedId = 0;
-let fullScreenChangedId = 0;
 let getSource = null;
 let icons = [];
+let notificationDaemon;
 
 function init() {
-    getSource = Lang.bind(Main.notificationDaemon, NotificationDaemon.NotificationDaemon.prototype._getSource);
+    if (Main.legacyTray) {
+        notificationDaemon = Main.legacyTray;
+        NotificationDaemon.STANDARD_TRAY_ICON_IMPLEMENTATIONS = imports.ui.legacyTray.STANDARD_TRAY_ICON_IMPLEMENTATIONS;
+    }
+    else if (Main.notificationDaemon._fdoNotificationDaemon) {
+        notificationDaemon = Main.notificationDaemon._fdoNotificationDaemon;
+        getSource = Lang.bind(notificationDaemon, NotificationDaemon.FdoNotificationDaemon.prototype._getSource);
+    }
+    else {
+        notificationDaemon = Main.notificationDaemon;
+        getSource = Lang.bind(notificationDaemon, NotificationDaemon.NotificationDaemon.prototype._getSource);
+    }
 }
 
 function enable() {
-    moveToTop();
-
-    // TrayIcons do not survive leaving the stage (they end up as white squares), so work around this
-    // by temporarily move them back to the message tray while we are in fullscreen.
-    fullScreenChangedId = Main.layoutManager.connect('primary-fullscreen-changed', function (o, state) {
-            if (state) {
-                moveToTray();
-            }
-            else {
-                moveToTop();
-            }
-    });
+    GLib.idle_add(GLib.PRIORITY_LOW, moveToTop);
 }
 
 function createSource (title, pid, ndata, sender, trayIcon) { 
@@ -44,20 +49,63 @@ function createSource (title, pid, ndata, sender, trayIcon) {
 function onTrayIconAdded(o, icon, role) {
     let wmClass = icon.wm_class ? icon.wm_class.toLowerCase() : '';
     if (NotificationDaemon.STANDARD_TRAY_ICON_IMPLEMENTATIONS[wmClass] !== undefined)
-            return;
+        return;
 
     let buttonBox = new PanelMenu.ButtonBox();
     let box = buttonBox.actor;
     let parent = box.get_parent();
 
-    icon.height = Panel.PANEL_ICON_SIZE;
+    let scaleFactor = St.ThemeContext.get_for_stage(global.stage).scale_factor;
+    let iconSize = Panel.PANEL_ICON_SIZE * scaleFactor;
+
+    icon.set_size(iconSize, iconSize);
     box.add_actor(icon);
+
+    icon.reactive = true;
 
     if (parent)
         parent.remove_actor(box);
 
     icons.push(icon);
     Main.panel._rightBox.insert_child_at_index(box, 0);
+
+    let clickProxy = new St.Bin({ width: iconSize, height: iconSize });
+    clickProxy.reactive = true;
+    Main.uiGroup.add_actor(clickProxy);
+
+    icon._proxyAlloc = Main.panel._rightBox.connect('allocation-changed', function() {
+        Meta.later_add(Meta.LaterType.BEFORE_REDRAW, function() {
+            let [x, y] = icon.get_transformed_position();
+            clickProxy.set_position(x, y);
+        });
+    });
+
+    icon.connect("destroy", function() {
+        Main.panel._rightBox.disconnect(icon._proxyAlloc);
+        clickProxy.destroy();
+    });
+
+    clickProxy.connect('button-release-event', function(actor, event) {
+        icon.click(event);
+    });
+
+    icon._clickProxy = clickProxy;
+
+    /* Fixme: HACK */
+    Meta.later_add(Meta.LaterType.BEFORE_REDRAW, function() {
+        let [x, y] = icon.get_transformed_position();
+        clickProxy.set_position(x, y);
+        return false;
+    });
+    let timerId = 0;
+    let i = 0;
+    timerId = Mainloop.timeout_add(500, function() {
+        icon.set_size(icon.width == iconSize ? iconSize - 1 : iconSize,
+                      icon.width == iconSize ? iconSize - 1 : iconSize);
+        i++;
+        if (i == 2)
+            Mainloop.source_remove(timerId);
+    });
 }
 
 function onTrayIconRemoved(o, icon) {
@@ -68,53 +116,72 @@ function onTrayIconRemoved(o, icon) {
 }
 
 function moveToTop() {
-    Main.notificationDaemon._trayManager.disconnect(Main.notificationDaemon._trayIconAddedId);
-    Main.notificationDaemon._trayManager.disconnect(Main.notificationDaemon._trayIconRemovedId);
-    trayAddedId = Main.notificationDaemon._trayManager.connect('tray-icon-added', onTrayIconAdded);
-    trayRemovedId = Main.notificationDaemon._trayManager.connect('tray-icon-removed', onTrayIconRemoved);
+    notificationDaemon._trayManager.disconnect(notificationDaemon._trayIconAddedId);
+    notificationDaemon._trayManager.disconnect(notificationDaemon._trayIconRemovedId);
+    trayAddedId = notificationDaemon._trayManager.connect('tray-icon-added', onTrayIconAdded);
+    trayRemovedId = notificationDaemon._trayManager.connect('tray-icon-removed', onTrayIconRemoved);
     
-    Main.notificationDaemon._getSource = createSource;
+    notificationDaemon._getSource = createSource;
 
     let toDestroy = [];
-    for (let i = 0; i < Main.notificationDaemon._sources.length; i++) {
-        let source = Main.notificationDaemon._sources[i];
-        if (!source.trayIcon)
-            continue;
-        let parent = source.trayIcon.get_parent();
-        parent.remove_actor(source.trayIcon);
-        onTrayIconAdded(this, source.trayIcon, source.initialTitle);
-        toDestroy.push(source);
+    if (notificationDaemon._sources) {
+        for (let i = 0; i < notificationDaemon._sources.length; i++) {
+            let source = notificationDaemon._sources[i];
+            if (!source.trayIcon)
+                continue;
+            let parent = source.trayIcon.get_parent();
+            parent.remove_actor(source.trayIcon);
+            onTrayIconAdded(this, source.trayIcon, source.initialTitle);
+            toDestroy.push(source);
+        }
+    }
+    else {
+        for (let i = 0; i < notificationDaemon._iconBox.get_n_children(); i++) {
+            let button = notificationDaemon._iconBox.get_child_at_index(i);
+            let icon = button.child;
+            button.remove_actor(icon);
+            onTrayIconAdded(this, icon, '');
+            toDestroy.push(button);
+        }
     }
 
-     for (let i = 0; i < toDestroy.length; i++) {
+    for (let i = 0; i < toDestroy.length; i++) {
         toDestroy[i].destroy();
-     }
+    }
 }
 
 function moveToTray() {
     if (trayAddedId != 0) {
-        Main.notificationDaemon._trayManager.disconnect(trayAddedId);
+        notificationDaemon._trayManager.disconnect(trayAddedId);
         trayAddedId = 0;
     }
 
     if (trayRemovedId != 0) {
-        Main.notificationDaemon._trayManager.disconnect(trayRemovedId);
+        notificationDaemon._trayManager.disconnect(trayRemovedId);
         trayRemovedId = 0;
     }
     
-    Main.notificationDaemon._trayIconAddedId = Main.notificationDaemon._trayManager.connect('tray-icon-added',
-                                                Lang.bind(Main.notificationDaemon, Main.notificationDaemon._onTrayIconAdded));
-    Main.notificationDaemon._trayIconRemovedId = Main.notificationDaemon._trayManager.connect('tray-icon-removed', 
-                                                Lang.bind(Main.notificationDaemon, Main.notificationDaemon._onTrayIconRemoved));
+    notificationDaemon._trayIconAddedId = notificationDaemon._trayManager.connect('tray-icon-added',
+                                                Lang.bind(notificationDaemon, notificationDaemon._onTrayIconAdded));
+    notificationDaemon._trayIconRemovedId = notificationDaemon._trayManager.connect('tray-icon-removed',
+                                                Lang.bind(notificationDaemon, notificationDaemon._onTrayIconRemoved));
 
-    Main.notificationDaemon._getSource = getSource;
+    notificationDaemon._getSource = getSource;
 
     for (let i = 0; i < icons.length; i++) {
         let icon = icons[i];
         let parent = icon.get_parent();
+        if (icon._clicked) {
+            icon.disconnect(icon._clicked);
+        }
+        icon._clicked = undefined;
+        if (icon._proxyAlloc) {
+            Main.panel._rightBox.disconnect(icon._proxyAlloc);
+        }
+        icon._clickProxy.destroy();
         parent.remove_actor(icon);
         parent.destroy();
-        Main.notificationDaemon._onTrayIconAdded(Main.notificationDaemon, icon);
+        notificationDaemon._onTrayIconAdded(notificationDaemon, icon);
     }
     
     icons = [];
@@ -122,9 +189,4 @@ function moveToTray() {
 
 function disable() {
     moveToTray();
-
-    if (fullScreenChangedId != 0) {
-        Main.layoutManager.disconnect(fullScreenChangedId);
-        fullScreenChangedId = 0;
-    }
 }
