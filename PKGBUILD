310437d2c5f06fbea063f0c96a2de6eb6512cd81
Alain Kalker <a.c.kalker@gmail.com>
2015-07-02 20:16:12 +0200

    Update package
    - Cleanup whitespace

--- PKGBUILD
+++ PKGBUILD
@@ -9,14 +9,14 @@ url="http://www.chromium.org/chromium-os"
 license=('GPL')
 depends=('chromiumos-ui' 'matchbox-window-manager')
 source=($_pkgbase.session
-	$_pkgbase-lastuser.session
-	$pkgname.sh)
+        $_pkgbase-lastuser.session
+        $pkgname.sh)
 md5sums=('141fb7abef51fee459d193300003611a'
          '512a7421e124b24cb3e822d0b637b4bc'
-         '1b8dda2e9b4657313a7c14a0cdb8791f')
+         '9fdd31dafaa424137527143782a84863')
 
 package() {
-	install -Dm644 "$srcdir/$_pkgbase.session" "$pkgdir/usr/share/xsessions/$_pkgbase.desktop"
-	install -Dm644 "$srcdir/$_pkgbase-lastuser.session" "$pkgdir/usr/share/xsessions/$_pkgbase-lastuser.desktop"
-	install -D "$srcdir/$pkgname.sh" "$pkgdir/usr/bin/$pkgname"
+  install -Dm644 "$srcdir/$_pkgbase.session" "$pkgdir/usr/share/xsessions/$_pkgbase.desktop"
+  install -Dm644 "$srcdir/$_pkgbase-lastuser.session" "$pkgdir/usr/share/xsessions/$_pkgbase-lastuser.desktop"
+  install -D "$srcdir/$pkgname.sh" "$pkgdir/usr/bin/$pkgname"
 }
--- chromiumos-ui-session.sh
+++ chromiumos-ui-session.sh
@@ -8,73 +8,73 @@ set -m
 
 LOGINFO=$(basename "$0")"($$)"
 log() {
-	sed -e "s/^/$LOGINFO: /" >&2
+    sed -e "s/^/$LOGINFO: /" >&2
 }
 
 get_pgid() {
-	ps -o pgid= $1 | tr -d ' '
+    ps -o pgid= $1 | tr -d ' '
 }
 
 get_pgid_pids() {
-	pgrep -g $1
+    pgrep -g $1
 }
 
 log_remaining_pids() {
-	local pids
-	pids=$*
+    local pids
+    pids=$*
 
-	echo "Remaining processes: ${pids:-none}" | log
+    echo "Remaining processes: ${pids:-none}" | log
 }
 
 kill_pgid() {
-	local pgid signal
-	pgid=$1
-	signal=${2:-TERM}
+    local pgid signal
+    pgid=$1
+    signal=${2:-TERM}
 
-	echo "Sending SIG$signal to PGID: $pgid" | log
-	kill -$signal -$pgid
+    echo "Sending SIG$signal to PGID: $pgid" | log
+    kill -$signal -$pgid
 }
 
 kill_group_pgid() {
-	local pgid pause pids
-	pgid=$1
-	pause=$2
+    local pgid pause pids
+    pgid=$1
+    pause=$2
 
-	for signal in TERM KILL; do
-		# Check if any processes remain
-		pids=$(get_pgid_pids $pgid)
-		[[ -z "$pids" ]] && return
+    for signal in TERM KILL; do
+        # Check if any processes remain
+        pids=$(get_pgid_pids $pgid)
+        [[ -z "$pids" ]] && return
 
-		# Give processes some time to exit on their own
-		[[ -n "$pause" ]] && sleep .5
+        # Give processes some time to exit on their own
+        [[ -n "$pause" ]] && sleep .5
 
-		kill_pgid $pgid $signal
+        kill_pgid $pgid $signal
 
-		for i in 1 2 3 4; do
-			# Wait for remaining processes to exit on their own
-			sleep .5
+        for i in 1 2 3 4; do
+            # Wait for remaining processes to exit on their own
+            sleep .5
 
-			# Check if any processes remain
-			pids=$(get_pgid_pids $pgid)
-			[[ -z "$pids" ]] && return
+            # Check if any processes remain
+            pids=$(get_pgid_pids $pgid)
+            [[ -z "$pids" ]] && return
 
-			log_remaining_pids $pids
-		done
-	done
+            log_remaining_pids $pids
+        done
+    done
 }
 
 log_process_tree_sid() {
-	echo "Process tree for SID: $1:" | log
-	ps jf -s $1 | log
+    echo "Process tree for SID: $1:" | log
+    ps jf -s $1 | log
 }
 
 cleanup() {
-	echo "Starting cleanup." | log
-	log_process_tree_sid $SID
-	kill_group_pgid $CHROMIUMOS_UI_PGID pause
-	kill_group_pgid $MATCHBOX_WM_PGID
-	log_process_tree_sid $SID
-	echo "Finished cleanup." | log
+    echo "Starting cleanup." | log
+    log_process_tree_sid $SID
+    kill_group_pgid $CHROMIUMOS_UI_PGID pause
+    kill_group_pgid $MATCHBOX_WM_PGID
+    log_process_tree_sid $SID
+    echo "Finished cleanup." | log
 }
 
 echo "Starting session." | log
