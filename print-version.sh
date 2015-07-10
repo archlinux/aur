0ce2258d9c7b7dcd7f72aa3172edbd9e85488948
Jeremy Audet <ichimonji10@gmail.com>
2015-07-10 08:25:40 -0400

    Update script for checking installed libs
    Do not change functionality of `print-version.sh` script. Deduplicate some code
    by making use of a for loop. (Duh!)

--- print-version.sh
+++ print-version.sh
@@ -3,16 +3,11 @@
 # Print the currently installed django-extensions versions.
 #
 
-python - <<EOF
+for interpreter in python python2; do
+"${interpreter}" - <<EOF
 import faker
 import sys
 print('Python version: {}'.format(tuple(sys.version_info)))
 print('faker version: {}'.format(faker.VERSION))
 EOF
-
-python2 - <<EOF
-import faker
-import sys
-print('Python version: {}'.format(tuple(sys.version_info)))
-print('faker version: {}'.format(faker.VERSION))
-EOF
+done
