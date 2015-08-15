c15a2b05df55ee9a4afe61c4f53b46860e45d612
Chris Severance <aur.severach@spamgourmet.com>
2015-08-14 22:45:52 -0400

    Update to 15.01

--- basis-pro5-install.sh
+++ basis-pro5-install.sh
@@ -1,27 +1,31 @@
 post_upgrade() {
-  #mandb -q
   systemctl daemon-reload
-  #depmod -a
-  systemctl -q is-enabled "basis_lmgrd.service"
-  if [ "$?" -eq "0" ]; then  
+  if systemctl -q is-enabled "basis_lmgrd.service"; then
     systemctl start "basis_lmgrd.service"
   fi
   #echo "Startup scripts updated or installed"
   # This prevents editing of the supplied utilites. Please make a copy.
   # chattr ensures that even root can't do it
-  chattr +i /usr/local/basis/pro5/ext/* /usr/local/basis/pro5/std/* /usr/local/basis/pro5/graphics/*
+  chattr +i "/usr/local/basis/pro5"/{ext,std,graphics}/*
 }
 
 post_install() {
   post_upgrade
   # It's a shame this path is hard coded into the license manager.
   # This, or maybe a binary patch?
-  ln -s /tmp /usr/tmp || :
+  # Binary patch complete. gnu sed is binary compatible.
+  # ln -s '/tmp' '/usr/tmp' || :
 }
 
 pre_upgrade() {
   systemctl stop "basis_lmgrd.service"
-  chattr -i /usr/local/basis/pro5/ext/* /usr/local/basis/pro5/std/* /usr/local/basis/pro5/graphics/*
+  if ! chattr -i "/usr/local/basis/pro5"/{ext,std,graphics}/*; then
+    # We must do this because of bug https://bugs.archlinux.org/task/45988
+    case "/usr/local/basis" in
+    '/usr/local/basis') chattr -i '/usr/share/basis/pro5'/{ext,std,graphics}/*;;
+    '/usr/share/basis') chattr -i '/usr/local/basis/pro5'/{ext,std,graphics}/*;;
+    esac
+  fi
 }
 
 pre_remove() {
@@ -31,8 +35,11 @@ pre_remove() {
 post_remove() {
   systemctl daemon-reload
   #echo "startup scripts removed"
-  #mandb -q
-  if [ -L /usr/tmp ]; then
-    rm -f /usr/tmp
-  fi
+  # Having created this outside of the package manager, it isn't safe to delete.
+  # Another package may have created it first.
+  # Another Install may have configured based on its presence.
+  # The admin will need to remove it and put it back if there's problems.
+  #if [ -L '/usr/tmp' ]; then
+  #  rm -f '/usr/tmp'
+  #fi
 }
