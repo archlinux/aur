c070c2e3526056cf73d10a9106cdc8e4672084e7
Fernando Ortiz <fortiz2k@gmail.com>
2015-08-27 00:27:02 -0500

    add update.sh script and add more ignore items

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if ! [ -f pkgsum.sh ]; then
+  wget https://gist.github.com/nandub/04f1f049a5e55476b642/raw/02c13a02fb7e9121d1390b3a4f5f6fca5d11e07b/pkgsum.sh
+  chmod +x pkgsum.sh
+fi
+
+get_latest() {
+  wget -q -O- 'http://primesieve.org/downloads/' | \
+  sed -n 's,.*primesieve-\([0-9][^>]*\)\.tar.*,\1,p' | \
+  grep -v '\(linux\|mac\|win\)' | \
+  sort -r | \
+  head -1
+}
+latest_version=pkgver=$(get_latest)
+current_version=$(cat PKGBUILD | grep pkgver=[^d.].[^d.].[^d.])
+
+if ! [ "$latest_version" = "$current_version" ]; then
+  echo Updating the package with the latest version
+  echo latest: $latest_version
+  echo current: $current_version
+  sed -i.bak "s/$current_version/$latest_version/g" PKGBUILD
+  sed -i.bak "s/pkgrel=[^d.]/pkgrel=1/g" PKGBUILD
+  ./pkgsum.sh
+  mksrcinfo
+else
+  echo Nothing to update.
+fi
