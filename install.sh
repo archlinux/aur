ae68505b86a380d9884e9040f0778a2b3130c034
Maarten de Boer <info@maartendeboer.net>
2015-09-01 21:49:26 +0200

    Remove outdated message from install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 post_install() {
 	fc-cache
-	cat <<- EOF
-		==> Infinality fontconfig settings are enabled system-wide by default. If you don't like this, replace the symlink /etc/fonts/conf.d/52-infinality.conf with an empty file - It will be preserved between updates.
-		==> Refer to /etc/fonts/infinality/README for available customization options. For best experience, install either Windows, Apple or Google fonts. More information is available at http://www.infinality.net.
-	EOF
 }
 
 post_upgrade() {
-	post_install $0
+	fc-cache
 }
