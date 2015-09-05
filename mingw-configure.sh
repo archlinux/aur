ba97829aad400afa7c197a74dbcdb92b8778c8fb
xantares <xantares09@hotmail.com>
2015-09-05 12:45:51 +0200

    add optional last dir arg

--- mingw-configure.sh
+++ mingw-configure.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
+
+# check if last arg is a path to configure, else use parent
+for last; do true; done
+if test -x "${last}/configure"
+then
+  config_path="$last"
+else
+  config_path=".."
+fi
+
 mingw_c_flags="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4"
 LDFLAGS=""
 export CFLAGS="$mingw_c_flags $CFLAGS"
 export CXXFLAGS="$mingw_c_flags $CXXFLAGS"
-../configure --host=@TRIPLE@ --target=@TRIPLE@ --build="$CHOST" \
+${config_path}/configure --host=@TRIPLE@ --target=@TRIPLE@ --build="$CHOST" \
   --prefix=/usr/@TRIPLE@ --libdir=/usr/@TRIPLE@/lib --includedir=/usr/@TRIPLE@/include \
   --enable-shared --enable-static "$@"
 
