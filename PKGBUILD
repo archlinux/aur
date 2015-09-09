archlinux/aur

commit e338dd353d48d5a2bd8798b13747800f124e945d

Updating packaging script from aur comment
Adding check_core_version.py and EXPECTED_CORE_VERSION files to the
packaging script, as per AUR comment/patch from archdria.

PKGBUILD
@@ -96,7 +96,7 @@ package() {
     "$pkgdir/usr/share/vim/vimfiles"
   cp -r "$srcdir/YouCompleteMe/third_party/"{pythonfutures,requests,requests-futures,retries} \
     "$pkgdir/usr/share/vim/vimfiles/third_party"
-  cp -r "$srcdir/YouCompleteMe/third_party/ycmd/"{ycmd,ycm_client_support.so,ycm_core.so} \
+  cp -r "$srcdir/YouCompleteMe/third_party/ycmd/"{ycmd,ycm_client_support.so,ycm_core.so,check_core_version.py,EXPECTED_CORE_VERSION} \
     "$pkgdir/usr/share/vim/vimfiles/third_party/ycmd"
   cp -r "$srcdir/YouCompleteMe/third_party/ycmd/third_party/"{argparse,bottle,frozendict,jedi,waitress} \
     "$pkgdir/usr/share/vim/vimfiles/third_party/ycmd/third_party"

commit 5c71b57afade6ce70ecf25cb0c0181a4ecff1486

Fixing packaging break caused by e338dd35
Replacing EXPECTED_CORE_VERSION with CORE_VERSION.

PKGBUILD
@@ -1,4 +1,5 @@
 # Maintainer: Erik Beran <eberan AT_gmail_DOT com>
+# Contributor: Thor K. H. <thor at roht dot no>
 # Contributor: Babken Vardanyan <483ken 4tgma1l
 # Contributor: mikezackles
 # Contributor: z33ky
@@ -12,7 +13,7 @@
 # Contributor: archdria
 
 pkgname=vim-youcompleteme-git
-pkgver=1393.4436d51
+pkgver=1517.a2808ee
 pkgver() {
   cd "YouCompleteMe"
   echo $(git rev-list --count master).$(git rev-parse --short master)
@@ -97,7 +98,7 @@ package() {
     "$pkgdir/usr/share/vim/vimfiles"
   cp -r "$srcdir/YouCompleteMe/third_party/"{pythonfutures,requests,requests-futures,retries} \
     "$pkgdir/usr/share/vim/vimfiles/third_party"
-  cp -r "$srcdir/YouCompleteMe/third_party/ycmd/"{ycmd,ycm_client_support.so,ycm_core.so,check_core_version.py,EXPECTED_CORE_VERSION} \
+  cp -r "$srcdir/YouCompleteMe/third_party/ycmd/"{ycmd,ycm_client_support.so,ycm_core.so,check_core_version.py,CORE_VERSION} \
     "$pkgdir/usr/share/vim/vimfiles/third_party/ycmd"
   cp -r "$srcdir/YouCompleteMe/third_party/ycmd/third_party/"{argparse,bottle,frozendict,jedi,waitress} \
     "$pkgdir/usr/share/vim/vimfiles/third_party/ycmd/third_party"