a110dc3017320167309c5516c220c936ad4df9ce
Blake Alexander <blakea@localhost.localdomain>
2015-09-12 17:20:44 +1000

    Initial commit

---
+++ vendor.js
@@ -0,0 +1,12 @@
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default mailer checking.
+pref("mail.shell.checkDefaultMail", false);
+
+// Don't disable our bundled extensions in the application directory
+pref("extensions.autoDisableScopes", 11);
+pref("extensions.shownSelectionUI", true);
+
+// Enable system alerts
+pref("mail.biff.use_system_alert", true);
