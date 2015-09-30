8b999f35c97a6f0a9d9107fa44e9127633751315
Konstantin Stepanov <konstantin.stepanov@adform.com>
2015-09-30 12:08:20 +0300

    use exec to run java from shell script

--- ensime-server.sh
+++ ensime-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/java -Densime.config=${1:-.}/.ensime -cp /usr/lib/jvm/default/lib/tools.jar:/usr/lib/ensime/ensime_2.11.6-0.9.10-SNAPSHOT-assembly.jar org.ensime.server.Server
+exec /usr/bin/java -Densime.config=${1:-.}/.ensime -cp /usr/lib/jvm/default/lib/tools.jar:/usr/lib/ensime/ensime_2.11.6-0.9.10-SNAPSHOT-assembly.jar org.ensime.server.Server
