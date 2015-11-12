2dbb05382384ffd65f6242be3739d71d28b5e22e
TheLoko <juliolokooo@gmail.com>
2015-11-12 23:28:16 +0100

    Initial import

---
+++ oracle_env.csh
@@ -0,0 +1,4 @@
+setenv ORACLE_HOME /usr/lib/oracle/product/11.2.0/xe
+setenv ORACLE_SID XE
+setenv NLS_LANG `$ORACLE_HOME/bin/nls_lang.sh`
+setenv PATH $ORACLE_HOME/bin:$PATH
---
+++ oracle_env.sh
@@ -0,0 +1,4 @@
+export ORACLE_HOME=/usr/lib/oracle/product/11.2.0/xe
+export ORACLE_SID=XE
+export NLS_LANG=`$ORACLE_HOME/bin/nls_lang.sh`
+export PATH=$ORACLE_HOME/bin:$PATH
