11be584918075f99b9125256c435d6e2077ed738
maz-1 <ohmygod19993@gmail.com>
2015-12-08 08:50:57 +0800

    bump

--- imchooser-helper.sh
+++ imchooser-helper.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 case $1 in
 disable)
- sed -i 's:^Exec=fcitx-autostart$:Exec=/usr/share/imchooser/imchooser-helper fcitx:g' /etc/xdg/autostart/fcitx-autostart.desktop
+ match=$(cat /etc/xdg/autostart/fcitx-autostart.desktop | grep -o imchooser-helper)
+ [[ z$match == zimchooser-helper ]] || sed -i 's:^Exec=[^\n]*$:Exec=/usr/share/imchooser/imchooser-helper fcitx:g' /etc/xdg/autostart/fcitx-autostart.desktop
 ;;
 enable)
- sed -i 's:^Exec=/usr/share/imchooser/imchooser-helper fcitx:Exec=fcitx-autostart:g' /etc/xdg/autostart/fcitx-autostart.desktop
+ sed -i 's:^Exec=[^\n]*$:Exec=fcitx-autostart:g' /etc/xdg/autostart/fcitx-autostart.desktop
 ;;
 fcitx)
  [[ "$XDG_SESSION_DESKTOP" == "KDE" ]] || /usr/bin/fcitx-autostart
