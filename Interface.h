b938f412bcff17c9434f065d028e91731d83b868
DimaSmirnov <info@igus.lv>
2015-12-20 16:21:17 +0200

    update AUR base

--- Interface.h
+++
@@ -1,8 +0,0 @@
-void ShowHelpWindow() {
-	printf("\nDowngrader, version %s (libalpm.so.8)\n",VERSION);
-	printf(" usage: downgrader [ -h ] [-q] [ -l <package name> ] <PACKAGE_NAME>\n");
-	printf("  options:\n");
-	printf("   -h   <print this help and exit>\n");
-	printf("   -q   <quiet working, without any notifications> \n");
-	printf("   -l N   <gave a list of available packages versions for downgrade package N>\n");
-} 
