9c816ed4e890350a7e8a3cdbbeaa5ded4d73081d
Michel Zou <xantares09@hotmail.com>
2016-04-03 09:34:08 +0200

    use WINEPATH

--- mingw-wine.sh
+++ mingw-wine.sh
@@ -4,21 +4,18 @@ set -e
 
 mingw_prefix=/usr/@TRIPLE@
 
-# wine requires the dlls to lie next to the exe
-if test -f "$1"
-then
-  path=`dirname $1`
-  if ! test -f "${path}/libstdc++-6.dll"
-  then
-    cp ${mingw_prefix}/bin/*.dll ${path}
-  fi
-fi
-
 # run it in a custom WINEPREFIX to not mess with default ~/.wine
 # also default prefix might be a 32 bits prefix, which will fail to run x86_64 exes
 if ! test -d "${WINEPREFIX}"
 then
-  WINEPREFIX=~/.wine-@TRIPLE@ wine "$@"
-else
-  wine "$@"
+  export WINEPREFIX=~/.wine-@TRIPLE@
 fi
+
+# WINEPATH is used to find dlls, otherwise they should lie next to the exe
+if test -z "${WINEPATH}"
+then
+  export WINEPATH=${mingw_prefix}/bin
+fi
+
+wine "$@"
+
