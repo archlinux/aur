2df2d2a3d0d8125ca1df267384b76ebc92090c6e
Joshua Hardy <joshinsilico@gmail.com>
2016-04-10 22:44:11 +1000

    fixed config file

--- eman2.sh
+++ eman2.sh
@@ -1,1 +1,6 @@
 source /opt/eman2/eman2.bashrc
+export EMAN2DIR=/opt/eman2
+export PATH=$PATH:${EMAN2DIR}/bin:${EMAN2DIR}/extlib/bin
+export PYTHONPATH=${PYTHONPATH}:${EMAN2DIR}/lib:${EMAN2DIR}/bin
+alias sparx=sx.py
+
