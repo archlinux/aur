fd28407e630b6cc4fe8d9136136c98737c1871b1
mlatman <mlatman@gmail.com>
2016-04-16 13:20:30 -0400

    Commit version 2.0.12 release 7

--- PKGBUILD
+++ PKGBUILD
@@ -1,12 +1,7 @@
-# This is an example PKGBUILD file. Use this as a start to creating your own,
-# and remove these comments. For more information, see 'man PKGBUILD'.
-# NOTE: Please fill out the license field for your package! If it is unknown,
-# then please put 'unknown'.
-
 # Maintainer: Michael Latman <mlatman@gmail.com>
 pkgname=whatsie
 pkgver=2.0.12
-pkgrel=6
+pkgrel=7
 epoch=
 pkgdesc="A simple & beautiful desktop client for WhatsApp Web."
 arch=('any')
---
+++ generateAur.sh
@@ -0,0 +1,70 @@
+pkgver=2.0.12
+pkgrel=7
+echo "# Maintainer: Michael Latman <mlatman@gmail.com>
+pkgname=whatsie
+pkgver=$pkgver
+pkgrel=$pkgrel
+epoch=
+pkgdesc=\"A simple & beautiful desktop client for WhatsApp Web.\"
+arch=('any')
+url=\"https://whatsie.chat\"
+license=('MIT')
+groups=()
+depends=(\"dpkg\")
+makedepends=()
+checkdepends=()
+optdepends=('lsb-release')
+provides=()
+conflicts=()
+replaces=()
+backup=()
+options=()
+install=\".INSTALL\"
+changelog=
+source=()
+noextract=()
+md5sums=()
+validpgpkeys=()
+package(){
+wget \"https://github.com/Aluxian/Whatsie/releases/download/v\$pkgver/whatsie-\$pkgver-linux-amd64.deb\" -P build_step/
+dpkg -x \"build_step/whatsie-\$pkgver-linux-amd64.deb\" \"\$pkgdir/\"
+
+}
+md5sums=()" > PKGBUILD
+
+
+echo "pkgname = whatsie
+pkgver = $pkgver
+pkgdesc = A simple & beautiful desktop client for WhatsApp Web.
+url = https://whatsie.chat
+builddate = 1460577681
+packager = whatsie
+arch = x86_64
+size = 140662880
+license = MIT
+backup = etc/xdg/autostart/whatsie.desktop
+depend = bash-completion
+depend = desktop-file-utils
+depend = gconf
+depend = gtk2
+depend = gvfs
+depend = hicolor-icon-theme
+depend = libcap
+depend = libgcrypt
+depend = libgcrypt11
+depend = libnotify
+depend = libsystemd
+depend = libxtst
+depend = nss
+depend = xdg-utils
+optdepend = gir1.2-gnomekeyring-1.0
+optdepend = libgnome-keyring
+optdepend = lsb-release" > .PKGINFO
+
+echo "pkgbase = whatsie
+	pkgver = $pkgver
+	pkgrel = $pkgrel
+	arch = any
+	license = MIT
+	depends = dpkg
+pkgname = whatsie" > .SRCINFO
