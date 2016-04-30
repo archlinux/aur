d93eead6bfd945c6027d26e2e5f89b6075547186
BuildTools <unconfigured@null.spigotmc.org>
2016-04-30 13:58:52 -0700

    configure path fix

--- PKGBUILD
+++ PKGBUILD
@@ -1,13 +1,13 @@
 pkgname=djgpp-configure
 pkgver=20160430
-pkgrel=1
+pkgrel=2
 pkgdesc="Configure script wrapper for DJGPP"
 arch=(any)
 url="about:blank"
 license=("GPL")
 depends=(djgpp-pkg-config)
 source=("$pkgname.sh")
-md5sums=('3bc2f47fd5c0a44ea6e330f98618403f')
+md5sums=('2259094ddf6aa9da0b6da76d4eac5495')
 
 package() {
   install -Dm755 $pkgname.sh "$pkgdir/usr/bin/i686-pc-msdosdjgpp-configure"
--- djgpp-configure.sh
+++ djgpp-configure.sh
@@ -4,6 +4,6 @@ djgpp_c_flags="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=s
 LDFLAGS=""
 export CFLAGS="$djgpp_c_flags $CFLAGS"
 export CXXFLAGS="$djgpp_c_flags $CXXFLAGS"
-${config_path}/configure --host=i686-pc-msdosdjgpp --target=i686-pc-msdosdjgpp --build="$CHOST" \
+./configure --host=i686-pc-msdosdjgpp --target=i686-pc-msdosdjgpp --build="$CHOST" \
   --prefix=/usr/i686-pc-msdosdjgpp --libdir=/usr/i686-pc-msdosdjgpp/lib --includedir=/usr/i686-pc-msdosdjgpp/include \
   --disable-shared --enable-static "$@"
