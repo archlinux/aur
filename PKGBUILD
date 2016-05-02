0c4ffb3b4de9213ae2d024753fee8d9caf240650
max.bra <max.bra.gtalk@gmail.com>
2016-05-02 18:51:36 +0200

    v. 4.7

--- PKGBUILD
+++ PKGBUILD
@@ -4,8 +4,8 @@
 # Contributor: Nathan Owe <ndowens04 at gmail>
 
 pkgname=filebot
-pkgver=4.6.1
-pkgrel=2
+pkgver=4.7
+pkgrel=1
 pkgdesc="The ultimate tool to rename TV/anime shows, download subtitles, and validate checksums"
 arch=('any')
 url="http://filebot.sourceforge.net/"
@@ -14,8 +14,8 @@ license=('GPL')
 depends=('java-runtime>=8' 'fontconfig' 'chromaprint')
 source=(http://downloads.sourceforge.net/project/$pkgname/$pkgname/FileBot_$pkgver/FileBot_$pkgver-portable.zip
         $pkgname-arch.sh $pkgname.svg $pkgname.desktop)
-md5sums=('29845bf89eeeab2c6e7c6669ea034876'
-         'f9469bf391160033df1a7961fd732141'
+md5sums=('5bccdb98a0052f628aed38f857122656'
+         '1820bf0b4dc22bb8b96ff519ff1cd29d'
          '04f46be047049448dba3f0de29fe192d'
          'f37edd0bba7570904d28ab1681c7a7f3')
 #noextract=(FileBot_$pkgver.jar)
--- filebot-arch.sh
+++ filebot-arch.sh
@@ -4,9 +4,11 @@
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
-# FileBot settings
-# EXTRACTOR="SevenZipNativeBindings"	# use the lib7-Zip-JBinding.so native library
-# EXTRACTOR="SevenZipExecutable"	# use the 7z executable
-EXTRACTOR="ApacheVFS"			# use Apache Commons VFS2 with junrar plugin
 
-java $JAVA_OPTS -Dunixfs=false -DuseGVFS=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding="UTF-8" -Djava.net.useSystemProxies=true -Dsun.net.client.defaultConnectTimeout=10000 -Dsun.net.client.defaultReadTimeout=60000 -Djna.nosys=true -Dapplication.deployment=aur -Dapplication.update=skip -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Dnet.filebot.AcoustID.fpcalc="fpcalc" -Dapplication.dir=$HOME/.filebot -Djava.io.tmpdir=/tmp/.filebot -Djna.library.path=/usr/share/java -jar /usr/share/java/filebot/filebot.jar "$@"
+# choose extractor
+EXTRACTOR="ApacheVFS"					# use Apache Commons VFS2 with junrar plugin
+# EXTRACTOR="SevenZipExecutable"		# use the 7z executable
+# EXTRACTOR="SevenZipNativeBindings"	# use the lib7-Zip-JBinding.so native library
+
+# start filebot
+java -Dunixfs=false -DuseGVFS=false -DuseExtendedFileAttributes=true -DuseCreationDate=false -Dfile.encoding="UTF-8" -Dsun.jnu.encoding="UTF-8" -Djava.net.useSystemProxies=false -Djna.nosys=true -Dapplication.deployment=portable -Dnet.filebot.Archive.extractor="$EXTRACTOR" -Dnet.filebot.AcoustID.fpcalc="fpcalc" -Dapplication.dir=$HOME/.config/filebot -Djava.io.tmpdir=/tmp/filebot -Duser.home=$HOME/.config/filebot -Dapplication.update=skip -Djna.library.path=/usr/share/java $JAVA_OPTS -jar /usr/share/java/filebot/filebot.jar "$@"
