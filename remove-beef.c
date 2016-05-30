b5091250660c8bce644621b95cf4670180adb982
Hythlodaeus <matteodelseppiaomm@gmail.com>
2016-05-30 14:43:28 +0000

    Remove c source

--- remove-beef.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-           system("sudo rm -rf /usr/share/beef /usr/bin/beef-xss-update /usr/bin/beef-xss");
-           system("sudo pacman -Rsn beef-xss-git");
-           printf("\033[22;34mSuccessfully uninstalled beef-xss\033[0m\n");
-           return 0;
-}
