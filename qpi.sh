cf183670e25841af8235987a0404af79286ad44d
Donald Carr <sirspudd@gmail.com>
2016-06-07 15:25:49 -0700

    Enable the use of the virtual keyboard module by default
    Change-Id: Ic104fe5afe8a5b295bb0a51fee687325baaa92c4

--- qpi.sh
+++ qpi.sh
@@ -1,5 +1,6 @@
 export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_CLIENT_BUFFER_INTEGRATION=brcm
+export QT_IM_MODULE=qtvirtualkeyboard
 
 # Required as of 5.7.0 beta/RC
 export QSG_RENDER_LOOP=basic
