2a6d2827a7e438758a7197e28f167f1250120213
Donald Carr <sirspudd@gmail.com>
2016-06-07 15:23:47 -0700

    Wayland ceases to render frames if clients are launched without single threaded rendering
    Change-Id: I829e655dcda429a659707913817cc05d25da2f8b

--- qpi.sh
+++ qpi.sh
@@ -1,5 +1,8 @@
 export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_CLIENT_BUFFER_INTEGRATION=brcm
 
+# Required as of 5.7.0 beta/RC
+export QSG_RENDER_LOOP=basic
+
 PATH="localpiprefix/bin:${PATH}"
 export PATH
