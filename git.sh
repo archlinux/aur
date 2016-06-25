d5ba55b4a50a60cf38cfc891971322fa224e67ec
erikdubois <erik.dubois@gmail.com>
2016-06-25 17:36:01 +0200

    commit from @ Sat Jun 25 17:36:01 CEST 2016

--- git.sh
+++ git.sh
@@ -4,18 +4,6 @@
 # Website 	: 	http://www.erikdubois.be
 ############################################################################
 
-# icon cache
-echo "Yltra-Flat"
-gtk-update-icon-cache -t Yltra-Flat
-echo "Yltra-Flat-Arch-Blue"
-gtk-update-icon-cache Yltra-Flat-Arch-Blue
-echo "Yltra-Flat-Majestic"
-gtk-update-icon-cache Yltra-Flat-Majestic
-echo "Yltra-Flat-Green"
-gtk-update-icon-cache Yltra-Flat-Mint-Green
-echo "Yltra-Flat-Orange"
-gtk-update-icon-cache Yltra-Flat-Orange
-
 
 mksrcinfo
 git add --all .
