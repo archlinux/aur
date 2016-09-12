39b288ca09144e0212cec2b15377cbd2e323aae3
Julian Xhokaxhiu <julian.xhokaxhiu@cinteo.com>
2016-09-12 10:27:23 +0200

    Fix path to URL autostart txt file
    And update the Install file to generate it on first install

--- chromium-fullscreen.sh
+++ chromium-fullscreen.sh
@@ -1,2 +1,2 @@
 # Autostart Chromium in Fullscreen mode
-/usr/bin/xinit /usr/bin/chromium --desktop-window-1080p --kiosk $(cat ~/url.txt)
+/usr/bin/xinit /usr/bin/chromium --desktop-window-1080p --kiosk $(cat /home/chromium/chromium-autostart-url.txt)
