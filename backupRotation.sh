2128924a770b41884b3be5a7a64dcd59b7eb4fdf
torben <t.sickert[~at~]gmail.com>
2016-10-04 22:24:35 +0200

    automatic update

--- backupRotation.sh
+++ backupRotation.sh
@@ -44,26 +44,31 @@
 # password       ACCOUNT_PASSWORD
 
 # account        default : gmail
-
 # endregion
 # region default options
-sourcePath='/tmp/source/'
-targetPath='/tmp/backup/'
-eMailAddress='' # Notification are disabled by default.
-dailyTargetPath='daily/'
-weeklyTargetPath='weekly/'
-monthlyTargetPath='monthly/'
-targetDailyFileName="$(date +'%d-%m-%Y')"
-targetWeeklyFileName="$(date +'%V sav. %m-%Y')"
-targetMonthlyFileName="$(date +'%m-%Y')"
-backupWeekDayNumber=6 # Saturday
-backupMonthDayNumber=1
-numberOfDailyRetentionDays=14 # Daily backups for the last 14 days.
-numberOfWeeklyRetentionDays=56 # Weekly backups for the last 2 month.
-numberOfMonthlyRetentionDays=365 # Monthly backups for the last year.
-backupCommand='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose "$sourcePath" "$targetFilePath" && tar --dereference --create --verbose --gzip --file "${targetFilePath}.tar.gz" "$targetFilePath" && rm --recursive --verbose "$targetFilePath"'
+# Example:
+# declare -A source_target_mappings=(
+#     ['SOURCE_URL1']='TARGET_URL1 RECIPIENT_E_MAIL_ADDRESS' \
+#     ['SOURCE_URL2']='TARGET_URL2 ANOTHER_RECIPIENT_E_MAIL_ADDRESS')
+declare -A source_target_mappings=()
+sender_e_mail_address='ACCOUNT_E_MAIL_ADDRESS'
+replier_e_mail_address="$sender_e_mail_address"
+daily_target_path='daily/'
+weekly_target_path='weekly/'
+monthly_target_path='monthly/'
+target_daily_file_name="$(date +'%d-%m-%Y')"
+target_weekly_file_name="$(date +'%V sav. %m-%Y')"
+target_monthly_file_name="$(date +'%m-%Y')"
+backup_week_day_number=6 # Saturday
+backup_month_day_number=1
+number_of_daily_retention_days=14 # Daily backups for the last 14 days.
+number_of_weekly_retention_days=56 # Weekly backups for the last 2 month.
+number_of_monthly_retention_days=365 # Monthly backups for the last year.
+backup_command='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose "$sourcePath" "$targetFilePath" && tar --dereference --create --verbose --gzip --file "${targetFilePath}.tar.gz" "$targetFilePath" && rm --recursive --verbose "$targetFilePath"'
 # Folder to delete is the last command line argument.
-cleanupCommand='rm --recursive --verbose'
+cleanup_command='rm --recursive --verbose'
+verbose=false
+name='NODE_NAME'
 # endregion
 # region load options if present
 if [ -f /etc/backupRotation ]; then
@@ -72,45 +77,54 @@ fi
 # endregion
 # region controller
 # Get current month and week day number
-monthDayNumber="$(date +'%d')"
-weekDayNumber="$(date +'%u')"
-# Check if source files exist and send an email if not
-if [ ! -d "$sourcePath" ]; then
-    date="$(date)"
-    message="Source files on \"$sourcePath\" should be backed up but aren't available."
-    if hash msmtp && [[ "$eMailAddress" != '' ]]; then
-        msmtp -t <<EOF
-From: $eMailAddress
-To: $eMailAddress
-Reply-To: $eMailAddress
-Date: $date
+month_day_number="$(date +'%d')"
+week_day_umber="$(date +'%u')"
+for source_path in "${!source_target_mappings[@]}"; do
+    target_path="$(echo "${source_target_mappings[$source_path]}" | \
+        grep '^[^ ]+' --only-matching --extended-regexp)"
+    # TODO you have to check for remote files also!
+    # Check if source files exist and send an email if not
+    if [ ! -d "$source_path" ]; then
+        message="Source files on \"$source_path\" should be backed up but aren't available."
+        if hash msmtp && [[ "$e_mail_address" != '' ]]; then
+            for e_mail_address in $(echo "${source_target_mappings[$source_path]}" | \
+                grep ' .+$' --only-matching --extended-regexp)
+            do
+                $verbose && echo "$message" >/dev/stderr
+                msmtp -t <<EOF
+From: $sender_e_mail_address
+To: $e_mail_address
+Reply-To: $replier_e_mail_address
+Date: $(date)
 Subject: Source files doesn't exist.
 
 $message
 
 EOF
+            done
+        fi
+        echo "$message" &>/dev/stderr
+        return 1
     fi
-    echo "$message" &>/dev/stderr
-    return 1
-fi
-if [[ "$monthDayNumber" == "$backupMonthDayNumber" ]]; then
-    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-elif [[ "$weekDayNumber" == "$backupMontheDayNumber" ]]; then
-    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-else
-    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-fi
-mkdir --parents "$(dirname "$targetFilePath")"
-eval "$backupCommand"
-# Clean outdated daily backups.
-find "$targetPath" -mtime +"$numberOfDailyRetentionDays" -type d -exec \
-    "$cleanupCommand" {} \;
-# Clean outdated weekly backups.
-find "$targetPath" -mtime +"$numberOfWeeklyRetentionDays" -type d -exec \
-    "$cleanupCommand" {} \;
-# Clean outdated monthly backups.
-find "$targetPath" -mtime +"$numberOfMonthlyRetentionDays" -type d -exec \
-    "$cleanupCommand" {} \;
+    if [[ "$month_day_number" == "$backup_month_day_number" ]]; then
+        target_file_path="${target_path}${daily_target_path}${target_daily_file_name}"
+    elif [[ "$week_day_number" == "$backup_monthe_day_number" ]]; then
+        target_file_path="${target_path}${daily_target_path}${target_daily_file_name}"
+    else
+        target_file_path="${target_path}${daily_target_path}${target_daily_file_name}"
+    fi
+    mkdir --parents "$(dirname "$target_file_path")"
+    eval "$backup_command"
+    # Clean outdated daily backups.
+    find "$target_path" -mtime +"$number_of_daily_retention_days" -type d -exec \
+        "$cleanup_command" {} \;
+    # Clean outdated weekly backups.
+    find "$target_path" -mtime +"$number_of_weekly_retention_days" -type d -exec \
+        "$cleanup_command" {} \;
+    # Clean outdated monthly backups.
+    find "$target_path" -mtime +"$number_of_monthly_retention_days" -type d -exec \
+        "$cleanup_command" {} \;
+done
 # endregion
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
