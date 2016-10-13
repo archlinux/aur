980c9369bbf1ea85d46de8b3da0a2b71a3b2d138
torben <t.sickert[~at~]gmail.com>
2016-10-13 13:27:10 +0200

    automatic update

--- backupRotation.sh
+++ backupRotation.sh
@@ -86,7 +86,7 @@ for source_path in "${!source_target_mappings[@]}"; do
         grep '^[^ ]+' --only-matching --extended-regexp)"
     target_file_path="${target_path}/${daily_target_path}${target_daily_file_name}"
     if [[ "$month_day_number" == "$backup_month_day_number" ]]; then
-        target_file_path="${target_path}/${daily_target_path}${target_monthly_file_name}"
+        target_file_path="${target_path}/${monthly_target_path}${target_monthly_file_name}"
         ln --symbolic --force "$target_file_path" \
             "${target_path}/${daily_target_path}${target_daily_file_name}"
         if [[ "$week_day_number" == "$backup_week_day_number" ]]; then
@@ -94,7 +94,7 @@ for source_path in "${!source_target_mappings[@]}"; do
                 "${target_path}/${daily_target_path}${target_weekly_file_name}"
         fi
     elif [[ "$week_day_number" == "$backup_week_day_number" ]]; then
-        target_file_path="${target_path}/${daily_target_path}${target_weekly_file_name}"
+        target_file_path="${target_path}/${weekly_target_path}${target_weekly_file_name}"
         ln --symbolic --force "$target_file_path" \
             "${target_path}/${daily_target_path}${target_daily_file_name}"
     fi
