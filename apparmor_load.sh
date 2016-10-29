2b8893f406c0531188bda41d5a045a6173abb7e9
Peter Maloney <peter.maloney@yahoo.ca>
2016-10-29 10:58:01 +0200

    fixed handling spaces in filenames

--- apparmor_load.sh
+++ apparmor_load.sh
@@ -2,4 +2,4 @@
 
 aa_profiles='/etc/apparmor.d/'
 aa_log='/var/log/apparmor.init.log'
-/usr/bin/apparmor_parser -r $(find "$aa_profiles" -maxdepth 1 -type f) 2>> "$aa_log"
+find "$aa_profiles" -maxdepth 1 -type f -exec /usr/bin/apparmor_parser -r {} + 2>> "$aa_log"
--- apparmor_unload.sh
+++ apparmor_unload.sh
@@ -2,7 +2,4 @@
 
 aa_profiles='/etc/apparmor.d/'
 aa_log='/var/log/apparmor.init.log'
-PROFILES=`find "$aa_profiles" -maxdepth 1 -type f`
-for profile in $PROFILES; do
-    apparmor_parser -R "$profile" 2>> "$aa_log"
-done
+find "$aa_profiles" -maxdepth 1 -type f -exec /usr/bin/apparmor_parser -R {} \; 2>> "$aa_log"
