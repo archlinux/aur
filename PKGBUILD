eba6fef77b567d3f97f24908f7eaf120f7093053
Shadow53 <shadow53@shadow53.com>
2016-11-24 21:24:53 -0800

    Updated to 2.04, added multiuser support

--- PKGBUILD
+++ PKGBUILD
@@ -2,59 +2,61 @@
 # Contributer: Benjamin Colard <benjamin at colard dot be>
 
 pkgname=darkmod
-pkgver=2.03
-pkgrel=2
+pkgver=2.04
+pkgrel=1
 pkgdesc="DOOM III standalone mod based on the Thief series by Looking Glass Studios"
 arch=('i686' 'x86_64')
 url="http://www.thedarkmod.com/"
 license=('CCPL')
 
+install='darkmod.install'
+
 makedepends=('unzip')
-[ "$CARCH" == i686   ] && depends=('glew' 'libpng' 'libjpeg-turbo' 'libpng12' 'curl' 'libxxf86vm' 'libpulse' 'xterm')
+[ "$CARCH" == i686   ] && depends=('glew' 'libpng' 'libjpeg-turbo' 'libpng12' 'curl' 'libxxf86vm' 'libpulse')
 
-[ "$CARCH" == x86_64 ] && depends=('lib32-glew' 'lib32-libpng' 'lib32-libjpeg-turbo' 'lib32-libpng12' 'lib32-curl' 'lib32-libxxf86vm' 'lib32-libpulse' 'xterm')
+[ "$CARCH" == x86_64 ] && depends=('lib32-glew' 'lib32-libpng' 'lib32-libjpeg-turbo' 'lib32-libpng12' 'lib32-curl' 'lib32-libxxf86vm' 'lib32-libpulse' 'lib32-alsa-plugins')
 
-install="$pkgname".install
 source=(http://www.fidcal.com/darkuser/tdm_update_linux.zip
         "$pkgname".desktop
-        "$pkgname".png)
+        "$pkgname".png
+        "$pkgname".sh)
 
-md5sums=('c2e619ad6e5f39501d620b6f68e95233'
+md5sums=('3e39c23cc754f2ac55e05b5a5b56023c'
          'bc6537224b60c4ccfd04a51d80e340c1'
-         '5309e528ce22f28ecc0e9781e43379e0')
+         '5309e528ce22f28ecc0e9781e43379e0'
+         'a7a6ca33f20bb920ba0688cb8c36874c')
 
 build() {
   cd "$srcdir"
-	if [ ! -d "$srcdir/darkmod" ]; then
-	    mkdir $srcdir/darkmod	
-	fi
-	cd darkmod
-	unzip -o $srcdir/tdm_update_linux.zip
-	chmod +x tdm_update.linux
-	xterm -e $srcdir/darkmod/tdm_update.linux	
-	rm -f TheDarkMod.exe tdm_update.exe
+  if [ ! -d "$srcdir/darkmod" ]; then
+    mkdir $srcdir/darkmod	
+  fi
+  cd darkmod
+  unzip -o $srcdir/tdm_update_linux.zip
+  chmod +x tdm_update.linux
+
+  # If you know which mirrors are closest to you, edit the
+  # tdm_mirrors.txt file and uncomment the keep mirrors argument
+  xterm -e $srcdir/darkmod/tdm_update.linux #--keep-mirrors	
+  rm -f $srcdir/darkmod/{TheDarkMod.exe,tdm_update.exe,tdm_update.linux,tdm_mirrors.txt,ca-bundle.crt,crc_info.txt,tdm_update.log}
 }
 
 package() {
   cd "$srcdir"/darkmod
 
-  install -m755 -d "$pkgdir"/opt/darkmod/
+  install -m755 -d "$pkgdir"/opt/"$pkgname"/
   install -m755 -d "$pkgdir"/usr/bin/
   install -m755 -d "$pkgdir"/usr/share/{applications,icons,licenses/"$pkgname"}
 
-  cp -r * "$pkgdir"/opt/darkmod/
-  find "$pkgdir"/opt/darkmod/ -type d -exec chmod 755 {} \;
-  find "$pkgdir"/opt/darkmod/ -type f -exec chmod 644 {} \;
-  chmod 755 "$pkgdir"/opt/darkmod/thedarkmod.x86
-  install -m755 -d "$srcdir"/darkmod/ "$pkgdir"/usr/bin/
+  cp -r * "$pkgdir"/opt/"$pkgname"/
+  find "$pkgdir"/opt/"$pkgname"/ -type d -exec chmod 755 {} \;
+  find "$pkgdir"/opt/"$pkgname"/ -type f -exec chmod 644 {} \;
+  chmod 755 "$pkgdir"/opt/"$pkgname"/thedarkmod.x86
+  install -DTm755 "$srcdir"/"$pkgname".sh "$pkgdir"/usr/bin/"$pkgname"
 
-  install -Dm644 LICENSE.txt "$pkgdir"/usr/share/licenses/"$pkgname"/LICENSE
-  rm -f "$pkgdir"/opt/darkmod/LICENSE.txt
+  install -DTm644 LICENSE.txt "$pkgdir"/usr/share/licenses/"$pkgname"/LICENSE
+  rm -f "$pkgdir"/opt/"$pkgname"/LICENSE.txt
 
   install -Dm644 "$srcdir"/"$pkgname".png "$pkgdir"/usr/share/icons/
   install -Dm644 "$srcdir"/"$pkgname".desktop "$pkgdir"/usr/share/applications/
-  
-  chgrp --recursive games $pkgdir/opt/darkmod
-  chmod g=rwx -R $pkgdir/opt/darkmod
-
 }
---
+++ darkmod.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Should return false if darkmod folder does not exist
+if [ ! -d $(eval echo ~$USER)/.local/share/darkmod/fms ]; then
+    mkdir -p $(eval echo ~$USER)/.local/share/darkmod/fms
+fi
+
+function link_file() {
+    if [ ! -f "$(eval echo ~$USER)/.local/share/darkmod/$1" ]; then
+        echo -e "Linking missing game file $1\n"
+        ln -s "/opt/darkmod/$1" "$(eval echo ~$USER)/.local/share/darkmod/$1"
+    fi
+}
+
+link_file "tdm_ai_humanoid_nobles01.pk4"
+link_file "tdm_models01.pk4"
+link_file "tdm_sound_vocals06.pk4"
+link_file "tdm_textures_sfx01.pk4"
+link_file "tdm_ai_humanoid_pagans01.pk4"
+link_file "tdm_models02.pk4"
+link_file "tdm_sound_vocals07.pk4"
+link_file "tdm_textures_stone_brick01.pk4"
+link_file "tdm_ai_humanoid_townsfolk01.pk4"
+link_file "tdm_models_decls01.pk4"
+link_file "tdm_sound_vocals_decls01.pk4"
+link_file "tdm_textures_stone_cobblestones01.pk4"
+link_file "tdm_ai_humanoid_undead01.pk4"
+link_file "tdm_player01.pk4"
+link_file "tdm_standalone.pk4"
+link_file "tdm_textures_stone_flat01.pk4"
+link_file "tdm_ai_monsters_spiders01.pk4"
+link_file "tdm_prefabs01.pk4"
+link_file "tdm_textures_base01.pk4"
+link_file "tdm_textures_stone_natural01.pk4"
+link_file "tdm_ai_steambots01.pk4"
+link_file "tdm_sound_ambient01.pk4"
+link_file "tdm_textures_carpet01.pk4"
+link_file "tdm_textures_stone_sculpted01.pk4"
+link_file "tdm_base01.pk4"
+link_file "tdm_sound_ambient02.pk4"
+link_file "tdm_textures_decals01.pk4"
+link_file "tdm_textures_window01.pk4"
+
+if [ ! -d "$(eval echo ~$USER)/.local/share/darkmod/fms/saintlucia" ]; then
+    echo -e "Linking missing game file fms/saintlucia\n"
+    ln -s "/opt/darkmod/fms/saintlucia" "$(eval echo ~$USER)/.local/share/darkmod/fms/saintlucia"
+fi
+if [ ! -d "$(eval echo ~$USER)/.local/share/darkmod/fms/training_mission" ]; then
+    echo -e "Linking missing game file fms/training_mission\n"
+    ln -s "/opt/darkmod/fms/training_mission" "$(eval echo ~$USER)/.local/share/darkmod/fms/training_mission"
+fi
+
+link_file "tdm_defs01.pk4"
+link_file "tdm_sound_ambient03.pk4"
+link_file "tdm_textures_door01.pk4"
+link_file "tdm_textures_wood01.pk4"
+link_file "tdm_env01.pk4"
+link_file "tdm_sound_ambient_decls01.pk4"
+link_file "tdm_textures_fabric01.pk4"
+link_file "tdm_ai_animals01.pk4"
+link_file "tdm_fonts01.pk4"
+link_file "tdm_sound_sfx01.pk4"
+link_file "tdm_textures_glass01.pk4"
+link_file "tdm_ai_base01.pk4"
+link_file "tdm_game01.pk4"
+link_file "tdm_sound_sfx02.pk4"
+link_file "tdm_textures_metal01.pk4"
+link_file "tdm_version_info.txt"
+link_file "tdm_ai_humanoid_builders01.pk4"
+link_file "tdm_game02.pk4"
+link_file "tdm_sound_vocals01.pk4"
+link_file "tdm_textures_nature01.pk4"
+#link_file "thedarkmod.x86" # May want to copy this instead
+link_file "tdm_ai_humanoid_females01.pk4"
+link_file "tdm_gui01.pk4"
+link_file "tdm_sound_vocals02.pk4"
+link_file "tdm_textures_other01.pk4"
+link_file "tdm_ai_humanoid_guards01.pk4"
+link_file "tdm_gui_credits01.pk4"
+link_file "tdm_sound_vocals03.pk4"
+link_file "tdm_textures_paint_paper01.pk4"
+link_file "tdm_ai_humanoid_heads01.pk4"
+link_file "tdm_sound_vocals04.pk4"
+link_file "tdm_textures_plaster01.pk4"
+link_file "tdm_ai_humanoid_mages01.pk4"
+link_file "tdm_sound_vocals05.pk4"
+link_file "tdm_textures_roof01.pk4"
+
+if [ ! -f $(eval echo ~$USER)/.local/share/darkmod/darkmod.ini ]; then
+    echo -e "Copying darkmod.ini"
+    cp /opt/darkmod/darkmod.ini $(eval echo ~$USER)/.local/share/darkmod/darkmod.ini
+fi
+
+if [ ! -f $(eval echo ~$USER)/.local/share/darkmod/thedarkmod.x86 ]; then
+    echo -e "Copying darkmod executable"
+    cp /opt/darkmod/thedarkmod.x86 $(eval echo ~$USER)/.local/share/darkmod/thedarkmod.x86
+    chmod +x $(eval echo ~$USER)/.local/share/darkmod/thedarkmod.x86
+fi
+
+$(eval echo ~$USER)/.local/share/darkmod/thedarkmod.x86
