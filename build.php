92ff7c20ec36aca6e5795ca1c869c06572583069
James Houghton <jamesthoughton@gmail.com>
2016-12-26 09:18:21 -0500

    Update build.php

--- build.php
+++ build.php
@@ -17,13 +17,13 @@ check($ret, 'Build failed');
 //  0 if no changes
 //  1 if changes
 exec("git -C $AUR_BUILD_DIR/larryshell diff --exit-code HEAD", $arr, $ret);
-#check(1 - $ret, 'No changes');
+check(1 - $ret, 'No changes');
 
 exec("git -C $AUR_BUILD_DIR add .");
 $sha = shell_exec("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
 exec("git -C $AUR_BUILD_DIR commit -m 'Automatic version bump -- $sha'", $arr, $ret);
-
 check($ret, 'Failed to commit');
+
 // DAWT
 exec("git -C $AUR_BUILD_DIR push dawt master", $arr, $ret);
 check($ret, 'Failed to push to GitHub');
