7c8b7e40489b4ef9c552abf1996aeb2caae2b056
James Houghton <jamesthoughton@gmail.com>
2016-12-26 06:44:08 -0500

    More PHP changes

--- build.php
+++ build.php
@@ -5,24 +5,30 @@ $ret = 0;
 
 $AUR_BUILD_DIR="/srv/http/larryshell_aur";
 
-function check(r, m) {
+function check($r, $m) {
     if($r) {
         die("{success:false, message:'$m'}");
     }
 }
-exec("cd $AUR_BUILD_DIR && make clean && make && make build && git add .", $arr, $ret);
-check($ret, 'No changes');
+exec("cd $AUR_BUILD_DIR && make clean && make && make build", $arr, $ret);
+check($ret, 'Build failed');
 
-sha = shell_exec("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
-exec("git commit -m 'Automatic version bump -- $sha'", $arr, $ret);
+// --exit-code:
+//  0 if no changes
+//  1 if changes
+exec("git -C $AUR_BUILD_DIR diff --exit-code HEAD", $arr, $ret);
+check(1 - $ret, 'No changes');
+
+$sha = shell_exec("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
+exec("git -C $AUR_BUILD_DIR commit -m 'Automatic version bump -- $sha'", $arr, $ret);
 check($ret, 'Failed to commit');
 
 // DAWT
-exec("git push origin master", $arr, $ret);
+exec("git -C $AUR_BUILD_DIR push origin master", $arr, $ret);
 check($ret, 'Failed to push to GitHub');
 
 // AUR
-exec("git push aur master", $arr, $ret);
+exec("git -C $AUR_BUILD_DIR push aur master", $arr, $ret);
 check($ret, 'Failed to push to AUR');
 
 echo "{success:true}";
