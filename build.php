f362b282e9b528aa30c7feb6a46a9d183cfa4582
James Houghton <jamesthoughton@gmail.com>
2016-12-28 04:56:47 -0500

    Remove unneeded

--- build.php
+++ build.php
@@ -8,36 +8,32 @@ if($cmp != 0) {
     die("{success:false, message:'Password incorrect'}");
 }
 
-$arr = [];
-$ret = 0;
-
-$AUR_BUILD_DIR="/srv/http/larryshell_aur";
 
 function check($r, $m) {
     if($r) {
         die("{success:false, message:'$m'}");
     }
 }
-exec("cd $AUR_BUILD_DIR && make clean && make && make build", $arr, $ret);
+system("cd $AUR_BUILD_DIR && make clean && make && make build",  $ret);
 check($ret, 'Build failed');
 
 // --exit-code:
 //  0 if no changes
 //  1 if changes
-exec("git -C $AUR_BUILD_DIR/larryshell diff --exit-code HEAD", $arr, $ret);
+system("git -C $AUR_BUILD_DIR/larryshell diff --exit-code HEAD", $ret);
 check(1 - $ret, 'No changes');
 
-exec("git -C $AUR_BUILD_DIR add .");
-$sha = shell_exec("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
-exec("git -C $AUR_BUILD_DIR commit -m 'Automatic version bump -- $sha'", $arr, $ret);
-
+system("git -C $AUR_BUILD_DIR add .");
+$sha = system("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
+system("git -C $AUR_BUILD_DIR commit -m 'Automatic version bump -- $sha'", $ret);
 check($ret, 'Failed to commit');
+
 // DAWT
-exec("git -C $AUR_BUILD_DIR push dawt master", $arr, $ret);
+system("git -C $AUR_BUILD_DIR push dawt master", $ret);
 check($ret, 'Failed to push to GitHub');
 
 // AUR
-exec("git -C $AUR_BUILD_DIR push origin master", $arr, $ret);
+system("git -C $AUR_BUILD_DIR push origin master", $ret);
 check($ret, 'Failed to push to AUR');
 
 echo "{success:true}";
