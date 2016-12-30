0431b17167e0ecaff1bb2e59a2f42303100e73d0
zoe <chp321@gmail.com>
2016-12-30 16:01:15 +0100

    Update to 0.2-1

--- PKGBUILD
+++ PKGBUILD
@@ -2,15 +2,15 @@
 
 pkgname=kf5-servicemenus-md5sha1calc
 _srcname=md5sha1calc
-pkgver=0.1
-pkgrel=7
+pkgver=0.2
+pkgrel=1
 pkgdesc="A service menu in dolphin (right click) to compute md5sum and sha1sum of a file"
 arch=('i686' 'x86_64')
 license=('GPL')
-depends=('plasma-workspace' 'dolphin' 'kdialog' 'bash')
+depends=('coreutils' 'plasma-workspace' 'dolphin' 'kdialog' 'bash')
 install=${_srcname}.install
 source=(${_srcname}.sh ${_srcname}.desktop ${_srcname}.install ${_srcname}-48.png)
-sha1sums=('c4e93592c578bca7497defc7ab8df4dce35d0b8e'
+sha1sums=('5aa0022ee87b6b579c73dd6407cb2fe0e873b16c'
           'd65e1718ecfb0ffd21dd31565cc0aeec20e57161'
           '03a6f50b4e2ddd87cfa62a318323f3120846499e'
           '17f74c726c406e516ac59eb91f8b7dd089c466bd')
--- md5sha1calc.sh
+++ md5sha1calc.sh
@@ -4,18 +4,26 @@ CheckedFile="$1"
 
 MD5SUM=`md5sum "$CheckedFile" | awk '{ print $1;}'`
 SHA1SUM=`sha1sum "$CheckedFile" | awk '{ print $1;}'`
+SHA224SUM=`sha224sum "$CheckedFile" | awk '{ print $1;}'`
+SHA256SUM=`sha256sum "$CheckedFile" | awk '{ print $1;}'`
+SHA384SUM=`sha384sum "$CheckedFile" | awk '{ print $1;}'`
+SHA512SUM=`sha512sum "$CheckedFile" | awk '{ print $1;}'`
 
-      kdialog --yesno "File : $CheckedFile \n md5sum : $MD5SUM\n sha1sum : $SHA1SUM\n\n  Save to file?"
+
+      kdialog --yesno "File : $CheckedFile \n md5sum : $MD5SUM\n sha1sum : $SHA1SUM\n sha224 : $SHA224SUM\n sha256 : $SHA256SUM\n sha384 : $SHA384SUM\n sha512 : $SHA512SUM\n\n  Save to file?"
 
         if [ $? == 0 ];then
-		Result=`kdialog --menu "select" md5 md5sum sha1 sha1sum`
+		Result=`kdialog --menu "select" md5 md5sum sha1 sha1sum sha224 sha224sum sha256 sha256sum sha384 sha384sum sha512 sha512sum`
 		
 		if [ $? == 0 ];then
-			if [ "$Result" == "md5" ];then
-				echo $MD5SUM > $CheckedFile".md5"
-			else
-				echo $SHA1SUM > $CheckedFile".sha1"
-			fi
+		    case "$Result" in
+		        "md5")    echo $MD5SUM    > $CheckedFile".md5" ;;
+		        "sha1")   echo $SHA1SUM   > $CheckedFile".sha1" ;;
+		        "sha224") echo $SHA224SUM > $CheckedFile".sha224" ;;
+		        "sha256") echo $SHA256SUM > $CheckedFile".sha256" ;;
+		        "sha384") echo $SHA384SUM > $CheckedFile".sha384" ;;
+		        "sha512") echo $SHA512SUM > $CheckedFile".sha512" ;;
+		    esac    
 		fi
 	fi
 
