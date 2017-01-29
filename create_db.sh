5392c5ab87d0c1d98ede201da70fe0c3c72debc7
nroi <nroi@mailbox.org>
2017-01-29 18:54:58 +0100

    Clear warnings

--- create_db.sh
+++ create_db.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 CMD=$(cat <<EOF
-:ok = :mnesia.create_schema([node])
+:ok = :mnesia.create_schema([node()])
 :ok = :mnesia.start()
 {:atomic, :ok} = :mnesia.create_table(ContentLength, [attributes: [:path, :content_length],
-                                                      disc_copies: [node]])
+                                                      disc_copies: [node()]])
 EOF
 )
 ELIXIR_ERL_OPTIONS="-sname cpcache -mnesia dir '/var/cache/cpcache/mnesia'" /usr/bin/elixir -e "$CMD"
