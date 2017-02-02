3933828d5f66fbc090f47cd728cb7915a89b4ff1
James Houghton <jamesthoughton@gmail.com>
2017-02-01 23:33:30 -0500

    Change some build files

--- build.php
+++ build.php
@@ -1,27 +1,34 @@
 <?php
 
-$good_passwd="a8fa52a1efe257d9b722aaff1c935a517916294d06d2d7db7cc65b63a0244c92a0e93e9fc00b16caa3ed719078f1e4173200634e957ea2ee9387a097f1e983a2";
-$passwd=escapeshellcmd($_GET["password"]);
+$body = file_get_contents('php://input');
+$request = json_decode($body, TRUE);
+if($request['action'] != "release") {
+    die("{success:false,message:'We did not receive a release request'}");
+}
+
+$good_passwd="ef09039ff36e10fe6b7264a3b8703af3ef5fb624fd47384115ab3ae538d77ae8f9e9a7b9b39e805fa1122c14aefa414cde05e96773b90c4f2ade5b18014e6fa9";
+$passwd=escapeshellcmd(trim($_SERVER['X-Hub-Signature']));
 $hashed_passwd=shell_exec("printf $passwd | sha512sum | cut -d ' ' -f 1");
 $cmp = strcmp(trim($hashed_passwd), trim($good_passwd));
 if($cmp != 0) {
-    die("{success:false, message:'Password incorrect'}");
+    die("{success:false, message:'Password incorrect', password: '$passwd', hashed_password: '$hashed_passwd'}");
 }
-
 
 function check($r, $m) {
     if($r) {
         die("{success:false, message:'$m'}");
     }
 }
-system("cd $AUR_BUILD_DIR && make clean && make && make build",  $ret);
-check($ret, 'Build failed');
 
 // --exit-code:
 //  0 if no changes
 //  1 if changes
-system("git -C $AUR_BUILD_DIR/larryshell diff --exit-code HEAD", $ret);
+system("git -C $AUR_BUILD_DIR/larryshell fetch");
+system("git -C $AUR_BUILD_DIR/larryshell diff --exit-code HEAD FETCH_HEAD", $ret);
 check(1 - $ret, 'No changes');
+
+system("bash $AUR_BUILD_DIR/build.sh",  $ret);
+check($ret, 'Build failed');
 
 system("git -C $AUR_BUILD_DIR add .");
 $sha = system("git -C $AUR_BUILD_DIR/larryshell rev-parse --short HEAD");
---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+make clean
+make
+make build
