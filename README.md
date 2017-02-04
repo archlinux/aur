9765c23e5b15b0795d47c4dc1916f7fefc5eaa87
Grégoire Seux <kamaradclimber@gmail.com>
2017-02-04 10:40:59 +0100

    Automatically fetch latest version from github in update.sh

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 
-To update the package:
+# To update the package:
 
 - update home-assistant version in PKGBUILD
 - update the sha256sum
 - run makepkg -si
 - ensure everything is running properly
 - makepkg --printsrcinfo > .SRCINFO
+
+# Helper: update.sh
+
+You can run `update.sh` to run all these actions automatically.
+It will:
+- fetch the latest home-assistant version (if none specified)
+- update the PKGBUILD
+- build the package and install it
+- wait for xx seconds to let home-assistant restart and display service status
--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-VERSION=$1
-
 set -e
+LAST_VERSION=$(ruby -r "net/http" -r "uri" -r "json" -e 'uri = URI.parse("https://api.github.com/repos/home-assistant/home-assistant/releases"); response = Net::HTTP.get_response(uri); if response.code.to_i != 200 then puts response.code.inspect; exit(1); end; puts JSON.parse(response.body).first["name"]')
+
+VERSION=$1
+echo "Will update to ${VERSION:=$LAST_VERSION}"
+
 sed -i -re "s/^pkgver=.*$/pkgver=$VERSION/" PKGBUILD
 # Reset release to 1
 sed -i -re "s/^pkgrel=.*$/pkgrel=1/" PKGBUILD
