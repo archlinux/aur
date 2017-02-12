a78e0df7a84f8841f3b9a7171cdcb91a0533d8b4
trickart <trickart412@gmail.com>
2017-02-12 20:45:38 +0900

    Update mod-generate-dictionary.sh

--- mod-generate-dictionary.sh
+++ mod-generate-dictionary.sh
@@ -2,31 +2,28 @@
 
 [[ ! -f generate-dictionary.sh ]] && { echo "generate-dictionary.sh not found."; exit 1; }
 
-sed -i \
+sed -i.bak \
     -e '1s/sh/sh -e/' \
     -e 's/ruby \(.*\)/ruby -EUTF-8 \1/g' \
     generate-dictionary.sh \
     edict-katakana-english/generate-dictionary.sh
 
-# Comment out the specifying versions and initial cleanup
-# cp $srcdir/KEN_ALL.CSV to chimei/
-# Specify $srcdir/JIGYOSYO.CSV
-# Modify mozc-\$MOZCVER to mozc
-# Remove making xz archive
-sed -i \
-    -e 's/^\(MOZCVER=.*\|DICVER=.*\|NICODIC=.*\)/#&/' \
-    -e '/rm -rf mozc-$MOZCVER/s/^/#/i' \
-    -e '/echo "extract official mozc..."/,+1s/^/#/ig' \
-    -e '/rm -f \*\.zip/,+6s/^/#/ig' \
+# 1) Comment out specifying versions and initial cleanup
+# 2) cp $srcdir/KEN_ALL.CSV to chimei/
+# 3) Specify $srcdir/JIGYOSYO.CSV
+# 4) Modify mozc-\$MOZCVER to mozc
+# 5) Comment out making xz archive
+sed -i.bak \
+    -e 's/^\(MOZCVER=.*\|DICVER=.*\|NICODIC=.*\)/#&/g' \
+    -e '/rm -rf mozc-$MOZCVER/s/^/#/g' \
+    -e '/echo "extract official mozc..."/,+1s/^/#/g' \
+    -e '/rm -f \*\.zip/,+6s/^/#/g' \
     -e '/cd chimei/a cp -f ../../KEN_ALL.CSV .' \
     -e 's|JIGYOSYO.CSV|../../JIGYOSYO.CSV|' \
     -e 's|mozc-\$MOZCVER|mozc|g' \
-    -e '/# compress mozc-ut/,+4s/^/#/ig' \
-    -e 's|^cp -r docs/|cp -r docs-ut/|' \
+    -e '/# compress mozc-ut/,+4s/^/#/g' \
     generate-dictionary.sh
 
-sed -i \
-    -e '/rm edict.*/,/gzip -dk edict.*/s/^/#/ig' \
+sed -i.bak \
+    -e '/^\(#\|ruby\|$\)/!s/^/#/g' \
     edict-katakana-english/generate-dictionary.sh
-
-#sed -i '71s/s\[5\]/& != "" and &/' chimei/get-entries.rb
