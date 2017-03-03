2e35a3a8bca3ecd3ff03a715be04d9d856ddf0eb
Marius Knaust <marius.knaust@gmail.com>
2017-03-03 15:09:42 +0100

    Replaced info file with notification and log entry

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Marius Knaust <marius.knaust@gmail.com>
 
 pkgname=spotlight
-pkgver=1.4
+pkgver=1.5
 pkgrel=1
 pkgdesc="Displays a new background image daily"
 arch=('any')
@@ -15,14 +15,17 @@ depends=('wget'
          'libsystemd')
 source=('spotlight.sh'
         'spotlight.service'
-        'spotlight.timer')
-md5sums=('efc8c0612da69f6faec5ffb4132ac809'
+        'spotlight.timer'
+        'spotlight.desktop')
+md5sums=('3513346c4a6be5c3aad07bd4b3e64489'
          '3f71203246d7003af3f05b1891ccfd5f'
-         '5cf181a3dd50993e0dfbf1835c9d2364')
-
+         '5cf181a3dd50993e0dfbf1835c9d2364'
+         '30fbf3665eca3ea98d00d38c5829c1ad')
+         
 package()
 {
 	install -m755 -D ${srcdir}/spotlight.sh ${pkgdir}/usr/bin/spotlight.sh
 	install -m644 -D ${srcdir}/spotlight.service ${pkgdir}/usr/lib/systemd/user/spotlight.service
 	install -m644 -D ${srcdir}/spotlight.timer ${pkgdir}/usr/lib/systemd/user/spotlight.timer
+	install -m644 -D ${srcdir}/spotlight.desktop ${pkgdir}/usr/share/applications/spotlight.desktop
 }
--- spotlight.sh
+++ spotlight.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
 item=$(wget -qO- "https://arc.msn.com/v3/Delivery/Cache?pid=279978&fmt=json&ua=WindowsShellClient&lc=en,en-US&ctry=US" | jq -r ".batchrsp.items | .[0].item")
-url=$(echo $item | jq -r ".ad.image_fullscreen_001_landscape.u")
+
+landscapeUrl=$(echo $item | jq -r ".ad.image_fullscreen_001_landscape.u")
 title=$(echo $item | jq -r ".ad.title_text.tx")
 titleUrl=$(echo $item | jq -r ".ad.title_destination_url.u" | perl -pe 's/.*?(http.*)/\1/')
 
 mkdir -p "$HOME/.spotlight"
-wget -qO "$HOME/.spotlight/background.jpg" $url
-echo "$title" > "$HOME/.spotlight/background_info"
-echo "$titleUrl" >> "$HOME/.spotlight/background_info"
+path="$HOME/.spotlight/background.jpg"
+	
+wget -qO "$path" "$landscapeUrl"
 
 gsettings set org.gnome.desktop.background picture-options "zoom"
-gsettings set org.gnome.desktop.background picture-uri "file:///$HOME/.spotlight/background.jpg"
+gsettings set org.gnome.desktop.background picture-uri "file://$path"
+
+notify-send "Background changed" "$title ($titleUrl)" --icon=preferences-desktop-wallpaper --urgency=low --hint=string:desktop-entry:spotlight
+echo "Background changed to $title ($titleUrl)" | systemd-cat -t spotlight
