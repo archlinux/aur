1522ffd16cb6a76ac5a8e909f6ce5f54b5513334
Chris Trotman <chris@trotman.io>
2017-04-02 14:58:17 +1000

    Fix the hash sums

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export version=$1
-export hash=$(curl https://github.com/hashicorp/terraform/archive/v$version.tar.gz | sha256sum | awk {'print $1'})
+export hash=$(curl -L https://github.com/hashicorp/terraform/archive/v$version.tar.gz | sha256sum | awk {'print $1'})
 
 cat PKGBUILD.template| envsubst | sed  's/\${{/\${/g' > PKGBUILD
 mksrcinfo
