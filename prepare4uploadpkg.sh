1ac07b413b07345c4dd0aaf01b9613c4fda59209
John A Ginis <drxspace@gmail.com>
2017-06-03 12:09:12 +0300

    New package commit

--- prepare4uploadpkg.sh
+++ prepare4uploadpkg.sh
@@ -14,11 +14,10 @@ sed -i "$(replace United-Arch.tar.gz)" PKGBUILD
 
 rm -rfv src/ United-Arch/ United-Arch.tar.gz gtk-theme-united-archers-git-*.tar.xz
 
-namcap PKGBUILD && {
-	makepkg --printsrcinfo > .SRCINFO
-	git add . # PKGBUILD prepare4uploadpkg.sh .SRCINFO
-	git commit -m "New package commit"
-	git push origin
-}
+namcap PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
+git add . # PKGBUILD prepare4uploadpkg.sh .SRCINFO
+git commit -m "New package commit"
+git push
 
 exit
