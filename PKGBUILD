b4bdd401f5a33f23c14a6998d440d85399efc611
Leo Schwarz <mail@leoschwarz.com>
2017-06-07 17:01:39 +0200

    Update to 54.0b14.
    Note that this is now based on the Firefox Beta channel as the Aurora
    Channel is deprecated by Mozilla.

--- PKGBUILD
+++ PKGBUILD
@@ -10,23 +10,18 @@
 _name=firefox
 _channel=developer
 
-_release_year=2017
-_release_month=05
-_release_day=11
-_release_time=07-47-36
-_release_version=54.0a2
-
 pkgname="${_name}-${_channel}-de"
 pkgdesc='Standalone web browser from mozilla.org, developer build - German'
 url='http://www.mozilla.org/firefox/developer'
-pkgver="${_release_version}_${_release_year}${_release_month}${_release_day}"
+pkgver=54.0b14
 pkgrel=1
 arch=('x86_64')
 license=('MPL' 'GPL' 'LGPL')
 _file="${_name}-${_release_version}.de.linux-x86_64"
 _release_dir="${_release_year}-${_release_month}-${_release_day}-${_release_time}-mozilla-aurora-l10n"
-source=("firefox_$pkgver.tar.bz2::https://ftp.mozilla.org/pub/firefox/nightly/$_release_year/$_release_month/$_release_dir/$_file.tar.bz2"
-        "firefox_$pkgver.tar.bz2.asc::https://ftp.mozilla.org/pub/firefox/nightly/$_release_year/$_release_month/$_release_dir/$_file.tar.bz2.asc"
+source=("https://ftp.mozilla.org/pub/devedition/releases/$pkgver/linux-x86_64/de/firefox-$pkgver.tar.bz2"
+        "https://ftp.mozilla.org/pub/devedition/releases/$pkgver/SHA512SUMS"
+        "https://ftp.mozilla.org/pub/devedition/releases/$pkgver/SHA512SUMS.asc"
         "firefox-$_channel.desktop"
         "vendor.js")
 depends=('alsa-lib' 'libxt' 'libnotify' 'mime-types' 'nss' 'gtk2' 'gtk3' 'sqlite' 'dbus-glib')
@@ -34,6 +29,22 @@ provides=(firefox-developer)
 conflicts=(firefox-developer)
 
 validpgpkeys=('14F26682D0916CDD81E37B6D61B7B526D98F0353')
+
+prepare() {
+    # Check if hash of the source archive matches the one provided by Mozilla (which was signed with GPG).
+    _checksum=`grep "linux-x86_64/de/firefox-$pkgver.tar.bz2" $srcdir/SHA512SUMS | cut -f1 -d " "`
+    _actual=`sha512sum $srcdir/firefox-$pkgver.tar.bz2 | cut -f1 -d " "`
+
+    msg2 "Checking integrity of firefox-$pkgver.tar.bz2"
+
+    if [[ $_checksum == $_actual ]];
+    then
+        msg2 "Checksum verified successfully."
+    else
+        msg2 "Checksum verification failed!"
+        exit 1
+    fi
+}
 
 package() {
     install -d $pkgdir/{usr/{bin,share/{applications,pixmaps}},opt}
@@ -45,7 +56,8 @@ package() {
     install -Dm644 $srcdir/vendor.js $pkgdir/opt/firefox-$_channel/browser/defaults/preferences/vendor.js
 }
 
-sha512sums=('db1549197170fe907c55293854a3128e42f62de3178c183eb0805b17f5bb609bf50b39880f5e4156193376077c735fd4fc9d191dca2f183394e3778cbf46b8e6'
+sha512sums=('f871bbc4cce559fad162d4579c46623a81c8a9faec2b0328f5f3e9492bfcdb64d5311cc8aa75ec3e8ebb2c0f2823193fff163a345e3cca43e70df0bd355e998a'
+            'a41b356b0d48a88d311f291c21e0ea7a7a8723a5fbd0f783fb102c97a50e90323b736641e1c4370cc8863b60bad0bc2028d68f4a4a10ed66d6055cfa37d2d6a6'
             'SKIP'
             'f79af68a2d34dd60c89755094ac5d8675fe968d18897683efebdeec56c5ed56ca87276d83dc48c0ba91e2697dc4c39c29fe49245d8eb22ef84e8515917ea7e2b'
             'bae5a952d9b92e7a0ccc82f2caac3578e0368ea6676f0a4bc69d3ce276ef4f70802888f882dda53f9eb8e52911fb31e09ef497188bcd630762e1c0f5293cc010')
--- update.rb
+++ update.rb
@@ -1,72 +1,51 @@
 #!/usr/bin/env ruby
-# Initial work on updater script for pkg files.
-# This still is mostly a hack and could be vastly improved.
 
-require 'date'
 require 'net/http'
 
-FF_VERSION = "54.0a2"
+# carch: "x86_64" or "i686"
+def path_for_version(version, carch="x86_64")
+	"https://ftp.mozilla.org/pub/devedition/releases/#{version}/linux-#{carch}/de/firefox-#{version}.tar.bz2"
+end
 
 # Check current version.
 pkgbuild = File.read "PKGBUILD"
-current_version = Date.new(
-	/^_release_year=(\d+)$/.match(pkgbuild)[1].to_i,
-	/^_release_month=(\d+)$/.match(pkgbuild)[1].to_i,
-	/^_release_day=(\d+)$/.match(pkgbuild)[1].to_i
-)
+current_version = /^pkgver=(.*)$/.match(pkgbuild)[1].to_s
+puts "Current packaged version: #{current_version}"
 
-def attempt_upgrade (date)
-	# Format date components.
-	year = date.year
-	month = "%02d" % date.month
-	day = "%02d" % date.day
-	puts "Attempting upgrade to version #{date}"
-
-	# Find directory on server.
-	request_url = URI "https://ftp.mozilla.org/pub/firefox/nightly/#{year}/#{month}/"
-	response = Net::HTTP.get(request_url)
-	search = %r~href="/(pub/firefox/nightly/#{year}/#{month}/#{year}-#{month}-#{day}-([0-9-]+)-mozilla-aurora-l10n/)"~
-	matches = response.scan(search)
-	return false if matches.empty?
-	release_url, release_time = matches.max_by{|_, t| t}
-
+def attempt_upgrade(version)
 	# Check if the release actually exists (sometimes it's not included in the directory).
-	base_url = "https://ftp.mozilla.org/#{release_url}"
-	filename = "firefox-#{FF_VERSION}.de.linux-x86_64.tar.bz2"
-	uri = URI(base_url + filename)
+	uri = URI(path_for_version(version))
+	puts "attempting upgrade (#{version}), checking: #{uri}"
 	
 	request = Net::HTTP.new uri.host
 	response = request.request_head uri.path
 	if response.code.to_i != 200
-		puts "Found directory on server (#{year}-#{month}-#{day}) but no file."
 		return false
 	end
 
 	# Update PKGBUILD file.
 	pkgbuild = File.read "PKGBUILD"
-	pkgbuild.gsub!(/^_release_year=.*$/, "_release_year=#{year}")
-	pkgbuild.gsub!(/^_release_month=.*$/, "_release_month=#{month}")
-	pkgbuild.gsub!(/^_release_day=.*$/, "_release_day=#{day}")
-	pkgbuild.gsub!(/^_release_time=.*$/, "_release_time=#{release_time}")
-	pkgbuild.gsub!(/^_release_version=.*$/, "_release_version=#{FF_VERSION}")
+	pkgbuild.gsub!(/^pkgver=.*$/, "pkgver=#{version}")
 	
 	File.open "PKGBUILD", "w" do |file|
-	    file.write pkgbuild
+		file.write pkgbuild
 	end
 	
 	true
 end
 
-# Update logic. Start with tomorrow (to account for timezone differences) and work back in time
-# and pick the most recent version or cancel if we arrive at the current version.
-new_version = Date.today.next_day
+# Update logic: Start with current version, alway incrementing the beta part by one and see if it exists, if yes
+#	we perform an additional step if not we are happy with what we've found.
 success = false
-while current_version < new_version
-	success = attempt_upgrade(new_version)
-	if success
-		break
+v_num, v_patch = current_version.split("b")
+loop do
+	v_patch = v_patch.to_i + 1
+	new_version = "#{v_num}b#{v_patch}"
+
+	if attempt_upgrade(new_version)
+		success = false
 	else
-		new_version = new_version.prev_day
+		break
 	end
 end
 
@@ -82,10 +61,10 @@ if success
 
 	# If run with --publish it will be uploaded to the remotes.
 	if ARGV.include? "--publish"
-	    puts "Publishing update to AUR."
-	    system "git add PKGBUILD .SRCINFO"
-	    system "git commit -m 'Automatic upgrade to version #{FF_VERSION} (#{new_version}).'"
-	    system "git push"
+		puts "Publishing update to AUR."
+		system "git add PKGBUILD .SRCINFO"
+		system "git commit -m 'Automatic upgrade to version #{FF_VERSION} (#{new_version}).'"
+		system "git push"
 	end
 else
 	puts "No update was available."
