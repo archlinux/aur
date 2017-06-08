e96f7381b5787d4e1c6146127e3b6f54a3848e93
eolianoe <eolianoe@gmail.com>
2017-06-08 14:15:51 +0200

    Fix path

--- pgi-compilers.sh
+++ pgi-compilers.sh
@@ -1,7 +1,8 @@
 export PGI=/opt/pgi
 export PGI_HOME=${PGI}/linux86-64/@release@
 export PATH=${PATH}:${PGI_HOME}/bin
-export PATH=${PGI_HOME}/mpi/openmpi/bin:${PATH}
-export PATH=${PGI_HOME}/mpi/mvapich/bin:${PATH}
+export PATH=${PATH}:${PGI_HOME}/mpi/openmpi/bin
+export PATH=${PATH}:${PGI_HOME}/mpi/mvapich/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PGI_HOME}/lib
 export MANPATH=${MANPATH}:${PGI_HOME}/man
 export LM_LICENSE_FILE=${LM_LICENSE_FILE}:${PGI}/license.dat
