11e83a9954415a7c26df4237e60181646707b129
torben <t.sickert[~at~]gmail.com>
2017-06-20 12:11:19 +0200

    automatic update

--- PKGBUILD
+++ PKGBUILD
@@ -10,7 +10,7 @@
 # 3.0 unported license. see http://creativecommons.org/licenses/by/3.0/deed.de
 # endregion
 pkgname=backup-rotation
-pkgver=1.0.78
+pkgver=1.0.79
 pkgrel=31
 pkgdesc='This script allows you to create a local or remote backup rotation for your files.'
 arch=('any')
--- backupRotation.sh
+++ backupRotation.sh
@@ -67,7 +67,7 @@ number_of_daily_retention_days=14 # Daily backups for the last 14 days.
 number_of_weekly_retention_days=56 # Weekly backups for the last 2 month.
 number_of_monthly_retention_days=365 # Monthly backups for the last year.
 target_file_extension='.tar.gz'
-backup_command='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose --exclude=.git --exclude=.npm --exclude=node_modules --exclude=.local "$source_path" "$target_file_path" && tar --create --verbose --gzip --file "${target_file_path}${target_file_extension}" "$target_file_path" && rm --recursive --verbose "$target_file_path"'
+backup_command='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose --exclude=.git --exclude=.npm --exclude=node_modules --exclude=.local "$source_path" "$target_file_path" && tar --create --verbose --gzip --file "${target_file_path}${target_file_extension}" "$target_file_path"; rm --recursive --verbose "$target_file_path"'
 post_backup_command=''
 # Folder to delete is the last command line argument.
 cleanup_command='rm --recursive --verbose'
