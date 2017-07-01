45077bcc60340b22614479fc74fec351ab9fbc78
Bleuzen <supgesu@gmail.com>
2017-07-02 01:32:41 +0200

    removed old Blizcord.sh

--- Blizcord.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $(pidof -x "Blizcord.sh"| wc -w) -gt 2 ]; then 
-    echo "Another instance is already running"
-    exit
-fi
-
-java -jar Blizcord.exe --disable-update-checker "$@"
