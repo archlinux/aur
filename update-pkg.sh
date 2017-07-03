f5ceca605cfe7260a7dc69dd413c0af7978b45f8
papajoker <patrickc538@gmail.com>
2017-07-03 08:26:05 +0200

    version 55 new branch beta

---
+++ update-pkg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# get last pkgver
+pkgver=$(curl -s https://hg.mozilla.org/releases/mozilla-beta/tags | grep -Eo ">FIREFOX_.*_.*_RELEASE<" -m1  | awk -F'_' '{print $2"."$3}')
+echo "last FF-dev release: ${pkgver}"
+sed -ie "s/pkgver=.*/pkgver=${pkgver}/" ./PKGBUILD
+
+#makepkg -g -f -p PKGBUILD
+updpkgsums
+
+makepkg --printsrcinfo >.SRCINFO
+
+makepkg -scf
