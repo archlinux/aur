91c2e22786733808951c3489639160d0c46674ec
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2017-07-24 09:40:40 +0200

    Small cleanup in update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-pkgver=$(curl -s https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016 | \grep -Eo ">Beta Build [0-9]+\.3\.[0-9]+</a>" | \grep -Eo '[0-9]+\.3\.[0-9]+' | sort -rV | head -1)
+pkgver=$(curl -s https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016 | grep -Eo ">Beta Build [0-9]+\.3\.[0-9]+</a>" | grep -Eo '[0-9]+\.3\.[0-9]+' | sort -rV | head -1)
 sed "s/^pkgver=.*/pkgver=$pkgver/" -i PKGBUILD
 updpkgsums
