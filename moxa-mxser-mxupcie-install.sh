0b5c2fab73a37f56412852b8ed350917cb40c79c
Chris Severance <aur.severach@spamgourmet.com>
2017-08-14 12:14:42 -0400

    Initial Import

---
+++ moxa-mxser-mxupcie-install.sh
@@ -0,0 +1,45 @@
+_pkgname='moxa-mxser-mxupcie'
+_modulenames=('mxupcie' 'mxser')
+_origmodname='8250_moxa'
+
+post_upgrade() {
+  set -u
+  if [ ! -d "/usr/src/${_pkgname}"-*/ ]; then
+    depmod -a
+    rmmod "${_origmodname}" 2> /dev/null # blacklisted on boot
+    local _modulename
+    for _modulename in "${_modulenames[@]}"; do
+      if ! lsmod | cut -d' ' -f1 | grep -q "^${_modulename}"'$'; then
+        #echo 'Reboot or load now with:'
+        #echo "  sudo modprobe '${_modulename}'"
+        modprobe "${_modulename}"
+      fi
+    done
+  fi
+  systemctl daemon-reload
+  set +u
+}
+
+post_install() {
+  set -u
+  post_upgrade
+  systemctl enable 'moxa-mxser-settings.service'
+  systemctl start 'moxa-mxser-settings.service'
+  set +u
+}
+
+pre_remove() {
+  set -u
+  if [ ! -d "/usr/src/${_pkgname}"-*/ ]; then
+    local _modulename
+    for _modulename in "${_modulenames[@]}"; do
+      rmmod "${_modulename}"
+    done
+  fi
+  systemctl disable 'moxa-mxser-settings.service'
+  set +u
+}
+
+post_remove() {
+  systemctl daemon-reload
+}
