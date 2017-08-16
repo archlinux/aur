6aeaea1fc2d5ca16102efac1921f8f60611846ed
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-08-16 21:01:34 +0200

    Revert "use pango v1.40.5-1 #45"
    The next update of gitkraken doesn't need anymore this quick fix.
    
    This reverts commit 03fb342d7ebb05f4b1ea144085a0c256c3d71022.

--- gitkraken.sh
+++ gitkraken.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-LD_PRELOAD=/usr/lib/libcurl-openssl-1.0.so.4.4.0:/opt/gitkraken/libpangoft2-1.0.so.0.4000.5 /opt/gitkraken/gitkraken "$@"
+LD_PRELOAD=/usr/lib/libcurl-openssl-1.0.so.4.4.0 /opt/gitkraken/gitkraken "$@"
