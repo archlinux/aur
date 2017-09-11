611fd40ada2433160015c567849e014af61883ce
papajoker <patrickc538@gmail.com>
2017-09-11 12:21:52 +0200

    best source update

--- update-pkg.sh
+++ update-pkg.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 # get last pkgver
-pkgver=$(curl -s https://hg.mozilla.org/releases/mozilla-beta/tags | grep -Eo ">FIREFOX_.*_.*_RELEASE<" -m1  | awk -F'_' '{print $2"."$3}')
+pkgver=$(curl -s "https://ftp.mozilla.org/pub/devedition/releases/" | awk -F'/' '/href.*releases\// {print $5}' | sort -Vr | head -n1)
 echo "last FF-dev release: ${pkgver}"
+
 sed -i -e "s/pkgver=.*/pkgver=${pkgver}/" ./PKGBUILD
 
 #makepkg -g -f -p PKGBUILD
