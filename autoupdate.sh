dbc59d17314fd94934a00424eda62e0ab0c8de3a
Angelo Salton <gsalton4@hotmail.com>
2017-10-28 14:54:57 -0200

    Included auto-update script

---
+++ autoupdate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl "http://www.dynare.org/release/source/MD5SUMS" | tail -n 1 > md5list.txt
+read -a words < md5list.txt
+
+# replace md5hash
+sed "s/md5sums=('.*')/md5sums=('${words[0]}')/" PKGBUILD > PKGBUILD
+
+# replace version
+dynarever="$(echo "${words[1]}" | sed 's/.tar.xz//')"
+dynarever="$(echo $dynarever | sed 's/dynare-//')"
+sed "s/pkgver=[0-9].[0-9].[0-9]/pkgver=${dynarever}/g" PKGBUILD > PKGBUILD
+
+# update
+git add PKGBUILD .SRCINFO
+git commit -c="Updated to ${dynarever}"
+git push
