a2df5d2b7398471bb5ea9c4ec3814023d7e0a5ec
Antony Kellermann <aokellermann@gmail.com>
2018-01-04 01:21:05 -0500

    fixed null reference

--- portfolio.c
+++ portfolio.c
@@ -1,8 +1,8 @@
 #include "portfolio.h"
 
-void portfolio_file_init(){
+void portfolio_file_init() {
     char* home = getenv("HOME");
-    char* path = (char*)malloc(strlen(home) + 21);
+    char* path = (char*) malloc(strlen(home) + 21);
     memcpy(path, home, strlen(home) + 1);
     memcpy(&path[strlen(home)], "/.getcrypt_portfolio", 21);
     portfolio_file = path;
@@ -38,8 +38,8 @@ void portfolio_modify(char* id, double amount, double amount_spent, FILE* fp, in
                 break;
             end[i] = c;
         }
-        if (end[strlen(end)-1] == '\n')
-            end[strlen(end)-1] = '\0';
+        if (end[strlen(end) - 1] == '\n')
+            end[strlen(end) - 1] = '\0';
         remove(portfolio_file);
         fp = fopen(portfolio_file, "w");
         fprintf(fp, "%s%lf %lf", beginning, new_amount, new_amount_spent);
@@ -49,7 +49,7 @@ void portfolio_modify(char* id, double amount, double amount_spent, FILE* fp, in
         free(end);
         fclose(fp);
     } else {
-        if (option == REMOVE){
+        if (option == REMOVE) {
             printf("You don't have any %s to remove!\n", id);
             return;
         }
@@ -74,14 +74,14 @@ double portfolio_get_amount_spent(char* id, FILE* fp) {
     return 0;
 }
 
-void portfolio_print_all(STRING api_data, JSON crypto_data, FILE* fp){
-    char* str = (char*)calloc(64, sizeof(char));
-    char* id = (char*)calloc(32, sizeof(char));
+void portfolio_print_all(STRING api_data, JSON crypto_data, FILE* fp) {
+    char* str = (char*) calloc(64, sizeof(char));
+    char* id = (char*) calloc(32, sizeof(char));
     double* data;
-    double total_owned=0, total_spent=0;
+    double total_owned = 0, total_spent = 0;
     int i;
     char c;
-    while (fgets(str, 63, fp) != NULL){
+    while (fgets(str, 63, fp) != NULL) {
         i = 0;
         while ((c = str[i]) != ' ') {
             id[i] = c;
@@ -90,8 +90,10 @@ void portfolio_print_all(STRING api_data, JSON crypto_data, FILE* fp){
         string_write_data(api_data, id);
         crypto_data = json_init(api_data, 0);
         data = portfolio_print_currency(id, json_get_data(crypto_data), fp);
-        total_owned += data[0];
-        total_spent += data[1];
+        if (data != NULL) {
+            total_owned += data[0];
+            total_spent += data[1];
+        }
         memset(str, '\0', 64);
         memset(id, '\0', 32);
         string_destroy(&api_data);
@@ -107,17 +109,14 @@ void portfolio_print_all(STRING api_data, JSON crypto_data, FILE* fp){
 }
 
 double* portfolio_print_currency(char* id, const double* data, FILE* fp) {
-    double* a = malloc(sizeof(double)*2);
+    double* a = malloc(sizeof(double) * 2);
     if (portfolio_get_amount_spent(id, fp)) {
         a[0] = portfolio_get_amount(id, fp) * data[PRICE_USD];
         a[1] = portfolio_get_amount_spent(id, fp);
         if (a[0] == 0 && a[1] == 0)
             printf("Your portfolio does not contain %s\n", id);
         else printf("%lf %s. Purchased for: $%lf. Current value: $%lf.\n", a[0] / data[PRICE_USD], id, a[1], a[0]);
-    } else {
-        printf("Your portfolio does not contain %s\n", id);
-        return NULL;
-    }
+    } else return NULL;
     return a;
 }
 
