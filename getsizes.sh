7b2f9a81c128a5023e50093a2019024cfb06ff09
Rafael Fontenelle <rafaelff@gnome.org>
2018-01-14 17:44:28 -0200

    getsizes.sh: adapt output for python-markdown syntax

--- getsizes.sh
+++ getsizes.sh
@@ -6,14 +6,19 @@ test -f PKGBUILD && source PKGBUILD || exit 1
 test -d src/$_name || exit 1
 test -d pkg/$pkgname/usr/share/ddnet/data/types/ || exit 1
 
-echo -e "Sizes as of $pkgver :\n"
 
-du -shD $pkgname-$pkgver-$pkgrel-$arch.pkg.tar.xz
-du -sh src/$_name | cut -d' ' -f1
-echo ""
+BLK="    "
+echo -e "${BLK} Sizes as of $pkgver\n "
+
+echo -e "${BLK} Source code repository:"
+echo -e "${BLK} $(du -sh src/$_name | cut -d' ' -f1)\n "
+
+echo -e "${BLK} Package:"
+echo -e "${BLK} $(du -shD $pkgname-$pkgver-$pkgrel-$arch.pkg.tar.xz)\n "
 
 cd pkg/$pkgname
+echo -e "${BLK} Server types packaged:"
 for type in $(ls usr/share/ddnet/data/types/); do
-    du -sh usr/share/ddnet/data/types/$type
+    echo -e "${BLK} $(du -sh usr/share/ddnet/data/types/$type)"
 done
 
