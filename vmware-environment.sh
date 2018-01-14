b091575043e114ae3f1741823aec15c4e9941ae0
Jean-Marc Lenoir <archlinux@jihemel.com>
2018-01-14 11:59:24 +0100

    Initial commit.

---
+++ vmware-environment.sh
@@ -0,0 +1,5 @@
+# Need the next line to avoid a problem with incompatible libraries
+export LD_LIBRARY_PATH=/usr/lib/vmware/lib/libatk-1.0.so.0:/usr/lib/vmware/lib/libgio-2.0.so.0:$LD_LIBRARY_PATH
+
+# Uncomment the line below if you always have a problem of incompatible libraries
+#export VMWARE_USE_SHIPPED_LIBS=yes
