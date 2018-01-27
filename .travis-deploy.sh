a12f5c6852f087ec1c8c7c2cdb30b2bcaca3658f
Grégoire Seux <kamaradclimber@gmail.com>
2018-01-27 11:23:42 +0100

    Extract deploy in script

---
+++ .travis-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "Pull request, skipping publication to aur"
+  exit 0
+fi
+
+mkdir -p ~/.ssh
+echo -e "Host aur.archlinux.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+mv id_rsa ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+git remote add aur git+ssh://aur@aur.archlinux.org/home-assistant.git
+
+echo "Pushing to AUR"
+
+git push aur HEAD:master
