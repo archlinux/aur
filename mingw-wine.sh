58120d0d4be2468d4921bc3a667cee07d45cc06d
Michel Zou <xantares09@hotmail.com>
2018-02-23 16:30:30 +0100

    Freeze WINEPREFIX

--- mingw-wine.sh
+++ mingw-wine.sh
@@ -6,10 +6,7 @@ mingw_prefix=/usr/@TRIPLE@
 
 # run it in a custom WINEPREFIX to not mess with default ~/.wine
 # also default prefix might be a 32 bits prefix, which will fail to run x86_64 exes
-if test -z "${WINEPREFIX}"
-then
-  export WINEPREFIX=~/.wine-@TRIPLE@
-fi
+export WINEPREFIX=~/.wine-@TRIPLE@
 
 # WINEPATH is used to find dlls, otherwise they should lie next to the exe
 if test -z "${WINEPATH}"
@@ -17,13 +14,13 @@ then
   export WINEPATH=${mingw_prefix}/bin
 fi
 
+
 if test "@TRIPLE@" = "x86_64-w64-mingw32"
 then
   export WINEARCH=win64
 else
   export WINEARCH=win32
 fi
-
 
 wine "$@"
 
