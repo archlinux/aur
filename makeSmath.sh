b95684d20d09daaa84fe652d094684633195cbb3
Tom van den Brink <tom@RivaVirtus.localdomain>
2018-04-06 22:27:24 +0200

    updated script and https

--- makeSmath.sh
+++ makeSmath.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-_pkgcode=PwQc8
-pkgver=0_98_6179
+_pkgcode=XTRcr
+pkgver=0_99_6670
 
-wget --post-data "RefMem=&PostBack=${_pkgcode}" http://smath.info/file/${_pkgcode}/SMathStudioDesktop.${pkgver}.mono.tar.gz
+wget --post-data "RefMem=&PostBack=${_pkgcode}" https://smath.info/file/${_pkgcode}/SMathStudioDesktop.${pkgver}.mono.tar.gz
 
 
 _pkgmd5sum=$(md5sum SMathStudioDesktop.${pkgver}.mono.tar.gz |awk '{ print $1 }')
@@ -14,7 +14,7 @@ sed -i "14s/.*/_pkgcode=${_pkgcode}/" PKGBUILD
 sed -i "23s/.*/md5sums=\(\'${_pkgmd5sum}\'\)/" PKGBUILD
 rm SMathStudioDesktop.${pkgver}.mono.tar.gz
 
-makepkg --printsrcinfo > .SRCINFO
+#makepkg --printsrcinfo > .SRCINFO
 #First do the git commit by hand
 # git add PKGBUILD .SRCINFO
 # git commit -m "Update to ${pkgver}"
