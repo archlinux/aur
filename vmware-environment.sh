bb474ddc59030a06a859ad04c5343fc907b5088b
Jean-Marc Lenoir <archlinux@jihemel.com>
2018-05-02 15:05:26 +0200

    Does not load GTK theme if not compatible with GTK 3.14

--- vmware-environment.sh
+++ vmware-environment.sh
@@ -5,6 +5,10 @@
 export GTK_IM_MODULE_FILE=/dev/null
 
 # Workaround to use the GTK theme
-if [ -z $GTK_THEME ]; then
-	export GTK_THEME=$(gtk-query-settings gtk-theme-name | cut -d '"' -f2)
+if [ -z "$GTK_THEME" ]; then
+	theme=$(gtk-query-settings gtk-theme-name | cut -d '"' -f2)
+	# Hack to know if the theme is compatible with GTK 3.14 (used by VMware)
+	if [ \( -d ~/.themes/"$theme" -a -d ~/.themes/"$theme"/gtk-3.20 \) -o \( -d /usr/share/themes/"$theme" -a -d /usr/share/themes/"$theme"/gtk-3.20 \) ]; then
+		export GTK_THEME="$theme"
+	fi
 fi
