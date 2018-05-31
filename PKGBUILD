093ec37366d2b335d14a8645b23c23f40ad53ff4
Kohei Suzuki <eagletmt@gmail.com>
2018-05-31 23:17:59 +0900

    Update to v1.5.6

--- PKGBUILD
+++ PKGBUILD
@@ -1,13 +1,13 @@
 # Maintainer: Kohei Suzuki <eagletmt@gmail.com>
 pkgname=mitamae
-pkgver=1.5.1
+pkgver=1.5.6
 pkgrel=1
 pkgdesc="Configuration management tool embedding mruby"
 arch=('i686' 'x86_64')
 url="https://github.com/k0kubun/mitamae"
 license=('MIT')
 depends=('openssl' 'pacman')
-makedepends=('ruby' 'git')
+makedepends=('ruby-rake' 'git')
 _mruby_revision=1.3.0
 source=("https://github.com/k0kubun/${pkgname}/archive/v${pkgver}.tar.gz"
         "mruby-${_mruby_revision}.tar.gz::https://github.com/mruby/mruby/archive/${_mruby_revision}.tar.gz"
@@ -37,6 +37,6 @@ package() {
   install -m644 LICENSE "$pkgdir/usr/share/licenses/$pkgname/"
 }
 
-sha512sums=('040b05b8fcd989c2d3eb0f0acb8bde004f41083d758955c72080ca7ed3d90ca85f8798c5aaa4f4c65f7cb190fedf01c9e0d2657520668887fdac435eff52981a'
+sha512sums=('b55721a7f5b8c8421af49d82a455580ca452d4559d8f56ba7e613707bae1c5227c4eda47f3eb76c657dd91b7925978e3c4277fc38d525e26fb5efd1854d19bff'
             '13a57306706d2d60693151919ae15bb3621e6e7ed3b5e9c6d3b1c8d44e52b898c1ad394b39946d730ff82a19f5e3b7c2a374f9dcc3b6c6f990581e504f1cb9cb'
-            'cf060f65baead3fb9df4b08f2630e749c768da280be77946c0b514cbc8864851dfd0651a6f63cc186cc28421afeca14a6c7a32585ac3dc121299cd5a7f8eb32d')
+            'b68c111f31d97f4ce4932ab84ac4ad150db0e1046ccfca8a2091e452e43bb23ded0184d2dbf36dd422d4266890d9f89c7ad4f09a1b8b4ca020eb1540949e3ecf')
--- build_config.rb
+++ build_config.rb
@@ -4,6 +4,7 @@ MRuby::Build.new do |conf|
   conf.gem __dir__
   conf.gem github: 'eagletmt/mitamae-pacman', checksum_hash: 'v0.2.0'
   conf.gem github: 'eagletmt/mitamae-secrets', checksum_hash: 'v0.1.1'
+  conf.gem github: 'ksss/mruby-at_exit', checksum_hash: '09af1ecdce7b39582023c7614f8305386ee4d789' # Latest
   conf.gem github: 'IceDragon200/mruby-catch-throw', checksum_hash: 'v1.1.0' # Latest
   conf.gem github: 'iij/mruby-dir', checksum_hash: '14bc5c3e51eac16ebc9075b7b62132a0cf5ae724' # Latest
   conf.gem github: 'gromnitsky/mruby-dir-glob', checksum_hash: '334c040a2e2c4c2689f8c3440168011f64d57ada' # Latest
@@ -11,23 +12,23 @@ MRuby::Build.new do |conf|
   conf.gem github: 'k0kubun/mruby-erb', checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074' # Specified by mitamae
   conf.gem github: 'iij/mruby-errno', checksum_hash: 'b4415207ff6ea62360619c89a1cff83259dc4db0' # Latest
   conf.gem github: 'eagletmt/mruby-etc', checksum_hash: 'v0.1.0' # Specified by mitamae
-  conf.gem github: 'ksss/mruby-file-stat', checksum_hash: '2d3ea9b5d59d2b41133228a71c110b75cb30a31e' # Latest
+  conf.gem github: 'ksss/mruby-file-stat', checksum_hash: '2d3ea9b5d59d2b41133228a71c110b75cb30a31e' # Specified by mitamae
   conf.gem github: 'k0kubun/mruby-hashie', checksum_hash: 'bfdbb8aebc8786bc9e88469dae87a8dfe8ec4300' # Specified by mitamae
   conf.gem github: 'mattn/mruby-http', checksum_hash: '22d4fccd820705f6bef8e8f4989bdb4cf435f576' # Latest
-  conf.gem github: 'matsumotory/mruby-httprequest', checksum_hash: 'ab6bacc8a7e92fe541d01d9d9b1515a888b0bce3' # Latest
-  conf.gem github: 'iij/mruby-iijson', checksum_hash: '2472d063fce158249862b43bf51a84aec63336c7' # Latest
-  conf.gem github: 'iij/mruby-io', checksum_hash: '86d58a2d3727f556e6e6ce10d170b6170911a263' # Specified by mitamae
-  conf.gem github: 'iij/mruby-mtest', checksum_hash: '8e942541a94944ab2bdfad7c960756f6ecbe14ee' # Latest
-  conf.gem github: 'mattn/mruby-onig-regexp', checksum_hash: 'c269089a5db1046481a1bbbe7adb713350a73f6c' # Latest
+  conf.gem github: 'matsumotory/mruby-httprequest', checksum_hash: '628d43dca8b9231b75cb966a471f171e735bb907' # Latest
+  conf.gem github: 'iij/mruby-iijson', checksum_hash: '589c2328f29a9334d47e3c1accc1677277d323dd' # Latest
+  conf.gem github: 'iij/mruby-io', checksum_hash: '6836f424c5ff95d0114a426010b22254804bc9a3' # Specified by mitamae
+  conf.gem github: 'mattn/mruby-onig-regexp', checksum_hash: '973b2ac4be1532155612627050a86c1237d9a29a' # Latest
   conf.gem github: 'k0kubun/mruby-open3', checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78' # Specified by mitamae
   conf.gem github: 'fastly/mruby-optparse', checksum_hash: 'e6397a090d1efe04d5dab57c63897f0d79bbad89' # Latest
-  conf.gem github: 'iij/mruby-pack', checksum_hash: '7e014efe45ac7c8f5a0418b6f180634d33e0a9dd' # Latest
-  conf.gem github: 'luisbebop/mruby-polarssl', checksum_hash: '8b0865024255bf1cf325db3b8c93b46984a7e4f5' # Latest
-  conf.gem github: 'iij/mruby-process', checksum_hash: '7308e8f4737cb33e3a3d92e60169983a6ebddd08' # Latest
+  conf.gem github: 'iij/mruby-pack', checksum_hash: '383a9c79e191d524a9a2b4107cc5043ecbf6190b' # Latest
+  conf.gem github: 'luisbebop/mruby-polarssl', checksum_hash: '62c425beba695215b4191d75dcbcc16ed33b6981' # Latest
+  conf.gem github: 'iij/mruby-process', checksum_hash: 'bd288a930d4f61fbda1865deb8997245dc5fd38c' # Latest
   conf.gem github: 'k0kubun/mruby-shellwords', checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8' # Specified by mitamae
-  conf.gem github: 'matsumotory/mruby-simplehttp', checksum_hash: 'f1c3ae60f728944af58f29d5d2eb48ab40f11738' # Latest
-  conf.gem github: 'iij/mruby-socket', checksum_hash: '3dad125a1cd93e70a1762e9c6a1d5e01554ad71c' # Latest
-  conf.gem github: 'k0kubun/mruby-specinfra', checksum_hash: '22a54436fe53afd310ad4473d28edd03dd960a24' # Specified by mitamae
-  conf.gem github: 'iij/mruby-tempfile', checksum_hash: 'c21fc7303336ccbf0cbed70afe75e43bbf00a83b' # Latest
-  conf.gem github: 'hone/mruby-yaml', checksum_hash: 'dd34566926a6f90667983052efc40143427fea49' # Latest
+  conf.gem github: 'matsumotory/mruby-simplehttp', checksum_hash: 'fc2ecd21eedcafd9bb7cad6a014d4f33c87a6196' # Latest
+  conf.gem github: 'iij/mruby-socket', checksum_hash: 'fae4a589bbfca1915f7e11b18160890f1535a364' # Latest
+  conf.gem github: 'k0kubun/mruby-specinfra', checksum_hash: 'd802a755cfa94675c6df80547ca553abb323ec7f' # Specified by mitamae
+  conf.gem github: 'iij/mruby-tempfile', checksum_hash: 'e2f56882fab5249f5203dc8259ac2504a3221ee4' # Latest
+  conf.gem github: 'zzak/mruby-uri', checksum_hash: 'e465bf687ba832cea6c9dd30db78ff6690148105' # Latest
+  conf.gem github: 'hone/mruby-yaml', checksum_hash: '1cebec2eb2a7a6b8cfecc3f25967ed7eef5adab0' # Latest
 end
