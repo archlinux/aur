e3544e0859e2f901ab4326603246102a75da829b
zoe <chp321@gmail.com>
2018-06-19 11:10:16 +0200

    Add interval pages option

--- PKGBUILD
+++ PKGBUILD
@@ -1,15 +1,15 @@
 # Maintainer: zoe <chp321 AT gmail DOT com>
 
 pkgname=pdf-remove-blank-pages
-pkgver=0.1
+pkgver=0.2
 pkgrel=1
 pkgdesc="Automatically remove blank pages from pdf. For text-pdf only, not for images-pdf (removes pages with no text in it)"
 arch=(any)
 url="https://repolinux.wordpress.com/2012/04/05/automatically-remove-blank-pages-from-pdf/"
 license=("GPLv3+")
-depends=('python' 'bash' 'pdftk' 'poppler')
+depends=('python' 'bash' 'pdftk' 'poppler' 'gawk')
 source=("non-blank-page-ranges.py" "pdf-remove-blank-pages.sh")
-md5sums=('1a9c76689a828c18b1263ef11c3ee990' 'b461a834fc260404862e202b154c474e')
+md5sums=('1a9c76689a828c18b1263ef11c3ee990' 'e368efb443e71690e7319ed05bef21d9')
 
 package() {
    
@@ -17,5 +17,5 @@ package() {
   install -D -m 0755 $srcdir/non-blank-page-ranges.py \
 		             $pkgdir/usr/bin/non-blank-page-ranges.py
   install -D -m 0755 $srcdir/${pkgname}.sh \
-		             $pkgdir/usr/bin/${pkgname}.sh
+		             $pkgdir/usr/bin/${pkgname}
 }
--- pdf-remove-blank-pages.sh
+++ pdf-remove-blank-pages.sh
@@ -1,28 +1,60 @@
 #! /bin/bash
 
-for filename in "$@"; do
+echo $@
+BEGIN=1
+END=0
+filename=""
+
+while getopts b:f:e:h option
+do
+   case "${option}" in
+      b) BEGIN=${OPTARG};;
+      f) filename=${OPTARG};;
+      e) END=${OPTARG};;
+      h) echo "Usage: pdf-remove-blank-pages [-b <FIRST_PAGE>] [-e <LAST_PAGE>] -f <PDF-file>"
+                 echo "       [FIRST_PAGE LAST_PAGE] = pages-interval where to remove blank pages ; default is 1-End"
+                 echo "       pdf-remove-blank-pages -h : displays that help and exit"
+                 exit;;
+      *) echo "invalid parameter"
+         echo " "
+         echo "Usage: pdf-remove-blank-pages [-b <FIRST_PAGE>] [-e <LAST_PAGE>] -f <PDF-file>"
+         echo "       [FIRST_PAGE LAST_PAGE] = pages-interval where to remove blank pages ; default is 1-End"
+         echo "       pdf-remove-blank-pages -h : displays that help and exit"
+         exit;;
+   esac
+done
+
+if [ $filename == "" ]; then 
+  echo "-f <PDF-file> is missing ; aborted"
+  exit 99
+fi  
+
+if [ $END == 0 ]; then 
+  END=$(pdfinfo $filename | grep Pages | awk '{print $2}')
+fi  
+
+echo "BEGIN="$BEGIN
+echo "END="$END
+echo "filename="$filename
+
   # get non-blank ranges
-  ranges="$(pdftotext "$filename" - | \
+  ranges="$(pdftotext -f $BEGIN -l $END "$filename" - | \
     "/usr/bin/non-blank-page-ranges.py")"
 
   if [ -z "$ranges" ]; then
     echo "no non-blank pages found in $filename" >&2
-    continue
   fi
 
   # rename pdf
   if [ -e "${filename}.old.pdf" ]; then
     echo "file exists: ${filename}.old.pdf" >&2
-    continue
   fi
 
   mv -n "$filename" "${filename}.old.pdf"
 
   if [ -e "$filename" -o ! -e "${filename}.old.pdf" ]; then
     echo "couldn't rename file $filename" >&2
-    continue
   fi
 
   # create new pdf with non-blank pages only
   pdftk "${filename}.old.pdf" cat $ranges output "$filename"
-done
