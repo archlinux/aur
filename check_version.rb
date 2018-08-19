7fd63c2e01b25be5586e11fd14d2e85259b72b5a
Jeremy Audet <jerebear@protonmail.com>
2018-08-19 11:06:53 -0400

    Drop unnecessary check_version script
    Experience has shown that it's not useful.

--- check_version.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# Print the currently installed Cri version to stdout.
-#
-# This script exists soley for the convenience of the package maintainer.
-require 'cri/version'
-
-puts("Cri::VERSION is #{Cri::VERSION}")
