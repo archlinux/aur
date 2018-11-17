fbcebb3adac8c7c3674ce8f4f915ab223a055005
ncoif <ncoif@gmx.com>
2018-11-17 15:15:39 +0000

    use updpkgsums for auto-updater script

--- auto-update.sh
+++ auto-update.sh
@@ -11,16 +11,12 @@ VERSION=$1
 
 echo "Auto-updating version $VERSION..."
 
-echo "Downloading new package..."
-wget https://github.com/vim/vim/archive/v$VERSION.tar.gz
-
-SHA1SUM=`sha1sum v$VERSION.tar.gz | awk '{ print $1 }'`
-echo "sha1sum is $SHA1SUM"
-
-echo "Generate new PKGBUILD"
+echo "Generating a new PKGBUILD"
 cp PKGBUILD.template PKGBUILD
 sed -i "s/__VERSION__/$VERSION/g" PKGBUILD
-sed -i "s/__SHA1SUM__/$SHA1SUM/g" PKGBUILD
+
+echo "Updating the checksum"
+updpkgsums
 
 echo "Updating .SRCINFO"
 makepkg --printsrcinfo > .SRCINFO
@@ -30,4 +26,4 @@ git add .SRCINFO PKGBUILD
 git commit -m "vim-runtime version $VERSION"
 
 echo "clean-up"
-rm -v v$VERSION.tar.gz
+rm -v vim-$VERSION.tar.gz
