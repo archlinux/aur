e782100d98e31d4a5e1a0587f08ef02da80e440f
Zhanibek Adilbekov <zhanibek.adilbekov@protonmail.com>
2018-11-19 12:01:08 +0600

    Fixed: cert auto gen script

--- pixelserv-ca-init.sh
+++ pixelserv-ca-init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-openssl genrsa -out /var/cache/pixelserv/ca.key 1024
-openssl req -key /var/cache/pixelserv/ca.key -new -x509 -days 3650 -sha256 \
+
+# generate cert
+sudo -u nobody openssl genrsa -out /var/cache/pixelserv/ca.key 1024
+sudo -u nobody openssl req -key /var/cache/pixelserv/ca.key -new -x509 -days 3650 -sha256 \
     -extensions v3_ca -out /var/cache/pixelserv/ca.crt -subj "/CN=Pixelserv CA"
+
+# trust cert
+sudo cp /var/cache/pixelserv/ca.crt /usr/share/ca-certificates/trust-source/anchors/ca.pixelserv.crt
+sudo trust extract-compat
