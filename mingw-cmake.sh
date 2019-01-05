fe8d4e5fa3958d2fe3055c62fc2ab5288a80ab1e
Michel Zou <xantares09@hotmail.com>
2019-01-05 09:50:29 +0100

    flags: remove -g, add -fno-plt, set ldflags

--- mingw-cmake.sh
+++ mingw-cmake.sh
@@ -3,7 +3,14 @@ mingw_prefix=/usr/@TRIPLE@
 
 export PKG_CONFIG_LIBDIR="${mingw_prefix}/lib/pkgconfig"
 
-mingw_c_flags="-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions --param=ssp-buffer-size=4"
+default_mingw_pp_flags="-D_FORTIFY_SOURCE=2"
+default_mingw_compiler_flags="$default_mingw_pp_flags -O2 -pipe -fno-plt -fexceptions --param=ssp-buffer-size=4"
+default_mingw_linker_flags="-Wl,-O1,--sort-common,--as-needed"
+
+export CPPFLAGS="${MINGW_CPPFLAGS:-$default_mingw_pp_flags $CPPFLAGS}"
+export CFLAGS="${MINGW_CFLAGS:-$default_mingw_compiler_flags $CFLAGS}"
+export CXXFLAGS="${MINGW_CXXFLAGS:-$default_mingw_compiler_flags $CXXFLAGS}"
+export LDFLAGS="${MINGW_LDFLAGS:-$default_mingw_linker_flags $LDFLAGS}"
 
 PATH=${mingw_prefix}/bin:$PATH cmake \
     -DCMAKE_INSTALL_PREFIX:PATH=${mingw_prefix} \
@@ -18,6 +25,8 @@ PATH=${mingw_prefix}/bin:$PATH cmake \
     -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
     -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS_RELEASE="$mingw_c_flags $CFLAGS" \
-    -DCMAKE_CXX_FLAGS_RELEASE="$mingw_c_flags $CXXFLAGS" \
+    -DCMAKE_C_FLAGS_RELEASE="$CFLAGS" \
+    -DCMAKE_CXX_FLAGS_RELEASE="$CXXFLAGS" \
+    -DCMAKE_SHARED_LINKER_FLAGS_RELEASE="$LDFLAGS" \
+    -DCMAKE_EXE_LINKER_FLAGS_RELEASE="$LDFLAGS" \
     "$@"
