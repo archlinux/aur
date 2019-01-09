18e9322df6bcead9cbe0abf806bae42156b20c57
Erik Dubois <erik.dubois@gmail.com>
2019-01-09 07:55:14 +0100

    start ignoring git.sh

--- git.sh
+++ git.sh
@@ -1,16 +1,9 @@
-#!/bin/bash 
-############################################################################
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-############################################################################
-
-
+#!/bin/bash
 mksrcinfo
 git add --all .
-
-# Committing to the local repository with a message containing the time details
-curtime=$(date)
-git commit -m "commit from @ $curtime"
-
-# Push the local snapshot to a remote destination
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+read input
+git commit -m "$input"
 git push -u origin master
