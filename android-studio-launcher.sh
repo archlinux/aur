4d2d003c852c8594e2747783499c5915fab960b8
Aleksandr Krotov <san.crot@yandex.ru>
2019-01-15 14:12:11 +0300

    Specified directories with variables

--- android-studio-launcher.sh
+++ android-studio-launcher.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+SAVE_DIR=~/.AndroidStudio_sdk_local
+MOUNT_DIR=~/.AndroidStudio_sdk
+
 if whereis android-studio>/dev/null 2>&1; then
 	if whereis unionfs-fuse>/dev/null 2>&1; then 
-		mkdir -p ~/.AndroidStudio_sdk_local
-		mkdir -p ~/.AndroidStudio_sdk
+		mkdir -p $SAVE_DIR
+		mkdir -p $MOUNT_DIR
 
-		unionfs -o cow -o umask=000 ~/.AndroidStudio_sdk_local=RW:/opt/android-sdk=RO ~/.AndroidStudio_sdk
+		unionfs -o cow -o umask=000 $SAVE_DIR=RW:/opt/android-sdk=RO $MOUNT_DIR
 	else
 		echo "==> Please be sure to have union-fuse installed in your path"
 	fi
