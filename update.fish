46122f89271fe562f09540702d474e6043781e98
Roey Darwish Dror <roey.ghost@gmail.com>
2019-01-22 13:55:36 +0200

    Don't use a special variable in the update script

--- update.fish
+++ update.fish
@@ -1,9 +1,9 @@
 #!/usr/bin/env fish
 
-set version $argv[1]
-set hash (string sub -l 64 (curl -L https://github.com/r-darwish/topgrade/archive/v$version.tar.gz ^/dev/null | sha256sum))
+set -l package_version $argv[1]
+set -l hash (string sub -l 64 (curl -L https://github.com/r-darwish/topgrade/archive/v$package_version.tar.gz ^/dev/null | sha256sum))
 
 sed -E "s/sha256sums=.*\$/sha256sums=\('$hash'\)/g" -i PKGBUILD
-sed -E "s/pkgver=.*\$/pkgver=$version/g" -i PKGBUILD
+sed -E "s/pkgver=.*\$/pkgver=$package_version/g" -i PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
 makepkg
