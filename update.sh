22926402e1e6b937b07c33d4cb6b0e2a88485947
McModder <mcmodder@mcmodder.ru>
2019-02-01 21:17:19 +0300

    Updated PKGBUILD update script

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ echo "PKGBUILD: ${_current}, REMOTE: ${_remote}"
 if [ "${_remote}" != "${_current}" ]; then
     sed -i "s/_version=.*/_version=${_remote}/" PKGBUILD
     echo "Updated"
-else
-    echo "Nothing to do"
+    exit 0
 fi
+echo "Nothing to do"
+exit 1
