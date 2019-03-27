e5480f2de1bbab1682b5c8ab0ccbcd50b12ea413
BloodyAltair <me@bloody.pw>
2019-03-27 21:37:45 +0300

    + Fixed system version determination

--- pkg_upgrader.sh
+++ pkg_upgrader.sh
@@ -8,7 +8,7 @@
 # DON'T FORGET TO REPLACE `yay` to your favorite AUR helper
 # #########################################################
 
-RELEASE_TIMESTAMP=$(curl -sL https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central-l10n/ | grep -A2 "firefox-$(cat PKGBUILD  | grep -oP "^_version=\K(.*)"  | awk '{print $1}').ru.linux-x86_64.tar.bz2\"" | tail -n1 | cut -f2 -d'>' | cut -f1 -d'<' | date +"%s" -f -)
+RELEASE_TIMESTAMP=$(curl -sL https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-central-l10n/ | grep -A2 "firefox-$(cat PKGBUILD  | grep -oP "^_version=\K(.*)"  | awk '{print $1}').ru.linux-$(uname -m).tar.bz2\"" | tail -n1 | cut -f2 -d'>' | cut -f1 -d'<' | date +"%s" -f -)
 LOCAL_TIMESTAMP=$(cat ~/.firefox-nightly-ru.release.timestamp || echo 0)
 
 if (("$RELEASE_TIMESTAMP" > "$LOCAL_TIMESTAMP")); then
