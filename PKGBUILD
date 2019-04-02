b55b8fae8a2ab9c9775a8c6f3e05a30af368e401
oda <oda@headjapan.com>
2019-04-02 12:40:25 +0900

    initial import

---
+++ PKGBUILD
@@ -0,0 +1,30 @@
+# maintainer: odknt <oda.kenta.hj at gmail.com>
+pkgname=bspwmbar
+pkgver=0.0.1
+pkgrel=1
+arch=(x86_64)
+depends=(bspwm alsa-lib libxft fontconfig)
+source=("https://github.com/odknt/bspwmbar/archive/v${pkgver}.tar.gz"
+        'config.h')
+sha256sums=('06df858cbade8aaf17a742dd0f7ddf57d25bcf94eded4c74525c3112aecd2a83'
+            'SKIP')
+
+prepare() {
+  cd "${srcdir}/${pkgname}-${pkgver}"
+
+  cp ../config.h ./config.h
+}
+
+build() {
+  cd "${srcdir}/${pkgname}-${pkgver}"
+
+  make PREFIX=/usr
+}
+
+package() {
+  cd "${srcdir}/${pkgname}-${pkgver}"
+
+  make install PREFIX=/usr DESTDIR="${pkgdir}"
+
+  install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
+}
---
+++ config.h
@@ -0,0 +1,53 @@
+/* See LICENSE file for copyright and license details. */
+
+#ifndef BSPWMBAR_CONFIG_H_
+#define BSPWMBAR_CONFIG_H_
+
+#define THERMAL_PATH "/sys/class/thermal/thermal_zone0/temp"
+/* k10temp */
+/* #define THERMAL_PATH "/sys/class/hwmon/hwmon1/temp1_input" */
+
+#define NAME_MAXSZ  32
+#define TITLE_MAXSZ 50
+#define WS_MAXSZ    5
+#define BAR_HEIGHT  24
+
+#define BGCOLOR    0
+#define ALTFGCOLOR 1
+#define ALTBGCOLOR 8
+#define FGCOLOR    2
+#define LOGOCOLOR  3
+
+static const char *colors[] = {
+	"#222222", /* black */
+	"#7f7f7f", /* gray */
+	"#e5e5e5", /* white */
+	"#1793d1", /* logo color */
+
+	"#449f3d", /* success color */
+	"#2f8419", /* normal color */
+	"#f5a70a", /* warning color */
+	"#ed5456", /* critical color */
+
+	"#555555", /* dark gray */
+};
+
+static const char *font_names[] = {
+	"sans-serif:size=10",
+	"emoji:size=10",
+};
+
+/* for modules on the right (float: right;) */
+static Module modules[] = {
+	/* function    argument        event handler */
+	{ datetime,    "%H:%M",        NULL },
+	{ thermal,     THERMAL_PATH,   NULL },
+	{ filesystem,  "/",            NULL },
+	{ volume,      NULL,           volume_ev },
+	/*
+	{ memory,      NULL,           NULL },
+	{ loadavg,     NULL,           NULL },
+	*/
+};
+
+#endif
