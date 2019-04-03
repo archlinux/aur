0e3f720ef0c376ecfd88e09fb405667eb13cc36e
Chris Severance <aur.severach@spamgourmet.com>
2019-04-03 17:53:59 -0400

    autu: Update to 1.4-2 Improve blacklist for other TI devices

--- moxa-mxu11x0-install.sh
+++ moxa-mxu11x0-install.sh
@@ -2,18 +2,27 @@ _pkgname='moxa-mxu11x0'
 _modulename='mxu11x0'
 _builtinmod='ti_usb_3410_5052'
 
-post_upgrade() {
-  set -u
+_ui() {
   if [ ! -d "/usr/src/${_pkgname}"-*/ ]; then
     # depmod -a # now done by pacman hook
     modprobe -r "${_builtinmod}"
     modprobe "${_modulename}"
+  elif [ "$1" = 'i' ]; then
+    echo "You can switch to the new module immediately with"
+    echo "  modprobe -r ${_builtinmod}"
+    echo "  modprobe ${_builtinmod}"
+    # echo "  modprobe ${_modulename}"
   fi
+}
+
+post_upgrade() {
+  set -u
+  _ui 'u'
   set +u
 }
 
 post_install() {
-  post_upgrade
+  _ui 'i'
 }
 
 pre_remove() {
