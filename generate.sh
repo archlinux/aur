8e86ab536d30ec6bf2883ccb65a93110b4176cb1
Lucas Lugao <lugaosmurf@gmail.com>
2019-04-18 17:21:03 +0200

    Automated update to version 1.0+devel.r4321.g083f7eab9b at Thu Apr 18 15:21:03 UTC 2019

--- generate.sh
+++ generate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 AUR_INKSCAPE_GIT_UPSTREAM="https://aur.archlinux.org/inkscape-git.git"
 LOCAL_TEMP_DIR="$(mktemp -d)"
 git clone "${AUR_INKSCAPE_GIT_UPSTREAM}" "${LOCAL_TEMP_DIR}"
@@ -21,3 +23,12 @@ cat "${LOCAL_TEMP_DIR}/PKGBUILD" |
         print "  git clone --depth 1 " inkscape_upstream " \"$_gitname\""
     }' > PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
+
+if [ ! -z "$(git status --porcelain)" ]; then
+    git add . 
+    NEW_VERSION="$(cat PKGBUILD | awk -F= '/pkgver/{printf("%s",$2)}')"
+    git commit -m "Automated update to version ${NEW_VERSION} at $(date -u)"
+    git push
+fi
+
+rm -r -f "${LOCAL_TEMP_DIR}"
