218e4f6a54983b8ecee9ab4a4e62cb588bb44081
Maarten de Vries <maarten@de-vri.es>
2019-04-27 20:30:06 +0200

    Fix firmware installation.

---
+++ ueye-config.cmake
@@ -0,0 +1,7 @@
+set(ueye_MAJOR_VERSION 4)
+set(ueye_MINOR_VERSION 90)
+set(ueye_PATCH_VERSION 06)
+set(ueye_VERSION ${ueye_MAJOR_VERSION}.${ueye_MINOR_VERSION}.${ueye_PATCH_VERSION})
+
+add_library(ueye SHARED IMPORTED)
+set_target_properties(ueye PROPERTIES IMPORTED_LOCATION /usr/lib/libueye_api.so)
