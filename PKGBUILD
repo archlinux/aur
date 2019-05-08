62339ad0739e729b5aa4beb6a46c584903b931a2
oda <oda@headjapan.com>
2019-05-08 12:31:20 +0900

    update to v0.2.0

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # maintainer: odknt <oda.kenta.hj at gmail.com>
 pkgname=bspwmbar
-pkgver=0.1.3
+pkgver=0.2.0
 pkgrel=1
 pkgdesc='A lightweight status bar for bspwm'
 arch=(x86_64)
@@ -8,8 +8,8 @@ license=('ISC')
 url='https://github.com/odknt/bspwmbar'
 depends=(bspwm alsa-lib libxft fontconfig)
 source=("https://github.com/odknt/bspwmbar/archive/v${pkgver}.tar.gz"
-        'config.h')
-sha256sums=('77911b0586fe002e2758032308f770fbc9e687b06fd8c2a25512a0b734b8f48e'
+        'config.def.h')
+sha256sums=('0f81370e64ae2946dd585ae2e09150b174834a6238d0eb5365ec3ca40981cca2'
             'SKIP')
 
 prepare() {
--- config.def.h
+++ config.def.h
@@ -3,20 +3,27 @@
 #ifndef BSPWMBAR_CONFIG_H_
 #define BSPWMBAR_CONFIG_H_
 
-#define THERMAL_PATH "/sys/class/hwmon/hwmon1/temp1_input"
+#include "bspwmbar.h"
 
+/* intel */
+#define THERMAL_PATH "/sys/class/thermal/thermal_zone0/temp"
+/* k10temp */
+/* #define THERMAL_PATH "/sys/class/hwmon/hwmon1/temp1_input" */
+
+#define FONT_MAXSZ  3
 #define NAME_MAXSZ  32
 #define TITLE_MAXSZ 50
 #define WS_MAXSZ    5
 #define BAR_HEIGHT  24
-
 #define BGCOLOR    0
 #define ALTFGCOLOR 1
 #define ALTBGCOLOR 8
 #define FGCOLOR    2
 #define LOGOCOLOR  3
 
-static const char *colors[] = {
+const char *workspace_chars[] = { "", "" };
+
+const char *colors[] = {
 	"#222222", /* black */
 	"#7f7f7f", /* gray */
 	"#e5e5e5", /* white */
@@ -30,19 +37,30 @@ static const char *colors[] = {
 	"#555555", /* dark gray */
 };
 
-static const char *fontname = "sans-serif:size=10";
+const char *fontname = "sans-serif:size=10";
+
+/* init, deinit, handler for polling fd */
+const Poller pollers[] = {
+	/* init         deinit           handler */
+	{ alsa_connect, alsa_disconnect, alsa_update },
+};
 
 /* for modules on the right (float: right;) */
-static Module modules[] = {
+const Module modules[] = {
 	/* function    argument        event handler */
+	/* float: left; */
+	{ logo,        "",           NULL },
+	{ workspace,   NULL,           NULL },
+	{ windowtitle, "…",           NULL },
+
+	/* float: right; */
+	{ float_right, NULL,           NULL },
 	{ datetime,    "%H:%M",        NULL },
 	{ thermal,     THERMAL_PATH,   NULL },
 	{ filesystem,  "/",            NULL },
 	{ volume,      NULL,           volume_ev },
-	/*
-	{ memory,      NULL,           NULL },
-	{ loadavg,     NULL,           NULL },
-	*/
+	{ memgraph,    NULL,           NULL },
+	{ cpugraph,    NULL,           NULL },
 };
 
 #endif
