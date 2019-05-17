0cbea06ef9f61faecd928847e9f3b447ff7f56a0
zer0def <zer0def@zer0def.0>
2019-05-17 08:09:01 +0200

    Display transfer speeds for .rar download.

--- moddb-download.sh
+++ moddb-download.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -e
-curl -C- -o $1 -sSL https://www.moddb.com$(curl -sSL $2 | tidy -quiet -numeric -asxml -utf8 2>/dev/null | xmlstarlet select -N "x=http://www.w3.org/1999/xhtml" -t -v "/x:html/x:body/x:p/x:a[1]/@href")
+curl -C- -o $1 -SL https://www.moddb.com$(curl -sSL $2 | tidy -quiet -numeric -asxml -utf8 2>/dev/null | xmlstarlet select -N "x=http://www.w3.org/1999/xhtml" -t -v "/x:html/x:body/x:p/x:a[1]/@href")
