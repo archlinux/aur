b6c907337c7d99422eba6b8dcc1e863fc14da6a3
RONTheCookie <me@ronthecookie.me>
2019-05-26 22:10:04 +0300

    add update script

---
+++ update.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+fs.writeFileSync(
+    "PKGBUILD",
+    fs
+        .readFileSync("PKGBUILD")
+        .toString()
+        .replace(
+            /pkgver=.+/g,
+            "pkgver=" + process.argv[process.argv.length - 1]
+        )
+);
+
+require("child_process").execSync("makepkg --printsrcinfo > .SRCINFO");
+require("child_process").execSync("updpkgsums");
