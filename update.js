ad9a7eac3aaa3a89811021b8ebfe648890effc70
RONTheCookie <me@ronthecookie.me>
2019-05-26 20:36:47 +0300

    update script

---
+++ update.js
@@ -0,0 +1,9 @@
+// Yes, I'm that lazy.
+
+const fs = require("fs");
+fs.writeFileSync("PKGBUILD", fs.readFileSync("PKGBUILD").toString().replace(
+    /pkgver=.+/g,
+    "pkgver=" + process.argv[process.argv.length - 1]
+));
+
+require("child_process").execSync("makepkg --printsrcinfo > .SRCINFO");
