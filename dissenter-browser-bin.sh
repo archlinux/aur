03f3924425003698649333c789659a972f4957b7
Michael Wakaba <michaelwakaba4@gmail.com>
2019-06-13 07:53:15 +0300

    Fixed the icon and menu entry issues for GNOME

--- dissenter-browser-bin.sh
+++ dissenter-browser-bin.sh
@@ -2,7 +2,7 @@
 
 if [[ ! (-r /proc/sys/kernel/unprivileged_userns_clone && $(< /proc/sys/kernel/unprivileged_userns_clone) == 1 && -n $(zcat /proc/config.gz | grep CONFIG_USER_NS=y) ) ]]; then
     >&2 echo "User namespaces are not detected as enabled on your system, brave will run with the sandbox disabled"
-    FLAG="--no-sandbox"
+    FLAG="--class=dissenter-browser --no-sandbox"
 fi
 
 exec "/opt/gab/dissenter/dissenter-browser" $@ $FLAG 
