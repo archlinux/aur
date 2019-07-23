d046e2452e24f4c2afc390a127ac2aacc2751308
chris_l <archlinux@christopher-luna.com>
2019-07-23 16:40:19 -0500

    gameProfiles must be a copy, not a symlink

--- cemu.sh
+++ cemu.sh
@@ -7,10 +7,15 @@ if [ ! -d "$HOME"/.cemu ] ; then
   ln -s /usr/share/cemu/Cemu.exe "$HOME"/.cemu/Cemu.exe || exit 1
   ln -s /usr/share/cemu/dbghelp.dll "$HOME"/.cemu/dbghelp.dll || exit 1
   ln -s /usr/share/cemu/keystone.dll "$HOME"/.cemu/keystone.dll || exit 1
-  ln -s /usr/share/cemu/gameProfiles "$HOME"/.cemu/gameProfiles || exit 1
+  cp -r /usr/share/cemu/gameProfiles "$HOME"/.cemu/ || exit 1
   cp -r /usr/share/cemu/hfiomlc01 "$HOME"/.cemu/ || exit 1
   cp -r /usr/share/cemu/mlc01 "$HOME"/.cemu/ || exit 1
   cp -r /usr/share/cemu/shaderCache "$HOME"/.cemu/ || exit 1
+fi
+
+if [ -L "$HOME"/.cemu/gameProfiles ] ; then
+  rm "$HOME"/.cemu/gameProfiles
+  cp -r /usr/share/cemu/gameProfiles "$HOME"/.cemu/ || exit 1
 fi
 
 if [ ! -f "$HOME"/.cemu/wine/drive_c/windows/syswow64/vcruntime140.dll ]; then
