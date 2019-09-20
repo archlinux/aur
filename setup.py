599c2ada1dd189f9440c6706361ba3697e24a6c5
Ollie Ford <dev@ojford.com>
2019-09-20 18:27:54 +0100

    Use black, isort, and mypy

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@
 
 from distutils.core import setup
 
-
 setup(
-    name='iosevka-generate',
-    version='0.1.1',
-    description='Generate custom Iosevka fonts from a configuration file',
-    author='Oliver Ford',
-    author_email='dev@ojford.com',
-    url='https://github.com/OJFord/iosevka-generate',
-    scripts=['iosevka-generate'],
+    name="iosevka-generate",
+    version="0.1.1",
+    description="Generate custom Iosevka fonts from a configuration file",
+    author="Oliver Ford",
+    author_email="dev@ojford.com",
+    url="https://github.com/OJFord/iosevka-generate",
+    scripts=["iosevka-generate"],
 )
