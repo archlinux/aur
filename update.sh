d3f9223c8c25ede8fa07736bb7b022a53cd9701b
Florian Moser <git@famoser.ch>
2019-11-10 13:36:28 +0100

    update package

---
+++ update.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+set -e
+updpkgsums
+makepkg -si
+makepkg --printsrcinfo > .SRCINFO
+set +e
+git add *
+git commit -m "update package"
+git push
+git remote add arch ssh://aur@aur.archlinux.org/symfony-cli.git
+git push arch
