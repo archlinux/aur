165308c3fa97d341897c57822c93cdae84060357
Michel Zou <xantares09@hotmail.com>
2019-11-10 12:50:28 +0100

    FORTIFY_SOURCE=0

--- mingw-configure.sh
+++ mingw-configure.sh
@@ -8,7 +8,7 @@ else
   config_path=".."
 fi
 
-default_mingw_pp_flags="-D_FORTIFY_SOURCE=2"
+default_mingw_pp_flags="-D_FORTIFY_SOURCE=0"
 default_mingw_compiler_flags="-O2 -pipe -fno-plt -fexceptions --param=ssp-buffer-size=4"
 default_mingw_linker_flags="-Wl,-O1,--sort-common,--as-needed"
 
