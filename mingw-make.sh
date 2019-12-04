7990e5472d5b38a443cd6aa29f02c0613b7b764f
Michel Zou <xantares09@hotmail.com>
2019-12-04 22:38:37 +0100

    strip

--- mingw-make.sh
+++ mingw-make.sh
@@ -2,4 +2,4 @@
 
 source mingw-env @TRIPLE@
 
-make AR=@TRIPLE@-ar AS=@TRIPLE@-as CC=@TRIPLE@-gcc CXX=@TRIPLE@-g++ CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LD=@TRIPLE@-ld LDFLAGS="${LDFLAGS}" RANLIB=@TRIPLE@-ranlib F77=@TRIPLE@-gfortran F90=@TRIPLE@-gfortran F95=@TRIPLE@-gfortran "$@"
+make AR=@TRIPLE@-ar AS=@TRIPLE@-as CC=@TRIPLE@-gcc CXX=@TRIPLE@-g++ CPPFLAGS="${CPPFLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS}" LD=@TRIPLE@-ld LDFLAGS="${LDFLAGS}" RANLIB=@TRIPLE@-ranlib F77=@TRIPLE@-gfortran F90=@TRIPLE@-gfortran F95=@TRIPLE@-gfortran STRIP=@TRIPLE@-strip NM=@TRIPLE@-nm "$@"
