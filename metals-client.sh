7c28f0697a4569b652fc46e147d9f8446705d084
David Strawn <isomarcte@gmail.com>
2019-12-04 17:08:56 -0700

    Allow Customization Of JDK Which Runs Metals
    Adds support for customizing the JDK which runs metals and sets an ordered list of defaults.
    
    Changes `depends` to bin JDK >= 8 && <= 11, but `make-depends` is >= 8 && < 11 because version 0.7.6 of metals does not compile with JDK 11 (but it does run on it).

--- metals-client.sh
+++ metals-client.sh
@@ -4,29 +4,55 @@ set -e
 
 declare -r _INSTALL_DIR='/usr/share/java/metals'
 declare _CP="$_INSTALL_DIR/lib:$_INSTALL_DIR/metals"
-declare -r -A _JAVA_8_ARCH_RUNTIME_TO_PACKAGE=([java-8-openjdk]='jre8-openjdk-headless')
-declare -r _JAVA_RUNTIME_PATH_PREFIX='/usr/lib/jvm'
-declare -r -A _JAVA_8_ARCH_PACKAGE_PATH_SUFFIXES=([jre8-openjdk-headless]='/java-8-openjdk/jre/bin')
 
-function ensure_java_8 {
-    for _PACKAGE in "${_JAVA_8_ARCH_RUNTIME_TO_PACKAGE[@]}"
-    do
-        if [ -z "$_PACKAGE" ]
+# This is an ordered array of JDK paths to attempt to use if
+# METALS_JDK_PATH is unset. They are based off of the default
+# installation locations of the various JDKs in Arch Linux.
+#
+# We only explicitly try known working versions, at the time of
+# writing that is >= 8 and <= 11. We prefer newer versions to older
+# ones.
+declare -r -a _JAVA_ARCH_PACKAGE_PATHS=('/usr/lib/jvm/java-11-openjdk/bin'
+                                        '/usr/lib/jvm/java-10-openjdk/bin'
+                                        '/usr/lib/jvm/java-8-openjdk/jre/bin'
+                                       )
+
+function ensure_comaptible_jdk {
+    local -r _LEN="${#_JAVA_ARCH_PACKAGE_PATHS[@]}"
+    local _INDEX=0
+    local _PACKAGE_PATH
+
+    if [ -n "$METALS_JDK_PATH" ]
+    then
+        if [ -d "$METALS_JDK_PATH" ]
         then
-            continue
+            if [ -x "${METALS_JDK_PATH}/java" ]
+            then
+                export PATH="$METALS_JDK_PATH:$PATH"
+                return 0
+            else
+                echo "Directory specified at METALS_JDK_PATH=${METALS_JDK_PATH} does not contain an executable file named \"java\"." 1>&2
+                exit 1
+            fi
         else
-            local _PACKAGE_SUFFIX="${_JAVA_8_ARCH_PACKAGE_PATH_SUFFIXES[${_PACKAGE}]}"
-            local _PACKAGE_PATH="$_JAVA_RUNTIME_PATH_PREFIX$_PACKAGE_SUFFIX"
-            if [ -n "$_PACKAGE_SUFFIX" ] && [ -d "$_PACKAGE_PATH" ]
+            echo "METALS_JDK_PATH value set in environment, but $METALS_JDK_PATH is not a directory." 1>&2
+            exit 2
+        fi
+    else
+        while [ $_INDEX -lt "$_LEN" ]
+        do
+            _PACKAGE_PATH="${_JAVA_ARCH_PACKAGE_PATHS[$_INDEX]}"
+            if [ -d "$_PACKAGE_PATH" ]
             then
                 export PATH="$_PACKAGE_PATH:$PATH"
                 return 0
             else
+                _INDEX=$((_INDEX + 1))
                 continue
             fi
-        fi
-    done
-    echo 'Unable to find a Java 8 environment with certainty. Attempt to use the current environment.' 1>&2
+        done
+        echo 'Unable to find a Java >=8 and <=11 environment to use with certainty. To use Metals you should have a JDK >=8 and <=11 installed. Attempting to use the current environment.' 1>&2
+    fi
 }
 
 function build_cp {
@@ -37,7 +63,7 @@ function build_cp {
 }
 
 function main {
-    ensure_java_8
+    ensure_comaptible_jdk
 
     build_cp
 
