7c0bbfe5b5ea3cadf21617caa296212738a5b225
alerighi <alerighi4@gmail.com>
2020-01-08 19:06:03 +0100

    Fix launch script

--- turingarena.sh
+++ turingarena.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Wrapper script to launch turingarena
 . /etc/default/turingarena
-exec /usr/lib/turingarena/turingarena admin --database-url="$DATABASE" $@
+exec /usr/lib/turingarena/turingarena admin --database-url="$DATABASE" "$@"
