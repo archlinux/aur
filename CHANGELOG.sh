eb55443898df5e2c90bb20f3148a93cda3d4cbe6
Gustavo Costa <gusbemacbe@gmail.com>
2020-01-19 05:13:39 -0300

    Added official Adwaita++’s AUR

---
+++ CHANGELOG.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BRIGHTORANGE="\e[0;38;2;175;104;76m"
+BRIGHTPINK="\e[0;38;2;245;88;89m"
+BROWN="\e[0;38;2;175;104;76m"
+DARKRED="\e[0;38;2;124;22;34m"
+PINK="\e[0;38;2;213;38;57m"
+SHAMROCK="\e[0;38;2;46;194;126m"
+SKYBLUE="\e[0;38;2;37;178;238m"
+YELLOW="\e[0;38;2;254;209;75m"
+
+BOLD="\033[1m"
+
+NC='\033[0m' # No Color
+
+echo -e "${BRIGHTPINK}${BOLD}From the version 5.0, the Adwaita++ versions will be named after a tree or leaf randomly in Latin.${NC}"
+echo -e " "
+
+echo -e "${BRIGHTORANGE}#${NC} ${BRIGHTPINK}5.0${NC} – ${BROWN}${BOLD}Paubrasilia echinata${NC}" 
+echo -e "${PINK}➡${NC} New Adwaita++ logotype, based on GNOME Project's original Adwaita icons theme logotype"
+echo -e "${PINK}➡${NC} ${SKYBLUE}@gusbemacbe${NC} has upgrade the new battery design that GNOME Project developers did not upgrade for a decade!"
+echo -e "${PINK}➡${NC} Increased from 2170 to 3009 apps icons!"
+echo -e "${PINK}➡${NC} ${SKYBLUE}@gusbemacbe${NC} has completed the full support for KDE and made more than 2 thousand beautiful Breeze icons in pure Adwaita++ style!"
+echo -e "${PINK}➡${NC} Added new 23 places icons."
+echo -e "${PINK}➡${NC} A new colour of places icons, inspired by the latest Yaru’s places icons."
+echo -e " "
---
+++ PKGBUILD
@@ -0,0 +1,25 @@
+# Maintainer: Gustavo Costa <gusbemacbe@gmail.com>
+
+pkgname=adwaita-plus-git
+pkgver=5.0
+pkgrel=1
+pkgdesc="A third-party icons theme, based on new GNOME Project's Adwaita"
+arch=('any')
+url="https://github.com/Bonandry/${pkgname/-git/}"
+license=('GPL3')
+changelog=CHANGELOG.sh
+makedepends=('git')
+conflicts=(${pkgname/-git/})
+provides=(${pkgname/-git/})
+source=("git+${url}.git")
+sha256sums=('SKIP')
+
+pkgver() 
+{
+    git -C ${pkgname/-git/} describe --long --tags | sed 's/\([^-]*-g\)/r\1/;s/-/./g;s/^v//'
+}
+
+package() 
+{
+    make -C ${pkgname/-git/} DESTDIR="$pkgdir/" install
+}
