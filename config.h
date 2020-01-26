cbf0123bb5f23cfd5adaf1b907647ce7971d4851
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-01-26 23:17:43 +0300

    dejavu

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:size=10:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font Mono:size=10";
 static int borderpx = 2;
 
 /*
