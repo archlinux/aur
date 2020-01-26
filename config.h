ad5d02a1ac54dcc144f06b96eee3b6c641698309
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-01-26 23:21:10 +0300

    dejavu

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "FiraCode:size=10" };//-*-terminus-medium-*-normal--24-240-72--72-c-120-*-1
-static const char dmenufont[]       = "FiraCode:size=10";
+static const char *fonts[]          = { "DejaVuSansMono Nerd Font Mono:size=10" };//-*-terminus-medium-*-normal--24-240-72--72-c-120-*-1
+static const char dmenufont[]       = "DejaVuSansMono Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
