1fc60e888f0d701c0a54d12bd05ca76004436b17
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2020-01-28 20:28:07 +0300

    font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font Mono:size=10";
+static char *font = "DejaVuSansMono Nerd Font Mono:size=11";
 static int borderpx = 2;
 
 /*
