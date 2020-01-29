e84b340920cfca2a9143c0701a613c2566e25132
neeshy <neeshy@tfwno.gf>
2020-01-28 23:06:44 -0500

    mem-suspend: use POSIX functions instead of C standard functions

--- mem-suspend.c
+++ mem-suspend.c
@@ -1,20 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <errno.h>
 
 static const char *state_path = "/sys/power/state";
 
 int main(void)
 {
-  FILE *fp = fopen(state_path, "w");
-  if (fp)
+  int fd = open(state_path, O_WRONLY | O_TRUNC);
+  if (fd != -1)
   {
-    fputs("mem", fp);
+    write(fd, "mem", 3);
   }
   else
   {
     perror(state_path);
     return errno;
   }
-  return EXIT_SUCCESS;
+  return 0;
 }
