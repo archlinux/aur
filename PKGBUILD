675d04e5ebff3eaa2ceaa0ec03f0652df1df6813
Sumner Evans <sumner.evans98@gmail.com>
2020-02-01 01:19:38 -0700

    Install logo and .desktop file with AUR package

--- PKGBUILD
+++ PKGBUILD
@@ -4,7 +4,7 @@ pkgbase='sublime-music'
 pkgname=('sublime-music')
 _module='sublime-music'
 pkgver='0.8.10'
-pkgrel=1
+pkgrel=2
 pkgdesc='A native Subsonic/Airsonic/*sonic client for Linux. Build using Python and GTK+.'
 url='https://gitlab.com/sumner/sublime-music'
 depends=(
@@ -23,20 +23,63 @@ depends=(
     'python-requests'
     'python-yaml'
 )
-makedepends=('python-setuptools')
+makedepends=(
+    'python-setuptools'
+    'inkscape'
+    'make'
+)
 license=('GPL3')
 arch=('any')
-source=('https://files.pythonhosted.org/packages/source/s/sublime-music/sublime-music-0.8.10.tar.gz')
-sha256sums=('1dbbb5a60419ec4b3adbde6bf3fc20cd4a414f7bbcfa478a952e314615727c9e')
+source=(
+    'https://files.pythonhosted.org/packages/source/s/sublime-music/sublime-music-0.8.10.tar.gz'
+    'https://gitlab.com/sumner/sublime-music/-/archive/master/sublime-music-master.tar.gz'
+)
+md5sums=('0de18ff308d578d9b2b9baf61c86ce72'
+         '4efb20f1a0c95a6c33d17767a9859623')
 
 
 build() {
-    cd "${srcdir}/${_module}-${pkgver}"
+    pushd ${srcdir}
+
+    pushd "${_module}-${pkgver}"
     python setup.py build
+    popd
+
+    pushd sublime-music-master/docs/logo
+    make
+    popd
+
+    popd
 }
 
 package() {
-    depends+=()
-    cd "${srcdir}/${_module}-${pkgver}"
+    pushd ${srcdir}
+
+    pushd "${_module}-${pkgver}"
     python setup.py install --root="${pkgdir}" --optimize=1 --skip-build
+    popd
+
+    pushd sublime-music-master
+
+    ls -lah
+
+    desktop-file-install --dir=${pkgdir}/usr/share/applications sublime-music.desktop
+
+    pushd docs/logo/build
+    install -Dm644 16x16.png ${pkgdir}/usr/share/icons/hicolor/16x16/apps/sublime-music.png
+    install -Dm644 22x22.png ${pkgdir}/usr/share/icons/hicolor/22x22/apps/sublime-music.png
+    install -Dm644 32x32.png ${pkgdir}/usr/share/icons/hicolor/32x32/apps/sublime-music.png
+    install -Dm644 36x36.png ${pkgdir}/usr/share/icons/hicolor/36x36/apps/sublime-music.png
+    install -Dm644 48x48.png ${pkgdir}/usr/share/icons/hicolor/48x48/apps/sublime-music.png
+    install -Dm644 64x64.png ${pkgdir}/usr/share/icons/hicolor/64x64/apps/sublime-music.png
+    install -Dm644 72x72.png ${pkgdir}/usr/share/icons/hicolor/72x72/apps/sublime-music.png
+    install -Dm644 96x96.png ${pkgdir}/usr/share/icons/hicolor/96x96/apps/sublime-music.png
+    install -Dm644 128x128.png ${pkgdir}/usr/share/icons/hicolor/128x128/apps/sublime-music.png
+    install -Dm644 192x192.png ${pkgdir}/usr/share/icons/hicolor/192x192/apps/sublime-music.png
+    install -Dm644 512x512.png ${pkgdir}/usr/share/icons/hicolor/512x512/apps/sublime-music.png
+    install -Dm644 1024x1024.png ${pkgdir}/usr/share/icons/hicolor/1024x1024/apps/sublime-music.png
+    popd
+    popd
+
+    popd
 }
--- update.sh
+++ update.sh
@@ -26,27 +26,28 @@ if [[ $# == 0 ]]; then
     echo 'Usage: ./update.sh VERSION_NUMBER'
     exit 1
 fi
+pkgrel=1
+if [[ $# == 2 ]]; then
+    pkgrel=$2
+fi
 
-src=https://files.pythonhosted.org/packages/source/${PROJ_NAME:0:1}/${PROJ_NAME}/${PROJ_NAME}-$1.tar.gz
+SRCS=(
+    https://files.pythonhosted.org/packages/source/${PROJ_NAME:0:1}/${PROJ_NAME}/${PROJ_NAME}-$1.tar.gz
+    https://gitlab.com/sumner/sublime-music/-/archive/master/sublime-music-master.tar.gz
+)
 
-# Get the sha256sum sum of the package.
-mkdir -p dist
-pushd dist
-wget $src
-sha=$(sha256sum "${PROJ_NAME}-$1.tar.gz" | cut -d ' ' -f 1)
-popd
-
+printf '' > PKGBUILD
 echo "# Maintainer: Sumner Evans <sumner.evans98 at gmail dot com>
 
 pkgbase='${AUR_NAME}'
 pkgname=('${AUR_NAME}')
 _module='${PROJ_NAME}'
 pkgver='$1'
-pkgrel=1
+pkgrel=$pkgrel
 pkgdesc='${DESCRIPTION}'
 url='${URL}'
 depends=(
-    'python'" > PKGBUILD
+    'python'" >> PKGBUILD
 
 # Include the dependencies.
 for d in ${DEPENDS[*]}; do
@@ -54,23 +55,71 @@ for d in ${DEPENDS[*]}; do
 done
 
 echo ")
-makedepends=('python-setuptools')
+makedepends=(
+    'python-setuptools'
+    'inkscape'
+    'make'
+)
 license=('${LICENSE}')
 arch=('any')
-source=('${src}')
-sha256sums=('${sha}')
+source=(" >> PKGBUILD
+
+# Include the dependencies.
+for s in ${SRCS[*]}; do
+    echo "    '$s'" >> PKGBUILD
+done
+
+echo ")
+md5sums=()
 ${ADDITIONAL}
 
 build() {
-    cd \"\${srcdir}/\${_module}-\${pkgver}\"
+    pushd \${srcdir}
+
+    pushd \"\${_module}-\${pkgver}\"
     python setup.py build
+    popd
+
+    pushd sublime-music-master/docs/logo
+    make
+    popd
+
+    popd
 }
 
 package() {
-    depends+=()
-    cd \"\${srcdir}/\${_module}-\${pkgver}\"
+    pushd \${srcdir}
+
+    pushd \"\${_module}-\${pkgver}\"
     python setup.py install --root=\"\${pkgdir}\" --optimize=1 --skip-build
+    popd
+
+    pushd sublime-music-master
+
+    ls -lah
+
+    desktop-file-install --dir=\${pkgdir}/usr/share/applications sublime-music.desktop
+
+    pushd docs/logo/build
+    install -Dm644 16x16.png \${pkgdir}/usr/share/icons/hicolor/16x16/apps/sublime-music.png
+    install -Dm644 22x22.png \${pkgdir}/usr/share/icons/hicolor/22x22/apps/sublime-music.png
+    install -Dm644 32x32.png \${pkgdir}/usr/share/icons/hicolor/32x32/apps/sublime-music.png
+    install -Dm644 36x36.png \${pkgdir}/usr/share/icons/hicolor/36x36/apps/sublime-music.png
+    install -Dm644 48x48.png \${pkgdir}/usr/share/icons/hicolor/48x48/apps/sublime-music.png
+    install -Dm644 64x64.png \${pkgdir}/usr/share/icons/hicolor/64x64/apps/sublime-music.png
+    install -Dm644 72x72.png \${pkgdir}/usr/share/icons/hicolor/72x72/apps/sublime-music.png
+    install -Dm644 96x96.png \${pkgdir}/usr/share/icons/hicolor/96x96/apps/sublime-music.png
+    install -Dm644 128x128.png \${pkgdir}/usr/share/icons/hicolor/128x128/apps/sublime-music.png
+    install -Dm644 192x192.png \${pkgdir}/usr/share/icons/hicolor/192x192/apps/sublime-music.png
+    install -Dm644 512x512.png \${pkgdir}/usr/share/icons/hicolor/512x512/apps/sublime-music.png
+    install -Dm644 1024x1024.png \${pkgdir}/usr/share/icons/hicolor/1024x1024/apps/sublime-music.png
+    popd
+    popd
+
+    popd
 }" >> PKGBUILD
+
+updpkgsums
 
 make
 
