406edb058f0b6c4cfc9f1ac2b9ca037f245bdcd4
German Lashevich <german.lashevich@gmail.com>
2020-02-06 14:59:54 +0100

    Get GKE version automatically

--- generate-pkgbuild.sh
+++ generate-pkgbuild.sh
@@ -6,25 +6,14 @@ usage() {
     echo "Usage: $0 {stable|regular|rapid}" >&2
 }
 
-case $1 in
-    -h)
-        usage
-        exit 0
-        ;;
-    stable)
-        readonly major_version='1.13'
-        ;;
-    regular)
-        readonly major_version='1.14'
-        ;;
-    rapid)
-        readonly major_version='1.16'
-        ;;
-    *)
-        usage
-        exit 1
-        ;;
-esac
+get_major_version() {
+    curl https://cloud.google.com/feeds/kubernetes-engine-$1-channel-release-notes.xml \
+        | sed 's/xmlns=".*"//g' \
+        | xmllint --nocdata --xpath '(/feed/entry)[1]/content/text()' - \
+        | rg -o 'v(\d\.\d+)\.\d+-gke\.\d+' -r '$1'
+}
+
+readonly major_version=$(get_major_version $1)
 
 readonly pkgname=kubectl
 
