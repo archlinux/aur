97384a01ab472949f0e9ef5cae764b488fd0e4b5
Kenneth Endfinger <kaendfinger@gmail.com>
2020-02-08 01:23:33 -0500

    Update Check for Update Script

--- check-for-update.sh
+++ check-for-update.sh
@@ -7,7 +7,7 @@ REPOSITORY="clearlinux/abireport"
 
 PKG=$(cat .SRCINFO | grep "pkgbase" | head -n1 | awk '{print $3}')
 CURRENT=$(cat .SRCINFO | grep "pkgver" | head -n1 | awk '{print $3}')
-LATEST=$(curl --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -v -- "-" | head -n1 | sed 's/^v//g')
+LATEST=$(curl -n --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -v -- "-" | head -n1 | sed 's/^v//g')
 if [ "${LATEST}" != "${CURRENT}" ]
 then
   echo "${PKG} : AUR ${CURRENT} != GitHub ${LATEST}"
