4dfa25e4536b954afb5d4433dc63e59bb905663d
Kenneth Endfinger <kaendfinger@gmail.com>
2020-02-08 01:24:12 -0500

    Update Check for Update Script

--- check-for-update.sh
+++ check-for-update.sh
@@ -7,7 +7,7 @@ REPOSITORY="storaged-project/blivet-gui"
 
 PKG=$(cat .SRCINFO | grep "pkgbase" | head -n1 | awk '{print $3}')
 CURRENT=$(cat .SRCINFO | grep "pkgver" | head -n1 | awk '{print $3}')
-LATEST=$(curl --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -E -- "^[0-9]" | head -n1 | sed 's/\-/./g')
+LATEST=$(curl -n --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -E -- "^[0-9]" | head -n1 | sed 's/\-/./g')
 if [ "${LATEST}" != "${CURRENT}" ]
 then
   echo "${PKG} : AUR ${CURRENT} != GitHub ${LATEST}"
