f59f46a95deaa6b889c24fac9d5babe7c2565a0e
Kenneth Endfinger <kaendfinger@gmail.com>
2020-02-08 01:23:09 -0500

    Update Check for Update Script

--- check-for-update.sh
+++ check-for-update.sh
@@ -7,7 +7,7 @@ REPOSITORY="blackducksoftware/ohcount"
 
 PKG=$(cat .SRCINFO | grep "pkgbase" | head -n1 | awk '{print $3}')
 CURRENT=$(cat .SRCINFO | grep "pkgver" | head -n1 | awk '{print $3}')
-LATEST=$(curl --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -v -- "^v" | head -n1)
+LATEST=$(curl -n --silent -L "https://api.github.com/repos/${REPOSITORY}/tags" | jq -r '.[].name' | grep -v -- "^v" | head -n1)
 if [ "${LATEST}" != "${CURRENT}" ]
 then
   echo "${PKG} : AUR ${CURRENT} != GitHub ${LATEST}"
