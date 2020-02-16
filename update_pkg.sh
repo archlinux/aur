53ddf2d18b8da40b763acec02efeb24216b1c486
Matti Viljanen <matti.viljanen@kapsi.fi>
2020-02-16 16:26:50 +0200

    Make output more verbose

--- update_pkg.sh
+++ update_pkg.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+echo "Current version:"
+grep "pkgver=" PKGBUILD
+grep "pkgver2=" PKGBUILD
+grep "pkgver3=" PKGBUILD
+
+echo "Searching for updates..."
+
 REPO='http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/'
 PACKAGE=$(wget -qO- $REPO | perl -ne '/(?<=href=")(chromium-codecs-ffmpeg-extra_(\d*\.\d*\.\d*\.\d*)-(\dubuntu\d).(\d{2}\.\d{2}(?:\.\d)?)_amd64.deb)(?=").*\d*\.\d*M/ and print "$1#$2#$3#$4\n";' | sort | tail -n 1)
 
@@ -7,6 +15,13 @@ ver1=$(echo $PACKAGE | cut -d# -f2)
 ver2=$(echo $PACKAGE | cut -d# -f3)
 ver3=$(echo $PACKAGE | cut -d# -f4)
 
+echo "Available version:"
+echo "pkgver=$ver1"
+echo "pkgver2=$ver2"
+echo "pkgver3=$ver3"
+
+echo "Downloading updates..."
+
 cur=$(pwd)
 tmp=$(mktemp -d)
 cd $tmp
@@ -14,9 +29,13 @@ wget -q "$REPO$deb"
 md5=$(md5sum $deb | cut -d\  -f1)
 cd $cur
 
+echo "Updating package..."
+
 sed -i "s|^pkgver=.*$|pkgver=${ver1}|" PKGBUILD
 sed -i "s|^pkgver2=.*$|pkgver2=${ver2}|" PKGBUILD
 sed -i "s|^pkgver3=.*$|pkgver3=${ver3}|" PKGBUILD
 sed -i "s|^md5sums.*$|md5sums=(\"${md5}\")|" PKGBUILD
 
 makepkg --printsrcinfo > .SRCINFO
+
+echo "Done."
