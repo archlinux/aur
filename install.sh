9a260439c605d9a5f640ef90a83ea6198f244d9d
Yash Karandikar <nerdstep710@gmail.com>
2020-02-17 18:52:50 -0600

    Create new installation method

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Installing..."
+echo "Please note that this method of installation in unsupported and we will not provide help for this method. Use AUR instead."
+sudo install -Dm777 ./haur /usr/bin/haur
+sudo install -Dm777 ./haur-clear-cache /usr/bin/haur-clear-cache
