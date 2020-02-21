4e6a1ffee98a95a328a8f244a6b9875ad7f8a30b
Joan Figueras <ffigue@gmail.com>
2020-02-21 11:46:38 +0100

    Forgot jisti.sh

--- jitsi.sh
+++ jitsi.sh
@@ -9,6 +9,19 @@ else
   JAVABIN="java"
 fi
 
+if ! ${JAVABIN} -version 2>&1 | grep version | grep -q 1.8; then
+  if command -v zenity > /dev/null; then
+    zenity --error --no-wrap --text="Your java version is $(archlinux-java get) but you need Java JRE/JDK 1.8\nPlease install Java 1.8 or set your PATH to the right binary\nMore info: https://wiki.archlinux.org/index.php/Java"
+  elif command -v kdialog > /dev/null; then
+    kdialog --sorry "Your java version is $(archlinux-java get) but you need Java JRE/JDK 1.8\nPlease install Java 1.8 or set your PATH to the right binary\nMore info: https://wiki.archlinux.org/index.php/Java" --title="Invalid Java version"
+  elif command -v xmessage > /dev/null; then
+    xmessage -center "$(echo -e "Your java version is $(archlinux-java get) but you need Java JRE/JDK 1.8\nPlease install Java 1.8 or set your PATH to the right binary\nMore info: https://wiki.archlinux.org/index.php/Java")"
+  else
+    echo -e "Your java version is $(archlinux-java get) but you need Java JRE/JDK 1.8\nPlease install Java 1.8 or set your PATH to the right binary\nMore info: https://wiki.archlinux.org/index.php/Java"
+  fi
+  exit 1
+fi
+
 SCDIR=/usr/lib/jitsi
 LIBPATH="${SCDIR}/lib"
 CLASSPATH="${LIBPATH}/felix.jar:${SCDIR}/sc-bundles/sc-launcher.jar:${SCDIR}/sc-bundles/util.jar:${SCDIR}/sc-bundles/dnsjava.jar:${LIBPATH}"
