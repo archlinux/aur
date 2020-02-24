12f4974c85f6fa21688d8bc364e316f9cfb27503
Sorah Fukumori <her@sorah.jp>
2020-02-25 02:59:27 +0900

    mitamae-1.11.0-1

--- PKGBUILD
+++ PKGBUILD
@@ -1,10 +1,10 @@
 # Maintainer: Kohei Suzuki <eagletmt@gmail.com>
 # Maintainer: Sorah Fukumori <her@sorah.jp>
 pkgname=mitamae
-pkgver=1.8.0
+pkgver=1.11.0
 pkgrel=1
 pkgdesc="Configuration management tool embedding mruby"
-arch=('i686' 'x86_64')
+arch=('i686' 'x86_64' 'aarch64')
 url="https://github.com/k0kubun/mitamae"
 license=('MIT')
 depends=('openssl' 'pacman')
@@ -37,6 +37,6 @@ package() {
   install -m755 mruby/build/host/bin/mitamae-secrets "$pkgdir/usr/bin/"
   install -m644 LICENSE "$pkgdir/usr/share/licenses/$pkgname/"
 }
-sha512sums=('5fd70e9fa1224fe491ba8186752425d49e70befbb470c34d61415c00f5f07a93cdf38afce55c0395bfdbc80007461e2648aaa6d1f17ad4cf913f996f5407e60b'
+sha512sums=('3b440302521d26ec1cdf0707c1048380eacc5af5e6ec7ea38e73b1b1aea075d3056a01d9d54eef478ffa43caa7b4c7da80189db52456ea3438ee6c2d81e55b92'
             '74403f928c5af52c615ee2490b0aa685b4d62238f3da3778b3e44748e523e1916f110b42f2e73729be24d561b89a9ae498ff32f549ebe241e97a70d83cf39ded'
-            '8dc2f04d2251e2568d4151e5362497687922560ec383795634a8cec7483858ff23943dcedff25391b95586e67247adbd1936168835df973d218450f68bb55802')
+            'a6168fc5931699842db003f7f3ad2b06c90eb465712709fd12b02f17ec24c9900cc70711fdb8086025318db97b32890e0771082b4c88afc8d867144e035d9512')
--- build_config.rb
+++ build_config.rb
@@ -4,26 +4,29 @@ MRuby::Build.new do |conf|
   conf.gem __dir__
   conf.gem github: 'eagletmt/mitamae-pacman', checksum_hash: 'v0.2.0'
   conf.gem github: 'eagletmt/mitamae-secrets', checksum_hash: 'v0.1.1'
+
+  conf.gem github: 'IceDragon200/mruby-catch-throw', checksum_hash: '2b6eaff4232b4a9473b864df53c2917080af5dcf' # Latest
   conf.gem github: 'eagletmt/mruby-etc', checksum_hash: 'v0.1.0' # Specified by mitamae
   conf.gem github: 'fastly/mruby-optparse', checksum_hash: 'e6397a090d1efe04d5dab57c63897f0d79bbad89' # Latest
   conf.gem github: 'gromnitsky/mruby-dir-glob', checksum_hash: '334c040a2e2c4c2689f8c3440168011f64d57ada' # Latest
-  conf.gem github: 'IceDragon200/mruby-catch-throw', checksum_hash: 'b2900a21e71efb4702c2750e6e9312f46d6d2560' # Latest
   conf.gem github: 'iij/mruby-dir', checksum_hash: '89dceefa1250fb1ae868d4cb52498e9e24293cd1' # Latest
   conf.gem github: 'iij/mruby-env', checksum_hash: '056ae324451ef16a50c7887e117f0ea30921b71b' # Latest
   conf.gem github: 'iij/mruby-errno', checksum_hash: 'b4415207ff6ea62360619c89a1cff83259dc4db0' # Latest
-  conf.gem github: 'iij/mruby-io', checksum_hash: '6836f424c5ff95d0114a426010b22254804bc9a3' # Specified by mitamae
-  conf.gem github: 'iij/mruby-process', checksum_hash: 'bd288a930d4f61fbda1865deb8997245dc5fd38c' # Latest
-  conf.gem github: 'iij/mruby-socket', checksum_hash: 'a8b6d6ee4c6ccea81a805cc9204b36c3792123c9' # Specified by mitamae
-  conf.gem github: 'iij/mruby-tempfile', checksum_hash: '41a7e6302ec258e3bf428adb4b84499ded72cef4' # Latest
+  conf.gem github: 'iij/mruby-iijson', checksum_hash: 'c5e730c30090d3cddae258f57ab9508edb3e9fce' # Latest
+  conf.gem github: 'iij/mruby-pack', checksum_hash: '383a9c79e191d524a9a2b4107cc5043ecbf6190b' # Latest
+  conf.gem github: 'iij/mruby-process', checksum_hash: '95da206a5764f4e80146979b8e35bd7a9afd6850' # Latest
   conf.gem github: 'k0kubun/mruby-erb', checksum_hash: '978257e478633542c440c9248e8cdf33c5ad2074' # Specified by mitamae
   conf.gem github: 'k0kubun/mruby-hashie', checksum_hash: 'c69255a94debcd641f2087b569f5625509bde698' # Specified by mitamae
   conf.gem github: 'k0kubun/mruby-open3', checksum_hash: 'b7480b6300a81d0e5fac469a36a383518e3dfc78' # Specified by mitamae
   conf.gem github: 'k0kubun/mruby-shellwords', checksum_hash: '2a284d99b2121615e43d6accdb0e4cde1868a0d8' # Specified by mitamae
-  conf.gem github: 'k0kubun/mruby-specinfra', checksum_hash: 'efb18feeee0cb8d0d49c7a9eadbc0fc074c2be1c' # Specified by mitamae
-  conf.gem github: 'k0kubun/mruby-yaml', checksum_hash: 'b0b57f077f7d910516183f14750a3eeee5a7ecbc' # Latest
+  conf.gem github: 'k0kubun/mruby-specinfra', checksum_hash: '95994e41b5d40f50dbf0e10ff20a38bd124fd30c' # Specified by mitamae
+  conf.gem github: 'k0kubun/mruby-tempfile', checksum_hash: 'e628c8fcb4bca3f3456640a8b56d1ae98c594e24' # Specified by mitamae
   conf.gem github: 'ksss/mruby-at_exit', checksum_hash: '09af1ecdce7b39582023c7614f8305386ee4d789' # Latest
   conf.gem github: 'ksss/mruby-file-stat', checksum_hash: '2d3ea9b5d59d2b41133228a71c110b75cb30a31e' # Specified by mitamae
+  conf.gem github: 'ksss/mruby-singleton', checksum_hash: '73dd4bae1a47d82e49b8f85bf27f49ec4462052e' # Latest
   conf.gem github: 'mattn/mruby-json', checksum_hash: '054699892b0d681110e7348440cddb70a3fd671e' # Latest
-  conf.gem github: 'mattn/mruby-onig-regexp', checksum_hash: '0667cffa26db180ce3f6ed3b0341e9f30f1ae247' # Latest
+  conf.gem github: 'mattn/mruby-onig-regexp', checksum_hash: 'e55923997ba33586b58c11bd3ca2d40c11a8dede' # Latest
+  conf.gem github: 'mrbgems/mruby-yaml', checksum_hash: '0606652a6e99d902cd3101cf2d757a7c0c37a7fd' # Specified by mitamae
+  conf.gem github: 'tatsushid/mruby-schash', checksum_hash: 'c8470d4be2404b4cfbb8011daebc2ca9bb1cdb80' # Latest
   conf.gem github: 'zzak/mruby-uri', checksum_hash: 'b3108ae56a48990eb7b79f44aca4ec76e1e60ad8' # Latest
 end
