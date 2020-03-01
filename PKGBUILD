8b1af2f191b01d6c13a161c4bed111c22e48f61d
David Anderson <dave@natulte.net>
2020-02-29 20:23:22 -0800

    Initial commit.

---
+++ PKGBUILD
@@ -0,0 +1,23 @@
+# Maintainer: David Anderson <dave@natulte.net>
+
+pkgname="tailscale-unstable-bin"
+_version="0.95-388"
+pkgver="0.95_388"
+pkgrel="1"
+pkgdesc="A mesh VPN that makes it easy to connect your devices, wherever they are."
+arch=("x86_64")
+url="https://tailscale.com"
+license=("MIT")
+depends=("glibc")
+backup=("etc/default/tailscaled")
+source=("$pkgname-$pkgver.tgz::https://pkgs.tailscale.com/unstable/tailscale_${_version}_amd64.tgz")
+sha256sums=('4a6cc42123ee56c944e38337513f3d2806beebd4ef3e2c6abf5c6f5415af9d66')
+install="tailscale.install"
+
+package() {
+    cd tailscale_${_version}_amd64
+  mkdir -p "$pkgdir/usr/bin" "$pkgdir/etc/default" "$pkgdir/usr/lib/systemd/system"
+  install -m755 tailscale tailscaled "$pkgdir/usr/bin"
+  install -m644 systemd/tailscaled.defaults "$pkgdir/etc/default/tailscaled"
+  install -m644 systemd/tailscaled.service "$pkgdir/usr/lib/systemd/system"
+}
---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -euo pipefail
+
+tarball=$(curl https://pkgs.tailscale.com/unstable/ 2>/dev/null | grep amd64.tgz | cut -f2 -d'"')
+version=$(echo $tarball | cut -f2 -d'_')
+pkgver=$(echo $version | tr '-' '_')
+sha256=$(curl https://pkgs.tailscale.com/unstable/$tarball 2>/dev/null | sha256sum | cut -f1 -d' ')
+
+cat >PKGBUILD <<EOF
+# Maintainer: David Anderson <dave@natulte.net>
+
+pkgname="tailscale-unstable-bin"
+_version="$version"
+pkgver="$pkgver"
+pkgrel="1"
+pkgdesc="A mesh VPN that makes it easy to connect your devices, wherever they are."
+arch=("x86_64")
+url="https://tailscale.com"
+license=("MIT")
+depends=("glibc")
+backup=("etc/default/tailscaled")
+source=("\$pkgname-\$pkgver.tgz::https://pkgs.tailscale.com/unstable/tailscale_\${_version}_amd64.tgz")
+sha256sums=('$sha256')
+install="tailscale.install"
+
+package() {
+    cd tailscale_\${_version}_amd64
+  mkdir -p "\$pkgdir/usr/bin" "\$pkgdir/etc/default" "\$pkgdir/usr/lib/systemd/system"
+  install -m755 tailscale tailscaled "\$pkgdir/usr/bin"
+  install -m644 systemd/tailscaled.defaults "\$pkgdir/etc/default/tailscaled"
+  install -m644 systemd/tailscaled.service "\$pkgdir/usr/lib/systemd/system"
+}
+EOF
