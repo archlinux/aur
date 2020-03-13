2e524141903154015a50d0491aae31a79767ae79
frealgagu <frealgagu@gmail.com>
2020-03-13 11:03:30 -0500

    Adding support for other languages.

--- whatsapp-nativefier-inject.js
+++ whatsapp-nativefier-inject.js
@@ -1,2 +1,17 @@
-if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) !== -1)
-  navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
+const regexes = [
+  // en
+  /whatsapp works with.*to use whatsapp.*update/i,
+  // es
+  /whatsapp funciona con.*para usar whatsapp.*actualiza/i,
+  // pt
+  /o whatsapp funciona com.*para utilizar o whatsapp.*atualize/i,
+  // ge
+  /whatsapp funktioniert mit.*um whatsapp zu benutzen.*aktualisiere/i
+]
+
+for (const regex of regexes) {
+  if (document.body.innerText.replace(/\n/g, ' ').search(regex) !== -1) {
+    navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
+    return;
+  }
+}
