800d8e355aafe722261ea074d987936b65102546
frealgagu <frealgagu@gmail.com>
2020-03-25 10:20:13 -0500

    Adding unzip make dependency and modifying inject.js to support multiple languages with a single regex.

--- whatsapp-nativefier-inject.js
+++ whatsapp-nativefier-inject.js
@@ -1,17 +1,15 @@
-const regexes = [
-  // en
-  /whatsapp works with.*to use whatsapp.*update/i,
-  // es
-  /whatsapp funciona con.*para usar whatsapp.*actualiza/i,
-  // pt
-  /o whatsapp funciona com.*para utilizar o whatsapp.*atualize/i,
-  // ge
-  /whatsapp funktioniert mit.*um whatsapp zu benutzen.*aktualisiere/i
-]
+// ==UserScript==
+// @include https://web.whatsapp.com/
+// ==/UserScript==
 
-for (const regex of regexes) {
-  if (document.body.innerText.replace(/\n/g, ' ').search(regex) !== -1) {
-    navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
-    return;
-  }
+// Quirk for WhatsApp Web, based on:
+// https://github.com/jiahaog/nativefier/issues/719
+
+"use strict";
+
+if (document.querySelector("a[href='https://support.google.com/chrome/answer/95414']")) {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+        registration.unregister();
+        document.location.reload();
+    });
 }
