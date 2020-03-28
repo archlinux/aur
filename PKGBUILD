d4f2778bc7cb6dd74f87652bc208e48019b20946
Jean-Francois Chevrette <jfchevrette@gmail.com>
2020-03-28 18:43:11 -0400

    update config

--- PKGBUILD
+++ PKGBUILD
@@ -3,7 +3,7 @@
 pkgname=st-jfchevrette
 _pkgname=st
 pkgver=0.8.2
-pkgrel=1
+pkgrel=2
 pkgdesc="A simple terminal implementation for X"
 url="http://st.suckless.org"
 arch=('i686' 'x86_64')
@@ -34,3 +34,5 @@ package() {
   install -m644 -D LICENSE "$pkgdir/usr/share/licenses/${_pkgname}/LICENSE"
   install -m644 -D README "$pkgdir/usr/share/doc/${_pkgname}/README"
 }
+sha256sums=('aeb74e10aa11ed364e1bcc635a81a523119093e63befd2f231f8b0705b15bf35'
+            'd1b74290829e2f0713f3055b82b3c0e456d87e2ab966566370505b726e890ca8')
--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Mononoki:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -16,7 +16,7 @@ static int borderpx = 2;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/bin/zsh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
