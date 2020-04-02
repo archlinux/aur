3872ffe387226345ad1272478200b77e2d9d0a48
Mark Blakeney <mark.blakeney@bullet-systems.net>
2020-04-02 11:01:41 +1000

    Fix install script message

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ post_install() {
 	echo -ne "\e[34;1m"
 	echo "=== INSTALLATION NOTES for $_pkgname ==="
 	echo -ne "\e[39;1m"
-	echo "Users should add \"source /etc/$_pkgname.bashrc\" to their \$HOME/.bashrc."
+	echo "Users should add \"source /usr/share/cdhist/$_pkgname.bashrc\" to their \$HOME/.bashrc."
 	echo -ne "\e[0m"
 }
