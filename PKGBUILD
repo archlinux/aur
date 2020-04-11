3feecae714c2ad4a219a8f37ff7a366c8dbc1961
Sumner Evans <me@sumnerevans.com>
2020-04-11 16:10:19 -0600

    Initial commit for navidrome-git

---
+++ PKGBUILD
@@ -0,0 +1,33 @@
+# Maintainer: Sumner Evans <me [at] sumnerevans [dot] com>
+
+pkgbase='navidrome-git'
+pkgname=(navidrome-git)
+pkgver=v0.14.2.r8.g803a577
+pkgrel=1
+pkgdesc='Music Server and Streamer compatible with Subsonic/Airsonic'
+url='https://www.navidrome.org/'
+license=('GPL3')
+arch=(x86_64 armv6h armv7h aarch64)
+provides=('navidrome')
+conflicts=('navidrome')
+makedepends=('go' 'git' 'npm')
+source=('navidrome-git::git+https://github.com/deluan/navidrome.git')
+sha256sums=('SKIP')
+
+pkgver() {
+    cd "${srcdir}/${pkgname}"
+
+    git describe --long --tags | sed 's/\([^-]*-g\)/r\1/;s/-/./g'
+}
+
+build() {
+    cd "${srcdir}/${pkgname}"
+
+    make setup
+    make buildall
+}
+
+package() {
+  install -Dm755 "${srcdir}/${pkgname}/navidrome" "$pkgdir/usr/bin/navidrome"
+}
+
---
+++ update.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+
+AUR_NAME=navidrome-git
+EXECUTABLE_NAME=navidrome
+DESCRIPTION="Music Server and Streamer compatible with Subsonic/Airsonic"
+URL=https://www.navidrome.org/
+LICENSE='GPL3'
+ADDITIONAL=
+
+pkgrel=1
+if [[ $# == 1 ]]; then
+    pkgrel=$1
+fi
+
+printf '' > PKGBUILD
+echo "# Maintainer: Sumner Evans <me [at] sumnerevans [dot] com>
+
+pkgbase='${AUR_NAME}'
+pkgname=(${AUR_NAME})
+pkgver='v0.14.2.8.g803a577'
+pkgrel=$pkgrel
+pkgdesc='${DESCRIPTION}'
+url='${URL}'
+license=('${LICENSE}')
+arch=(x86_64 armv6h armv7h aarch64)
+provides=('${EXECUTABLE_NAME}')
+conflicts=('${EXECUTABLE_NAME}')
+makedepends=('go' 'git' 'npm')
+source=('${AUR_NAME}::git+https://github.com/deluan/navidrome.git')
+sha256sums=('SKIP')
+
+pkgver() {
+    cd \"\${srcdir}/\${pkgname}\"
+
+    git describe --long --tags | sed 's/\\([^-]*-g\\)/r\\1/;s/-/./g'
+}
+
+build() {
+    cd \"\${srcdir}/\${pkgname}\"
+
+    make setup
+    make buildall
+}
+
+package() {
+  install -Dm755 \"\${srcdir}/\${pkgname}/navidrome\" \"\$pkgdir/usr/bin/${EXECUTABLE_NAME}\"
+}
+" >> PKGBUILD
+
+makepkg --printsrcinfo > .SRCINFO
+
+# Test
+makepkg -f
