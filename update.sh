2c4521ba67527fa1e2df85741605d7ad3ee9fe99
Nicolai Schmid <nicolai@schmid.uno>
2020-05-19 22:17:04 +0200

    chore: add update script for automated updates

---
+++ update.sh
@@ -0,0 +1,18 @@
+VERSION=$(curl -s https://api.github.com/repos/buchen/portfolio/releases/latest | jq -r .tag_name)
+
+sed -i -e 's/pkgver=.*/pkgver='$VERSION'/' PKGBUILD
+
+updpkgsums
+
+# Check whether this changed anything
+if (git diff --exit-code PKGBUILD); then
+	echo "Package ${PKG} has most recent version ${VER}"
+	exit 0
+fi
+
+# Update .SRCINFO
+makepkg --printsrcinfo > .SRCINFO
+
+# Commit changes
+git add PKGBUILD .SRCINFO
+echo git commit -m "feat: update to v${VERSION}"
