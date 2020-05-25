67c9770391f36a463058f6d29230be82b3d14c5a
Jannis Mattheis <jannis.mattheis@omikron.net>
2020-05-25 09:13:46 +0200

    Add build-in-docker script

---
+++ build-in-docker.sh
@@ -0,0 +1,9 @@
+docker run --rm -v "$PWD:/build" archlinux/base bash -c "
+    pacman -Syu --noconfirm base-devel pacman-contrib;
+    useradd -u $UID -m build;
+    echo 'build      ALL = NOPASSWD: ALL' >> /etc/sudoers;
+    su build -c '
+        cd /build && \
+        updpkgsums && \
+        makepkg --printsrcinfo > .SRCINFO && \
+        makepkg --noconfirm -si && /usr/bin/snage --version'"
