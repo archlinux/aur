2af3d9ab4204ba4383902dc0de2d5297639608e6
Maarten de Vries <maarten@de-vri.es>
2020-06-02 12:03:39 +0200

    Fix version numbers in cmake files.

--- ueye-config.cmake
+++ ueye-config.cmake
@@ -1,7 +1,7 @@
 set(ueye_MAJOR_VERSION 4)
-set(ueye_MINOR_VERSION 92)
+set(ueye_MINOR_VERSION 93)
 set(ueye_PATCH_VERSION 0)
 set(ueye_VERSION ${ueye_MAJOR_VERSION}.${ueye_MINOR_VERSION}.${ueye_PATCH_VERSION})
 
 add_library(ueye SHARED IMPORTED)
-set_target_properties(ueye PROPERTIES IMPORTED_LOCATION /usr/lib/libueye_api.so.4.92)
+set_target_properties(ueye PROPERTIES IMPORTED_LOCATION /usr/lib/libueye_api.so.4.93)
