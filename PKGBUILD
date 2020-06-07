50c9f0a334020608fccac0015e03b12898f90774
Sumner Evans <me@sumnerevans.com>
2020-06-07 02:47:40 -0600

    Initial commit: version 1.5.1

---
+++ PKGBUILD
@@ -0,0 +1,34 @@
+# Maintainer: Sumner Evans <sumner.evans98 at gmail dot com>
+
+pkgbase='python-marshmallow-enum'
+pkgname=('python-marshmallow-enum')
+_module='marshmallow-enum'
+pkgver='1.5.1'
+pkgrel=1
+pkgdesc='Enum handling for Marshmallow'
+url='https://github.com/justanr/marshmallow_enum'
+depends=(
+    'python'
+)
+optdepends=()
+makedepends=(
+    'python-setuptools'
+)
+license=('MIT')
+arch=('any')
+source=(
+    'https://files.pythonhosted.org/packages/source/m/marshmallow-enum/marshmallow-enum-1.5.1.tar.gz'
+)
+md5sums=('7f4624f5b553c50895c33b5195108c10')
+
+
+build() {
+    cd "${srcdir}/${_module}-${pkgver}"
+    python setup.py build
+}
+
+package() {
+    pushd "${srcdir}/${_module}-${pkgver}"
+    python setup.py install --root="${pkgdir}" --optimize=1 --skip-build
+    popd
+}
---
+++ update.sh
@@ -0,0 +1,75 @@
+#! /bin/sh
+
+AUR_NAME=python-marshmallow-enum
+PROJ_NAME=marshmallow-enum
+DESCRIPTION="Enum handling for Marshmallow"
+URL=https://github.com/justanr/marshmallow_enum
+DEPENDS=()
+LICENSE='MIT'
+ADDITIONAL=
+
+if [[ $# == 0 ]]; then
+    echo 'Usage: ./update.sh VERSION_NUMBER'
+    exit 1
+fi
+pkgrel=1
+if [[ $# == 2 ]]; then
+    pkgrel=$2
+fi
+
+SRCS=(
+    https://files.pythonhosted.org/packages/source/${PROJ_NAME:0:1}/${PROJ_NAME}/${PROJ_NAME}-$1.tar.gz
+)
+
+printf '' > PKGBUILD
+echo "# Maintainer: Sumner Evans <sumner.evans98 at gmail dot com>
+
+pkgbase='${AUR_NAME}'
+pkgname=('${AUR_NAME}')
+_module='${PROJ_NAME}'
+pkgver='$1'
+pkgrel=$pkgrel
+pkgdesc='${DESCRIPTION}'
+url='${URL}'
+depends=(
+    'python'" >> PKGBUILD
+
+# Include the dependencies.
+for d in ${DEPENDS[*]}; do
+    echo "    '$d'" >> PKGBUILD
+done
+
+echo ")
+optdepends=()
+makedepends=(
+    'python-setuptools'
+)
+license=('${LICENSE}')
+arch=('any')
+source=(" >> PKGBUILD
+
+# Include the dependencies.
+for s in ${SRCS[*]}; do
+    echo "    '$s'" >> PKGBUILD
+done
+
+echo ")
+md5sums=()
+${ADDITIONAL}
+
+build() {
+    cd \"\${srcdir}/\${_module}-\${pkgver}\"
+    python setup.py build
+}
+
+package() {
+    pushd \"\${srcdir}/\${_module}-\${pkgver}\"
+    python setup.py install --root=\"\${pkgdir}\" --optimize=1 --skip-build
+    popd
+}" >> PKGBUILD
+
+updpkgsums
+makepkg --printsrcinfo > .SRCINFO
+
+# Test
+makepkg -f
