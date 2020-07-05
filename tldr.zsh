6aef5cff068da9f84fcda4546c6b0a86d1fce0dc
Mubashshir <ahmubashshir@gmail.com>
2020-07-05 10:27:50 +0600

    Update and fix pkgbuild.
     Fix pkgbuild.
     Add zsh auto complete script.

---
+++ tldr.zsh
@@ -0,0 +1,26 @@
+#compdef tldr
+#
+# Zsh auto complete function for zsh
+#
+
+local state
+_arguments \
+{-l,--list}"[show all available pages]" \
+{-p,--platform}"[show page from specific platform rather than autodetecting]:platforms:->platform" \
+{-u,-c,--update}"[update, force retrieving latest copies of locally cached files]" \
+": :->page"
+case $state in
+    (page)
+        _values "Pages" $(tldr -l 2>/dev/null)
+    ;;
+    (platform)
+        _values "Platforms" \
+            'common[Common pages]' \
+            'linux[Linux only pages]' \
+            'osx[OS X only pages]' \
+            'sunos[SunOS only pages]' \
+            'windows[Windows only pages]'
+    ;;
+    (*) true
+    ;;
+esac
