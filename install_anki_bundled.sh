dfcca0524877339fba81bbdd5eb880a174cb21c9
s7hoang <s7hoang@gmail.com>
2020-07-17 19:22:11 -0400

    Add Clarifying Comments For Bundled Anki Install Script
      mplayer seems very extra.

--- install_anki_bundled.sh
+++ install_anki_bundled.sh
@@ -7,3 +7,9 @@ python-pyaudio python-requests python-decorator \
 python-psutil python-distro python-markdown \
 python-pyqt5 python-pyqtwebengine
 sudo -u anki-sync-server sh tools/build_ui.sh #for aqt dependency
+
+# mplayer is not included because I thought it was a lot of extra dependencies
+# for functionality that seems very extra (video capability). If you need this
+# you can install that yourself with:
+#
+#   sudo pacman -S --asdeps mplayer
