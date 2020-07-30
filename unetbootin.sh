61e6a38bed95a747e2133bdad49f552a32269c75
gardenapple <gardenapple@posteo.net>
2020-07-30 21:39:22 +0300

    Improve start script

--- unetbootin.sh
+++ unetbootin.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #export QT_X11_NO_MITSHM=1
-if [ $(which pkexec) ]; then
+if type pkexec &> /dev/null; then
   pkexec "/usr/bin/unetbootin.elf" "$@" 2>/tmp/_polkit_error
   #pkexec --disable-internal-agent "/usr/bin/unetbootin.elf" "$@" 2>/tmp/_polkit_error
   if [ -s /tmp/_polkit_error ]; then
-    zenity --error --text="`cat /tmp/_polkit_error`"
+    if type zenity &> /dev/null; then
+      zenity --error --text="`cat /tmp/_polkit_error`"
+    else
+      cat /tmp/_polkit_error
+    fi
     rm /tmp/_polkit_error
   fi
 else
