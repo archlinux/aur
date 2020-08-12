a1af82761600c5b9a6c0ceb3e4231215e56bb741
Ratan Rai Sur <ratan.r.sur@gmail.com>
2020-08-12 12:44:28 -0500

    pkgrel check
    Signed-off-by: Ratan Rai Sur <ratan.r.sur@gmail.com>

--- update.sh
+++ update.sh
@@ -3,6 +3,12 @@
 #stop on failure
 set -e
 
+pkgrel=$(grep pkgrel PKGBUILD | cut -d '=' -f 2)
+if [ "$pkgrel" != "1" ]; then
+  echo "pkgrel is $pkgrel, not 1. Continue?"
+  read -q
+fi
+
 echo "Updating to verion $1"
 sed -i -e "s/pkgver=.*/pkgver=$1/g" PKGBUILD
 
