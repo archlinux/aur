5ab8b1ebe664a6c2fd2ee89e7dfe48d002280a1d
Matti Viljanen <matti.viljanen@kapsi.fi>
2020-09-12 15:18:48 +0300

    Check that the .deb files exist

--- update_pkg.sh
+++ update_pkg.sh
@@ -16,6 +16,18 @@ _ubudist=$(grep "_ubudist=" PKGBUILD | cut -d "=" -f 2-)
 echo "Current version:   $pkgver-$_ubuver-$_ubudist"
 echo "Available version: $ver1-$ver2-$ver3"
 
+echo "Checking that the files exist..."
+wget --quiet --spider $REPO/chromium-codecs-ffmpeg-extra_${pkgver}-${_ubuver}.${_ubudist}_amd64.deb
+if [[ "$?" == "0" ]]; then echo "x86_64:  yes"; else echo "x86_64:   no"; fi
+wget --quiet --spider $REPO/chromium-codecs-ffmpeg-extra_${pkgver}-${_ubuver}.${_ubudist}_i386.deb
+if [[ "$?" == "0" ]]; then echo "i686:    yes"; else echo "i686:     no"; fi
+
+REPO='http://ports.ubuntu.com/ubuntu-ports/pool/universe/c/chromium-browser/'
+
+wget --quiet --spider $REPO/chromium-codecs-ffmpeg-extra_${pkgver}-${_ubuver}.${_ubudist}_armhf.deb
+if [[ "$?" == "0" ]]; then echo "arm*:    yes"; else echo "arm*:     no"; fi
+wget --quiet --spider $REPO/chromium-codecs-ffmpeg-extra_${pkgver}-${_ubuver}.${_ubudist}_arm64.deb
+if [[ "$?" == "0" ]]; then echo "aarch64: yes"; else echo "aarch64:  no"; fi
 
 echo "Updating PKGBUILD..."
 sed -i "s|^pkgver=.*$|pkgver=${ver1}|" PKGBUILD
