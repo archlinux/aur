09e06a14652cdd788c66259ccb7342eaa38bad87
Greg White <gwhite@kupulau.com>
2020-09-23 11:31:31 -0400

    Remove obsolete check for user_ns

--- brave-nightly-bin.sh
+++ brave-nightly-bin.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$BRAVE_FLAGS" ]; then
-    if [[ ! (-r /proc/sys/kernel/unprivileged_userns_clone && $(< /proc/sys/kernel/unprivileged_userns_clone) == 1 && -n $(zcat /proc/config.gz | grep CONFIG_USER_NS=y) ) ]]; then
-        >&2 echo "User namespaces are not detected as enabled on your system, brave will run with the sandbox disabled"
-        FLAG="--no-sandbox"
-    fi
-fi
 if [[ -z "${CHROME_USER_DATA_DIR}" ]]; then
     export CHROME_USER_DATA_DIR=~/.config/BraveSoftware/Brave-Browser-Nightly
 fi
