a355e359005b9996e6b267727f4f6f834859fe6e
relrel <relrelbachar@gmail.com>
2020-09-26 09:40:41 +0300

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+CHROMIUM_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/linux")
+V8_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/v8.json?version=$CHROMIUM_VERSION" | jq -r '.v8_version')
+
+sed -i -e "s/^pkgver=.*/pkgver=$V8_VERSION/" -e 's/pkgrel=.*/pkgrel=1/' PKGBUILD
+if (git diff --quiet PKGBUILD); then
+	echo "Nothing to do"
+	exit
+fi
+
+echo "Updating to $V8_VERSION..."
+makepkg --printsrcinfo > .SRCINFO
+git restore --staged .
+git add PKGBUILD .SRCINFO
+git commit -m "Update to $V8_VERSION"
