d13ce6a3d29be22ce16d13768f97a3482bbc3c1c
Israel Roldan <israel.alberto.rv@gmail.com>
2020-09-30 03:36:05 +0000

    Automatic deployment on Wed Sep 30 03:36:05 AM UTC 2020 from the official repository in GitHub using CI (Continuous Integration).

--- upload-package.sh
+++ upload-package.sh
@@ -16,9 +16,16 @@
 makepkg -f
 rm -fR battery-discharging-beep* pkg src .SRCINFO
 makepkg --printsrcinfo > .SRCINFO
+echo ""
+
+git diff --exit-code
 git add .
 git status
+
 echo "Write your commit comment: "
 read -r COMMENT
 git commit -m "${COMMENT}"
+echo ""
+
 git push
+echo ""
