adf7cb89bd6cb647afc87b46e7d126e282a5b926
Peter K. Moss <peter@moss.dk>
2020-11-04 18:44:32 +0100

    Played around with the update script, no longer here

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-makepkg -f &&
-makepkg --printsrcinfo > .SRCINFO &&
-git add PKGBUILD .SRCINFO &&
-git commit -m "Updated to version $(makepkg --printsrcinfo | grep pkgver | cut -d ' ' -f 3)" &&
-git push
