3dc682048aa6299ad6d159965a0cedb219d28447
Christian Hesse <mail@eworm.de>
2020-11-04 23:59:09 +0100

    commit netinstall 6.47.7-1

---
+++ PKGBUILD
@@ -0,0 +1,31 @@
+# Maintainer: Christian Hesse <mail@eworm.de>
+
+pkgname=netinstall
+pkgver=6.47.7
+pkgrel=1
+pkgdesc='Mikrotik Netinstall for RouterOS'
+arch=('i686' 'x86_64')
+url='https://www.mikrotik.com/'
+license=('custom')
+depends=('wine' 'libcap')
+makedepends=('icoutils')
+install=netinstall.install
+source=("https://download.mikrotik.com/routeros/${pkgver}/netinstall-${pkgver}.zip"
+	'netinstall.c'
+	'netinstall.desktop')
+sha256sums=('c27212f20520ccdae2eb8df78b3ebe146707c9c4464d3daacc23abad5f0e1699'
+            '0788e62837867c93147988ff3fbd8b005aa21af7a46fd0faec97fe1e38b2af95'
+            '742b8ebf2b66697f24a27b5e6920dd4a4b92dec0fea928e8f58e499246284623')
+
+build() {
+	wrestool -x -t 14 netinstall.exe > netinstall.ico
+	gcc ${CFLAGS} ${LDFLAGS} -o netinstall netinstall.c -lcap
+}
+
+package() {
+	install -D -m0755 netinstall.exe ${pkgdir}/usr/share/netinstall/netinstall.exe
+	install -D -m0755 netinstall ${pkgdir}/usr/bin/netinstall
+	install -D -m0644 netinstall.desktop ${pkgdir}/usr/share/applications/netinstall.desktop
+	install -D -m0644 netinstall.ico ${pkgdir}/usr/share/pixmaps/netinstall.ico
+	install -D -m0644 LICENSE.txt ${pkgdir}/usr/share/licenses/netinstall/LICENSE.txt
+}
---
+++ netinstall.c
@@ -0,0 +1,66 @@
+/*
+ * (C) 2020 by Christian Hesse <mail@eworm.de>
+ *
+ * This software may be used and distributed according to the terms
+ * of the GNU General Public License, incorporated herein by reference.
+ *
+ * Running Mikrotik netinstall with unprivileged user requires ambient
+ * capability CAP_NET_BIND_SERVICE. This is a wrapper to run it in wine.
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/capability.h>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char ** argv) {
+  cap_value_t newcap[1];
+  cap_t caps = cap_get_proc();
+  newcap[0] = CAP_NET_BIND_SERVICE;
+  cap_set_flag(caps, CAP_INHERITABLE, 1, newcap, CAP_SET);
+  cap_set_proc(caps);
+  cap_free(caps);
+
+  if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_RAISE, CAP_NET_BIND_SERVICE, 0, 0)) {
+    perror("prctl: can not set CAP_NET_BIND_SERVICE");
+    return EXIT_FAILURE;
+  }
+  
+  struct stat st;
+  const char *home = getenv("HOME");
+  char wineprefix[256];
+  snprintf(wineprefix, sizeof wineprefix, "%s/.netinstall/wine/", home);
+
+  setenv("WINEPREFIX", wineprefix, 1);
+  setenv("WINEARCH", "win64", 1);
+  setenv("WINEDLLOVERRIDES", "mscoree=", 1);
+  setenv("WINEDEBUG", "-all", 1);
+
+  if (chdir(home) == -1) {
+    perror("chdir: changing to home directory");
+    return EXIT_FAILURE;
+  }
+
+  if (stat(".netinstall/", &st) == -1) {
+    if (mkdir(".netinstall/", 0777) == -1) {
+      perror("mkdir: creating directory '.netinstall/'");
+      return EXIT_FAILURE;
+    }
+    
+    if (system("wineboot -u") != 0) {
+      perror("system: running wineboot");
+      return EXIT_FAILURE;
+    }
+  }
+
+  const char *pathname = "/usr/bin/wine";
+  char * const newargv[] = { "wine", "/usr/share/netinstall/netinstall.exe", NULL };
+  execv(pathname, newargv);
+
+  /* execv() returns only on error */
+  perror("execv");
+  return EXIT_FAILURE;
+}
