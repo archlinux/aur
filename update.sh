1c75fe792a07bbb0d2b19ddee4d49a8b9464a76c
relrel <relrelbachar@gmail.com>
2020-11-21 08:25:41 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 set -euo pipefail
-
 cd "$(dirname "$0")"
 
 if [ -n "$(git status --porcelain)" ]; then
-	echo "Working directory not clean"
+	echo "Working directory is not clean"
 	exit 1
 fi
+
+git pull
 
 read -r -d "\n" TAG_NAME RELEASE_NAME <<< "$(curl -sSf "https://api.github.com/repos/ruffle-rs/ruffle/releases?per_page=1" | jq -r ".[0] | .tag_name, .name")" || true
 
@@ -24,3 +25,4 @@ echo "Updating to $RELEASE_NAME..."
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
 git commit -am "Update to $RELEASE_NAME"
+git push
