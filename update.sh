28b519bbe6c0e1e790b842001819c495369110d0
relrel <relrelbachar@gmail.com>
2020-11-26 11:22:22 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,22 +1,25 @@
 #!/bin/sh
 
 set -euo pipefail
-
 cd "$(dirname "$0")"
 
-CHROMIUM_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/linux")
-V8_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/v8.json?version=$CHROMIUM_VERSION" | jq -r '.v8_version')
-
-sed -e "s/^pkgver=.*/pkgver=$V8_VERSION/" \
-    -e 's/pkgrel=.*/pkgrel=1/' \
-    -i PKGBUILD
-if (git diff --quiet PKGBUILD); then
-	echo "Nothing to do"
-	exit
+if [ -n "$(git status --porcelain)" ]; then
+	echo "Working directory is not clean"
+	exit 1
 fi
+git pull
+
+CHROMIUM_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/linux")
+V8_VERSION=$(curl -sSf "https://omahaproxy.appspot.com/v8.json?version=$CHROMIUM_VERSION" | jq -r ".v8_version")
+sed -e "s/^pkgver=.*/pkgver=$V8_VERSION/" -i PKGBUILD
+if [ -z "$(git status --porcelain)" ]; then
+	echo "Nothing to do"
+	exit 1
+fi
+sed -e "s/^pkgrel=.*/pkgrel=1/" -i PKGBUILD
 
 echo "Updating to $V8_VERSION..."
+updpkgsums
 makepkg --printsrcinfo > .SRCINFO
-git restore --staged .
-git add PKGBUILD .SRCINFO
-git commit -m "Update to $V8_VERSION"
+git commit -am "Update to $V8_VERSION"
+git push
