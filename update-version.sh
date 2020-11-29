ad7052ed4f167c60e3d2f5ebcec22c813769f5e4
Henry Jenkins <henry@henryjenkins.name>
2020-11-29 10:16:51 +0000

    Fix update script
    Old URL is broken, so consume checksums straight from github

--- update-version.sh
+++ update-version.sh
@@ -2,10 +2,11 @@
 #
 # Update the PKGBUILD with the lastest release
 
-VERSION=`curl -fsSL https://get.please.build/latest_version`
-SHA256URL="https://get.please.build/linux_amd64/${VERSION}/please_${VERSION}.tar.gz.sha256"
+VERSIONURL="https://get.please.build/latest_version"
+VERSION=`curl -fsSL ${VERSIONURL}`
+SHA256SUMURL="https://github.com/thought-machine/please/releases/download/v${VERSION}/please_${VERSION}_linux_amd64.tar.gz.sha256"
 
-SHA256SUM=`curl -fsSL ${SHA256URL} | cut -d ' ' -f1`
+SHA256SUM=`curl -fsSL ${SHA256SUMURL} | cut -d ' ' -f1`
 
 sed -i -e "s/pkgver=.*/pkgver=${VERSION}/" PKGBUILD
 sed -i -e "s/sha256sums_x86_64=.*/sha256sums_x86_64=(\"${SHA256SUM}\")/" PKGBUILD
