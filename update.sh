d4cc7e0f473b126b79ee7159294fb077c891d820
relrel <relrelbachar@gmail.com>
2020-11-30 05:32:35 +0000

    Pass --noconfirm to makepkg

--- update.sh
+++ update.sh
@@ -21,6 +21,6 @@ sed -e "s/^pkgrel=.*/pkgrel=1/" -i PKGBUILD
 echo "Updating to $RELEASE_NAME..."
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
-makepkg -sf
+makepkg -sf --noconfirm
 git commit -am "Update to $RELEASE_NAME"
 git push
