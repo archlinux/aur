4dd35dc1d629704b2e58297e81e070bf439052c4
aiyion.prime <git@aiyionpri.me>
2020-12-08 17:27:28 +0100

    workaround for gtk3 issue
    The project is old enough to not use any gtk3 features, yet;
    as soon as https://github.com/luhsra/MinimaxSimulator/issues/50
    or even https://github.com/javafxports/openjdk-jfx/issues/546
    is resolved, this can be reverted.

--- minimaxsimulator.sh
+++ minimaxsimulator.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /usr/bin/java -jar '/usr/share/java/minimaxsimulator/minimax_simulator.jar' "$@"
+exec /usr/bin/java -Djdk.gtk.version=2 -jar '/usr/share/java/minimaxsimulator/minimax_simulator.jar' "$@"
