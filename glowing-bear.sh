cd0120babc185e5607361b46623da90d69d15eb3
rany <rany2@riseup.net>
2021-01-01 02:03:28 +0200

    update

--- glowing-bear.sh
+++ glowing-bear.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # workaround for init.json issue
-cd "$HOME/.config/glowing-bear"
 mkdir -p "$HOME/.config/glowing-bear"
+cd "$HOME/.config/glowing-bear"
+
+# set node_modules path
+export NODE_PATH="/usr/lib/glowing-bear/node_modules"
 
 # start electron glowing-bear
 exec electron /usr/lib/glowing-bear/webapp/electron-main.js "${@}"
