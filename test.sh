9f877a4b06ab9cc5e244e00b0065644df7c1cce7
Mateusz Galazyn <carbolymer@gmail.com>
2021-01-12 09:24:19 +0100

    Update .SRCINFO

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 updpkgsums && \
+  makepkg --printsrcinfo | tee .SRCINFO && \
   makepkg -f && \
   podman build . -t zim && \
   podman run --rm -it zim /usr/bin/zsh -c 'sudo pacman --noconfirm -U zsh-zim-git-* && zsh'
