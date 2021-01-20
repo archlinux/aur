694146b4299fb665469311892188f827d6fda000
Oliver Ford <dev@ojford.com>
2021-01-20 13:29:39 +0000

    Prepare v0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="iosevka-generate",
-    version="0.3.0",
+    version="0.3.1",
     description="Generate custom Iosevka fonts from a configuration file",
     author="Oliver Ford",
     author_email="dev@ojford.com",
