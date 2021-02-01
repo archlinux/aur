a9b58338e74b781c2e0f86434e267520d4559fb7
Iain Earl <iain@itmecho.com>
2021-02-01 09:43:50 +0000

    Add release script to repo

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+version=$1
+tmp_tar=$(mktemp)
+trap "rm ${tmp_tar}" EXIT
+wget https://github.com/denisidoro/navi/releases/download/v${version}/navi-v${version}-x86_64-unknown-linux-musl.tar.gz -O $tmp_tar
+checksum=$(sha256sum $tmp_tar | awk '{print $1}')
+sed -i "s/^pkgver='.*'/pkgver='${version}'/g" PKGBUILD
+sed -i "s/^sha256sums=('.*')/sha256sums=('${checksum}')/g" PKGBUILD
+makepkg -si
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git diff --cached
+git commit -S -m "Update to ${version}"
+echo -n "Press Enter to push..."
+read check
+git push
+paru -Si navi-bin
