6d520e65bbdee0d87850899267e733e7aceff84c
Xorg <xorgbreaker@gmail.com>
2021-02-17 21:03:34 +0100

    Improve text thumbnailer
    Suggested by @grayich in comment https://aur.archlinux.org/packages/tumbler-extra-thumbnailers/#comment-792107

--- text-thumbnailer.sh
+++ text-thumbnailer.sh
@@ -5,9 +5,25 @@ if [[ $# -lt 3 ]]; then
 	exit 255
 fi
 
+TMPFILE=$(mktemp /tmp/large.XXXXXX.png)
 SIZE="$1"
 INPUT_FILE_NAME="$2"
 OUTPUT_FILE_NAME="$3"
-CHOPPED_FILE_CONTENT="$(head --lines=50 "$INPUT_FILE_NAME")"
+CHOPPED_FILE_CONTENT="$(head --lines=36 "$INPUT_FILE_NAME")"
 
-convert -size ${SIZE}x${SIZE} -background white -pointsize 10 -border 5x5 -bordercolor "#CCC" caption:"$CHOPPED_FILE_CONTENT" "$OUTPUT_FILE_NAME"
+convert -size 210x254 \
+	-background "#fffaed" \
+	-fill black \
+	-border 1x1 -bordercolor "#00aaff" \
+	-font "Liberation-Mono" -pointsize 5 \
+	label:"$CHOPPED_FILE_CONTENT" "$TMPFILE"
+
+if [[ $SIZE -lt 256 ]]; then
+	convert -thumbnail ${SIZE}x${SIZE} -define png:compression-level=3 \
+		"$TMPFILE" "$OUTPUT_FILE_NAME"
+else
+    cat "$TMPFILE" > "$OUTPUT_FILE_NAME"
+fi
+
+rm "$TMPFILE"
+exit 0
