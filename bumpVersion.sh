c7e85856a10f214bc093fab3f858368c9922ef63
Linus Dierheimer <Linus@Dierheimer.de>
2021-02-19 18:03:01 +0100

    bumped version to r5

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -1,5 +1,5 @@
 makepkg -f
 makepkg --printsrcinfo > .SRCINFO
 git add .
-git commit -m "bumped version to $(git rev-list --count HEAD)"
+git commit -m "bumped version to r$(git rev-list --count HEAD)"
 git push
