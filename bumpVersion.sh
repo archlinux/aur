ad400826d7a6cc7e1f45f2e137d70b76d7508ab2
Linus Dierheimer <Linus@Dierheimer.de>
2021-02-22 14:05:17 +0100

    bumped version to r15

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -1,10 +1,12 @@
+makepkg -f
+
 version=0
 cd "fastfetch/"
 version="$(git rev-list --count HEAD)"
 cd ..
 
-makepkg -f
 makepkg --printsrcinfo > .SRCINFO
+
 git add .
 git commit -m "bumped version to r${version}"
 git push
