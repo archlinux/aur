7e6297fa9e2ef47d574d92665600103d675b9327
Tobias Schmidt <tobidt@gmail.com>
2021-03-07 20:30:33 +0100

    Allow user to edit commit before automatic push
    So that a badly formatted changelog can be adjusted manually.

--- publish.sh
+++ publish.sh
@@ -24,5 +24,5 @@ date=$(echo "$release" | jq -r .published_at | cut -dT -f1)
 echo -e "## $version ($date)\n\n$changelog\n\n$(cat CHANGELOG.md)" > CHANGELOG.md
 
 git add .SRCINFO CHANGELOG.md PKGBUILD
-git commit -m "Publish yopass $version" -v
+git commit --edit --message "Publish yopass $version" --verbose
 git push origin master
