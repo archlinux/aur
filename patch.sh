c909f6b70c70a11454711bdf0eb872a99aeec211
Sainnhe Park <sainnhe@gmail.com>
2021-04-06 14:19:56 +0800

    fix updating bug

--- patch.sh
+++ patch.sh
@@ -4,10 +4,12 @@ if [ "${1}" = "-R" ]; then
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/\t\t"serviceUrl": "https:\/\/open-vsx.org\/vscode\/gallery",/' \
     -e '/^[[:blank:]]*"cacheUrl/d' \
     -e 's/^[[:blank:]]*"itemUrl":.*/\t\t"itemUrl": "https:\/\/open-vsx.org\/vscode\/item"/' \
+    -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
     -e '/^[[:blank:]]*"documentationUrl/i\\t"linkProtectionTrustedDomains": ["https://open-vsx.org"],' \
     /usr/lib/code/product.json
 else
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/\t\t"serviceUrl": "https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery",/' \
+    -e '/^[[:blank:]]*"cacheUrl/d' \
     -e '/^[[:blank:]]*"serviceUrl/a\\t\t"cacheUrl": "https:\/\/vscode.blob.core.windows.net\/gallery\/index",' \
     -e 's/^[[:blank:]]*"itemUrl":.*/\t\t"itemUrl": "https:\/\/marketplace.visualstudio.com\/items"/' \
     -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
