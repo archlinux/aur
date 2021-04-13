88312371d3086ab159c5145689bd6ebe8153f847
Knut Ahlers <knut@ahlers.me>
2021-04-14 00:36:23 +0200

    Add auto-update script
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ update_version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -euxo pipefail
+
+# Read pkg name
+PKG=$(awk -F '=' '/pkgname=/{ print $2 }' PKGBUILD)
+
+# Get latest version
+VER=$(
+	curl -s https://api.github.com/repos/vuejs/eslint-plugin-vue/releases/latest |
+		jq -r '.tag_name' |
+		sed 's/^v//'
+)
+[[ -n $VER ]] || exit 1
+
+# Insert latest version into PKGBUILD
+sed -i \
+	-e "s/^pkgver=.*/pkgver=${VER}/" \
+	PKGBUILD
+
+# Check whether this changed anything
+if (git diff --exit-code PKGBUILD); then
+	echo "Package ${PKG} has most recent version ${VER}"
+	exit 0
+fi
+
+# Reset pkgrel
+sed -i \
+	-e 's/pkgrel=.*/pkgrel=1/' \
+	PKGBUILD
+
+# Update source hashes
+updpkgsums
+
+# Update .SRCINFO
+makepkg --printsrcinfo >.SRCINFO
+
+# Commit changes
+git add PKGBUILD .SRCINFO
+git commit -m "${PKG} v${VER}"
