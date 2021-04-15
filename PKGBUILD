990707f8d4879c0c5f1b23226bbc3fc6e24503d4
Thiago Almeida <thiagoalmeidasa@gmail.com>
2021-04-15 16:47:05 +0200

    Version bump 0.1.29

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Thiago Almeida <echo "dGhpYWdvYWxtZWlkYXNhQGdtYWlsLmNvbQo=" | base64 -d>
 
 pkgname="cloud-nuke-bin"
-pkgver=0.1.28
+pkgver=0.1.29
 pkgrel=1
 pkgdesc="A tool for cleaning up your cloud accounts by nuking (deleting) all resources within it."
 url="https://github.com/gruntwork-io/cloud-nuke"
@@ -10,8 +10,8 @@ license=('MIT')
 provides=('cloud-nuke')
 source_x86_64=("$pkgname::$url/releases/download/v${pkgver}/${pkgname/-bin/}_linux_amd64")
 source_i386=("$pkgname::$url/releases/download/v${pkgver}/${pkgname/-bin/}_linux_386")
-sha256sums_x86_64=(4847e823546b60a78410ce36c91cc9306d7c9f144e85135a81b61720b37df518)
-sha256sums_i386=(80674763912d1b77e055d7151bfea241f01e4b87fdb6e4d232a60a541992ee13)
+sha256sums_x86_64=(bb7288a790d27be8c5ff316d87555c813b5340acc8a03e789812bb73a660a81e)
+sha256sums_i386=(063c7e2c3b0355e97fce73c1bf644560f27e9ae55589f44630160bd0e48d9278)
 
 package() {
   install -Dm755 cloud-nuke-bin "$pkgdir/usr/bin/cloud-nuke"
--- update.sh
+++ update.sh
@@ -4,18 +4,31 @@ set -x
 
 set -eo pipefail
 
-[[ ! -z "$GITHUB_TOKEN" ]] && GITHUB_AUTH="Authorization: ${GITHUB_TOKEN}" || GITHUB_AUTH=""
+rm SHA256SUMS cloud-nuke_linux_amd64 cloud-nuke_linux_386 cloud-nuke-bin-*x86_64.pkg.tar.zst cloud-nuke-bin || true
+
+rm -rf pkg src || true
+
+[[ -n "$GITHUB_TOKEN" ]] && GITHUB_AUTH="Authorization: ${GITHUB_TOKEN}" || GITHUB_AUTH=""
 
 http https://api.github.com/repos/gruntwork-io/cloud-nuke/releases/latest \
-	"Accept: application/vnd.github.v3+json" ${GITHUB_AUTH} |
-	jq -r '.assets[] | select(.name | test("^(SHA256SUMS|cloud-nuke.*_linux_(amd64|386))$")) | .browser_download_url' |
-	xargs -n 1 -P 3 wget
+  "Accept: application/vnd.github.v3+json" ${GITHUB_AUTH} |
+  jq -r '.assets[] | select(.name | test("^(SHA256SUMS|cloud-nuke.*_linux_(amd64|386))$")) | .browser_download_url' |
+  xargs -n 1 -P 3 wget
+
+LATEST_RELEASE_INFO=$(http https://api.github.com/repos/gruntwork-io/cloud-nuke/releases/latest \
+  "Accept: application/vnd.github.v3+json" ${GITHUB_AUTH})
+
+# @TODO: Parse .browser_download_url to get individual download links
 
 sha256sum --ignore-missing --status -c SHA256SUMS
+
+pkgver=$(jq -r .name <<< "${LATEST_RELEASE_INFO}")
 
 X86_64_SHA=$(awk '/cloud-nuke.*_linux_amd64$/ {print $1}' SHA256SUMS)
 
 I386_SHA=$(awk '/cloud-nuke.*_linux_386$/ {print $1}' SHA256SUMS)
+
+sed -i -r 's/(pkgver=).*/\1'"${pkgver#v}"'/g' PKGBUILD
 
 sed -i -r 's/(sha256sums_x86_64=)\([A-Fa-f0-9]*\)/\1\('"$X86_64_SHA"'\)/g' PKGBUILD
 
