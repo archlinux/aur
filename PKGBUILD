c0b48cf9950323830ea3c0194d0bd33a025f4067
Thiago Almeida <thiagoalmeidasa@gmail.com>
2021-04-19 10:32:30 +0200

    Version bump to 0.7.1

--- PKGBUILD
+++ PKGBUILD
@@ -1,8 +1,8 @@
 # Maintainer: Thiago Almeida <echo "dGhpYWdvYWxtZWlkYXNhQGdtYWlsLmNvbQo=" | base64 -d>
 
 pkgname="qrcp-bin"
-pkgver=0.7.0
-pkgrel=3
+pkgver=0.7.1
+pkgrel=1
 pkgdesc="Transfer files over wifi from your computer to your mobile device by scanning a QR code without leaving the terminal."
 url="https://github.com/claudiodangelis/qrcp"
 arch=('x86_64' 'i386')
@@ -11,8 +11,8 @@ provides=('qrcp')
 conflicts=('qrcp-git' 'qr-filetransfer-git' 'qrcp')
 source_x86_64=("$pkgname-$pkgver.tar.gz::$url/releases/download/${pkgver}/${pkgname/-bin/}_${pkgver}_linux_x86_64.tar.gz")
 source_i386=("$pkgname-$pkgver.tar.gz::$url/releases/download/${pkgver}/${pkgname/-bin/}_${pkgver}_linux_i386.tar.gz")
-sha256sums_x86_64=('165c90c224ae5e311d2c1b9393e00bd2e2eaf92f6247fb2f2ecaa57d1f51801e')
-sha256sums_i386=('7357284cd89023f4d9b5d7f49c5a973dcdafb200ba5ac5873c5d51093527bf44')
+sha256sums_x86_64=(d0d6c72484131f42c2872e2d365aaca28b32ac3f0ed589da0808f6b37013b31d)
+sha256sums_i386=(f9c6a99921f89366ac9aa1469900b6cf51523102f1e4c4ed6e367bf9a5ad8afa)
 
 
 build() {
--- update.sh
+++ update.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-set -eou pipefail
+set -x
 
-GITHUB_TOKEN=${GITHUB_TOKEN}
+set -eo pipefail
+
+pkgname=qrcp-bin
+
+rm ${pkgname/-bin/}*  checksums.txt || true
+
+rm -rf pkg src || true
+
+[[ -n "$GITHUB_TOKEN" ]] && GITHUB_AUTH="Authorization: ${GITHUB_TOKEN}" || GITHUB_AUTH=""
 
 http https://api.github.com/repos/claudiodangelis/qrcp/releases/latest \
-	"Accept: application/vnd.github.v3+json" \
-	"Authorization: ${GITHUB_TOKEN}" |
-	jq -r '.assets[] | select(.name | test("^(checksums.txt|qrcp.*_linux_(x86_64|i386).tar.gz)$")) | .browser_download_url' |
-	xargs -n 1 -P 3 wget
+  "Accept: application/vnd.github.v3+json" ${GITHUB_AUTH} |
+  jq -r '.assets[] | select(.name | test("^(checksums.txt|qrcp.*_linux_(x86_64|i386).tar.gz)$")) | .browser_download_url' |
+  xargs -n 1 -P 3 wget
 
 sha256sum --ignore-missing --status -c checksums.txt
+
+pkgver=$(http https://api.github.com/repos/claudiodangelis/qrcp/releases/latest \
+  "Accept: application/vnd.github.v3+json" ${GITHUB_AUTH} |
+  jq -r '.name')
 
 I386_SHA=$(awk '/qrcp.*_linux_i386.tar.gz$/ {print $1}' checksums.txt)
 
 X86_64_SHA=$(awk '/qrcp.*_linux_x86_64.tar.gz$/ {print $1}' checksums.txt)
 
-sed -i 's/\(sha256sums_x86_64=\)(\d*)/\1'"$X86_64_SHA"'/g' PKGBUILD
+sed -i -r 's/(pkgver=).*/\1'"${pkgver#v}"'/g' PKGBUILD
 
-sed -i 's/\(sha256sums_i386=\)(\d*)/\1'"$I386_SHA"'/g' PKGBUILD
+sed -i -r 's/(sha256sums_x86_64=)\([A-Fa-f0-9]*\)/\1\('"$X86_64_SHA"'\)/g' PKGBUILD
+
+sed -i -r 's/(sha256sums_i386=)\([A-Fa-f0-9]*\)/\1\('"$I386_SHA"'\)/g' PKGBUILD
 
 makepkg --printsrcinfo >.SRCINFO
