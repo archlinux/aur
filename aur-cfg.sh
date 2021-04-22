deca2bd1270efd21b1a8bae3b235581f27bc7704
David Barri <japgolly@gmail.com>
2021-04-23 08:28:17 +1000

    Use external aur script

---
+++ aur-cfg.sh
@@ -0,0 +1,7 @@
+function get_latest_version {
+  curl -s https://github.com/CryptoGraphics/VerthashMiner/releases/latest | perl -pe 's!.*/tag/v?([0-9].+?)".*!$1!'
+}
+
+function clean_downloads {
+  rm -vf VerthashMiner-*.tar.gz
+}
