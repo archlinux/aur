8de637ad73c1c39c5af5571c41c6706a4dd03b42
David Barri <japgolly@gmail.com>
2021-04-23 08:10:34 +1000

    Add clean_downloads to aur-cfg.sh

--- aur-cfg.sh
+++ aur-cfg.sh
@@ -1,3 +1,7 @@
 function get_latest_version {
   curl -s https://update.tryshift.com/download/version | sed 's/.* version //; s/ .*/\n/'
 }
+
+function clean_downloads {
+  rm -vf shift-linux-*.zip
+}
