276a67de14dccaf4b229e1c457ccf4c3c23d263f
Dale Whinham <daleyo@gmail.com>
2021-05-18 17:15:01 +0100

    Initial commit

---
+++ ag-dsp-controller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export WINEPREFIX="$HOME"/.ag-dsp-controller/wine
+export WINEARCH=win32
+export WINEDEBUG=-all
+
+if [ ! -d "$HOME"/.ag-dsp-controller ] ; then
+   mkdir -p "$HOME"/.ag-dsp-controller/wine
+   wineboot -u
+fi
+
+wine /usr/share/ag-dsp-controller/ag_dsp_controller.exe "$@"
