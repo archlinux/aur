bda2ca4dc4907607afeae84afffbf9fe86f51602
Dale Whinham <daleyo@gmail.com>
2021-06-10 13:19:40 +0100

    Suppress wine-mono check when building prefixes

--- ag-dsp-controller.sh
+++ ag-dsp-controller.sh
@@ -3,6 +3,7 @@
 export WINEPREFIX="$HOME"/.ag-dsp-controller/wine
 export WINEARCH=win32
 export WINEDEBUG=-all
+export WINEDLLOVERRIDES=mscoree=d
 
 if [ ! -d "$HOME"/.ag-dsp-controller ] ; then
    mkdir -p "$HOME"/.ag-dsp-controller/wine
