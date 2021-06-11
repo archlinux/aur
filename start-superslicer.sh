80f118074d33fe18dba208c072c515df70bd025e
Lukas1818 <git@lukas1818.de>
2021-06-11 16:49:10 +0200

    update to 2.3.56.4

--- start-superslicer.sh
+++ start-superslicer.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-if [ -d "$HOME/.SuperSlicer" ] && [ ! -d "$HOME/.config/SuperSlicer-alpha" ]
-then
-	echo "copy data from ~/.SuperSlicer to the new data folder ~/.config/SuperSlicer-alpha"
-	cp -r "$HOME/.SuperSlicer" "$HOME/.config/SuperSlicer-alpha"
-fi
+import_settings()
+{
+	if [ -d "$1" ] && [ ! -d "$HOME/.config/SuperSlicer-alpha" ]
+	then
+		echo "import data from $1 to the new data folder ~/.config/SuperSlicer-alpha"
+		cp -r "$HOME/.SuperSlicer" "$HOME/.config/SuperSlicer-alpha"
+	fi
+}
+
+import_settings "$HOME/.config/SuperSlicer"
+import_settings "$HOME/.SuperSlicer"
 
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/wxgtk-dev/lib" /usr/share/SuperSlicer/superslicer "$@"
