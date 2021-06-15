41bcc4974886b53d1a938a0c82758ff3deff0b70
Alain Zscheile <zseri.devel@ytrizja.de>
2021-06-15 16:44:05 +0200

    add a flake.nix file to ease integration with NixOS
    suggested here: https://github.com/NixOS/nixpkgs/issues/117530#issuecomment-860937335
    
    this is easier than providing a package in nixpkgs and keeping that up-to-date
    and avoids duplicate book-keeping of certificate revisons.

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "dn42 root certificates for *.dn42 and 172.22.0.0/15";
+  outputs = { self }: {
+    nixosModule = { config, lib, ... }: {
+      options.security.pki.dn42.enable =
+        lib.mkEnableOption "dn42 root certificates";
+
+      security.pki.certificateFiles =
+        lib.mkIf config.options.security.pki.dn42.enable [ ./dn42.crt ];
+    };
+  };
+}
