e2a61df41de315dada97233d75ae404bc7b20aed
googol42 <>
2021-06-20 12:00:14 +0200

    add update script

---
+++ check-and-update-new-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+touch lastVersion
+lastVersion=`cat lastVersion`
+currentVersion=`curl https://www.mkgmap.org.uk/download/mkgmap.html --silent | grep "Download mkgmap release" | sed -rn 's/.*([[:digit:]][[:digit:]][[:digit:]][[:digit:]]).*/\1/p'`
+
+if [ "${lastVersion}" = "${currentVersion}" ]; then
+    exit 0
+fi
+
+sed -i "s/pkgver=.*/pkgver=${currentVersion}/g" PKGBUILD
+updpkgsums
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD .SRCINFO
+git commit -m "release version ${currentVersion}"
+git push
+
+echo $currentVersion > lastVersion
+
