0d99d504ce0b2c072e8e54e04a7f1d6e4cd726bd
kyope <gxb152@qq.com>
2021-07-08 15:52:04 +0800

    add -C option for black

--- black_pycharm_formatter.py
+++ black_pycharm_formatter.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     file_path = sys.argv[1]
     file_ext = file_path.split('.')[-1]
     if file_ext == 'py':
-        cmd = 'black -l 88 -S ' + file_path
+        cmd = 'black -l 88 -S -C' + file_path
     elif file_ext == 'ipynb':
-        cmd = 'jupytext ' + file_path + ' --pipe "black -l 88 -S {}"'
+        cmd = 'jupytext ' + file_path + ' --pipe "black -l 88 -S -C {}"'
     elif file_ext == 'sh':
         cmd = 'shfmt -w ' + file_path
     else:
--- black_pycharm_formatter.sh
+++ black_pycharm_formatter.sh
@@ -4,10 +4,10 @@ file_path=$1
 ext=${file_path##*.}
 case $ext in
 "py")
-	black -l 88 -S $file_path
+	black -l 88 -S -C $file_path
 	;;
 "ipynb")
-	jupytext $file_path --pipe "black -l 88 -S {}"
+	jupytext $file_path --pipe "black -l 88 -S -C {}"
 	;;
 "sh")
 	shfmt -w $file_path
