c69093b8ebe25d7a59d3a7913fde556321cfc4f6
Gregory Danielson <gregory.danielson3@gmail.com>
2021-07-09 12:14:11 -0500

    Slight correction to version number check

--- checkupdate.sh
+++ checkupdate.sh
@@ -3,7 +3,7 @@ readarray -t DOPPLERTEXT <<<"$(doppler | head -2 | sed -r 's/[[:cntrl:]]\[[0-9]{
 TO_CHECK="An update is available."
 
 if [ "${DOPPLERTEXT[0]}" == "$TO_CHECK" ]; then
-    NEWVERSION="$(echo "${DOPPLERTEXT[1]}" | grep -Eo '[0-9]\.[0-9]{2,3}\.[0-9]')"
+    NEWVERSION="$(echo "${DOPPLERTEXT[1]}" | grep -Eo '[0-9]\.[0-9]{1,2,3}\.[0-9]{1,2,3}')"
     if [[ $* == *--dry-run* ]]; then
         echo "New version available: $NEWVERSION"
     else
