14c34546916220a63ee3eebebcfa2d29217ed0d0
Gregory Danielson <gregory.danielson3@gmail.com>
2021-07-09 12:10:55 -0500

    Make update checking a little more convenient

--- Makefile
+++ Makefile
@@ -5,8 +5,12 @@ install:
 	makepkg -si
 
 update:
+	./checkupdate.sh
 	updpkgsums
 	makepkg --printsrcinfo > .SRCINFO
+
+check:
+	./checkupdate.sh --dry-run
 
 clean:
 	rm -rf doppler-cli-bin-*.pkg.tar.zst doppler-cli-bin.*.tar.gz pkg/ src/
---
+++ checkupdate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+readarray -t DOPPLERTEXT <<<"$(doppler | head -2 | sed -r 's/[[:cntrl:]]\[[0-9]{1,3}m//g')"
+TO_CHECK="An update is available."
+
+if [ "${DOPPLERTEXT[0]}" == "$TO_CHECK" ]; then
+    NEWVERSION="$(echo "${DOPPLERTEXT[1]}" | grep -Eo '[0-9]\.[0-9]{2,3}\.[0-9]')"
+    if [[ $* == *--dry-run* ]]; then
+        echo "New version available: $NEWVERSION"
+    else
+        sed -i "s/pkgver=.*/pkgver=$NEWVERSION/g" PKGBUILD
+        echo "Updated version to $NEWVERSION"
+    fi
+else
+    echo "No update available"
+fi
