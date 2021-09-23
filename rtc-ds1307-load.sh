379871b6c6be637b63f52d1d5691e5399cc1800c
Rajnish Mishra <contact.rajnishmishra@gmail.com>
2021-09-23 19:30:24 +0530

    minor fixes

--- rtc-ds1307-load.sh
+++ rtc-ds1307-load.sh
@@ -2,5 +2,8 @@
 #
 # Load i2c rtc ds1307 clock boot load script
 
-echo ds1307 0x68 >> /sys/class/i2c-adapter/i2c-2/new_device
-
+for i in $(i2cdetect -l | awk '{p=index($1, "-"); print substr($1, p+1)}'); do
+    if i2cdetect -y ${i} | grep -q -e "-- -- -- -- -- -- -- -- 68 -- -- -- -- -- -- --" ; then
+        echo ds1307 0x68 >> /sys/class/i2c-adapter/i2c-${i}/new_device
+    if
+done
