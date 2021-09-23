7fea08262b412a6253bf1e3bcc75ab32a52eebe2
Rajnish Mishra <contact.rajnishmishra@gmail.com>
2021-09-23 19:39:09 +0530

    minor fixes

--- rtc-ds1307-load.sh
+++ rtc-ds1307-load.sh
@@ -5,5 +5,5 @@
 for i in $(i2cdetect -l | awk '{p=index($1, "-"); print substr($1, p+1)}'); do
     if i2cdetect -y ${i} | grep -q -e "-- -- -- -- -- -- -- -- 68 -- -- -- -- -- -- --" ; then
         echo ds1307 0x68 >> /sys/class/i2c-adapter/i2c-${i}/new_device
-    if
+    fi
 done
