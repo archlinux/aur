9fc7b738f5890879a4e1611e96ba8cf285a5e4c8
SirNapkin1334 <sirnapkin@protonmail.com>
2021-09-23 17:32:22 -0700

    Update update script

--- update.sh
+++ update.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+# Update package to latest version
+
+# Grab latest version from API
+v=$(curl -sSL 'https://discordapp.com/api/v7/updates/development?platform=linux' | jq -r .name)
 
 # Update files
-perl -pi -e "s/pkgver=\\d+\\.\\d+\\.\\d+/pkgver=$1/" PKGBUILD
+perl -pi -e "s/pkgver=\\d+\\.\\d+\\.\\d+/pkgver=$v/" PKGBUILD
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
 
+# Build for me
+makepkg -si --noconfirm &
+
 # Push
 git add PKGBUILD .SRCINFO
-makepkg -si --noconfirm &
-git commit -m "Update to $1"
-git push
+git commit -m "Update to $v"
+git push &
 
+# Clean up
+rm -r pkg src "discord-development-$v*"
