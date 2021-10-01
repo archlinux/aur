211264aea33561f543c85515007d8adc9a17b137
Vincent PRELAT <vincent@prelat.fr>
2021-10-01 15:23:06 +0200

    add gtk3-1:3.24.30-2 lib from ALA to avoid symbol lookup error

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 pkgname=bricscad-fr_fr
 pkgver=21.2.06
-pkgrel=1
+pkgrel=2
 pkgdesc="Logiciel de CAO DWG"
 arch=("x86_64")
 url="https://www.bricsys.com"
@@ -12,14 +12,21 @@ _lang=fr_FR
 _os=2
 _version=7021
 
-
 # To download you must be logged in. The official website has no permanent link.
+DLAGENTS=("https::/usr/bin/python $PWD/dlagent.py %u %o $_os $_version")
+
 source=(
     dlagent.py
-    "BricsCAD-V${pkgver}-${pkgrel}-${_lang}.${arch}.rpm::${url}/BricsCAD-V${pkgver}-${pkgrel}-${_lang}.${arch}.rpm"
+    "BricsCAD-V${pkgver}-1-${_lang}.${arch}.rpm::${url}"
+    "gtk3-1:3.24.30-2-x86_64.pkg.tar.zst::https://archive.archlinux.org/packages/g/gtk3/gtk3-1%3A3.24.30-2-x86_64.pkg.tar.zst"
 )
 
-DLAGENTS=("https::/usr/bin/python $PWD/dlagent.py %u %o $_os $_version")
+noextract=('gtk3-1:3.24.30-2-x86_64.pkg.tar.zst')
+
+prepare() {
+    mkdir -p gtk3
+    tar xf './gtk3-1:3.24.30-2-x86_64.pkg.tar.zst' -C gtk3
+}
 
 package() {
     cp -dr --no-preserve=ownership ./usr "${pkgdir}"/
@@ -27,9 +34,11 @@ package() {
     cp -dr --no-preserve=ownership ./var "${pkgdir}"/
     # Allow creation of licence file for anyone
     chmod 757 "${pkgdir}"/var/bricsys
+    # patch for "symbol lookup error: /usr/lib/libtracker-sparql-3.0.so.0: undefined symbol: sqlite3_expanded_sql" error
+    cp -a --no-preserve=ownership gtk3/usr/lib/libgtk-3.so* "${pkgdir}"/opt/bricsys/bricscad/v21/
 }
 
-post_install(){
+post_install() {
     if [ "$1" = "configure" ] && [ -x "`which update-menus 2>/dev/null`" ]; then
         update-menus
     fi
@@ -50,7 +59,7 @@ post_install(){
         locale-gen ${_lang}
     fi
 }
-post_remove(){
+post_remove() {
     if [ -x "`which update-menus 2>/dev/null`" ]; then update-menus ; fi
 
     if [ -x "`which update-desktop-database 2>/dev/null`" ]; then update-desktop-database /usr/share/applications/ ; fi
@@ -59,7 +68,6 @@ post_remove(){
 
     if [ -x "`which gtk-update-icon-cache 2>/dev/null`" ] && [ -x /usr/share/icons/gnome/index.theme ]; then gtk-update-icon-cache --force /usr/share/icons/gnome ; fi
 }
-md5sums=(
-    '8252c41b4e228c18898e34c52e8e14a3'
-    '0b190e5a07ea1e6de7cad948e0305cb8'
-)
+md5sums=('6327d08422ca88ee89393e82727b2a6e'
+         '0b190e5a07ea1e6de7cad948e0305cb8'
+         'c1c55da4102090d790205b36ab213f94')
--- dlagent.py
+++ dlagent.py
@@ -12,50 +12,67 @@ target_file = sys.argv[2]
 os_number = sys.argv[3]
 version_number = sys.argv[4]
 
+if "www.bricsys.com" in file_url:
+    print("Please provide your Bricsys username/email along with your password to download the source file")
+    _username = input("Username or email: ")
+    _password = getpass.getpass()
 
-print("Please provide your Bricsys username/email along with your password to download the source file")
-_username = input("Username or email: ")
-_password = getpass.getpass()
-
-s = requests.session()
-r = s.get("https://www.bricsys.com/protected/download.do")
-soup = BeautifulSoup(r.text, 'html.parser')
-_url = soup.find_all("form", attrs={"id":"kc-form-login"})[0]["action"]
-
-
-headers = {'User-Agent': 'Mozilla/5.0'}
-payload = {'username':_username, 'password':_password}
-r = s.post(_url, headers=headers, data=payload)
-soup = BeautifulSoup(r.text, 'html.parser')
-try:
-    _csrf = soup.find_all("token")[0]["data-token"]
-except IndexError:
-    print("Wrong Bricsys credentials !")
-    exit(1)
+    s = requests.session()
+    r = s.get("https://www.bricsys.com/protected/download.do")
+    soup = BeautifulSoup(r.text, 'html.parser')
+    _url = soup.find_all("form", attrs={"id":"kc-form-login"})[0]["action"]
 
 
-r = s.get(f"https://www.bricsys.com/common/GetDownloadInstallsetData.json?i={version_number}&os={os_number}")
-rj = r.json()
-_du = rj['downloadURL']
-_i = rj['installsets'][0]['id']
-_if = rj['installsets'][0]['files'][0]['id']
-_su = rj['installsets'][0]['files'][0]['signedUrl']
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    payload = {'username':_username, 'password':_password}
+    r = s.post(_url, headers=headers, data=payload)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    try:
+        _csrf = soup.find_all("token")[0]["data-token"]
+    except IndexError:
+        print("Wrong Bricsys credentials !")
+        exit(1)
 
-headers = {'User-Agent': 'Mozilla/5.0', 'Referer': 'https://www.bricsys.com/protected/download.do'}
-payload = {'_csrf':_csrf, 'i':_i, 'if':_if, 'accept': 'true', 'signedUrl': _su}
 
-with open(target_file, "wb") as f:
-    r = s.post("https://www.bricsys.com/protected/download.do", headers=headers, data=payload, stream=True)
-    total_length = r.headers.get('content-length')
+    r = s.get(f"https://www.bricsys.com/common/GetDownloadInstallsetData.json?i={version_number}&os={os_number}")
+    rj = r.json()
+    _du = rj['downloadURL']
+    _i = rj['installsets'][0]['id']
+    _if = rj['installsets'][0]['files'][0]['id']
+    _su = rj['installsets'][0]['files'][0]['signedUrl']
 
-    if total_length is None: # no content length header
-        f.write(response.content)
-    else:
-        dl = 0
-        total_length = int(total_length)
-        for data in r.iter_content(chunk_size=4096):
-            dl += len(data)
-            f.write(data)
-            done = int(50 * dl / total_length)
-            sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {int(dl / total_length * 100)} %")
-            sys.stdout.flush()
+    headers = {'User-Agent': 'Mozilla/5.0', 'Referer': 'https://www.bricsys.com/protected/download.do'}
+    payload = {'_csrf':_csrf, 'i':_i, 'if':_if, 'accept': 'true', 'signedUrl': _su}
+
+    with open(target_file, "wb") as f:
+        r = s.post("https://www.bricsys.com/protected/download.do", headers=headers, data=payload, stream=True)
+        total_length = r.headers.get('content-length')
+
+        if total_length is None: # no content length header
+            f.write(r.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in r.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {int(dl / total_length * 100)} %")
+                sys.stdout.flush()
+
+else:
+    with open(target_file, "wb") as f:
+        r = requests.get(file_url, stream=True)
+        total_length = r.headers.get('content-length')
+
+        if total_length is None: # no content length header
+            f.write(r.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in r.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write(f"\r[{'=' * done}{' ' * (50-done)}] {int(dl / total_length * 100)} %")
+                sys.stdout.flush()
