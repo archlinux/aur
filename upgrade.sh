895769f119852458fb4fac61ae2841f16914caa3
dreamscached <dreamscache.d@gmail.com>
2021-10-10 12:55:34 +0200

    Add upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -lt 1 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+    echo "Usage: $0 [-h|--help] version" >&2
+    exit 1
+fi
+
+. ./PKGBUILD
+sed -i "s/$pkgver/$1/" PKGBUILD
+. ./PKGBUILD
+
+tmp="$(mktemp)"
+cleanup() { rm -f "$tmp"; }
+trap cleanup EXIT
+
+curl -sL "${source[0]}" -o "$tmp"
+sum="$(sha256sum "$tmp" | cut -c -64)"
+
+cleanup
+unset cleanup
+trap - EXIT
+
+sed -i -e "s/${sha256sums[0]}/$sum/g" PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
