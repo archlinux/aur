e4997d7e61df3d22a713cf1bdafe4b89472d6db6
Lars Grüter <lagru@mailbox.org>
2021-10-16 11:32:36 +0200

    Enable via timer in post_install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,10 +1,5 @@
 # Pre- and post-install scriptlet to be packaged.
 
 post_install() {
-    echo -ne "\e[34m"
-    echo "Note: For periodic execution of pacautomation enable the included "
-    echo -ne "      timer with: "
-    echo -ne "\e[1m" # Print the actual command in bold
-    echo "systemctl enable --now pacautomation.timer"
-    echo -ne "\e[0m"
+    systemctl enable --now pacautomation.timer
 }
