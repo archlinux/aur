9c03ded1fbf0723a5ba1f3125cd472c395060d46
Lars Grüter <lagru@mailbox.org>
2021-10-16 11:23:00 +0200

    Color installation notice in blue
    This should make the notive a bit more visible upon installation.

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,6 +1,10 @@
 # Pre- and post-install scriptlet to be packaged.
 
 post_install() {
-    echo "For periodic execution of pacautomation enable the included systemd "
-    echo "timer: systemctl enable --now pacautomation.timer"
+    echo -ne "\e[34m"
+    echo "Note: For periodic execution of pacautomation enable the included "
+    echo -ne "      timer with: "
+    echo -ne "\e[1m" # Print the actual command in bold
+    echo "systemctl enable --now pacautomation.timer"
+    echo -ne "\e[0m"
 }
