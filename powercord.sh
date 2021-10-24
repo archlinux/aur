3d8e7c46bf6bd27750a7349ec4fba2c89290b5d7
Łukasz Mariański <lmarianski@protonmail.com>
2021-10-24 23:35:01 +0200

    automatically link powercord modules to user dir

--- powercord.sh
+++ powercord.sh
@@ -3,4 +3,9 @@
 XDG_DATA_HOME=${XDG_DATA_HOME:=$HOME/.local/share}
 mkdir -p $XDG_DATA_HOME/powercord/{plugins,themes}
 
+for d in /usr/share/powercord/src/Powercord/plugins/*/
+do
+	ln -s $d $XDG_DATA_HOME/powercord/plugins/ &>/dev/null
+done
+
 exec electron13 /usr/share/powercord/app $@
