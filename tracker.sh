2ae5df575a6c8b1ddb39c8ff05b2a3710d84a574
Filippo Falezza <filippo.falezza@outlook.it>
2021-10-28 16:14:51 +0100

    fixed java path

--- tracker.sh
+++ tracker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TRACKER_HOME=/opt/tracker
-$TRACKER_HOME/jre/bin/java -jar $TRACKER_HOME/tracker_starter.jar $*
+/usr/lib/jvm/default-runtime/bin/java -jar $TRACKER_HOME/tracker_starter.jar $*
