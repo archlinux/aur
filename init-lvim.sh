9c3fceb170facd1cc70425b4d91bf79f230572f6
Direct-A <songyicheng0@gmail.com>
2021-11-04 20:14:16 +0800

    maintainer changed; common updata

--- init-lvim.sh
+++ init-lvim.sh
@@ -3,19 +3,35 @@ mkdir -p ~/.config/lvim
 ln -s /usr/share/doc/lunarvim/config.example-no-ts.lua ~/.config/lvim/config.lua
 mkdir -p ~/.local/share/lunarvim
 ln -s /usr/share/lunarvim ~/.local/share/lunarvim/lvim
+
+echo "Installing dependencies of NodeJS & Rust..."
+npm install -g neovim
+npm install -g tree-sitter-cli
+yarn global add neovim
+yarn global add tree-sitter-cli
+cargo install fd-find
+cargo install ripgrep
+
 echo "Installing Packer..."
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/lunarvim/site/pack/packer/start/packer.nvim
+
 echo "PackerInstall..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerInstall
+
 echo "PackerSync..."
 lvim --headless +'autocmd User PackerComplete sleep 100m | qall' +PackerSync
 rm ~/.config/lvim/config.lua
 cp /usr/share/doc/lunarvim/config.example.lua ~/.config/lvim/config.lua
+
 echo "Installing treesitter parsers.."
 ln -s /usr/share/lunarvim/prebuild/nvim-treesitter/parser/* \
-	~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser/
+  ~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser/
 ln -s /usr/share/lunarvim/prebuild/nvim-treesitter/parser-info/* \
-	~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser-info/
+  ~/.local/share/lunarvim/site/pack/packer/start/nvim-treesitter/parser-info/
+
+echo "Generate the new ftplugin template files.."
+lvim --headless +LvimUpdate +q
+
 echo
 echo "lunarvim runtime is inited for $(whoami)"
 echo "clean up by:"
