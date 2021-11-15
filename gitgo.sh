f56cd4e36125a937103437d820b284db76c22401
Mark Wagie <mark.wagie@tutanota.com>
2021-11-14 21:29:21 -0700

    remove unnecessary file

--- gitgo.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-if [ -z "$1" ];
-  then echo "Please supply a commit message wrapped in single quotes '' before you proceed!";
-  else
-    makepkg --printsrcinfo > .SRCINFO
-    git add .
-    git commit -am "$1"
-    git push;
-fi
