5c51ecc64c1a8dce5eb72ef754cf23f476fb6ecc
Knut Ahlers <knut@ahlers.me>
2021-12-02 01:57:55 +0100

    Update catalog api url
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- update_version.sh
+++ update_version.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 PKG=$(awk -F '=' '/pkgname=/{ print $2 }' PKGBUILD)
 
 # Get latest version
-VER=$(curl -sSfL "https://lv.luzifer.io/catalog-api/envrun/latest.txt?p=version")
+VER=$(curl -sSfL "https://lv.luzifer.io/v1/catalog/envrun/latest/version")
 
 # Insert latest version into PKGBUILD
 sed -i \
