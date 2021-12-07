bee5fb438e2c653b48414eb24ffc1bd19636163c
sylvaindurand <arch@durand.tf>
2021-12-07 14:32:23 +0100

    Updating to 1.10.2 (fixing sums)

--- .update.sh
+++ .update.sh
@@ -1,13 +1,25 @@
+pkgname=uptime-kuma
+url="https://github.com/louislam/${pkgname}"
+
 # Getting last version
 pkgver=`curl --silent \
   "https://api.github.com/repos/louislam/uptime-kuma/releases/latest" \
   | grep -Po '"tag_name": "\K.*?(?=")'`
 
-# Modifying PKGBUILD with the last version
+# Getting sha256sums
+sum1=($(curl -sL "${url}/archive/refs/tags/${pkgver}.tar.gz" | sha256sum))
+sum2=($(curl -sL "${url}/releases/download/${pkgver}/dist.tar.gz" | sha256sum))
+sum3=($(sha256sum uptime-kuma.service))
+sums="('$sum1' '$sum2' '$sum3')"
+
+
+# Modifying PKGBUILD with the last version and sums
 sed -i -e "/pkgver=/ s/=.*/=$pkgver/" PKGBUILD
+sed -i -e "/sha256sums=/ s/=.*/=$sums/" PKGBUILD
 
 # .SRCINFO generation
 makepkg --printsrcinfo > .SRCINFO
 
 # Git commit
+git diff
 git commit -am "Updating to $pkgver"
--- PKGBUILD
+++ PKGBUILD
@@ -2,7 +2,7 @@
 
 pkgname=uptime-kuma
 pkgver=1.10.2
-pkgrel=1
+pkgrel=2
 pkgdesc="A free and open source uptime monitoring solution"
 
 arch=('any')
@@ -16,6 +16,8 @@ source=(
   "${pkgname}-dist-${pkgver}.tar.gz::${url}/releases/download/${pkgver}/dist.tar.gz"
   "uptime-kuma.service"
 )
+
+sha256sums=('30901f70cf6e886c5d1bb544dd6f6b19ecb5311f7d13cbcb896db09a11320c69' '7c8aad01a94fef72192b8ed7993d3586ab9386822e6fb51d313f3eb279022e7e' '84ec5db5532ee2e218bc995ef1c04b7a4b9ebe4df198bba99fd031d6b9073796')
 
 package() {
   npm install -g --user root --prefix "$pkgdir"/usr "$srcdir"/$pkgname-$pkgver.tar.gz
