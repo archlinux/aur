a2d9fa109af4c04bc662590aa3652ae1d2d98c17
Ren Tatsumoto <tatsu@autistici.org>
2022-01-19 01:22:20 +0300

    update launcher

--- element-web.sh
+++ element-web.sh
@@ -5,20 +5,28 @@ pacman -Qq darkhttpd >/dev/null || { echo "Install darkhttpd to use this feature
 readonly addr=127.0.0.1 port=8000
 
 serve() {
-	darkhttpd /usr/share/webapps/element --port "$port" --addr "$addr" --log /dev/null --daemon
+	darkhttpd /usr/share/webapps/element --port "$port" --addr "$addr" --daemon --log /dev/null
 }
 
 stop() {
 	pkill --signal SIGINT -f 'webapps/element'
 }
 
-if [[ $1 == serve ]]; then
-	serve && xdg-open "http://$addr:$port/"
-elif [[ $1 == stop ]]; then
-	stop
-elif [[ $1 == restart ]]; then
+restart() {
 	stop
 	serve
+}
+
+status() {
+	if pgrep -f 'webapps/element'; then
+		echo "Running."
+	else
+		echo "Stopped."
+	fi
+}
+
+if declare -f -- "$1" >/dev/null; then
+	"$@"
 else
 	echo "USAGE: $(basename -- "$0") serve|stop|restart"
 fi
