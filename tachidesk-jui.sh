8e647d42c151e5488c53ef9a3c3a33d3f7bdd912
Mahor Foruzesh <mahorforuzesh@pm.me>
2022-01-25 13:34:15 +0330

    Update tachidesk-jui.sh

--- tachidesk-jui.sh
+++ tachidesk-jui.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-java=$(archlinux-java status | awk -F '-' 'NR>1 { gsub(/ /,""); gsub("\\(default)",""); if($2 >= 15) print }' | head -n1)
+java=$(archlinux-java status | awk -F '-' 'NR>1 { gsub(/ /,""); gsub("\\(default)",""); if($2 >= 17) print }' | head -n1)
 
 if [ "$java" = "" ]; then
-	echo "no java >= 15 installation found! please intall one."
+	echo "no java >= 17 installation found! please intall one."
 	exit 1
 fi
 
