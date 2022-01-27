c27676f3b39d38d86bf992ed5395d78ee5f0f9a5
Thomas Eizinger <thomas@eizinger.io>
2022-01-27 20:13:56 +1100

    Add auto-update script for photoprism

---
+++ check_for_update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+source ./PKGBUILD
+
+# Use GitHub CLI to get latest release tag
+LATEST_RELEASE=$(gh release -R $url view --json tagName --jq '.tagName')
+
+echo "Latest release is $LATEST_RELEASE";
+echo "Current version in AUR is $pkgver-$commit";
+
+readarray -d '-' -t DATE_AND_HASH<<<"$LATEST_RELEASE"
+
+RELEASE_DATE=$(python -c 'print("'"$LATEST_RELEASE"'".split("-")[0])')
+RELEASE_HASH=$(python -c 'print("'"$LATEST_RELEASE"'".split("-")[1])')
+
+if [ $RELEASE_DATE -eq $pkgver ]; then
+	echo "$LATEST_RELEASE is the latest version"
+	exit 0;
+fi
+
+echo $RELEASE_DATE;
+echo $RELEASE_HASH;
+
+
+sed -i 's/'${pkgver}'/'"$RELEASE_DATE"'/g' ./PKGBUILD
+sed -i 's/'${commit}'/'"$RELEASE_HASH"'/g' ./PKGBUILD
+
+git config --local core.name "Update Bot"
+git config --local core.email "thomas@eizinger.io"
+
+git commit -a
+
+gh pr create --title "Upgrade photoprism to $LATEST_RELEASE"
