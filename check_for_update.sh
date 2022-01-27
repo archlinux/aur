14f76210532bd0aa58570ed00ce201e8a4b5f69c
Thomas Eizinger <thomas@eizinger.io>
2022-01-27 20:23:36 +1100

    Always force-push in case we run the action again

--- check_for_update.sh
+++ check_for_update.sh
@@ -32,6 +32,6 @@ git config --local user.email "thomas@eizinger.io"
 
 git checkout -b "bump-photoprism-$LATEST_RELEASE"
 git commit -a -m "Upgrade photoprism to $LATEST_RELEASE"
-git push origin "bump-photoprism-$LATEST_RELEASE"
+git push -f origin "bump-photoprism-$LATEST_RELEASE"
 
 gh pr create --title "Upgrade photoprism to $LATEST_RELEASE"
