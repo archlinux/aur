70e7bbd0251a1988fd8425d6bbaf344db0355296
mynacol <paul@dismail.de>
2022-01-28 13:22:36 +0100

    Use better-suited case instead of if/else

--- dark-theme.sh
+++ dark-theme.sh
@@ -5,29 +5,34 @@ set -eu
 
 CURRENT=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
-if [ "$1" = "dark" ]; then
-  for f in "$HOME/.local/share/dark-mode.d/"*; do
-    "$f" &
-  done
-elif [ "$1" = "light" ]; then
-  for f in "$HOME/.local/share/light-mode.d/"*; do
-    "$f" &
-  done
-elif [ "$1" = "toggle" ]; then
-  if echo "$CURRENT" | grep -q "dark"; then
-    exec $0 light
-  else
-    exec $0 dark
-  fi
-elif [ "$1" = "reapply" ]; then
-  if echo "$CURRENT" | grep -q "dark"; then
-    exec $0 dark
-  else
-    exec $0 light
-  fi
-else
-  echo "Command $1 not valid."
-  exit 1
-fi
+case $1 in
+    dark)
+      for f in "$HOME/.local/share/dark-mode.d/"*; do
+        "$f" &
+      done
+    ;;
+    light)
+      for f in "$HOME/.local/share/light-mode.d/"*; do
+        "$f" &
+      done
+    ;;
+    toggle)
+      if echo "$CURRENT" | grep -q "dark"; then
+        exec $0 light
+      else
+        exec $0 dark
+      fi
+    ;;
+    reapply)
+      if echo "$CURRENT" | grep -q "dark"; then
+        exec $0 dark
+      else
+        exec $0 light
+      fi
+    ;;
+    *)
+      echo "Command $1 not valid."
+      exit 1
+esac
 
 wait
