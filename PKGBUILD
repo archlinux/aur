a4aed66ab3c0eab496143f10de39c02515fc3430
dplusplus <dplusplus>
2022-01-29 21:38:00 -0800

    Major redesign: allow multiple instances, use configuration file, re-arrange folders
    * A systemd template instance unit file is included, allowing multiple instances of TShock to run on the same machine
    * Create a unprivileged system user named "terraria" for running TShock
    * Folder locations are now more conventional for a server daemon
    * Some configuration values for the default systemd instace (tshock@default.server) are stored at '/etc/conf.d/tshock/default.conf'

--- PKGBUILD
+++ PKGBUILD
@@ -1,34 +1,52 @@
-# Maintainer: Sefa Eyeoglu <contact@scrumplex.net>
+# Maintainer: dplusplus <echo cmFuZG9tdGVzdG5hbWUtYXQtZ21haWwoZG90KWNvbQo= | base64 -d>
+# Contributor: Sefa Eyeoglu <contact@scrumplex.net>
 # Contributor: Philip Abernethy <chais.z3r0@gmail.com>
 
 _pkgname=tshock
 pkgname=${_pkgname}-bin
-pkgver=4.5.9
+pkgver=4.5.12
 # Because of tag 4.2200 is 4.2.2.1228 and tag 4.2202 is 4.2.2.0224 epoch must be 1
 epoch=1
-pkgrel=3
+pkgrel=1
 pkgdesc="A Terraria Server administration modification that runs on the open source TerrariaAPI."
-arch=(any)
-url="http://tshock.co/xf"
+arch=("x86_64" "aarch64")
+url="https://github.com/Pryaxis/TShock"
 license=("GPL3")
-depends=("mono")
+depends=("mono" "tmux")
 provides=($_pkgname)
 conflicts=($_pkgname)
-#install="${pkgname}.install"
-source=("${_pkgname}_${pkgver}.zip::https://github.com/Pryaxis/TShock/releases/download/v4.5.9/TShock4.5.9_Terraria.1.4.3.2.zip"
-		'tshock-bin.install'
-        'tshock.sh')
+source=(
+	"${_pkgname}_${pkgver}.zip::https://github.com/Pryaxis/TShock/releases/download/v${pkgver}/TShock${pkgver}_Terraria1.4.3.2.zip"
+	"${_pkgname}.sh"
+	"default.conf"
+	"${_pkgname}@.service"
+	"${_pkgname}.sysusers"
+	"${_pkgname}.tmpfiles"
+	"${_pkgname}.install"
+)
 noextract=("${_pkgname}_${pkgver}.zip")
-sha512sums=('495a40ff1b27436d39e983c57987a22bf747aff7d7561b24dce675d855a1306e8204cfb0f68547a665ef157995e71b93260617f07e383ae444e69b2027fcde02'
-			'SKIP'
-            'SKIP')
+sha512sums=('8257cbe6cdd2e66d0388e866e54f0c9935da05f720c57ea43e3176295581bf41c0a0b3b5b6f94a2667d7e8872225b8d9a091225182a2d8f849d3004a45040b08'
+	'SKIP'
+	'SKIP'
+	'SKIP'
+	'SKIP'
+	'SKIP'
+	'SKIP'
+)
 
-#backup=("etc/conf.d/${_pkgname}/default.conf")
+backup=("etc/conf.d/${_pkgname}/default.conf")
 
 package() {
-    install -Dm755 "${srcdir}/tshock.sh" "${pkgdir}/usr/bin/tshock"
+    install -Dm755 "${_pkgname}.sh" "${pkgdir}/usr/bin/${_pkgname}"
+
+	install -Dm644 "default.conf" "${pkgdir}/etc/conf.d/${_pkgname}/default.conf"
 
     # Unzip server files
-    install -d "${pkgdir}/usr/lib/tshock"
-    bsdtar -x -f "${srcdir}/${_pkgname}_${pkgver}.zip" -C "${pkgdir}/usr/lib/tshock"
+    install -d "${pkgdir}/srv/${_pkgname}"
+    bsdtar -x -f "${_pkgname}_${pkgver}.zip" -C "${pkgdir}/srv/${_pkgname}"
+
+
+    install -Dm644 "${_pkgname}@.service" "${pkgdir}/usr/lib/systemd/system/${_pkgname}@.service"
+    install -Dm644 "${_pkgname}.sysusers" "${pkgdir}/usr/lib/sysusers.d/${_pkgname}.conf"
+    install -Dm644 "${_pkgname}.tmpfiles" "${pkgdir}/usr/lib/tmpfiles.d/${_pkgname}.conf"
 }
--- tshock.sh
+++ tshock.sh
@@ -1,1 +1,100 @@
-mono /usr/lib/tshock/TerrariaServer.exe -logpath "$HOME/.config/tshock" -configpath "$HOME/.config/tshock" "$@"
+#!/bin/bash
+
+set -e
+
+CONFDIR='/etc/conf.d/tshock'
+
+if [ -n "$2" ]; then
+    INSTANCE=$2
+else
+    INSTANCE=default
+fi
+
+if [ -r ${CONFDIR}/${INSTANCE}.conf ]; then
+    source ${CONFDIR}/${INSTANCE}.conf
+else
+    echo "TShock could not be started because ${CONFDIR}/${INSTANCE}.conf could not be read."
+    exit 1
+fi
+
+TMUX_CONSOLE=tshock-console-${INSTANCE}
+
+## The following parameters can be added to TShock to alter the way a server initializes.
+## Options set via the command line will override all configuration options regardless.
+## These can be used either for personal use or in a GSP environment for easier hosting without hassle:
+## Terraria Server API Command Line:
+##   -ip <ipv4>          - Starts the server bound to a given IPv4 address
+##   -port <port>        - Starts the server bound to a given port
+##   -maxplayers <count> - Starts the server with a given player count
+##   -world <file.wld>   - Starts the server and immediately loads a given
+##                         world file
+##   -worldpath <path>   - Starts the server and changes the world path to a
+##                         given path
+##   -autocreate <1/2/3> - Starts the server and, if a world file isn't found,
+##                         automatically create the world file with a given
+##                         size, 1-3, 1 being small.
+##   -config <file>      - Starts the server with a given config file
+##   -connperip <n>      - Allows n number of connections per IP.
+##   -killinactivesocket - Kills connections which have not started the
+##                         protocol handshake.
+##   -lang <type>        - Sets the server's language.
+##   -ignoreversion      - Ignores API version checks for plugins allowing for
+##                         old plugins to run.
+##   -forceupdate        - Forces the server to continue running, and not
+##                         hibernating when no players are on. This results in
+##                         time passing, grass growing, and cpu running.
+## TShock Command Line:
+##   -configpath <path>     - The path tshock uses to resolve configs, log
+##                            files, and sqlite db.
+##   -worldpath <path>      - The path that Terraria Server uses to find all
+##                            world files.
+##   -logpath <path>        - Overrides the default log path and saves logs
+##                            here.
+##   -logformat <format>    - Format the name of log files, subject to C# date
+##                            standard abbreviations,
+##   -logclear <true/false> - Overwrites old config if it exists.
+##   -dump                  - Dumps permissions and config file descriptions for
+##                            wiki use.
+
+case "$1" in
+    start)
+        if tmux has-session -t ${TMUX_CONSOLE} &> /dev/null ; then
+            echo "TShock instance '$INSTANCE' is already running"
+            exit 1
+        else
+            echo "starting '${INSTANCE}'"
+            tmux new-session -d -s "${TMUX_CONSOLE}"
+            mono --server --gc=sgen -O=all /srv/tshock/TerrariaServer.exe -port ${PORT} -logpath /var/log/tshock/${INSTANCE} -worldpath ${WORLDDIR} -world ${WORLDDIR}/${WORLD}.wld -autocreate ${SIZE}
+            if [ $? -gt 0 ]; then
+                echo "Could not start instance '${INSTANCE}'"
+                exit 1
+            fi
+        fi
+        ;;
+
+    stop)
+        if tmux has-session -t ${TMUX_CONSOLE} &> /dev/null ; then
+            tmux send-keys -t ${TMUX_CONSOLE} 'broadcast NOTICE: Server shutting down in 5 seconds!' C-m
+            sleep 5
+            tmux send-keys -t ${TMUX_CONSOLE} 'exit' C-m
+            sleep 10
+        else
+            echo "Unable to stop '${INSTANCE}'; this TShock instance is not running"
+            exit 1
+        fi
+        ;;
+
+    console)
+        if tmux has-session -t ${TMUX_CONSOLE} &> /dev/null ; then
+            tmux attach -t ${TMUX_CONSOLE}
+        else
+            echo "Unable to get console for '${INSTANCE}'; this TShock instance is not running"
+            exit 1
+        fi
+        ;;
+
+    *)
+        echo "usage: $0 {start|stop|console} [instance]"
+esac
+
+exit 0
