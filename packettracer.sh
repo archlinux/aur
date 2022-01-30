6157bede08822c700451dd6cde7650f2a01debd8
runnytu <runnytu@gmail.com>
2022-01-30 20:51:33 +0100

    Update to 8.1.1

--- packettracer.sh
+++ packettracer.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 PT8HOME=/opt/packettracer/
 export PT8HOME
 
-function version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
+version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
-QT_VERSION=`qmake -v | grep "Using Qt version" | sed -r 's/.* ([0-9]+\.*[0-9]+\.*[0-9]*).*?/\1/'`
+QT_VERSION=$(qmake -v | grep "Using Qt version" | sed -r 's/.* ([0-9]+\.*[0-9]+\.*[0-9]*).*?/\1/')
 QT_VERSION_CHECK="5.5.0"
 
 if version_gt $QT_VERSION $QT_VERSION_CHECK;then
