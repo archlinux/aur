71987ba6b55fcf535bdefac9be0e920dfa24bdf8
Christopher Luna <dev@christopher-luna.com>
2022-02-11 19:02:00 -0600

    Updated to 1.26.2. Removed cemuhook

--- cemu.sh
+++ cemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WINEPREFIX="$HOME/.cemu/wine"
-export WINEDLLOVERRIDES="mscoree=;mshtml=;cemuhook.dll=n,b"
+export WINEDLLOVERRIDES="mscoree=;mshtml="
 
 # Allows to play BotW with Polaris video cards
 export R600_DEBUG="nohyperz"
@@ -8,7 +8,6 @@ export R600_DEBUG="nohyperz"
 if [ ! -d "$HOME"/.cemu ] ; then
   mkdir -p "$HOME"/.cemu/wine || exit 1
   ln -s /usr/share/cemu/Cemu.exe "$HOME"/.cemu/Cemu.exe || exit 1
-  ln -s /usr/share/cemu/cemuhook.dll "$HOME"/.cemu/cemuhook.dll || exit 1
   ln -s /usr/share/cemu/keystone.dll "$HOME"/.cemu/keystone.dll || exit 1
   ln -s /usr/share/cemu/sharedFonts "$HOME"/.cemu/sharedFonts || exit 1
   cp -r /usr/share/cemu/gameProfiles "$HOME"/.cemu/ || exit 1
@@ -18,7 +17,10 @@ fi
 
 if [ -d "$HOME"/.cemu ] && [ -L "$HOME"/.cemu/dbghelp.dll ] ; then
   rm "$HOME"/.cemu/dbghelp.dll
-  ln -s /usr/share/cemu/cemuhook.dll "$HOME"/.cemu/cemuhook.dll || exit 1
+fi
+
+if [ -d "$HOME"/.cemu ] && [ -L "$HOME"/.cemu/cemuhook.dll ] ; then
+  rm "$HOME"/.cemu/cemuhook.dll
 fi
 
 if [ ! -L "$HOME"/.cemu/sharedFonts ] && [ ! -d "$HOME"/.cemu/sharedFonts ] ; then
