10214c0c9e1118713138a651d438c94b27440c22
panda2134 <ljypanda@live.com>
2022-02-18 14:45:24 +0800

    build utils

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $1 <version>"; exit 1
+fi
+
+sed -i 's/^pkgver=.*$/pkgver='"$1"'/' PKGBUILD
+git commit -m "Updated to v$1"
+git tag "v$1"
+git push --all
+updpkgsums
