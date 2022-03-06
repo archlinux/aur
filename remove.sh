6064cae1c70a3a86c1ad10151c6e1a9ff15c0d60
coxackie <kostas.kardaras@gmail.com>
2022-03-06 23:45:49 +0000

    Update for Spotify 1.1.80.699

--- remove.sh
+++ remove.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /opt/spotify/Apps
 cp xpui.spa xpui.spa.bak  # create the backup
-unzip -p xpui.spa xpui.js | sed 's/{adsEnabled:\!0}/{adsEnabled:false}/' | sed 's/D&&h>=1024&&r().createElement(Pm,{onClick:T,className:Rm.Z.UpgradeButton}),//' > xpui.js
+unzip -p xpui.spa xpui.js | sed 's/{adsEnabled:\!0}/{adsEnabled:false}/' | sed 's/B&&f>=1024&&u().createElement(bF,{spec:I,onClick:P,className:kL.Z.UpgradeButton}),//' > xpui.js
 zip --update xpui.spa xpui.js
 rm xpui.js
 
