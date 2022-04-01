6f5283f9c4d7c9830fbaa9d9b1c93261c170de43
Sainnhe Park <sainnhe@gmail.com>
2022-04-01 12:00:32 +0800

    Add new json keys
    - Add `linkProtectionTrustedDomains`.
    - Add `resourceUrlTemplate`.
    - Add `controlUrl`.

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Sainnhe Park <sainnhe@gmail.com>
 pkgname=code-marketplace
-pkgver=1.54.3
-pkgrel=4
+pkgver=1.66.0
+pkgrel=1
 pkgdesc='Enable vscode marketplace in Code OSS'
 arch=('any')
 url='https://marketplace.visualstudio.com/vscode'
@@ -12,7 +12,7 @@ install="${pkgname}.install"
 source=('code-marketplace.hook'
         'patch.sh')
 md5sums=('2f5ebcc1795bb5eac95162059a9a496e'
-         '2b80e3904769499f4f5bed420a254846')
+         '9bd7d0737d86c05900ee32f6821e681a')
 
 package() {
   install -Dm 644 "${srcdir}"/code-marketplace.hook "${pkgdir}"/usr/share/libalpm/hooks/code-marketplace.hook
--- patch.sh
+++ patch.sh
@@ -4,14 +4,18 @@ if [ "${1}" = "-R" ]; then
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/\t\t"serviceUrl": "https:\/\/open-vsx.org\/vscode\/gallery",/' \
     -e '/^[[:blank:]]*"cacheUrl/d' \
     -e 's/^[[:blank:]]*"itemUrl":.*/\t\t"itemUrl": "https:\/\/open-vsx.org\/vscode\/item"/' \
+    -e '/^[[:blank:]]*"resourceUrlTemplate/d' \
+    -e '/^[[:blank:]]*"controlUrl/d' \
     -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
-    -e '/^[[:blank:]]*"documentationUrl/i\\t"linkProtectionTrustedDomains": ["https://open-vsx.org"],' \
+    -e '/^[[:blank:]]*"documentationUrl/i\\t"linkProtectionTrustedDomains": ["https:\/\/open-vsx.org"],' \
     /usr/lib/code/product.json
 else
   sed -i -e 's/^[[:blank:]]*"serviceUrl":.*/\t\t"serviceUrl": "https:\/\/marketplace.visualstudio.com\/_apis\/public\/gallery",/' \
     -e '/^[[:blank:]]*"cacheUrl/d' \
     -e '/^[[:blank:]]*"serviceUrl/a\\t\t"cacheUrl": "https:\/\/vscode.blob.core.windows.net\/gallery\/index",' \
-    -e 's/^[[:blank:]]*"itemUrl":.*/\t\t"itemUrl": "https:\/\/marketplace.visualstudio.com\/items"/' \
+    -e 's/^[[:blank:]]*"itemUrl":.*/\t\t"itemUrl": "https:\/\/marketplace.visualstudio.com\/items",/' \
+    -e '/^[[:blank:]]*"itemUrl/a\\t\t"resourceUrlTemplate": "https:\/\/{publisher}.vscode-unpkg.net\/{publisher}\/{name}\/{version}\/{path}",\n\t\t"controlUrl": "https:\/\/az764295.vo.msecnd.net\/extensions\/marketplace.json"' \
     -e '/^[[:blank:]]*"linkProtectionTrustedDomains/d' \
+    -e '/^[[:blank:]]*"documentationUrl/i\\t"linkProtectionTrustedDomains": ["https:\/\/*.visualstudio.com", "https:\/\/*.microsoft.com", "https:\/\/aka.ms", "https:\/\/vscode-auth.github.com", "https:\/\/client-auth-staging-14a768b.herokuapp.com", "https:\/\/*.gallerycdn.vsassets.io", "https:\/\/github.com\/microsoft\/", "https:\/\/github.com\/MicrosoftDocs\/", "https:\/\/login.microsoftonline.com"],' \
     /usr/lib/code/product.json
 fi
