e3140ced6a5bf8d675bc0778c25ac8cad00d47c2
Hetsh <git@hetsh.de>
2022-04-06 09:14:25 +0200

    Import from @peterfab9845

--- prepare_update.sh
+++ prepare_update.sh
@@ -17,22 +17,11 @@ source "$BUILD_FILE"
 if [ "$1" == "--aur-only" ]
 then
 	echo "Only updating \$pkgrel!"
+	sed -i "s|pkgrel\=.*|pkgrel\=$((pkgrel+1))|" "$BUILD_FILE"
 else
-	# Replace with latest commit to master branch
-	LATEST_COMMIT=$(git ls-remote "$url" master | cut -f 1)
-	if [ "$commit" = "$LATEST_COMMIT" ]
-	then
-		echo "Already on latest commit!"
-		exit 3
-	fi
-	sed -i "s|commit\=.*|commit\=('$LATEST_COMMIT')|" "$BUILD_FILE"
-
-	# Update package checksum
-	PKG_SHA=$(curl -L -s "$url/archive/$LATEST_COMMIT.tar.gz" | sha256sum | cut -d " " -f 1)
-	sed -i "s|sha256sums\=.*|sha256sums\=('$PKG_SHA')|" "$BUILD_FILE"
+	sed -i "s|pkgrel\=.*|pkgrel\=1|" "$BUILD_FILE"
+	# Have makepkg update the version using pkgver()
+	makepkg -o
 fi
-
-# Increment pkgrel on every "release"
-sed -i "s|pkgrel\=.*|pkgrel\=$((pkgrel+1))|" "$BUILD_FILE"
 
 makepkg --printsrcinfo > .SRCINFO
