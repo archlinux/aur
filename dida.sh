4d79ead48847ad69994e66a4845eaaa47339a4f1
Sukanka <su975853527@gmail.com>
2022-05-10 21:24:54 +0800

    add dida-flags.conf support

--- dida.sh
+++ dida.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/sh
-electron /usr/share/dida/dida.asar "$@"
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+
+# Allow users to override command-line options
+if [[ -f $XDG_CONFIG_HOME/dida-flags.conf ]]; then
+   DIDA_USER_FLAGS="$(sed 's/#.*//' $XDG_CONFIG_HOME/dida-flags.conf | tr '\n' ' ')"
+fi
+
+electron $DIDA_USER_FLAGS /usr/share/dida/dida.asar "$@"
