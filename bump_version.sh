0e8336e72b4179f039e14cf22e853e879c6b1e63
Cody Wyatt Neiman <neiman@cody.to>
2022-05-19 17:54:25 -0400

    feat: add ver bump script

---
+++ bump_version.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+get_latest_release_tag() {
+  # Resolve latest release tag from the GitHub API
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/'
+}
+
+sha512sum_first_src_sum() {
+  # Resolve PKGBUILD vars
+  local url=$(cat PKGBUILD | grep "url=" | sed -E 's/.*"([^"]+)".*/\1/')
+  local pkgbin=$(echo $url | sed -E 's,.*https://github.com/([^$]+),\1,')
+  local pkgver=$(get_latest_release_tag $pkgbin)
+  local pkgsrc=$(cat PKGBUILD | grep "_pkgsrc=" | sed -E 's/.*"([^"]+)".*/\1/' | sed -E 's/\$\{pkgver\}/'"$pkgver"'/g')
+
+  # Calculate sum on first in source()
+  wget "$url/releases/download/v$pkgver/$pkgsrc"
+  local sha=$(sha512sum $pkgsrc | cut -d " " -f1)
+  rm $pkgsrc
+  echo $sha
+}
+
+# Replace pkgver
+sed -Ei "s/pkgver=.*\$/pkgver=$(get_latest_release_tag "SteamGridDB/steam-rom-manager")/g" PKGBUILD
+
+# Replace first sha512sum
+sha=$(sha512sum_first_src_sum)
+echo "NEW SUM: $sha"
+sed -Ei 's/sha512sums=\(\"([0-9a-f]*)\"/sha512sums=\("'"$sha"'"/' PKGBUILD
+
+# Resolve .SRCINFO
+makepkg --printsrcinfo > .SRCINFO
