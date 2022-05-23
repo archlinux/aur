8fb0c31ebefc6abaa1b2936da974f3344a5e1d6e
Donald Carr <d@chaos-reins.com>
2022-05-23 10:33:01 -0700

    bcc optionally requires linux-headers: build modules against the Arch kernel
    Change-Id: Ic11aec3c879f0655883c13e6ff47b88d59b5bcbf

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 post_install() {
     echo "/sys/kernel/debug/sched needs to exist for CFS tuning to succeed"
     echo "Make sure CONFIG_SCHED_DEBUG is enabled in your kernel config if this is absent"
+
+    echo "This package builds a bpf module against the linux-headers on your install"
+    echo "hopefully these headers are applicable to running kernel(s) you have in play"
 }
