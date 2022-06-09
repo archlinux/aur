905f59598c163daf4167eb5e6416af25f8a42e08
Olivier Le Moal <olivier.lemoal@p1sec.com>
2022-06-09 16:41:51 +0200

    Shellcheck

--- responder.sh
+++ responder.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # If not run with "-h", "--help", or "--version", request root privileges
-if [[ $EUID != 0 && !("$@" =~ (-h|--version)) ]]; then
+if [[ $EUID != 0 && ! ("$*" =~ (-h|--version)) ]]; then
 	sudo "$0" "$@"
 	exit $?
 fi
 
-cd /usr/share/responder
+cd "/usr/share/responder" || { echo 'Cannot change to responder directory' ; exit 1; }
 exec python Responder.py "$@"
