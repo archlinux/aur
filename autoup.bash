3ed17d6767a1b50767a15998b797c645a040b945
Tobias Borgert <tobias.borgert@gmail.com>
2022-06-22 09:40:35 +0200

    Update to v5.25.1. Added clean-up to autoup.bash.

--- autoup.bash
+++ autoup.bash
@@ -5,24 +5,32 @@ if [ -z "$1" ]; then
 fi
 
 echo "Trying to auto-update this package to version ${1} ..."
+
+# Clean up
+rm -rf pkg src discover-*
+
+# Update the version in the PKGBUILD
 sed -ri 's/pkgver=([0-9]+\.[0-9]+\.[0-9]+\.{0,1}[0-9]*)/pkgver='"${1}"'/' PKGBUILD 
 if [ "$?" -ne 0 ]; then
     echo "Setting the version number in PKGBUILD failed."
     exit 1
 fi
 
+# Update the package sums in the PKGBUILD
 updpkgsums
 if [ "$?" -ne 0 ]; then
     echo "Updating the checksums failed."
     exit 1
 fi
 
+# Update .SRCINFO
 makepkg --printsrcinfo > .SRCINFO
 if [ "$?" -ne 0 ]; then
     echo "Recreating .SRCINFO failed."
     exit 1
 fi
 
+# Build & install the package
 makepkg -si --noconfirm
 if [ "$?" -ne 0 ]; then
     echo "Building and installing the package failed."
