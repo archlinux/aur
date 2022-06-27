134634823a4cec936c08ba5f0757a79ed5fd9f11
vsibille <vsibille@mit.edu>
2022-06-27 13:43:10 -0500

    Only download sources if the latest version differs from PKGBUILD

--- update_SDK.zsh
+++ update_SDK.zsh
@@ -1,12 +1,26 @@
 #!/usr/bin/env zsh
 
-function main(){
+function get_version(){
     local link=https://monkeysaudio.com/versionhistory.html
     local version=$(curl -s $link | awk '/Version [0-9]+\.[0-9]+/{print $3; exit}')
+    echo $version
+}
+
+function update_to(){
+    local version=$1
     sed -i "s/pkgver=.*/pkgver=$version/" PKGBUILD
     updpkgsums
     makepkg
     makepkg --printsrcinfo > .SRCINFO
+}
+
+function main(){
+    version=$(get_version)
+    if grep -q "pkgver=$version" PKGBUILD; then
+        printf "Up-to-date: version %s\n" $version
+    else
+        update_to $version
+    fi
 }
 
 main $@
