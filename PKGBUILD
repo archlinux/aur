5f8ab7fcf2df0c8acca18062bbb1f15669ed6d8d
Thiago Almeida <thiagoalmeidasa@gmail.com>
2022-07-07 22:31:49 +0200

    feat: Version bump to 0.12.2 and added support for aarch64

--- PKGBUILD
+++ PKGBUILD
@@ -1,17 +1,18 @@
 # Maintainer: Thiago Almeida <echo "dGhpYWdvYWxtZWlkYXNhQGdtYWlsLmNvbQo=" | base64 -d>
 
 pkgname="cloud-nuke-bin"
-pkgver=0.12.1
+pkgver=0.12.2
 pkgrel=1
 pkgdesc="A tool for cleaning up your cloud accounts by nuking (deleting) all resources within it."
 url="https://github.com/gruntwork-io/cloud-nuke"
-arch=('x86_64' 'i386')
+arch=('x86_64' 'i386' 'aarch64')
 license=('MIT')
 provides=('cloud-nuke')
 source_x86_64=("$pkgname::$url/releases/download/v${pkgver}/${pkgname/-bin/}_linux_amd64")
 source_i386=("$pkgname::$url/releases/download/v${pkgver}/${pkgname/-bin/}_linux_386")
-sha256sums_x86_64=(5e318c0a006a58e4e66315281bd93cbce63579ef00d866352366b5b78bd2656b)
-sha256sums_i386=(a79ff0f0228f8ac94e35a43fe2d2e67f7d0d297380040a333298f499535129bf)
+sha256sums_x86_64=(18b17b4529c6ecacde0cc52389c1a8940afeac435d181fddd33aff0a73208927)
+sha256sums_i386=(fd7fec4a27f05c7b6739c79ce79c1b08c7e11fc8858dd8e8113688199a15016d)
+sha256sums_aarch64=(b9aae27a5e5778cc3a4d9794c391139696cc417b106aeb78697100ee98fe321a)
 
 package() {
   install -Dm755 cloud-nuke-bin "$pkgdir/usr/bin/cloud-nuke"
--- update.sh
+++ update.sh
@@ -13,7 +13,7 @@ rm -rf pkg src || true
 LATEST_RELEASE_INFO=$(http https://api.github.com/repos/gruntwork-io/cloud-nuke/releases/latest \
   "Accept: application/vnd.github.v3+json" ${GITHUB_AUTH})
 
-jq -r '.assets[] | select(.name | test("^(SHA256SUMS|cloud-nuke.*_linux_(amd64|386))$")) | .browser_download_url' <<< "$LATEST_RELEASE_INFO" |
+jq -r '.assets[] | select(.name | test("^(SHA256SUMS|cloud-nuke.*_linux_(amd64|arm64|386))$")) | .browser_download_url' <<< "$LATEST_RELEASE_INFO" |
   xargs -n 1 -P 3 wget
 
 sha256sum --ignore-missing --status -c SHA256SUMS
@@ -24,10 +24,14 @@ X86_64_SHA=$(awk '/cloud-nuke.*_linux_amd64$/ {print $1}' SHA256SUMS)
 
 I386_SHA=$(awk '/cloud-nuke.*_linux_386$/ {print $1}' SHA256SUMS)
 
+AARCH64_SHA=$(awk '/cloud-nuke.*_linux_arm64$/ {print $1}' SHA256SUMS)
+
 sed -i -r 's/(pkgver=).*/\1'"${pkgver#v}"'/g' PKGBUILD
 
 sed -i -r 's/(sha256sums_x86_64=)\([A-Fa-f0-9]*\)/\1\('"$X86_64_SHA"'\)/g' PKGBUILD
 
 sed -i -r 's/(sha256sums_i386=)\([A-Fa-f0-9]*\)/\1\('"$I386_SHA"'\)/g' PKGBUILD
+
+sed -i -r 's/(sha256sums_aarch64=)\([A-Fa-f0-9]*\)/\1\('"$AARCH64_SHA"'\)/g' PKGBUILD
 
 makepkg --printsrcinfo >.SRCINFO
