d22395d5747c30c9560a35efc60ad160b85e2ccf
he.shuangyin <he.shuangyin@oe.21vianet.com>
2022-07-08 10:25:26 +0800

    test

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#   Copyright (C) 2016 Deepin, Inc.
+#
+#   Author:     Li LongYu <lilongyu@linuxdeepin.com>
+#               Peng Hao <penghao@linuxdeepin.com>
+
+#  fixed bugs Author: HSY <302606571@qq.com>
+
+version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
+
+BOTTLENAME="Deepin-WXWork"
+APPVER="3.1.12.6001deepin8"
+EXEC_PATH="c:/Program Files/WXWork/WXWork.exe"
+START_SHELL_PATH="/opt/deepinwine/tools/run_v4.sh"
+export MIME_TYPE=""
+export DEB_PACKAGE_NAME="com.qq.weixin.work.deepin"
+export APPRUN_CMD="deepin-wine5-stable"
+export LD_LIBRARY_PATH=/opt/apps/com.qq.weixin.work.deepin/lib/
+export GDK_BACKEND=x11
+
+DISABLE_ATTACH_FILE_DIALOG=""
+
+export SPECIFY_SHELL_DIR=`dirname $START_SHELL_PATH`
+
+ARCHIVE_FILE_DIR="/opt/apps/$DEB_PACKAGE_NAME/files"
+
+export WINEDLLPATH=/opt/$APPRUN_CMD/lib:/opt/$APPRUN_CMD/lib64
+
+export WINEPREDLL="$ARCHIVE_FILE_DIR/dlls"
+
+if [ -z "$DISABLE_ATTACH_FILE_DIALOG" ];then
+    export ATTACH_FILE_DIALOG=1
+fi
+
+if [ -n "$EXPORT_ENVS" ];then
+    export $EXPORT_ENVS
+fi
+
+if [ -n "$EXEC_PATH" ];then
+    if [ -z "${EXEC_PATH##*.lnk*}" ];then
+        $START_SHELL_PATH $BOTTLENAME $APPVER "C:/windows/command/start.exe" "/Unix" "$EXEC_PATH" "$@"
+    else
+        $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" "$@"
+    fi
+else
+    $START_SHELL_PATH $BOTTLENAME $APPVER "uninstaller.exe" "$@"
+fi
