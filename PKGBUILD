049445db7489dd313b823e3616373d2cd4dcba48
Armin Preiml <apreiml@strohwolke.at>
2022-07-18 19:35:07 +0200

    add aarch64 support

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Armin Preiml <apreiml@strohwolke.at>
 pkgname=hare-git
 _pkgname=hare
-pkgver=r2626.bd1c1c4a
+pkgver=r2629.f7584535
 pkgrel=1
 license=("MPL2")
 pkgdesc="The Hare systems programming language"
@@ -25,9 +25,11 @@ optdepends=(
 arch=("x86_64")
 url="https://harelang.org"
 source=("${pkgname%-*}::git+https://git.sr.ht/~sircmpwn/hare"
-	config.x86_64.mk)
+	config.x86_64.mk
+    config.aarch64.mk)
 sha512sums=('SKIP'
-            'bf0f1985de8ae414352b2a5c542c7c5f3ede8cdf4af5e22753226dc7d5192695fa55ef495bb10d9be682858533bdd771d38990d3271d9a4fa955fb15509b384f')
+            'bf0f1985de8ae414352b2a5c542c7c5f3ede8cdf4af5e22753226dc7d5192695fa55ef495bb10d9be682858533bdd771d38990d3271d9a4fa955fb15509b384f'
+            '33e9933d97a22ab1f650dc5dde3f97958114a9974065f79722adfa004c6737a87312f4fd1da07e52406aee6e0a81b8c04a49bcbae0919abcec3102b137b6d96e')
 provides=("hare")
 conflicts=("hare")
 # options=(debug)
---
+++ config.aarch64.mk
@@ -0,0 +1,48 @@
+## Install configuration
+
+PREFIX = /usr
+BINDIR = $(PREFIX)/bin
+MANDIR = $(PREFIX)/share/man
+SRCDIR = $(PREFIX)/src
+
+# Where to install the stdlib tree
+STDLIB = $(SRCDIR)/hare/stdlib
+
+# Default HAREPATH
+LOCALSRCDIR = /usr/local/src/hare
+HAREPATH = $(LOCALSRCDIR)/stdlib:$(LOCALSRCDIR)/third-party:$(SRCDIR)/hare/stdlib:$(SRCDIR)/hare/third-party
+
+## Build configuration
+
+# Platform to build for
+PLATFORM = linux
+ARCH = aarch64
+
+# External tools and flags
+HAREC = harec
+HAREFLAGS =
+QBE = qbe
+AS = as
+LD = ld
+AR = ar
+SCDOC = scdoc
+
+# Where to store build artifacts
+HARECACHE = .cache
+BINOUT = .bin
+
+# Cross-compiler toolchains
+AARCH64_AS=as
+AARCH64_AR=ar
+AARCH64_CC=gcc
+AARCH64_LD=ld
+
+RISCV64_AS=riscv64-linux-gnu-as
+RISCV64_AR=riscv64-linux-gnu-ar
+RISCV64_CC=riscv64-linux-gnu-gcc
+RISCV64_LD=riscv64-linux-gnu-ld
+
+X86_64_AS=x86_64-linux-gnu-as
+X86_64_AR=x86_64-linux-gnu-ar
+X86_64_CC=x86_64-linux-gnu-gcc
+X86_64_LD=x86_64-linux-gnu-ld
