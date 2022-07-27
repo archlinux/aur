bbf8ffbae8011b3e1edf896d5288d0c1e0eb1ca0
Greg White <gwhite@kupulau.com>
2022-07-27 08:41:57 -0400

    Add user flags to launch

--- brave-beta-bin.sh
+++ brave-beta-bin.sh
@@ -10,4 +10,4 @@ fi
 if [[ -z "${CHROME_USER_DATA_DIR}" ]]; then
     export CHROME_USER_DATA_DIR=~/.config/BraveSoftware/Brave-Browser-Beta
 fi
-exec "/opt/brave.com/brave-beta/brave-browser" "$BRAVE_FLAGS" "$FLAG" "$@"
+exec "/opt/brave.com/brave-beta/brave-browser" "$USER_FLAGS" "$BRAVE_FLAGS" "$FLAG" "$@"
