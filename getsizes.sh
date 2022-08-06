3927b3633bfca88d0d3f878dc7ca2818beb63788
Rafael Fontenelle <rafaelff@gnome.org>
2022-08-05 22:34:57 -0300

    Fix and simplify getsizes.sh

--- getsizes.sh
+++ getsizes.sh
@@ -1,24 +1,23 @@
-#!/bin/bash
+#!/bin/bash -e
 # Get sizes of ddnet-maps-git package file, and repository and types dirs
 # This should be ran only after a successful build
 
-test -f PKGBUILD && source PKGBUILD || exit 1
-test -d src/$_name || exit 1
-test -d pkg/$pkgname/usr/share/ddnet/data/types/ || exit 1
+source /etc/makepkg.conf
+source PKGBUILD
+test -d src/${pkgname%%-git}
+test -d pkg/$pkgname/usr/share/ddnet/data/types/
 
-
-BLK="    "
-echo -e "${BLK} Sizes as of $pkgver\n "
-
-echo -e "${BLK} Source code repository:"
-echo -e "${BLK} $(du -sh src/$_name | cut -d' ' -f1)\n "
-
-echo -e "${BLK} Package:"
-echo -e "${BLK} $(du -shD $pkgname-$pkgver-$pkgrel-$arch.pkg.tar)\n "
+echo "Sizes as of $pkgver"
+echo
+echo "Source code repository:"
+echo "   $(du -sh src/${pkgname%%-git} | cut -d' ' -f1)"
+echo
+echo "Package:"
+echo "   $(du -shD $pkgname-$pkgver-$pkgrel-$arch${PKGEXT})"
+echo
+echo "Server types packaged:"
 
 cd pkg/$pkgname
-echo -e "${BLK} Server types packaged:"
 for type in $(ls usr/share/ddnet/data/types/); do
-    echo -e "${BLK} $(du -sh usr/share/ddnet/data/types/$type)"
+    echo "   $(du -sh usr/share/ddnet/data/types/$type)"
 done
-
