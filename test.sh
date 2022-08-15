812c458f4c8ef110607974bc233460568fef68cb
Moses Narrow <moe_narrow@use.startmail.com>
2022-08-15 13:18:05 -0500

    add systray-git.PKGBUILD

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ echo -e "makepkg -fp PKGBUILD"
 makepkg -Cfp PKGBUILD
 echo -e "makepkg -fp git.PKGBUILD"
 makepkg -Cfp git.PKGBUILD
+echo -e "makepkg -fp systray-git.PKGBUILD"
+makepkg -Cfp systray-git.PKGBUILD
 echo -e "makepkg -fp deb.PKGBUILD"
 makepkg -Cfp deb.PKGBUILD
 echo -e "makepkg -fp dev.PKGBUILD"
