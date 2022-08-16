f9f860e2b7526de422d79a28f77a6c57e3911e63
michael.gruz <michael.gruz@gmail.com>
2022-08-16 20:52:38 +0200

    Fix cura5.sh

--- cura5.sh
+++ cura5.sh
@@ -2,4 +2,5 @@
 export QT_QPA_PLATAFORM=xdgdesktopportal
 export QT_QPA_PLATFORMTHEME=xdgdesktopportal
 cd /opt/cura5/
-LD_PRELOAD=/usr/lib/libstdc++.so.6 QT_QPA_PLATFORMTHEME=xdgdesktopportal QT_QPA_PLATAFORM=xdgdesktopportal ./Ultimaker-Cura $1
+QT_QPA_PLATFORMTHEME=xdgdesktopportal QT_QPA_PLATAFORM=xdgdesktopportal ./Ultimaker-Cura $1
+
