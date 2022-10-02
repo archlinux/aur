2b763f9de3a599e7bc4442c8beef0916d19298a6
Mark Blakeney <mark.blakeney@bullet-systems.net>
2022-10-02 10:10:07 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ post_install() {
 	echo -ne "\e[34;1m"
 	echo "=== INSTALLATION NOTES for $_pkgname ==="
 	echo -ne "\e[39;1m"
-	echo "Users should add \"source /usr/share/cdhist/$_pkgname.bashrc\" to their \$HOME/.bashrc."
+	echo "Users should add \". <(cdhist -i)\" to their \$HOME/.bashrc."
 	echo "See complete instructions at https://github.com/bulletmark/cdhist."
 	echo -ne "\e[0m"
 }
