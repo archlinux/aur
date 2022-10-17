48efa6a38bf517ab25e88c37a378a484da034565
Marc Rechté <marc.rechte@meteo.fr>
2022-10-17 13:44:16 +0200

    Ready for deployment 15.0 (2).

--- pgenv.sh
+++ pgenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PGDATA=/var/lib/postgres/data14
-export PATH=/opt/postgresql14/bin:$PATH
-export LD_LIBRARY_PATH=/opt/postgresql14/lib
+export PGDATA=/var/lib/postgres/data15
+export PATH=/opt/postgresql15/bin:$PATH
+export LD_LIBRARY_PATH=/opt/postgresql15/lib
