11435aa11128cdd6f241c4fd4e31ba5718f1f58d
michael.gruz <michael.gruz@gmail.com>
2022-10-20 07:16:00 +0200

    First commit.

---
+++ cura5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export QT_QPA_PLATAFORM=xdgdesktopportal
+export QT_QPA_PLATFORMTHEME=xdgdesktopportal
+cd /opt/cura5/
+LD_PRELOAD=/usr/lib/libstdc++.so.6 QT_QPA_PLATFORMTHEME=xdgdesktopportal QT_QPA_PLATAFORM=xdgdesktopportal ./Ultimaker-Cura $@
+
