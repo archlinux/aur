d46a2e7437cc2aca49cf2ad61c25b4c4b9b50211
German Lashevich <german.lashevich@gmail.com>
2022-10-25 11:11:12 +0200

    Add script for generating PKGBUILD

--- generate-pkgbuild.sh
+++ generate-pkgbuild.sh
@@ -1,6 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pkgver="$(curl -fsSL https://api.github.com/repos/zebradil/rustotpony/releases/latest | jq -r '.tag_name')"
+
+cat <<EOF
 # Maintainer: German Lashevich <german.lashevich@gmail.com>
 
-pkgver=0.3.2
+pkgver=${pkgver:?}
+EOF
+
+cat <<'EOF'
 
 _binname=totp
 
@@ -15,7 +25,7 @@ license=('MIT')
 # depends=('libx11' 'libxkbfile')
 makedepends=('rust' 'cargo')
 source=("${pkgname}-${pkgver}::https://github.com/zebradil/${pkgname}/archive/${pkgver}.tar.gz")
-sha1sums=('e8f275db1b56c1df0174c40e7185b386c803a9b3')
+sha1sums=('4a4634ea51ad9b40de09763f532087bfb9b0e142')
 
 build() {
   cd "${pkgname}-${pkgver}"
@@ -25,3 +35,4 @@ build() {
 package() {
   install -Dm755 "${srcdir}/${pkgname}-${pkgver}/target/release/${_binname}" "${pkgdir}/usr/bin/${_binname}"
 }
+EOF
