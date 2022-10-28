49921f3b91086ad005c07cf8b8217e0a5c47b1e1
vsibille <vsibille@mit.edu>
2022-10-28 12:31:02 -0500

    Rely on source/link to check if the package needs to be updated

--- update_MAC_SDK.zsh
+++ update_MAC_SDK.zsh
@@ -1,19 +1,35 @@
 #!/usr/bin/env zsh
 
-function get_last_version(){
-    local link=https://monkeysaudio.com/versionhistory.html
-    local version=$(curl -s $link | awk '/Version [0-9]+\.[0-9]+/{print $3; exit}')
-    echo $version
+function get_last_link(){
+    local page_link=https://monkeysaudio.com/developers.html
+    local link=$(curl -s $page_link | awk -F '"' '/latest version/{print $2}')
+    echo $link
+}
+
+function get_current_link(){
+    local link=$(awk -F\' '/source=/{print $2}' PKGBUILD)
+    echo $link
+}
+
+function get_version(){
+    local link=$1
+    digits=${link:t:r:s/MAC_//:s/_SDK//}
+    major=$digits[1].${digits[2,3]}
+    minor=${digits[4,-1]}
+    [[ -n $minor ]] && echo $major.$minor || echo $major
 }
 
 function patch_pkgbuild(){
-    local version=$1
+    local link=$1
+    local version=$2
+    sed -i "s@source=.*@source=('$link')@" PKGBUILD
     sed -i "s/pkgver=.*/pkgver=$version/" PKGBUILD
 }
 
 function update_to(){
-    local version=$1
-    patch_pkgbuild $version
+    local link=$1
+    local version=$2
+    patch_pkgbuild $link $version
     updpkgsums && makepkg && makepkg --printsrcinfo > .SRCINFO
 }
 
@@ -24,18 +40,20 @@ function sync_git(){
 }
 
 function try_update(){
-    local ver=$1
-    local old_ver=$(awk -F= '/pkgver=/{print $2}' PKGBUILD)
-    if [[ $ver == $old_ver ]]; then
+    local link=$1
+    local old_link=$(get_current_link)
+    local ver=$(get_version $link)
+    if [[ $link == $old_link ]]; then
         echo "Already up-to-date: version $ver"
     else
-        update_to $ver && sync_git $ver || patch_pkgbuild $old_ver
+        local old_ver=$(get_version $old_link)
+        update_to $link $ver && sync_git $ver || patch_pkgbuild $old_link $old_ver
     fi
 }
 
 function main(){
-    local ver=$(get_last_version)
-    [[ -n $ver ]] && try_update $ver || { echo "Can't retrieve online version" && return 1 }
+    local link=$(get_last_link)
+    [[ -n $link ]] && try_update $link || { echo "Can't retrieve SDK link" && return 1 }
 }
 
 cd $0:A:h && main $@
