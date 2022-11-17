1ecbdb30c877da769b26ced0cc11edd90ffc9e58
Donald Carr <d@chaos-reins.com>
2022-11-17 10:35:54 -0800

    Move to lld since lto might be enabled and mold does not jive with it
    Change-Id: I723c1092cd56a1c3251505581055b61c252709ce

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,4 +1,13 @@
 set( CMAKE_SYSTEM_NAME Linux )
+# e.g. to use mold with Clang
+# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")
+# set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=mold")
+# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")
+
+# e.g. to use mold with Clang
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
 
 set(CMAKE_CXX_COMPILER "clang++")
 set(CMAKE_C_COMPILER "clang")
