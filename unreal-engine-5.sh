161de0be0c6c7a8c1c2c912d2951fff2450a74cd
Neko-san <aur@mycat.anonaddy.me>
2022-11-20 14:36:38 -0600

    Updated unreal-engine.sh

--- unreal-engine-5.sh
+++ unreal-engine-5.sh
@@ -27,8 +27,8 @@ fi
 UE5desktopFileChecksum="$(sha256sum "${HOME}/local/share/applications/com.unrealengine.UE5Editor.desktop" | cut -f 1 -d ' ')"
 
 if [ "${UE5desktopFileChecksum}" == "ChecksumPlaceholder" ]; then
-    UE5editorLocation=$(find InstalledLocationPlaceholder -type f -iname 'UnrealEditor')
-    UE5editorPath=$(echo "${UE5editorLocation/UnrealEditor/}")
+    UE5editorLocation="$(find InstalledLocationPlaceholder -type f -iname 'UnrealEditor')"
+    UE5editorPath="$(echo ${UE5editorLocation/UnrealEditor/})"
     
     sed -i "7c\Exec=${UE5editorLocation} %F" "${HOME}/local/share/applications/com.unrealengine.UE5Editor.desktop"
     sed -i "14c\Path=${UE5editorPath}" "${HOME}/local/share/applications/com.unrealengine.UE5Editor.desktop"
