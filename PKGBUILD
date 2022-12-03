abe1f9bad8032ca2da92b5266ddc679b2b45184f
sukanka <su975853527@gmail.com>
2022-12-03 10:29:40 +0800

    try new wrapper, @AvianaCruz

--- PKGBUILD
+++ PKGBUILD
@@ -7,7 +7,7 @@ provides=('wemeet' 'tencent-meeting')
 pkgver=3.11.0.402
 _x86_md5=9b74d4127a16a011db8cb6300fa5fbc9
 _arm_md5=ce5d25cc8e8aae8ddd19295bc3b00d5e
-pkgrel=7
+pkgrel=8
 pkgdesc="Tencent Video Conferencing, tencent meeting 腾讯会议"
 arch=('x86_64' 'aarch64')
 license=('unknown')
@@ -18,7 +18,7 @@ source_aarch64=("${_pkgname}-${pkgver}-aarch64.deb::https://updatecdn.meeting.qq
 source=("${_pkgname}".sh)
 optdepends=('bubblewrap: Fix abnormal text color in dark mode.')
 makedepends=('patchelf')
-sha512sums=('52b4f410a31e3f63d27c84e12cd8c6724a56a4179682ca17dfb99f8e2d64a16cd54b64bffdb8449440f745be261336c71b85270d4717f3640ddde5c1ee7ed586')
+sha512sums=('22c1d02054c915ad31a49be524be80b67d98fc36cca8f05f800cb3bd2794044383e6b3e01a74282a58153a79f0556398e201505bcac180567f7adab55ec3b9f8')
 sha512sums_x86_64=('6b4a5a5499fd8a92dbf86ae04990434b3a7ff2ade7d768dbe70dd3f60e347c599281e1aed211673a3a7986f5cb7a014abefec0c859e087c291286467bf8fbe73')
 sha512sums_aarch64=('534657987d8030c0798731d72fade34c40a5863f4dcd289423eb0509115e290ad5507616c19d86e76c25ae5ea82272fe167ef45599f0ad1433531ff72f74ce78')
 
--- wemeet.sh
+++ wemeet.sh
@@ -3,18 +3,22 @@ export XDG_SESSION_TYPE=x11
 export QT_QPA_PLATFORM=xcb
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_STYLE_OVERRIDE=fusion # 解决使用自带qt情况下，字体颜色全白看不到的问题
-export IBUS_USE_PORTAL=1 # fix ibus
-FONTCONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/fontconfig"
+export IBUS_USE_PORTAL=1        # fix ibus
 unset WAYLAND_DISPLAY
+USER_RUN_DIR="/run/user/$(id -u)"
+CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}"
+FONTCONFIG_DIR="$CONFIG_DIR/fontconfig"
+KDE_ICON_CACHE_FILE="${XDG_CACHE_HOME:-$HOME/.cache}/icon-cache.kcache"
+WEMEET_APP_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/wemeetapp"
 
 # if pipewire-pulse installed
 if [ -f /usr/bin/pipewire-pulse ]; then
     export PULSE_LATENCY_MSEC=20
-fi;
+fi
 
-if [ -f "/usr/bin/bwrap" ];then
-    mkdir -p $FONTCONFIG_DIR
-    bwrap --dev-bind / / --tmpfs "${XDG_CONFIG_HOME:-$HOME/.config}" --ro-bind $FONTCONFIG_DIR $FONTCONFIG_DIR /opt/wemeet/bin/wemeetapp $*;
+if [ -f /usr/bin/bwrap ]; then
+    mkdir -p "$WEMEET_APP_DIR"
+    bwrap --new-session --die-with-parent --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --ro-bind / / --dev-bind /dev /dev --bind "$USER_RUN_DIR" "$USER_RUN_DIR" --tmpfs "$CONFIG_DIR" --ro-bind-try "$FONTCONFIG_DIR" "$FONTCONFIG_DIR" --bind-try "$KDE_ICON_CACHE_FILE" "$KDE_ICON_CACHE_FILE" --bind "$WEMEET_APP_DIR" "$WEMEET_APP_DIR" /opt/wemeet/bin/wemeetapp "$@"
 else
-    exec /opt/wemeet/bin/wemeetapp $*;
-fi;
+    exec /opt/wemeet/bin/wemeetapp "$@"
+fi
