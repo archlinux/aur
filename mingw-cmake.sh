ca13e4896ad9de9d33d9740940bc3925ed5ad512
Michel Zou <xantares09@hotmail.com>
2022-12-05 18:39:38 +0100

    CMAKE_BUILD_TYPE=None

--- mingw-cmake.sh
+++ mingw-cmake.sh
@@ -9,6 +9,7 @@ PATH=${mingw_prefix}/bin:$PATH cmake \
     -DCMAKE_INSTALL_LIBDIR:PATH=lib \
     -DCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
     -DCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES:PATH=${mingw_prefix}/include \
+    -DCMAKE_BUILD_TYPE=None \
     -DBUILD_SHARED_LIBS:BOOL=ON \
     -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-@TRIPLE@.cmake \
     -DCMAKE_CROSSCOMPILING_EMULATOR=/usr/bin/@TRIPLE@-wine \
