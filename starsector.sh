9112faaa7a00d27c6a8c4382931d1f477d1075ab
Phillip Schichtel <phillip@schich.tel>
2022-12-11 16:41:43 +0100

    correct the XDG variable

--- starsector.sh
+++ starsector.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # fix for users of special IM modules
 unset XMODIFIERS GTK_IM_MODULE QT_IM_MODULE
 
-state_dir="${XDG_STATE_HOME:-"$HOME/.local/share"}/starsector"
+state_dir="${XDG_DATA_HOME:-"$HOME/.local/share"}/starsector"
 saves_path="${state_dir}/saves"
 mods_path="${state_dir}/mods"
 screenshots_path="${state_dir}/screenshots"
