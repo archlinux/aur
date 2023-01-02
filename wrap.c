b21904a6c940fa5c1d0497e5133b1364c015c6d6
Aviana Cruz <gwencroft@proton.me>
2023-01-02 19:59:53 +0800

    Make the function of sending files actually work

--- wrap.c
+++ wrap.c
@@ -1,5 +1,5 @@
 // by https://aur.archlinux.org/account/AvianaCruz
-#include <stddef.h>
+#include <openssl/ssl.h>
 
 #ifdef WRAP_FORCE_SINK_HARDWARE
 #include <dlfcn.h>
@@ -8,7 +8,13 @@
 #include <string.h>
 #endif
 
-void *SSL_get_peer_certificate(const void *s) { return NULL; }
+#ifdef SSL_get_peer_certificate
+#undef SSL_get_peer_certificate
+#endif
+
+X509 *SSL_get_peer_certificate(const SSL *s) {
+  return SSL_get1_peer_certificate(s);
+}
 
 #ifdef WRAP_FORCE_SINK_HARDWARE
 typedef pa_operation *(*wrap_pa_get_sink_by_name_t)(pa_context *c,
