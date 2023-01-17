a4a3c998d7215fc70eabfe7ad230d88d832b8728
Max Harmathy <harmathy@mailbox.org>
2023-01-17 09:10:50 +0100

    Add migration of data directory

--- mediathekview.sh
+++ mediathekview.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+# Handle data directory to be compliant to XDG Base Directory specification.
+data_directory="${XDG_DATA_HOME:-$HOME/.local/share}/mediathekview"
+upstream_default="$HOME/.mediathek3"
+if [ -d "$upstream_default" ] && [ ! -d "$data_directory" ]; then
+  # migrate existing data
+  mv -v "$upstream_default" "$data_directory"
+fi
+
 PATH="/usr/lib/jvm/java-17-openjdk/bin:${PATH}"
-directory="${XDG_DATA_HOME:-$HOME/.local/share}/mediathekview"
 # requires preview features: https://github.com/mediathekview/MediathekView/issues/528
-java -jar /usr/share/java/mediathekview/MediathekView.jar "$@" "$directory"
+java -jar /usr/share/java/mediathekview/MediathekView.jar "$@" "$data_directory"
