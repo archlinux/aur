3685cbab792b6838ddcb08d5169ec9f7f1baadfa
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2023-02-12 10:45:57 -0600

    bump pkgrel ; fix checksums

--- updates.sh
+++ updates.sh
@@ -26,5 +26,5 @@ makepkg --printsrcinfo > .SRCINFO
 echo "don't forget to increment pkgrel if you edited the PKGBUILD"
 source PKGBUILD && echo "pkgver=${pkgver}" && echo "pkgrel=${pkgrel}"
 echo
-echo "git add -f" *PKGBUILD " .SRCINFO skywire.install updates.sh test.sh"
+echo "git add -f" *PKGBUILD " .SRCINFO skywire.install updates.sh test.sh skycoin"
 echo 'git commit -m " "'
