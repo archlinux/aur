420b7edd67db39d6b02cdf4931d30ed97236ea4b
Matheus <matheusgwdl@protonmail.com>
2023-03-04 13:32:32 +0100

    Fixed executable and adjusted permissions.

--- erpnext.sh
+++ erpnext.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # This script starts ERPNext.
 
-sudo -u erpnext bash -c "cd /usr/share/webapps/erpnext/ || exit 1"
-sudo -u erpnext bash -c "bench start \${@}"
+sudo -u erpnext bash -c "cd /usr/share/webapps/erpnext/ && bench start \${@}"
