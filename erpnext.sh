83ee1533c039f65d6692cad26847a4ecbc9424a7
Matheus <matheusgwdl@protonmail.com>
2023-03-11 10:11:28 +0100

    Fixed executable.

--- erpnext.sh
+++ erpnext.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # This script starts ERPNext.
 
-sudo -u erpnext bash -c "cd /usr/share/webapps/erpnext/ && bench start \${@}"
+sudo -u erpnext bash -c "cd /usr/share/webapps/erpnext/ && bench start ${*}"
