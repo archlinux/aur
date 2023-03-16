725f0db0edd42c8a661626756be26aae623769c2
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2023-03-16 18:47:19 +0530

    fix update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -1,10 +1,12 @@
+set -xeu
 version=$(curl -sL \
   -H "Accept: application/vnd.github+json" \
-  https://api.github.com/repos/quarto-dev/quarto-cli/releases  | jq "map(select(.prerelease))[1].name" | tr -d "v")
+  https://api.github.com/repos/quarto-dev/quarto-cli/releases  | jq "map(select(.prerelease))[0].name" | tr -d "v")
 echo $version
 sed "s/:version:/$version/" PKGBUILD.template > PKGBUILD
 
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
+git add -A
 git commit -am "Autorelease version $version"
 git push
