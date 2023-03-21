0635790d631f75f16ce4c7316fa42fec06bfc210
lukelabrie <lplcleary@gmail.com>
2023-03-21 16:46:53 +0100

    use fuel_pins.step again

--- step_to_h5m.py
+++ step_to_h5m.py
@@ -1,8 +1,6 @@
-
-import sys
 import CAD_to_OpenMC.assembly as ab
 a=ab.Assembly()
-a.stp_files=["~/.cache/yay/python-cad_to_openmc/src/CAD_to_OpenMC/examples/7pin.step"]
+a.stp_files=["./fuel_pins.step"]
 a.import_stp_files()
 a.merge_all()
 a.solids_to_h5m()
