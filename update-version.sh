4b970b0bef6fb756f11da54256a82bdd06cc4919
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2023-03-22 21:57:17 +0530

    Autorelease version "1.3.283"

--- update-version.sh
+++ update-version.sh
@@ -1,7 +1,12 @@
 set -eu
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 version=$(curl -sL \
-  -H "Accept: application/vnd.github+json" \
-  https://api.github.com/repos/quarto-dev/quarto-cli/releases  | jq "map(select(.prerelease))[0].name" | tr -d "v")
+	-H "Accept: application/vnd.github+json" \
+	https://api.github.com/repos/quarto-dev/quarto-cli/releases |\
+	jq "map(select(.prerelease))[0].name" |\
+	tr -d "v")
 echo $version
 sed "s/:version:/$version/" PKGBUILD.template > PKGBUILD
 
