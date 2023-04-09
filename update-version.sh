6d574cbbb58e9bdff0a1aab5466ad78cc2bdc22c
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2023-04-09 22:28:02 +0530

    Autorelease version 1.3.313

--- update-version.sh
+++ update-version.sh
@@ -5,12 +5,14 @@ cd $SCRIPT_DIR
 version=$(curl -sL \
 	-H "Accept: application/vnd.github+json" \
 	https://api.github.com/repos/quarto-dev/quarto-cli/releases |\
-	jq "map(select(.prerelease))[0].name" |\
+	jq "map(select(.prerelease))[0].name" -r |\
 	tr -d "v")
 echo $version
-sed "s/:version:/$version/" PKGBUILD.template > PKGBUILD
+sha256sum=$(curl -sL https://github.com/quarto-dev/quarto-cli/releases/download/v$version/quarto-$version-checksums.txt | grep "amd.*deb" | cut -f1 -d" ")
+echo $sha256sum
 
-updpkgsums
+sed "s/:version:/$version/;s/:shasum:/$sha256sum/" PKGBUILD.template > PKGBUILD
+
 makepkg --printsrcinfo > .SRCINFO
 git add -A
 git commit -am "Autorelease version $version"
