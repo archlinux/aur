9df5af4a262084524e865f82142f6588efbb71b1
Pellegrino Prevete <pellegrinoprevete@gmail.com>
2023-04-09 02:56:46 +0000

    add copyright notices and licence

--- enable-graphical-services.sh
+++ enable-graphical-services.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+#    enable-graphical-services
+#
+#    ----------------------------------------------------------------------
+#    Copyright © 2023  Pellegrino Prevete
+#
+#    All rights reserved
+#    ----------------------------------------------------------------------
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 _systemctl="/usr/bin/systemctl"
 
 _restart_if_not_running() {
--- run-as.py
+++ run-as.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python3
 """Run applications an another user"""
 
+#    run-as
+#
+#    ----------------------------------------------------------------------
+#    Copyright © 2023  Pellegrino Prevete
+#
+#    All rights reserved
+#    ----------------------------------------------------------------------
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 from argparse import ArgumentParser
 from os import environ
 from pwd import getpwall, getpwuid, getpwnam
 from shutil import which
 import subprocess
 from sys import stderr
-# sh = subprocess.run
-# from subprocess import check_output as sh
 
 description = "Run applications as another user"
 version = 0.1
