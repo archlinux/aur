7f5f8e017f7ccd7304a80b0600628c84de78a589
Roman Vasilev <2rvasilev@live.ru>
2023-04-21 22:25:39 +0300

    Update filebot.sh from upstream debian package

--- filebot.sh
+++ filebot.sh
@@ -16,13 +16,60 @@ APP_DATA="${HOME}/.config/filebot"
 LIBRARY_PATH="${FILEBOT_HOME}/lib/$(uname -m):/lib64"
 MODULE_PATH="${FILEBOT_HOME}/openjfx"
 
-/usr/lib/jvm/java-11-openjdk/bin/java \
+# /usr/lib/jvm/java-11-openjdk/bin/java
+
+# /usr/bin/java \
+#     -Dapplication.deployment=aur \
+#     --module-path "${MODULE_PATH}" \
+#     --add-modules ALL-MODULE-PATH \
+#     -Dapplication.update=skip \
+#     -Dnet.filebot.archive.extractor=ShellExecutables \
+#     --add-opens=java.base/java.lang=ALL-UNNAMED \
+#     --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+#     --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+#     --add-opens=java.base/java.util=ALL-UNNAMED \
+#     --add-opens=java.base/java.util.function=ALL-UNNAMED \
+#     --add-opens=java.base/java.util.regex=ALL-UNNAMED \
+#     --add-opens=java.base/java.net=ALL-UNNAMED \
+#     --add-opens=java.base/java.io=ALL-UNNAMED \
+#     --add-opens=java.base/java.nio=ALL-UNNAMED \
+#     --add-opens=java.base/java.nio.file=ALL-UNNAMED \
+#     --add-opens=java.base/java.nio.file.attribute=ALL-UNNAMED \
+#     --add-opens=java.base/java.nio.channels=ALL-UNNAMED \
+#     --add-opens=java.base/java.nio.charset=ALL-UNNAMED \
+#     --add-opens=java.base/java.time=ALL-UNNAMED \
+#     --add-opens=java.base/java.time.chrono=ALL-UNNAMED \
+#     --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+#     --add-opens=java.base/java.text=ALL-UNNAMED \
+#     --add-opens=java.base/sun.nio.fs=ALL-UNNAMED \
+#     --add-opens=java.logging/java.util.logging=ALL-UNNAMED \
+#     --add-opens=java.desktop/java.awt=ALL-UNNAMED \
+#     --add-opens=java.desktop/sun.awt=ALL-UNNAMED \
+#     --add-opens=java.desktop/sun.swing=ALL-UNNAMED \
+#     --add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED \
+#     --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED \
+#     -Djna.boot.library.path="${LIBRARY_PATH}" \
+#     -Djna.library.path="${LIBRARY_PATH}" \
+#     -Djava.library.path="${LIBRARY_PATH}" \
+#     -Dapplication.dir="${APP_DATA}" \
+#     -Dapplication.cache="${APP_DATA}/cache" \
+#     -Djava.io.tmpdir="${APP_DATA}/tmp" \
+#     -Dfile.encoding="UTF-8" \
+#     -Dsun.jnu.encoding="UTF-8" \
+#     -Dprism.order=sw \
+#     -Dnet.filebot.theme=Darcula \
+#     -DuseGVFS=true \
+#     -Dnet.filebot.gio.GVFS="${XDG_RUNTIME_DIR}/gvfs" \
+#     "${JAVA_OPTS[@]}" \
+#     "${FILEBOT_OPTS[@]}" \
+#     -jar "${FILEBOT_HOME}/jar/filebot.jar" \
+#     "$@"
+
+java \
     -Dapplication.deployment=aur \
-    --module-path "${MODULE_PATH}" \
+    --module-path "$MODULE_PATH" \
     --add-modules ALL-MODULE-PATH \
-    -Dapplication.update=skip \
     -Dnet.filebot.archive.extractor=ShellExecutables \
-    --illegal-access=permit \
     --add-opens=java.base/java.lang=ALL-UNNAMED \
     --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
     --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
@@ -47,19 +94,21 @@ MODULE_PATH="${FILEBOT_HOME}/openjfx"
     --add-opens=java.desktop/sun.swing=ALL-UNNAMED \
     --add-opens=java.desktop/javax.swing.text.html=ALL-UNNAMED \
     --add-opens=java.prefs/java.util.prefs=ALL-UNNAMED \
-    -Djna.boot.library.path="${LIBRARY_PATH}" \
-    -Djna.library.path="${LIBRARY_PATH}" \
-    -Djava.library.path="${LIBRARY_PATH}" \
-    -Dapplication.dir="${APP_DATA}" \
-    -Dapplication.cache="${APP_DATA}/cache" \
-    -Djava.io.tmpdir="${APP_DATA}/tmp" \
+    -XX:+DisableAttachMechanism \
+    -Djna.boot.library.path="$LIBRARY_PATH" \
+    -Djna.boot.library.name=jnidispatch \
+    -Djna.library.path="$LIBRARY_PATH" \
+    -Djava.library.path="$LIBRARY_PATH" \
+    -Dapplication.dir="$APP_DATA" \
+    -Dapplication.cache="$APP_DATA/cache" \
+    -Djava.io.tmpdir="$APP_DATA/tmp" \
     -Dfile.encoding="UTF-8" \
     -Dsun.jnu.encoding="UTF-8" \
+    -Dawt.useSystemAAFontSettings=on \
     -Dprism.order=sw \
     -Dnet.filebot.theme=Darcula \
     -DuseGVFS=true \
-    -Dnet.filebot.gio.GVFS="${XDG_RUNTIME_DIR}/gvfs" \
-    "${JAVA_OPTS[@]}" \
-    "${FILEBOT_OPTS[@]}" \
-    -jar "${FILEBOT_HOME}/jar/filebot.jar" \
+    -Dnet.filebot.gio.GVFS="$XDG_RUNTIME_DIR/gvfs" \
+    $JVM_OPTS $JAVA_OPTS $FILEBOT_OPTS \
+    -jar "$FILEBOT_HOME/jar/filebot.jar" \
     "$@"
