8aceee81dada8fa4eb96dcd615bd1bf232955b9c
Denton Liu <liu.denton@gmail.com>
2023-04-29 02:17:01 -0700

    Remove subdirectories in update-branch.sh

--- update-branch.sh
+++ update-branch.sh
@@ -7,18 +7,12 @@ LOCAL_BRANCH=upstream
 git fetch "$REMOTE_URL" "$REMOTE_BRANCH"
 git checkout -B "$LOCAL_BRANCH" FETCH_HEAD
 FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f --subdirectory-filter trunk \
-	--tree-filter '(makepkg --printsrcinfo; status=$?; echo; exit $status) >.SRCINFO || rm .SRCINFO' \
+	--tree-filter 'git rm -rf keys; { makepkg --printsrcinfo && echo; } >.SRCINFO || rm .SRCINFO' \
 	--commit-filter '
 		if [ -e .SRCINFO ]
 		then
 			git commit-tree "$@"
 		else
-			shift
-			while [ -n "$1" ]
-			do
-				shift
-				map "$1"
-				shift
-			done
+			skip_commit "$@"
 		fi'
 git checkout -
