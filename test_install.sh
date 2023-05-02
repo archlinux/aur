7effff69d2d8d617b2a46d7ac9088424b42831ae
lukelabrie <lplcleary@gmail.com>
2023-05-02 16:03:41 +0200

    update path to pymoab

--- test_install.sh
+++ test_install.sh
@@ -1,6 +1,6 @@
  # set non-standard paths
 export PATH=$PATH:/opt/openmc/bin:$HOME/.local/lib/python3.10/site-packages
-export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python3.10/site-packages
+export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python3.10/site-packages/pymoab-5.3.0-py3.10-linux-x86_64.egg
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/MOAB/lib
 var=`echo /opt/nuclear-data/*hdf5 | head -n1`
 export OPENMC_CROSS_SECTIONS=$var/cross_sections.xml
