0e0b97decc88cad02d2f105b79e3bfb19baf0694
Chris Werner Rau <cwrau@cwrau.info>
2023-05-04 11:56:43 +0200

    feat: template everything by default

--- PKGBUILD
+++ PKGBUILD
@@ -2,14 +2,14 @@
 
 _pkgname=helmrelease-tools
 pkgname=$_pkgname
-pkgver=1.2.0
+pkgver=1.3.0
 pkgrel=1
 pkgdesc="ZSH functions to work with flux HelmReleases"
 license=('APACHE')
 arch=('x86_64')
 depends=("yq" "git" "helm" "fd" "ripgrep" "helm-diff")
 source=("$pkgname.plugin.zsh" "_$pkgname")
-sha512sums=('d80c0293397052132e8aabccc90ce75a573c5332f0b2f6b16fbfd8ed3322ba70512f2a4b86a87b48da0ffb639fd4fdc85a2e55a381b551524fd3fe848316c017'
+sha512sums=('24222826de7cda6ac5d230d41cd076728de6e87cb7915a3c465988b1c90c26a9b986f54022565e96e2835f882e92a4803db428999444f3d064e2471ad6f1d081'
             'ff7a5fa4111c6e83a00b9e6731b6bfb6770600ed371d11f1347566fd81e7eb9e21993b9187012b426b4ae2080117953910bf75fc0b67ab1f345e7a702e3e2aa5')
 conflicts=("$_pkgname" "${_pkgname}-git")
 
--- helmrelease-tools.plugin.zsh
+++ helmrelease-tools.plugin.zsh
@@ -98,157 +98,153 @@ function helmrelease() {
         shift
         ;;
       -+([0-9]))
-      index="${1/-/}"
-      shift
-      ;;
-    -ALL)
-      index=ALL
-      shift
-      ;;
-    --)
-      shift
-      break
+        index="${1/-/}"
+        shift
+        ;;
+      --)
+        shift
+        break
+        ;;
+      *)
+        if [[ -f "$1" ]]; then
+          yaml=$(cat "$1")
+        elif [[ -d "$1" ]]; then
+          sourceParameter="$1"
+        elif [[ "$1" =~ ^https://* ]]; then
+          sourceParameter="$1"
+        else
+          echo "parameter '$1' is not supported" > /dev/stderr
+          return 1
+        fi
+        shift
+        ;;
+    esac
+  done
+
+  if [[ -z "$yaml" ]]; then
+    yaml=$(cat)
+  fi
+
+  numberOfHelmReleases=$(<<< "$yaml" | yq -ers '[.[] | select(.kind == "HelmRelease")] | length')
+  if [[ "$numberOfHelmReleases" -lt 1 ]]; then
+    echo "There are no HelmReleases in the input" > /dev/stderr
+    return 1
+  elif [[ "$numberOfHelmReleases" != 1 ]] && [[ "$subCommand" == "install" ]]; then
+    echo "You can only install 1 HelmReleases at the same time" > /dev/stderr
+    return 1
+  elif [[ "$numberOfHelmReleases" -gt 1 ]]; then
+    <<<"$yaml" | yq -erys '.[] | select(.kind != "HelmRelease") | select(.)' \
+      | if [[ "$subCommand" = "template" ]]; then
+          cat -
+        elif [[ "$subCommand" = "diff" ]]; then
+          kubectl diff -f - || true
+        fi
+    for index in {0..$((numberOfHelmReleases - 1))}; do
+      if [[ "$subCommand" = "template" ]]; then
+        echo ---
+      fi
+      <<<"$yaml" | yq -erys '([.[] | select(.kind == "HelmRelease")]['"$index"']),(.[] | select(.kind | IN(["GitRepository", "HelmRepository"][])))' | helmrelease "$subCommand" -
+    done
+  fi
+
+  helmReleaseYaml=$(_hr_getYaml "$yaml" "$index" HelmRelease)
+  [[ "$?" -ne 0 ]] && return 1
+  namespace=$(_hr_getNamespace "$helmReleaseYaml")
+  remoteKubeconfig="$(<<< "$helmReleaseYaml" | yq -r '.spec.kubeConfig.secretRef.name')"
+  if [[ "$remoteKubeconfig" != null ]]; then
+    REMOTE_KUBECONFIG="$(mktemp)"
+    trap "rm -f \"$REMOTE_KUBECONFIG\"" EXIT
+    kubectl --namespace=$(<<<"$helmReleaseYaml" | yq -r '.metadata.namespace') get secret $remoteKubeconfig -o jsonpath='{.data.value}' | base64 -d > "$REMOTE_KUBECONFIG"
+  fi
+  releaseName=$(_hr_getReleaseName "$helmReleaseYaml")
+  case "$subCommand" in
+    uninstall)
+      KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace $releaseName
       ;;
     *)
-      if [[ -f "$1" ]]; then
-        yaml=$(cat "$1")
-      elif [[ -d "$1" ]]; then
-        sourceParameter="$1"
-      elif [[ "$1" =~ ^https://* ]]; then
-        sourceParameter="$1"
-      else
-        echo "parameter '$1' is not supported" > /dev/stderr
-        return 1
-      fi
-      shift
-      ;;
-  esac
-done
-
-if [[ -z "$yaml" ]]; then
-  yaml=$(cat)
-fi
-
-numberOfHelmReleases=$(<<< "$yaml" | yq -ers '[.[] | select(.kind == "HelmRelease")] | length')
-if [[ "$numberOfHelmReleases" -lt 1 ]]; then
-  echo "There are no HelmReleases in the input" > /dev/stderr
-  return 1
-elif [[ "$numberOfHelmReleases" != 1 ]] && [[ "$subCommand" == "install" ]]; then
-  echo "You can only install 1 HelmReleases at the same time" > /dev/stderr
-  return 1
-elif [[ "$index" = "ALL" ]]; then
-  <<<"$yaml" | yq -erys '.[] | select(.kind != "HelmRelease") | select(.)' \
-    | if [[ "$subCommand" = "template" ]]; then
-    cat -
-  elif [[ "$subCommand" = "diff" ]]; then
-    kubectl diff -f - || true
-  fi
-  for index in {0..$((numberOfHelmReleases - 1))}; do
-    if [[ "$subCommand" = "template" ]]; then
-      echo ---
-    fi
-    <<<"$yaml" | yq -erys '([.[] | select(.kind == "HelmRelease")]['"$index"']),(.[] | select(.kind | IN(["GitRepository", "HelmRepository"][])))' | helmrelease "$subCommand" -
-  done
-fi
-
-helmReleaseYaml=$(_hr_getYaml "$yaml" "$index" HelmRelease)
-[[ "$?" -ne 0 ]] && return 1
-namespace=$(_hr_getNamespace "$helmReleaseYaml")
-remoteKubeconfig="$(<<< "$helmReleaseYaml" | yq -r '.spec.kubeConfig.secretRef.name')"
-if [[ "$remoteKubeconfig" != null ]]; then
-  REMOTE_KUBECONFIG="$(mktemp)"
-  trap "rm -f \"$REMOTE_KUBECONFIG\"" EXIT
-  kubectl --namespace=$(<<<"$helmReleaseYaml" | yq -r '.metadata.namespace') get secret $remoteKubeconfig -o jsonpath='{.data.value}' | base64 -d > "$REMOTE_KUBECONFIG"
-fi
-releaseName=$(_hr_getReleaseName "$helmReleaseYaml")
-case "$subCommand" in
-  uninstall)
-    KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace $releaseName
-    ;;
-  *)
-    values=$(<<< "$helmReleaseYaml" | yq -y -er .spec.values)
-    if [[ -d "$sourceParameter" ]]; then
-      KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace $releaseName "$sourceParameter" --values <(<<< "$values") ${@}
-    elif <<< "$helmReleaseYaml" | yq -e '.apiVersion == "helm.fluxcd.io/v1"' > /dev/null; then
-      if <<< "$helmReleaseYaml" | yq -e .spec.chart.git > /dev/null; then
-        local gitPath
-        local gitUrl
-        local gitRef
-        gitPath="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.path // "."')"
-        gitUrl="$(<<< "$helmReleaseYaml" | yq -er .spec.chart.git)"
-        gitRef="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.ref // "master"')"
-        KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" _hr_git "$subCommand" "$gitUrl" "$gitRef" "$gitPath" "$namespace" "$releaseName" "$values" "$@"
-      else
-        KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace --repo $(<<< "$helmReleaseYaml" | yq -er .spec.chart.repository) $releaseName $(<<< "$helmReleaseYaml" | yq -er .spec.chart.name) --version $(<<< "$helmReleaseYaml" | yq -er .spec.chart.version) --values <(<<< "$values") "$@"
-      fi
-    else
-      local sourceNamespace
-      local sourceName
-      local sourceKind
-      local sourceResource
-      local chartName
-      local helmRepositoryUrl
-      sourceNamespace=$(<<< "$helmReleaseYaml" | yq -er ".spec.chart.spec.sourceRef.namespace // \"$namespace\"")
-      sourceName=$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.sourceRef.name)
-      sourceKind=$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.sourceRef.kind)
-      if [[ -z "$sourceParameter" ]]; then
-        local sourceYaml
-        sourceYaml=$(_hr_getYaml "$yaml" "" "$sourceKind")
-        sourceResource=$(<<< "$sourceYaml" | yq -erys "[.[] | select( (.metadata.namespace == \"$sourceNamespace\") and (.metadata.name == \"$sourceName\") )][0]")
-        if [[ "$?" != 0 ]]; then
-          sourceResource=$(kubectl --namespace=$sourceNamespace get $sourceKind $sourceName -o yaml)
-          if [[ "$?" != 0 ]]; then
-            helmRepositoryUrl="https://teutonet.github.io/teutonet-helm-charts"
-            echo "Source resource '$sourceNamespace/$sourceKind/$sourceName' not found in cluster nor in input" > /dev/stderr
-            vared -p "Please specify Helm Repository URL: " helmRepositoryUrl > /dev/null
-            sourceKind=HelmRepository
-            sourceResource=$'spec:\n  url: '"$helmRepositoryUrl"
-          fi
-        fi
-      elif ! [[ -z "$sourceParameter" ]]; then
-        sourceResource=$'spec:\n  url: '"$sourceParameter"
-      fi
-      chartName="$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.chart)"
-      case "$sourceKind" in
-        GitRepository)
+      values=$(<<< "$helmReleaseYaml" | yq -y -er .spec.values)
+      if [[ -d "$sourceParameter" ]]; then
+        KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace $releaseName "$sourceParameter" --values <(<<< "$values") ${@}
+      elif <<< "$helmReleaseYaml" | yq -e '.apiVersion == "helm.fluxcd.io/v1"' > /dev/null; then
+        if <<< "$helmReleaseYaml" | yq -e .spec.chart.git > /dev/null; then
+          local gitPath
           local gitUrl
           local gitRef
-          gitUrl="$(<<< "$sourceResource" | yq -er .spec.url)"
-          gitRef="$(<<< "$sourceResource" | yq -er '.spec.ref | if .branch then .branch elif .tag then .tag elif .semver then .semver elif .commit then .commit else "master" end')"
-          KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" _hr_git "$subCommand" "$gitUrl" "$gitRef" "$chartName" "$namespace" "$releaseName" "$values" "$@"
-          ;;
-        HelmRepository)
-          local chartVersion
-          helmRepositoryUrl="$(<<< "$sourceResource" | yq -er .spec.url)"
-          chartVersion="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.spec.version // "x.x.x"')"
-          KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace --repo "$helmRepositoryUrl" $releaseName "$chartName" --version "$chartVersion" --values <(<<< "$values") "$@"
-          ;;
-        *)
-          echo "'$sourceKind' is not implemented" > /dev/stderr
-          return 1
-          ;;
-      esac
-    fi
-    ;;
-esac
+          gitPath="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.path // "."')"
+          gitUrl="$(<<< "$helmReleaseYaml" | yq -er .spec.chart.git)"
+          gitRef="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.ref // "master"')"
+          KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" _hr_git "$subCommand" "$gitUrl" "$gitRef" "$gitPath" "$namespace" "$releaseName" "$values" "$@"
+        else
+          KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace --repo $(<<< "$helmReleaseYaml" | yq -er .spec.chart.repository) $releaseName $(<<< "$helmReleaseYaml" | yq -er .spec.chart.name) --version $(<<< "$helmReleaseYaml" | yq -er .spec.chart.version) --values <(<<< "$values") "$@"
+        fi
+      else
+        local sourceNamespace
+        local sourceName
+        local sourceKind
+        local sourceResource
+        local chartName
+        local helmRepositoryUrl
+        sourceNamespace=$(<<< "$helmReleaseYaml" | yq -er ".spec.chart.spec.sourceRef.namespace // \"$namespace\"")
+        sourceName=$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.sourceRef.name)
+        sourceKind=$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.sourceRef.kind)
+        if [[ -z "$sourceParameter" ]]; then
+          local sourceYaml
+          sourceYaml=$(_hr_getYaml "$yaml" "" "$sourceKind")
+          sourceResource=$(<<< "$sourceYaml" | yq -erys "[.[] | select( (.metadata.namespace == \"$sourceNamespace\") and (.metadata.name == \"$sourceName\") )][0]")
+          if [[ "$?" != 0 ]]; then
+            sourceResource=$(kubectl --namespace=$sourceNamespace get $sourceKind $sourceName -o yaml)
+            if [[ "$?" != 0 ]]; then
+              helmRepositoryUrl="https://teutonet.github.io/teutonet-helm-charts"
+              echo "Source resource '$sourceNamespace/$sourceKind/$sourceName' not found in cluster nor in input" > /dev/stderr
+              vared -p "Please specify Helm Repository URL: " helmRepositoryUrl > /dev/null
+              sourceKind=HelmRepository
+              sourceResource=$'spec:\n  url: '"$helmRepositoryUrl"
+            fi
+          fi
+        elif ! [[ -z "$sourceParameter" ]]; then
+          sourceResource=$'spec:\n  url: '"$sourceParameter"
+        fi
+        chartName="$(<<< "$helmReleaseYaml" | yq -er .spec.chart.spec.chart)"
+        case "$sourceKind" in
+          GitRepository)
+            local gitUrl
+            local gitRef
+            gitUrl="$(<<< "$sourceResource" | yq -er .spec.url)"
+            gitRef="$(<<< "$sourceResource" | yq -er '.spec.ref | if .branch then .branch elif .tag then .tag elif .semver then .semver elif .commit then .commit else "master" end')"
+            KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" _hr_git "$subCommand" "$gitUrl" "$gitRef" "$chartName" "$namespace" "$releaseName" "$values" "$@"
+            ;;
+          HelmRepository)
+            local chartVersion
+            helmRepositoryUrl="$(<<< "$sourceResource" | yq -er .spec.url)"
+            chartVersion="$(<<< "$helmReleaseYaml" | yq -er '.spec.chart.spec.version // "x.x.x"')"
+            KUBECONFIG="${REMOTE_KUBECONFIG:-$KUBECONFIG}" helm "${commands[@]}" --namespace $namespace --repo "$helmRepositoryUrl" $releaseName "$chartName" --version "$chartVersion" --values <(<<< "$values") "$@"
+            ;;
+          *)
+            echo "'$sourceKind' is not implemented" > /dev/stderr
+            return 1
+            ;;
+        esac
+      fi
+      ;;
+  esac
 }
 
 function hr() {
-helmrelease "template" "$@"
+  helmrelease "template" "$@"
 }
 
 function hrDiff() {
-HELM_DIFF_USE_UPGRADE_DRY_RUN=true helmrelease "diff" "$@"
+  HELM_DIFF_USE_UPGRADE_DRY_RUN=true helmrelease "diff" "$@"
 }
 
 function hrUpgrade() {
-helmrelease "upgrade" "$@"
+  helmrelease "upgrade" "$@"
 }
 
 function hrUninstall() {
-helmrelease "uninstall" "$@"
+  helmrelease "uninstall" "$@"
 }
 
 function hrInstall() {
-helmrelease "install" "$@"
+  helmrelease "install" "$@"
 }
