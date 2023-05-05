a29cad687e58f159a048bff662be67d556afaf73
lukelabrie <lplcleary@gmail.com>
2023-05-05 18:22:46 +0200

    have test script check python version

--- test_install.sh
+++ test_install.sh
@@ -1,6 +1,7 @@
  # set non-standard paths
-export PATH=$PATH:/opt/openmc/bin:$HOME/.local/lib/python3.10/site-packages
-export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python3.10/site-packages/pymoab-5.3.0-py3.10-linux-x86_64.egg
+python_version=$(python -V 2>&1 | cut -d' ' -f2 | cut -d'.' -f1-2)
+export PATH=$PATH:/opt/openmc/bin:$HOME/.local/lib/python$python_version/site-packages
+export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python$python_version/site-packages/pymoab-5.3.0-py$python_version-linux-x86_64.egg
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/MOAB/lib
 var=`echo /opt/nuclear-data/*hdf5 | head -n1`
 export OPENMC_CROSS_SECTIONS=$var/cross_sections.xml
@@ -16,3 +17,6 @@ rm *.xml
 rm *.h5
 rm *.stl
 rm *.h5m
+
+
+PYTHONPATH=/opt/MOAB/lib/python3.10/site-packages/:$PYTHONPATH
