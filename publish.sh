1a9f6047f8b14f86deac65dd3e7b2c56c797e127
burdoto <kaleidox@comroid.org>
2023-05-07 22:15:54 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,6 @@
 ﻿#!/bin/bash
 
 set -e # exit on error
-tar="rgx.tar.gz"
 
 dotnet clean
 dotnet build -c Release
@@ -11,10 +10,6 @@ makepkg --printsrcinfo > .SRCINFO
 git clean -f
 git reset --hard
 (git add .SRCINFO && git commit -m "SRCINFO" && git push) || true
-
-# create tarball
-rm -f "$tar" || true
-tar -zcvf "$tar" LICENSE PKGBUILD .SRCINFO rgx.csproj ./**.cs bin/Release/net6.0
 
 # push to aur
 if [ -z "$(git remote | grep aur)" ]; then
