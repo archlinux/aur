5b79f543a1e70bfe6a6d4964f3123fa4cba26f83
shadichy <hotrobb360web@gmail.com>
2023-05-13 16:54:34 +0700

    version 2

--- 30_android-x86.sh
+++ 30_android-x86.sh
@@ -27,59 +27,56 @@ OS="Android"
 
 # Default to disabling partition uuid support to maintian compatibility with
 # older kernels.
-GRUB_DISABLE_LINUX_PARTUUID=${GRUB_DISABLE_LINUX_PARTUUID-true}
+: "${GRUB_DISABLE_LINUX_PARTUUID:=true}"
 
 # Android x68 supported file systems
 # Note that by default, Android initrds don't support btrfs and some other
 # filesystems
-ANDROID_SUPPORTED_FS=${ANDROID_SUPPORTED_FS-"ext2 ext3 ext4 vfat ntfs"}
+: "${ANDROID_SUPPORTED_FS:="ext2 ext3 ext4 vfat exfat ntfs"}"
 
 # Default parameters
 # GRUB_CMDLINE_ANDROID=${GRUB_CMDLINE_ANDROID-"androidboot.hardware=android_x86_64 androidboot.selinux=permissive acpi_sleep=s3_bios,s3_mode"}
 . /etc/default/grub
 
-_android_entry() {
+sub_indent() { echo -e "$submenu_indentation$*"; }
+gfind() { grep "$2" <<<"${1}"; }
+
+android_entry() {
 	title="$1"
 	src="$2"
 	kernel="$3"
 	initrd="$4"
 	disk="$5"
-	devid="$6"
+	device_id="$6"
 	type="$7"
 
-	if [ "$type" != "default" ]; then
+	[ "$type" != "default" ] &&
 		title="$title, with kernel ${kernel##*/}, and initrd ${initrd##*/}"
-	fi
 
-	echo "menuentry '$(echo "$title" | grub_quote)' ${CLASS} \$menuentry_id_option '$OS-${src##*/}-${kernel##*/}-${initrd##*/}-$type-$devid' {" | sed "s/^/$submenu_indentation/"
+	sub_indent "menuentry '$(echo "$title" | grub_quote)' ${CLASS} \$menuentry_id_option '$OS-${src##*/}-${kernel##*/}-${initrd##*/}-$type-$device_id' {"
 
 	save_default_entry | grub_add_tab
 
 	# Use ELILO's generic "efifb" when it's known to be available.
 	# FIXME: We need an interface to select vesafb in case efifb can't be used.
 	if [ ! "$GRUB_GFXPAYLOAD_LINUX" ]; then
-		echo "	load_video" | sed "s/^/$submenu_indentation/"
-		if grep -qx "CONFIG_FB_EFI=y" "${config}" 2>/dev/null &&
-			grep -qx "CONFIG_VT_HW_CONSOLE_BINDING=y" "${config}" 2>/dev/null; then
-			echo "	set gfxpayload=keep" | sed "s/^/$submenu_indentation/"
-		fi
+		sub_indent "	load_video"
+		grep -qx "CONFIG_FB_EFI=y" "${config}" 2>/dev/null &&
+			grep -qx "CONFIG_VT_HW_CONSOLE_BINDING=y" "${config}" 2>/dev/null &&
+			sub_indent "	set gfxpayload=keep"
+
 	else
-		if [ "x$GRUB_GFXPAYLOAD_LINUX" != xtext ]; then
-			echo "	load_video" | sed "s/^/$submenu_indentation/"
-		fi
-		echo "	set gfxpayload=$GRUB_GFXPAYLOAD_LINUX" | sed "s/^/$submenu_indentation/"
+		[ "$GRUB_GFXPAYLOAD_LINUX" = text ] ||
+			sub_indent "	load_video"
+		sub_indent "	set gfxpayload=$GRUB_GFXPAYLOAD_LINUX"
 	fi
 
-	echo "	insmod gzio" | sed "s/^/$submenu_indentation/"
+	sub_indent "	insmod gzio"
+	echo "$disk" | sed "s/^/$submenu_indentation	/gm"
+	sub_indent "	linux $kernel $GRUB_CMDLINE_ANDROID root=/dev/ram0 SRC=$src"
+	sub_indent "	initrd $initrd"
 
-	echo "$disk" | sed "s/^/$submenu_indentation	/"
-
-	echo "	linux $kernel $GRUB_CMDLINE_ANDROID root=/dev/ram0 SRC=$src" | sed "s/^/$submenu_indentation/"
-	echo "	initrd $initrd" | sed "s/^/$submenu_indentation/"
-
-	sed "s/^/$submenu_indentation/" <<EOF
-}
-EOF
+	sub_indent "}\n"
 }
 
 _mount_fs() {
@@ -92,11 +89,9 @@ _mount_fs() {
 
 _find_buildprop() {
 	if [ -d "$1" ]; then
-		if [ -f "$1/system/build.prop" ]; then
-			cat "$1/system/build.prop"
-		elif [ -f "$1/build.prop" ]; then
-			cat "$1/build.prop"
-		fi
+		buildprop_file=$1/system/build.prop
+		[ -f "$1/system/build.prop" ] || buildprop_file=$1/build.prop
+		cat "$buildprop_file"
 	elif [ -f "$1" ]; then
 		case "$1" in
 		*system.img)
@@ -115,52 +110,49 @@ _find_buildprop() {
 	fi
 }
 
-_grep_find() {
-	grep "$2" <<EOF
-${1}
-EOF
-}
-
-_find_os() {
-	avail_mntpt="$(lsblk -pndo mountpoints "$1" | tail -1)"
-	if [ "$avail_mntpt" ]; then
-		device="$avail_mntpt"
+find_os() {
+	pre_mounted="$(lsblk -pndo mountpoints "$1" | tail -1)"
+	if [ "$pre_mounted" ]; then
+		mountpoint="$pre_mounted"
 	else
-		[ "$2" = "ntfs" ] && isntfs="ntfs-3g"
-		device="$(_mount_fs "$1" "" "$isntfs")"
+		fs=$2
+		if [ "$2" = "ntfs" ]; then
+			modinfo ntfs3 >/dev/null && fs=ntfs3 || fs=ntfs-3g
+		fi
+		mountpoint="$(_mount_fs "$1" "" "$fs")"
 	fi
 
 	# The damn `find` command
-	foundpaths="$(find $device \( \
-		-iname 'system' -o \
-		-iname 'system.img' -o \
-		-iname 'system.sfs' -o \
-		-iname 'system.efs' \) \
-		-not \( -path "$device/bin/*" -prune \) \
-		-not \( -path "$device/boot/efi/*" -prune \) \
-		-not \( -path "$device/dev/*" -prune \) \
-		-not \( -path "$device/etc/*" -prune \) \
-		-not \( -path "$device/lib/*" -prune \) \
-		-not \( -path "$device/lib64/*" -prune \) \
-		-not \( -path "$device/lib32/*" -prune \) \
-		-not \( -path "$device/proc/*" -prune \) \
-		-not \( -path "$device/run/*" -prune \) \
-		-not \( -path "$device/sbin/*" -prune \) \
-		-not \( -path "$device/sys/*" -prune \) \
-		-not \( -path "$device/usr/*" -prune \) \
-		-not \( -path "$device/var/*" -prune \) \
-		-not \( -path "$device/tmp/*" -prune \) \
-		-not \( -path "$device/Program\ Files/*" -prune \) \
-		-not \( -path "$device/Program\ Files\ (x86)/*" -prune \) \
-		-not \( -path "$device/ProgramData/*" -prune \) \
-		-not \( -path "$device/Windows/*" -prune \) \
-		-not \( -path "$device/EFI/*" -prune \) \
+	foundpaths="$(find "$mountpoint" \( \
+		-name 'system' -o \
+		-name 'system.img' -o \
+		-name 'system.sfs' -o \
+		-name 'system.efs' \) \
+		-not \( -path "$mountpoint/bin/*" -prune \) \
+		-not \( -path "$mountpoint/boot/efi/*" -prune \) \
+		-not \( -path "$mountpoint/dev/*" -prune \) \
+		-not \( -path "$mountpoint/etc/*" -prune \) \
+		-not \( -path "$mountpoint/lib/*" -prune \) \
+		-not \( -path "$mountpoint/lib64/*" -prune \) \
+		-not \( -path "$mountpoint/lib32/*" -prune \) \
+		-not \( -path "$mountpoint/proc/*" -prune \) \
+		-not \( -path "$mountpoint/run/*" -prune \) \
+		-not \( -path "$mountpoint/sbin/*" -prune \) \
+		-not \( -path "$mountpoint/sys/*" -prune \) \
+		-not \( -path "$mountpoint/usr/*" -prune \) \
+		-not \( -path "$mountpoint/var/*" -prune \) \
+		-not \( -path "$mountpoint/tmp/*" -prune \) \
+		-not \( -path "$mountpoint/Program\ Files/*" -prune \) \
+		-not \( -path "$mountpoint/Program\ Files\ (x86)/*" -prune \) \
+		-not \( -path "$mountpoint/ProgramData/*" -prune \) \
+		-not \( -path "$mountpoint/Windows/*" -prune \) \
+		-not \( -path "$mountpoint/EFI/*" -prune \) \
 		2>/dev/null)"
 
 	if [ ! "$foundpaths" ]; then
-		if [ ! "$avail_mntpt" ]; then
-			umount -lf "$device"
-			rm -rf "$device"
+		if [ ! "$pre_mounted" ]; then
+			umount -lf "$mountpoint"
+			rm -rf "$mountpoint"
 		fi
 		return
 	fi
@@ -169,56 +161,48 @@ _find_os() {
 	disk="$(prepare_grub_to_access_device "$1")"
 	boot_device_id="$(grub_get_device_id "$1")"
 
-	IFS="
-"
-	for path in $foundpaths; do
+	while read -r path; do
 		[ "${path}" ] || continue
 		systempath="$path"
 
 		path=${path%/*}
 
 		# Getting build.prop contents
-		buildprop="$(_find_buildprop "$systempath")"
-		[ "${buildprop}" ] || continue
+		buildprop="$(_find_buildprop "$systempath")" || continue
 
-		buildver="$(
-			grep -E "ro.([a-z]*).version" <<EOF | grep -v build
-${buildprop}
-EOF
-		)"
-		# for ver in "${buildver[@]}"; do echo "> $ver"; done
-		# continue
+		# Parse version
+		buildver="$(grep -E "ro.([a-z]*).version" <<<"$(grep -v build <<<"${buildprop}")")"
 		for version in $buildver; do
 			case $version in
 			ro.bliss.version*)
 				os="Bliss OS"
-				ver="$(_grep_find "$buildprop" "ro.bliss.version=")"
+				ver="$(gfind "$buildprop" "ro.bliss.version=")"
 				break
 				;;
 			ro.lineage.version*)
 				os="Lineage OS"
-				ver="$(_grep_find "$buildprop" "ro.lineage.version=")"
+				ver="$(gfind "$buildprop" "ro.lineage.version=")"
 				;;
 			ro.phoenix.version*)
 				os="Phoenix OS"
-				ver="$(_grep_find "$buildprop" "ro.phoenix.version.code=")"
+				ver="$(gfind "$buildprop" "ro.phoenix.version.code=")"
 				break
 				;;
 			ro.primeos.version*)
 				os="Prime OS"
-				ver="$(_grep_find "$buildprop" "ro.primeos.version.name=")"
+				ver="$(gfind "$buildprop" "ro.primeos.version.name=")"
 				break
 				;;
 			*)
 				os="AOSP"
-				ver="$(_grep_find "$buildprop" "ro.build.version.release=") $(_grep_find "$buildprop" "ro.build.flavor=")"
+				ver="$(gfind "$buildprop" "ro.build.version.release=") $(gfind "$buildprop" "ro.build.flavor=")"
 				;;
 			esac
 		done
-
-		src=$(echo "${path#"$device"}" | sed 's/ /\\ /g')
 		ver=${ver##*=}
-		isdefault="default"
+
+		src=$(echo "${path#"$mountpoint"}" | sed 's/ /\\ /g')
+		type=default
 
 		echo "Found $os version $ver on $1" >&2
 
@@ -229,57 +213,54 @@ EOF
 		# yet, so it's empty. In a submenu it will be equal to '\t' (one tab).
 		submenu_indentation=""
 
+		# Checking for cpu id/manufacturer and microcode file
+		vendor=$(grep vendor_id /proc/cpuinfo | head -1 | awk '{print $3}')
+		case "$vendor" in
+		"GenuineIntel") : "intel" ;;
+		"AuthenticAMD") : "amd" ;;
+		esac
+		vendor=$_
+		microcode=$(find "$path" -maxdepth 1 -type f \( -iname "$vendor-ucode.img" -o -iname "$vendor-microcode.img" \) -print -quit 2>/dev/null)
+
 		for kernel in "$path"/vmlinu[xz]* "$path"/kernel*; do
 			[ -f "$kernel" ] || continue
-
-			# Checking for cpu id/manufacturer and microcode file
-			cpuid=$(grep vendor_id /proc/cpuinfo | head -1 | awk '{print $3}')
-			case "$cpuid" in
-			"GenuineIntel")
-				brand="intel"
-				;;
-			"AuthenticAMD")
-				brand="amd"
-				;;
-			esac
-			microcode=$(find "$path" -maxdepth 1 -type f \( -iname "$brand-ucode.img" -o -iname "$brand-microcode.img" \) -print -quit 2>/dev/null)
-
 			for initrd in "$path"/initrd*.img "$path"/initramfs*.img; do
-				[ -e "$initrd" ] || continue
+				[ -f "$initrd" ] || continue
 
-				entry="$(_android_entry "$os $ver" "$src" "${src}${kernel#"$path"}" "${microcode:+"${src}${microcode#"$path"} "}${src}${initrd#"$path"}" "$disk" "$boot_device_id" "$isdefault")"
+				entry() {
+					android_entry \
+						"$os $ver" \
+						"$src" \
+						"${src}${kernel#"$path"}" \
+						"${microcode:+"${src}${microcode#"$path"} "}${src}${initrd#"$path"}" \
+						"$disk" \
+						"$boot_device_id" \
+						"$1"
+				}
 
-				if [ "$isdefault" = "default" ]; then
-					MAIN_ENTRY="$entry"
-					isdefault="extended"
-					submenu_indentation="$grub_tab"
-					EXTENDED_ENTRIES="$EXTENDED_ENTRIES$(
-						sed "s/^/$submenu_indentation/" <<EOF
-$entry
-EOF
-					)
-"
-				else
-					EXTENDED_ENTRIES="$EXTENDED_ENTRIES$entry
-"
+				if [ "$type" = "default" ]; then
+					MAIN_ENTRY="$(entry default)"
+					type=extended
+					submenu_indentation=$grub_tab
 				fi
+				EXTENDED_ENTRIES="$EXTENDED_ENTRIES\n$(entry extended)"
 			done
 		done
 
-		cat <<EOF
+		echo -e "
+
 $MAIN_ENTRY
 
-submenu '$(echo "Advanced options for $os $ver" | grub_quote)' \$menuentry_id_option '$OS-${src##*/}-$isdefault-$boot_device_id' { 
+submenu '$(echo "Advanced options for $os $ver" | grub_quote)' \$menuentry_id_option '$OS-${src##*/}-$type-$boot_device_id' { 
 $EXTENDED_ENTRIES
 }
 
-EOF
-	done
-	unset IFS
+"
+	done <<<"$foundpaths"
 
-	if [ ! "$avail_mntpt" ]; then
-		umount -lf "$device"
-		rm -rf "$device"
+	if [ ! "$pre_mounted" ]; then
+		umount -lf "$mountpoint"
+		rm -rf "$mountpoint"
 	fi
 }
 
@@ -287,11 +268,16 @@ echo "Detecting android..." >&2
 
 # Scan for Android partitions that contain both system(folder, .img, .sfs)
 # and data(folder, .img, .sfs)
-BLKLIST="$(ls /sys/class/block/ | grep ".*[0-9]$" | grep -Ev "loop|ram|nbd|fd" | sed "s|^|/dev/|g")"
-for blk in $BLKLIST; do
-	blkfs=$(lsblk -prndo fstype "$blk")
-	[ "$blkfs" ] && { echo "$ANDROID_SUPPORTED_FS" | grep "$blkfs" >/dev/null; } || continue
-	cat <<EOF
-$(_find_os "$blk" "$blkfs")
-EOF
-done
+
+while read -r blk; do
+	blk_fs=$(lsblk -prndo fstype "$blk")
+	[ "$blk_fs" ] &&
+		gfind "$ANDROID_SUPPORTED_FS" "$blk_fs" >/dev/null ||
+		continue
+	find_os "$blk" "$blk_fs"
+done <<<"$(
+	ls /sys/class/block/ |
+		grep ".*[0-9]$" |
+		grep -Ev "loop|ram|nbd|fd" |
+		sed "s|^|/dev/|g"
+)"
--- PKGBUILD
+++ PKGBUILD
@@ -1,15 +1,17 @@
 # Maintainer: shadichy <shadichy.dev@gmail.com>
 
 pkgname='grub-android-x86'
-pkgver='0.0.1'
+pkgver='0.0.2'
 pkgrel='1'
-pkgdesc='GRUB Configuration for Android x86 detection (Only works with NTFS, extFS and vFat)'
+pkgdesc='GRUB Configuration for Android x86 detection (Only works with NTFS, extFS and vFat/exFat)'
 arch=('any')
 license=('MIT')
 url='https://github.com/shadichy/grub-android-x86.git'
-depends=( 'grub' 'bash' )
-source=( '30_android-x86.sh' )
-sha512sums=( 'SKIP' )
+depends=('grub' 'bash')
+source=('30_android-x86.sh')
+sha512sums=(
+	'3c35a20767b3c770b60774b0bbf2a06e5b2cd19e6654c9453e404df1fc519ad5abec2857d1c066fa39bd5f1c4e2bf97e23d3c10b34617770142bdad375de212d'
+)
 
 package() {
 	cd "$srcdir"
