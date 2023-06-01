545169efb8bc79db020bb11588cd108f0283216a
sedrubal <dev@sedrubal.de>
2023-06-01 15:59:06 +0200

    Make patch script nicer

--- patch_code_oss_product.py
+++ patch_code_oss_product.py
@@ -1,31 +1,41 @@
-#!/usr/bin/env python
-from sys import argv
-from json import load, dump, JSONDecodeError
+#!/usr/bin/env python3
 
-PRODUCT_JSON_LOCATION = "/usr/lib/code/product.json"
+import json
+import sys
+from pathlib import Path
+
+PRODUCT_JSON_PATH = Path("/usr/lib/code/product.json")
+
+EXTENSION_GALLERY_CODE_OSS = {
+    "serviceUrl": "https://open-vsx.org/vscode/gallery",
+    "itemUrl": "https://open-vsx.org/vscode/item",
+}
+EXTENSION_GALLERY_VSCODE = {
+    "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
+    "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
+    "itemUrl": "https://marketplace.visualstudio.com/items",
+}
+
+
+def main():
+    try:
+        with PRODUCT_JSON_PATH.open() as file:
+            product = json.load(file)
+    except json.JSONDecodeError as exc:
+        sys.exit(
+            f"error: couldn't parse local product.json or fetch a new one from the web: {exc}"
+        )
+
+    if "-R" in sys.argv:
+        product["extensionsGallery"] = EXTENSION_GALLERY_CODE_OSS
+        product["linkProtectionTrustedDomains"] = ["https://open-vsx.org"]
+    else:
+        product["extensionsGallery"] = EXTENSION_GALLERY_VSCODE
+        product.pop("linkProtectionTrustedDomains", None)
+
+    with PRODUCT_JSON_PATH.open(mode="w") as file:
+        json.dump(product, file, indent=2)
 
 
 if __name__ == "__main__":
-    try:
-        with open(PRODUCT_JSON_LOCATION) as file:
-            product = load(file)
-    except JSONDecodeError:
-        print(
-            "error: couldn't parse local product.json or fetch a new one from the web")
-        exit(1)
-    if '-R' in argv:
-        product["extensionsGallery"] = {
-            "serviceUrl": "https://open-vsx.org/vscode/gallery",
-            "itemUrl": "https://open-vsx.org/vscode/item",
-        }
-        product["linkProtectionTrustedDomains"] = ["https://open-vsx.org"]
-    else:
-        product["extensionsGallery"] = {
-            "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
-            "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
-            "itemUrl": "https://marketplace.visualstudio.com/items"
-        }
-        product.pop("linkProtectionTrustedDomains", None)
-
-    with open(PRODUCT_JSON_LOCATION, mode='w') as file:
-        dump(product, file, indent=2)
+    main()
