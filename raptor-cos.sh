57a0c671be824f932098d14cdc409234519234b7
Alexander Jacocks <alexander@redhat.com>
2023-06-11 20:56:30 -0400

    update raptor launch script

--- raptor-cos.sh
+++ raptor-cos.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-cd /usr/share/raptor-cos
+homedir=$( getent passwd "$USER" | cut -d: -f6 )
+rdir=${homedir}/.local/share/raptor-cos
+
+if [ ! -d "${rdir}" ]; then
+  mkdir -p "${rdir}"
+fi
+
+for file in /usr/share/raptor-cos/*; do
+  if [ ! -h "${rdir}/`basename ${file}`" ]; then
+    ln -s "${file}" "${rdir}/"
+  fi
+done
+
+cd "${rdir}"
 ./raptor-cos
