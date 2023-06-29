b035f27c4a5d0d904c1d77c2c25f54c2734c198a
Donald Carr <d@chaos-reins.com>
2023-06-29 01:04:41 -0700

    update toolchain.cmake to reflect qt-sdk
    Change-Id: Ie4ee527ebed6b5811bcd01e8fa2e243e7d328ad1

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,8 +1,9 @@
-set( CMAKE_SYSTEM_NAME Linux )
+# this triggers CMAKE_CROSSCOMPILING!
+#set( CMAKE_SYSTEM_NAME Linux )
 # e.g. to use mold with Clang
-# set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")
-# set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=mold")
-# set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=mold")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=mold")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=mold")
 
 set(CMAKE_DISABLE_PRECOMPILE_HEADERS 1)
 # e.g. to use mold with Clang
@@ -10,5 +11,6 @@ set(CMAKE_DISABLE_PRECOMPILE_HEADERS 1)
 #set(CMAKE_MODULE_LINKER_FLAGS_INIT "-fuse-ld=lld")
 #set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
 
-#set(CMAKE_CXX_COMPILER "clang++")
-#set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_TOOLCHAIN_PREFIX "llvm-")
