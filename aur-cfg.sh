1a00d5e8575e90c505b13f2a70c0d78ab28f1e05
David Barri <japgolly@gmail.com>
2023-07-03 15:25:17 +1000

    Fix version detection

--- aur-cfg.sh
+++ aur-cfg.sh
@@ -1,9 +1,10 @@
 function get_latest_version {
   curl -s https://get.atomicwallet.io/download/ \
     | grep 'atomicwallet-2.*\.rpm"' \
-    | sort \
+    | perl -pe 's/.*"atomicwallet-(.+?).rpm".*/\1/; s/\./ /g' \
+    | sort -k1n -k2n -k3n \
     | tail -1 \
-    | sed 's/.*wallet-\(2[0-9.-]*\)\.rpm.*/\1/'
+    | tr ' ' .
 }
 
 version_suffix_separator=-
