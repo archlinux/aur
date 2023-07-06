2d793c8ba0133643635ae16d78a8fba48569f61b
Mattia Borda <mattiagiovanni.borda@icloud.com>
2023-07-06 16:47:46 +0200

    Add missing sh file

---
+++ adwaita-for-steam.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DATA_DIR="/usr/share/adwaita-for-steam"
+SCRIPT="install.py"
+
+[[ ! -d "$DATA_DIR" ]] && echo "$DATA_DIR not found" && exit
+[[ ! -f "$DATA_DIR/install.py" ]] && echo "$DATA_DIR/install.py not found" && exit
+
+cd "$DATA_DIR" || exit
+python "install.py" "$@"
