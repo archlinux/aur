97028c450d3a683a089f834a256bdcb63578930b
zxp19821005 <zxp19821005@163.com>
2023-07-11 08:46:50 +0800

    update to 1.5.12

--- android-toolkit.sh
+++ android-toolkit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron /opt/android-toolkit/app.asar "$@"
+    exec electron25 /opt/android-toolkit/app.asar "$@"
 else
-    exec electron --no-sandbox /opt/android-toolkit/app.asar "$@"
+    exec electron25 --no-sandbox /opt/android-toolkit/app.asar "$@"
 fi
