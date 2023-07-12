4bfe5af1e4a16722d2927331ded7cac2f64e2917
zxp19821005 <zxp19821005@163.com>
2023-07-12 09:58:14 +0800

    first release

---
+++ electronim.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
+    exec electron /opt/electronim/app.asar "$@"
+else
+    exec electron --no-sandbox /opt/electronim/app.asar "$@"
+fi
