0d9ebf528cfb0c0a8d2ec7d8bfcee47e79b1ad8d
zxp19821005 <zxp19821005@163.com>
2023-07-15 12:52:45 +0800

    update to 2.30.0

--- listen1-desktop.sh
+++ listen1-desktop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+_ELECTRON=/usr/bin/electron13
+_ASAR="/opt/listen1-desktop/listen1-desktop.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron13 /opt/listen1-desktop/listen1-desktop.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} "$@"
 else
-    exec electron13 --no-sandbox /opt/listen1-desktop/listen1-desktop.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
 fi
