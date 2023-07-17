ec07f53b1200381e92be69aadc91acc4a098a2a8
zxp19821005 <zxp19821005@163.com>
2023-07-17 13:54:18 +0800

    update to 23.03.3

--- backend-ai-desktop.sh
+++ backend-ai-desktop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+_ELECTRON=/usr/bin/electron25
+_ASAR="/opt/backend-ai-desktop/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron /opt/backend-ai-desktop/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} "$@"
 else
-    exec electron --no-sandbox /opt/backend-ai-desktop/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
 fi
