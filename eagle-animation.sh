59953e767dba79dbfae95a2169be22ceda537f6d
zxp19821005 <zxp19821005@163.com>
2023-07-21 09:07:56 +0800

    update to 2.3.0

--- eagle-animation.sh
+++ eagle-animation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+_ELECTRON=/usr/bin/electron25
+_ASAR="/opt/eagle-animation/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron23 /opt/eagle-animation/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} "$@"
 else
-    exec electron23 --no-sandbox /opt/eagle-animation/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
 fi
