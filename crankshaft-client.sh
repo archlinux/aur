12cbd89da9df700ad2f9fe61e090a343127f6176
zxp19821005 <zxp19821005@163.com>
2023-08-02 18:37:23 +0800

    fix errors

--- crankshaft-client.sh
+++ crankshaft-client.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+_ELECTRON=/usr/bin/electron10
+_ASAR="/opt/crankshaft-client/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron /opt/crankshaft-client/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} "$@"
 else
-    exec electron --no-sandbox /opt/crankshaft-client/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
 fi
