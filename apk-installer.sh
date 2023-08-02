4b9504cbc9f4ec59d11264eb967557a10e883eb0
zxp19821005 <zxp19821005@163.com>
2023-08-02 11:22:43 +0800

    fix errors

--- apk-installer.sh
+++ apk-installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+_ELECTRON=/usr/bin/electron10
+_ASAR="/opt/apk-installer/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
-    exec electron /opt/apk-installer/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} "$@"
 else
-    exec electron --no-sandbox /opt/apk-installer/app.asar "$@"
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
 fi
