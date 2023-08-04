602179afb1c36911c27539194550dbfaef0a7463
Claudia Pellegrino <aur@cpellegrino.de>
2023-08-04 11:12:39 +0200

    Fix module search path

--- executable-shim.js
+++ executable-shim.js
@@ -1,14 +1,29 @@
 #!/usr/bin/env node
-const { argv, cwd, exit } = require('node:process');
 
-const patchAsar = require('../source/index.js');
-const args = process.argv.slice(2);
+const path = require('node:path');
+const { argv, cwd, env, exit } = require('node:process');
+
+const args = argv.slice(2);
 
 if (args.length < 2) {
-  console.info(`Usage: ${argv[1]} IN.asar PATCHDIR [OUT.asar]`)
+  console.info(
+    `Usage: ${path.basename(argv[1])} IN.asar PATCHDIR [OUT.asar]`);
   exit(1);
-} else if (args.length < 3) {
+}
+
+// Add patch directory’s parent to NODE_PATH to make it require-able
+const patchDir = args[1];
+const moduleSearchDir = path.dirname(path.resolve(patchDir));
+env.NODE_PATH = moduleSearchDir;
+require('module').Module._initPaths();
+
+const patchAsar = require('../source/index.js');
+
+if (args.length < 3) {
   patchAsar(...args, { workingDirectory: cwd() });
 } else {
-  patchAsar(args[0], args[1], { outputFile: args[2], workingDirectory: cwd() });
+  patchAsar(args[0], args[1], {
+    outputFile: args[2],
+    workingDirectory: cwd(),
+  });
 }
