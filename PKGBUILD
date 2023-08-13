archlinux/aur

commit 16b30022b227f81a6f30307140352a67cc5128e2

Update the package maintainer's name and E-mail in comment

PKGBUILD
@@ -1,4 +1,4 @@
-# Maintainer: Robin Nehls <aur@manol.is>
+# Maintainer: Vadim Yanitskiy <axilirator@gmail.com>
 
 pkgname=gr-fosphor-git
 pkgver=r133.7b6b996

commit ba489a7671e75809c394c1e59749abe1c603cd92

Add Robin Nehls back as a Contributor
https://wiki.archlinux.org/title/AUR_submission_guidelines#Rules_of_submission

Fixes: 16b3002 "Update the package maintainer's name and E-mail in comment"

PKGBUILD
@@ -1,4 +1,5 @@
 # Maintainer: Vadim Yanitskiy <axilirator@gmail.com>
+# Contributor: Robin Nehls <aur@manol.is>
 
 pkgname=gr-fosphor-git
 pkgver=r133.7b6b996