f5356c52ffdc70f469536df8c8e9196e11ebb706
Maarten de Vries <maarten@de-vri.es>
2023-08-29 16:58:38 +0200

    Update to 4.96.1.

--- ueye-config.cmake
+++ ueye-config.cmake
@@ -1,6 +1,6 @@
 set(ueye_MAJOR_VERSION 4)
-set(ueye_MINOR_VERSION 95)
-set(ueye_PATCH_VERSION 0)
+set(ueye_MINOR_VERSION 96)
+set(ueye_PATCH_VERSION 1)
 set(ueye_VERSION ${ueye_MAJOR_VERSION}.${ueye_MINOR_VERSION}.${ueye_PATCH_VERSION})
 
 add_library(ueye SHARED IMPORTED)
