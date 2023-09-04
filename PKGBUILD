81ba9e77681101de3269f27327b94c96fbc0d974
Alireza |S.N| <alireza6677@gmail.com>
2023-09-04 19:03:07 +0330

    Support the new nautilus versions

--- PKGBUILD
+++ PKGBUILD
@@ -1,21 +1,18 @@
 pkgname=nautilus-send-to-bluetooth
-pkgver=1.0.1
+pkgver=2.0
 pkgrel=1
 pkgdesc="Adds 'Send to bluetooth device' to nautilus right-click menu"
 arch=('any')
 url='https://github.com/alireza6677/nautilus-send-to-bluetooth'
 license=('GPL3')
-depends=( 'python' 'nautilus' 'gnome-bluetooth' 'python-nautilus')
+depends=( 'python' 'nautilus>=44' 'blueman' 'python-nautilus')
 provides=("nautilus-send-to-bluetooth")
-#source=("${pkgname}::git+https://github.com/alireza6677/apk-preview.git")
-#md5sums=('SKIP')
-
 
 package() {
 	cd "${srcdir}/" 
             
         install -d ${pkgdir}/usr/share/nautilus-python/extensions/
 
-        cp -a ../SendToBluetooth.py  ${pkgdir}/usr/share/nautilus-python/extensions/
+        cp -a ../SendToBluetooth.py ${pkgdir}/usr/share/nautilus-python/extensions/SendToBluetooth.py
 }
 
--- SendToBluetooth.py
+++ SendToBluetooth.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
 
-import os, gi
-
-try:
-    gi.require_version('Nautilus', '3.0')
-except:
-    exit(1)
-
-from gi.repository import Nautilus, GObject
+import os
 import subprocess
+from urllib.parse import unquote, urlparse
 
-class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
+import gi
+from gi.repository import GObject, Nautilus
+
+gi.require_version("Gtk", "4.0")
+
+
+def uri_to_path(file):
+    p = urlparse(file.get_activation_uri())
+    return os.path.abspath(os.path.join(p.netloc, unquote(p.path)))
+
+
+class SendToBluetoothExtension(GObject.GObject, Nautilus.MenuProvider):
     def __init__(self):
-        pass
+        GObject.Object.__init__(self)
 
     def send_action(self, menu, files):
         paths = []
         for file in files:
-            paths.append(file.get_uri())
-        subprocess.Popen(['/usr/bin/bluetooth-sendto'] + paths )
+            paths.append(uri_to_path(file))
+        subprocess.Popen(["/usr/bin/blueman-sendto"] + paths)
 
-    def get_file_items(self, window, files):
+    def get_file_items(self, *args):
+        files = args[-1]
         if len(files) < 1:
             return
-        
+
         for file in files:
             if file.is_directory():
                 return
@@ -31,9 +37,8 @@ class ColumnExtension(GObject.GObject, Nautilus.MenuProvider):
         item = Nautilus.MenuItem(
             name="SendToBluetooth::MenuItem",
             label="Send to bluetooth device",
-            tip="Send to bluetooth device"
+            tip="Send to bluetooth device",
         )
-        item.connect('activate', self.send_action, files)
-        
-        return [item]
+        item.connect("activate", self.send_action, files)
 
+        return [item]
