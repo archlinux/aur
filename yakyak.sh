38b40d9bdc5853ff3041f00ec03e2e25318c4730
zxp19821005 <zxp19821005@163.com>
2023-09-09 02:10:31 +0800

    fix errors

--- yakyak.sh
+++ yakyak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron13
-_ASAR="/opt/yakyak/app.asar"
+_ASAR="/opt/yakyak/yakyak.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
