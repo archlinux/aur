92cfd60ecc5ccef9bb8634165e8c0239b3a82f16
[object Object] <yamldeveloper@proton.me>
2023-09-10 07:26:05 +0300

    fix kde issue, message format

--- configure_xrdp.sh
+++ configure_xrdp.sh
@@ -12,12 +12,15 @@ if grep -qE '1|y|Y' "$XRDP_CONFIGURED" 2>/dev/null; then
   exit 0
 fi
 
-# both vsock/tcp
+# use both vsock/tcp
 # https://github.com/neutrinolabs/xrdp/issues/1260#issuecomment-753970464
+
+# https://devicetests.com/fix-xrdp-connection-issue-kde-plasma-ubuntu-20-04
 sed -i_orig -e 's/port=3389/port=vsock:\/\/-1:3389 tcp:\/\/:3389/g' \
   -e 's/security_layer=negotiate/security_layer=rdp/g' \
   -e 's/crypt_level=high/crypt_level=none/g' \
   -e 's/bitmap_compression=true/bitmap_compression=false/g' \
+  -e 's/^new_cursors=true/new_cursors=false/g' \
   /etc/xrdp/xrdp.ini
 
 # rename the redirected drives to 'shared-drives'
