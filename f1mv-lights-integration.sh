9952a90a4962c89f2519e90e5ee8d9ca2a59d277
zxp19821005 <zxp19821005@163.com>
2023-09-15 11:42:29 +0800

    update to 2.2.2

--- f1mv-lights-integration.sh
+++ f1mv-lights-integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron23
-_ASAR="/opt/f1mv-lights-integration/f1mv-lights-integration.asar"
+_ASAR="/opt/f1mv-lights-integration/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
