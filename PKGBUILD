88d9bf4b9229226da0340bc0e38a31790e9292f9
zxp19821005 <zxp19821005@163.com>
2023-09-18 12:49:36 +0800

    fix errors

--- PKGBUILD
+++ PKGBUILD
@@ -1,32 +1,32 @@
 # Maintainer: zxp19821005 <zxp19821005 at 163 dot com>
 _officalname=quarkclouddrive
 pkgname="deepin-wine-${_officalname}"
-_pkgdownloadname=QuarkCloudDrive
+_downname=QuarkCloudDrive
 _installpkgpath=quark-cloud-drive
 pkgver=2.5.46
 _sparkpkgname=cn.quarkclouddrive.spark
 _sparkver=2.5.39spark2
-pkgrel=1
+pkgrel=2
 pkgdesc="夸克网盘是夸克推出的一款云服务产品，可轻松进行照片、视频、文档、音频等文件的在线备份、同步和分享，并支持电脑/手机/iPad端等多个端。"
 arch=("x86_64")
 url="https://pan.quark.cn/"
 license=('custom:freeware')
-depends=('deepin-wine6-stable' 'spark-dwine-helper' 'xdg-utils' 'hicolor-icon-theme' 'sh')
+depends=('deepin-wine6-stable' 'spark-dwine-helper' 'xdg-utils' 'hicolor-icon-theme' 'sh' 'p7zip')
 optdepends=('wqy-microhei' 'wqy-zenhei')
-conflicts=()
+conflicts=("${_officalname}")
 provides=("Quark")
 install="${pkgname}.install"
-source=("${_sparkpkgname}_${_sparkver}_amd64.deb::https://mirrors.sdu.edu.cn/spark-store-repository/store/network/${_sparkpkgname}/${_sparkpkgname}_${_sparkver}_amd64.deb"
-    "${_installpkgpath}-${pkgver}.exe::https://pdds-c1-cdn.quark.cn/82-4/stfile/2308/715cf5188598f7c4928adcf2ff91b5d9/${_pkgdownloadname}-v${pkgver}-release-pckk%40other_ch-20230821203412.exe?auth_key=1694083125-0-0-7bbdf6a817b0865a087b15b903c179f4&SESSID=245778a2d455b8d11a0d968f5fdd4dcb&ext=uc.exe"
+source=("${_sparkpkgname}_${_sparkver}_amd64.deb::https://d.store.deepinos.org.cn/store/network/${_sparkpkgname}/${_sparkpkgname}_${_sparkver}_amd64.deb"
+    "${_installpkgpath}-${pkgver}.exe::https://webcdn.m.qq.com/spcmgr/download/${_downname}-v${pkgver}-release-pckk@tencent_promote-20230822152425%20(1).exe"
     "LICENSE.html::https://terms.alicdn.com/legal-agreement/terms/c_platform_service_agreement/20230516130129722/20230516130129722.html"
     "${pkgname}.install"
     "run.sh")
 sha256sums=('28d43ab249de81f945c033d28938e8252edc20f2bac887efd4648be3d67cffae'
-            '7d4647a22654b79a00885db9db352fa8cccef00ffd8fd72045e5cc3cca55e739'
+            'c420702180c344bbca373503ed7d33d6646d40712359d63dd2394332ca6154e0'
             'b8252eb8c22ea41cc43c9436f341a78cac19cbc71c3593fa1b042fc9136f6767'
             'd7f46cae43addb386fd3dddf469530b6942143c2a4ce00e1b92d7f256ed90b70'
-            'b6aead28726888e08b0ac7b94734f4d43facdaa7a7f503866d8276327d18594d')
-prepare() {
+            '729f0df646e061b61386d38f013151234034e00bb40dec9f281b0a9ab0cfab7c')
+build() {
     bsdtar -xf "${srcdir}/data.tar.xz"
     mv "${srcdir}/opt/apps/${_sparkpkgname}" "${srcdir}/opt/apps/${pkgname}"
     mkdir -p "${srcdir}/tmp"
@@ -35,7 +35,7 @@ prepare() {
     msg "Copying latest ${_officalname} installer to ${srcdir}/tmp/drive_c/Program Files (x86)/${_installpkgpath} ..."
     rm -r "${srcdir}/tmp/drive_c/Program Files (x86)/${_installpkgpath}"
     mkdir -p "${srcdir}/tmp/drive_c/Program Files (x86)/${_installpkgpath}"
-    install -m644 "${_installpkgpath}-${pkgver}.exe" "${srcdir}/tmp/drive_c/Program Files (x86)/${_installpkgpath}/${_pkgdownloadname}.exe"
+    install -m644 "${_installpkgpath}-${pkgver}.exe" -t "${srcdir}/tmp/drive_c/Program Files (x86)/${_installpkgpath}"
     msg "Repackaging app archive ..."
     rm -r "${srcdir}/opt/apps/${pkgname}/files/files.7z"
     7za a -t7z -r "${srcdir}/opt/apps/${pkgname}/files/files.7z" "${srcdir}/tmp/*"
--- run.sh
+++ run.sh
@@ -7,24 +7,94 @@
  
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 BOTTLENAME=Deepin-QuarkCloudDrive
-APPVER=2.5.46
+APPVER=2.5.39spark2
 WINEPREFIX="${HOME}/.deepinwine/${BOTTLENAME}"
+APP_INSTALLER="quark-cloud-drive-2.5.46.exe"
+APP_INSTALLER_PATH="c:/Program Files (x86)/quark-cloud-drive/${APP_INSTALLER}"
 EXEC_PATH="c:/Program Files (x86)/quark-cloud-drive/QuarkCloudDrive.exe"
+EXEC_FILE="${WINEPREFIX}/drive_c/Program Files (x86)/quark-cloud-drive/QuarkCloudDrive.exe"
 START_SHELL_PATH="/opt/deepinwine/tools/run_v4.sh"
 export MIME_TYPE=""
 export DEB_PACKAGE_NAME="deepin-wine-quarkclouddrive"
 export APPRUN_CMD="deepin-wine6-stable"
 DISABLE_ATTACH_FILE_DIALOG="1"
 export SPECIFY_SHELL_DIR=`dirname ${START_SHELL_PATH}`
-ARCHIVE_FILE_DIR="/opt/apps/$DEB_PACKAGE_NAME/files"
-export WINEPREDLL="$ARCHIVE_FILE_DIR/dlls"
-   
-if [ -z "$DISABLE_ATTACH_FILE_DIALOG" ];then
-    export ATTACH_FILE_DIALOG=1
+ARCHIVE_FILE_DIR="/opt/apps/${DEB_PACKAGE_NAME}/files"
+export WINEPREDLL="${ARCHIVE_FILE_DIR}/dlls"
+
+OpenWinecfg() {
+    echo "Launching winecfg with ${APPRUN_CMD} in ${WINEPREFIX} ..."
+    env WINEPREFIX="${WINEPREFIX}" "${APPRUN_CMD}" winecfg
+}
+
+DeployApp() {
+    # deploy bottle
+    echo "Deploying ${WINEPREFIX} ..."
+    rm -rf "${WINEPREFIX}"
+    # run installer
+    echo "Launching ${APP_INSTALLER_PATH} ..."
+    env WINEDLLOVERRIDES="winemenubuilder.exe=d" "${START_SHELL_PATH}" "${BOTTLENAME}" "${APPVER}" "${APP_INSTALLER_PATH}" "$@"
+    touch "${WINEPREFIX}/reinstalled"
+    echo "Creating ${WINEPREFIX}/PACKAGE_VERSION ..."
+    cat "/opt/apps/${DEB_PACKAGE_NAME}/files/files.md5sum" > "${WINEPREFIX}/PACKAGE_VERSION"
+}
+
+WakeApp() {
+    env WINEPREDLL="${ARCHIVE_FILE_DIR}/dlls" \
+        WINEDLLPATH="/opt/${APPRUN_CMD}/lib:/opt/${APPRUN_CMD}/lib64" \
+        WINEPREFIX="${WINEPREFIX} ${APPRUN_CMD} /opt/deepinwine/tools/sendkeys.exe w"
+}
+
+Run() {
+    if [ -z "${DISABLE_ATTACH_FILE_DIALOG}" ]; then
+        export ATTACH_FILE_DIALOG=1
+    fi
+
+    if [ -n "${EXPORT_ENVS}" ]; then
+        export "${EXPORT_ENVS}"
+    fi
+
+    if [ -n "${EXEC_PATH}" ]; then
+        if [ ! -f "${WINEPREFIX}/reinstalled" ] || [ ! -f "${EXEC_FILE}" ]; then
+            DeployApp
+            exit 0
+        fi
+
+        if [ -z "${EXEC_PATH##*.lnk*}" ]; then
+            echo "Launching  ${EXEC_PATH} lnk file ..."
+            "${START_SHELL_PATH}" "${BOTTLENAME}" "${APPVER}" "C:/windows/command/start.exe" "/Unix" "${EXEC_PATH}" "$@"
+        else
+            echo "Launching  $EXEC_PATH ..."
+            "${START_SHELL_PATH}" "${BOTTLENAME}" "${APPVER}" "${EXEC_PATH}" "$@"
+        fi
+    else
+        "${START_SHELL_PATH}" "${BOTTLENAME}" "${APPVER}" "uninstaller.exe" "$@"
+    fi
+}
+
+HelpApp() {
+    echo " Extra Commands:"
+    echo " winecfg          Open winecfg"
+    echo " -w/--wake        Wake up background program"
+    echo " -h/--help        Show program help info"
+}
+
+if [ -z $1 ]; then
+    Run "$@"
+    exit 0
 fi
-   
-if [ -n "$EXEC_PATH" ];then
-    $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" "$@"
-else
-    $START_SHELL_PATH $BOTTLENAME $APPVER "uninstaller.exe" "$@"
-fi
+case $1 in
+"winecfg")
+    OpenWinecfg
+    ;;
+"-w" | "--wake")
+    WakeApp
+    ;;
+"-h" | "--help")
+    HelpApp
+    ;;
+*)
+    Run "$@"
+    ;;
+esac
+exit 0
