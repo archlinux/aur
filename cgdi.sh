046999b3853f26782f3567f0e8d06fd5d0dbcb5d
zxp19821005 <zxp19821005@163.com>
2023-09-21 18:51:50 +0800

    update to 0.0.8

---
+++ cgdi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+_ELECTRON=/usr/bin/electron23
+_ASAR="/opt/cgdi/resources/app.asar"
+if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
+    exec ${_ELECTRON} ${_ASAR} "$@"
+else
+    exec ${_ELECTRON} ${_ASAR} --no-sandbox "$@"
+fi
