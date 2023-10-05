99a02b0052a8204acd5712048ce01e6df37f86e7
lukelabrie <lplcleary@gmail.com>
2023-10-05 14:36:50 +0200

    fix filepath in test_install script

--- test_install.sh
+++ test_install.sh
@@ -2,7 +2,7 @@
 arch=$(uname -m)
 python_version=$(python -V 2>&1 | cut -d' ' -f2 | cut -d'.' -f1-2)
 export PATH=$PATH:/opt/openmc/bin:$HOME/.local/lib/python$python_version/site-packages
-export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python$python_version/site-packages/pymoab-5.3.0-py$python_version-linux-$arch.egg
+export PYTHONPATH=$PYTHONPATH:/opt/MOAB/lib/python$python_version/site-packages
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/MOAB/lib
 var=`echo /opt/nuclear-data/*hdf5 | head -n1`
 export OPENMC_CROSS_SECTIONS=$var/cross_sections.xml
