0c1c06269bcd52ca451c2c80dc42df657725d5f9
zxp19821005 <zxp19821005@163.com>
2023-10-13 12:11:14 +0800

    fix errors

--- proton-mail-viewer.sh
+++ proton-mail-viewer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron26
-_ASAR="/opt/proton-mail-viewer/proton-mail-viewer.asar"
+_ASAR="/opt/proton-mail-viewer/resources/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
