4c92c6adf902de1e0e5f6c4499c2ac3caf15ddf3
Marcin Depa <marcin155@gmail.com>
2023-10-23 22:43:30 +0200

    5.4.0 version

--- buildAndInstall.sh
+++ buildAndInstall.sh
@@ -12,10 +12,10 @@ SHA512=$(wget -q -O - "https://mango-lychee.nyc3.cdn.digitaloceanspaces.com/Lych
 echo "Preparing PKG file"
 sed -i "s@sha512sums_x86_64=(.*)@sha512sums_x86_64=('$SHA512')@g" PKGBUILD
 sed -i "s@pkgver=.*@pkgver=$VERSION@g" PKGBUILD
-makepkg --printsrcinfo > .SRCINFO && makepkg
+makepkg --printsrcinfo > .SRCINFO && makepkg -f
 PKG_FILE=$(ls -t1 *.tar.zst | head -n 1)
 printf "Done, install $PKG_FILE? [Y/n]"
-read install
-if [ "$install" = "y" ]; then
-    sudo pacman -U *.tar.zst
+read -n1 install
+if [ "${install,,}" = "y" ]; then   # ${VAR,,} is for convert to lower case
+    sudo pacman -U "$PKG_FILE"
 fi
