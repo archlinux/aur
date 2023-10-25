afb7a7288fb58b85a47eed9ac0700734e63fbc04
zxp19821005 <zxp19821005@163.com>
2023-10-25 09:00:53 +0800

    update to 0.2.2

--- insomnium.sh
+++ insomnium.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron25
-_ASAR="/opt/insomnium/resources/app.asar"
+APPDIR="/opt/insomnium"
+export PATH="${APPDIR}:${APPDIR}/usr/sbin:${PATH}"
+export LD_LIBRARY_PATH="${APPDIR}/usr/lib":"${APPDIR}/swiftshader":"${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/resources/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
