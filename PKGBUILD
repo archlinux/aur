edbe7eea406b503a2617eb1204e5a2249d29aade
Klaus Alexander Seistrup <klaus@seistrup.dk>
2023-10-26 15:32:53 +0200

    Use $HELIX_DEFAULT_RUNTIME instead of $HELIX_RUNTIME

--- PKGBUILD
+++ PKGBUILD
@@ -2,16 +2,16 @@
 # -*- mode: sh -*-
 
 pkgname='usrbinhx'
-pkgver='2023.02.21'
+pkgver='2023.10.26'
 pkgrel=1
 pkgdesc='Provide /usr/bin/hx for the helix editor'
 arch=('any')
 url='https://codeberg.org/kas/gists'
 license=('AGPL3')
 conflicts=('hex' 'helixbinhx')
-depends=('helix')
+depends=('helix' 'sh')
 source=('hx.sh')
-sha256sums=('a4c0915dc04d642a176dca205c3b41e4947def6a9f1afd01b44c8c2ef9980a9c')
+sha256sums=('184cdaf013c0b3ac2fe4b95842f8f2ff97ae2fbe38e36e363badc419f9eaffa0')
 
 package() {
   install -Dm0755 'hx.sh' "$pkgdir/usr/bin/hx"
--- hx.sh
+++ hx.sh
@@ -5,18 +5,17 @@ ME="${0##*/}"
 # Copyright © 2022 Klaus Alexander Seistrup <klaus@seistrup.dk>
 # Licensed under the GNU Affero General Public License v3+.
 #
-# Updated: 2023-02-20
+# Updated: 2023-10-26
 
 die() {
   printf '%s: %s\n' "$ME" "$*" >&2
   exit 1
 }
 
-export HELIX_HX='/usr/lib/helix/hx'
-export HELIX_RUNTIME='/var/lib/helix/runtime'
+export HELIX_DEFAULT_RUNTIME='/usr/lib/helix/runtime'
 
-test -x "$HELIX_HX"      || die 'cannot find helix:' "$HELIX_HX"
-test -d "$HELIX_RUNTIME" || die 'no such directory:' "$HELIX_RUNTIME"
+export HELIX_HX='/usr/lib/helix/hx'
+test -x "$HELIX_HX" || die 'cannot find helix:' "$HELIX_HX"
 
 exec "$HELIX_HX" "$@"
 exit 127
