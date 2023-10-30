4bc3bacda4d71be053decec237436af3b7c452f1
zxp19821005 <zxp19821005@163.com>
2023-10-30 15:11:20 +0800

    update to 3.11.51

--- encrypt0r.sh
+++ encrypt0r.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron18
-APPDIR="/opt/encrypt0r"
-export PATH="${APPDIR}:${APPDIR}/usr/sbin:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/usr/lib":"${APPDIR}/swiftshader":"${LD_LIBRARY_PATH}"
-_ASAR="${APPDIR}/resources/app.asar"
+APPDIR="/usr/lib/encrypt0r"
+export PATH="${APPDIR}:${PATH}"
+export LD_LIBRARY_PATH="${APPDIR}/lib":"${APPDIR}/swiftshader":"${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app.asar"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
