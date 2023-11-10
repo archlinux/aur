905cc4c2c9e34379a4b309194852b674ebeaadaf
zxp19821005 <zxp19821005@163.com>
2023-11-10 09:47:59 +0800

    update to 0.3.4

--- lightrail-core.sh
+++ lightrail-core.sh
@@ -2,7 +2,7 @@
 _ELECTRON=/usr/bin/electron24
 APPDIR="/usr/lib/lightrail-core"
 export PATH="${APPDIR}:${PATH}"
-export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
+#export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
 _ASAR="${APPDIR}/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
