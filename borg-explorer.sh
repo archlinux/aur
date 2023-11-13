6138efa7df095abda3d259a32c97202d213ffd8b
zxp19821005 <zxp19821005@163.com>
2023-11-13 12:46:05 +0800

    fix errors

--- borg-explorer.sh
+++ borg-explorer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 _ELECTRON=/usr/bin/electron22
-_ASAR="/opt/borg-explorer/borg-explorer.asar"
+APPDIR="/usr/lib/borg-explorer"
+export PATH="${APPDIR}:${PATH}"
+#export LD_LIBRARY_PATH="${APPDIR}/lib:${LD_LIBRARY_PATH}"
+_ASAR="${APPDIR}/app"
 if [[ $EUID -ne 0 ]] || [[ $ELECTRON_RUN_AS_NODE ]]; then
     exec ${_ELECTRON} ${_ASAR} "$@"
 else
