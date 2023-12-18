a2edaeb60d4e0985ca63184fb6e88d276f6160c3
zxp19821005 <zxp19821005@163.com>
2023-12-18 11:35:26 +0800

    update to 1.5.3

--- gopeed.sh
+++ gopeed.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 _APPDIR=/opt/@appname@
-_APPNAME=@runappname@
+_RUNAPP=@runname@
 export PATH="${_APPDIR}:${PATH}"
 export LD_LIBRARY_PATH="${_APPDIR}/lib:${LD_LIBRARY_PATH}"
 cd "${_APPDIR}"
-exec "${_APPDIR}/${_APPNAME}"
+exec "${_APPDIR}/${_APPNAME}" "$@"
