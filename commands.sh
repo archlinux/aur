7c2f21240f9b8c6a3c7f7f03c00e9df2f639d733
Stephan Koglin-Fischer <stephan.koglin-fischer@funzt.dev>
2023-12-21 15:42:17 +0100

    fix: still doesn't work

--- commands.sh
+++ commands.sh
@@ -1,27 +1,3 @@
-# Maintainer: Stephan Koglin-Fischer <stephan dot koglin-fischer at funzt dot dev>
-pkgname=dashlane-cli-git
-pkgver=v1.15.1.r0.g8368d87
-pkgrel=49
-pkgdesc="Dashlane CLI GitHub repository version bundled with asdf-vm to ensure using the correct node version."
-arch=('x86_64')
-url="https://github.com/Dashlane/dashlane-cli"
-license=('MIT')
-depends=('asdf-vm')
-makedepends=('git')
-source=("$pkgname::git+$url.git" '.tool-versions')
-install=".INSTALL"
-b2sums=('SKIP'
-        '7d947ad3269a67f0987a277e10b082931ecce24330e82debd0d147f3ea24a0d9c04a91c0f711fce286a17a789c883bb626685bc08ac78040c28a5ca677a0cbf8')
-# option strip is important for dcli to work properly and avoid
-# 'Pkg: Error reading from file.' error in production
-options=(!strip)
-
-pkgver() {
-  cd "$srcdir/$pkgname"
-  git describe --long --tags --abbrev=7 | sed 's/^foo-//;s/\([^-]*-g\)/r\1/;s/-/./g'
-}
-
-<<<<<<< Updated upstream
 check-for-asdf() {
   if ! command -v asdf &> /dev/null
   then
@@ -33,7 +9,6 @@ check-for-asdf() {
     if [[ $SHELL == *"bash"* ]]; then
       # If the .bashrc file does not contain the asdf source command, we add it
       if ! grep -q ". /opt/asdf-vm/asdf.sh" ~/.bashrc; then
-        echo "Patching .bashrc"
         echo -e "\n. /opt/asdf-vm/asdf.sh" >> ~/.bashrc
       fi
       source ~/.bashrc
@@ -60,6 +35,11 @@ check-for-asdf() {
         echo -e "\n. /opt/asdf-vm/asdf.sh" >> ~/.zshrc
       fi
       source ~/.zshrc
+      # Check if asdf is available now
+      if ! command -v asdf &> /dev/null then
+        echo "asdf could not be found"
+        echo "You need to add asdf to your shell's initialization file manually."
+      fi
     else
       echo "Unsupported shell. Please add asdf to your shell's initialization file manually."
       echo "Consider opening an issue or contribute a pull request to add support for your shell."
@@ -69,16 +49,39 @@ check-for-asdf() {
   fi
 }
 
-=======
->>>>>>> Stashed changes
-prepare() {
-  $SHELL -c "source ./commands.sh && prepare_commands"
+prepare_command() {
+  check-for-asdf
+
+  # Install all plugins stated in .tool-versions
+  awk '{print $1}' .tool-versions | xargs -n 1 asdf plugin-add
+  asdf install
+
+  cd "$srcdir/$pkgname"
+  yarn install
 }
 
-build() {
-  $SHELL -c "source ./commands.sh && build_commands"
+build_command() {
+  check-for-asdf
+  
+  cd "$srcdir/$pkgname"
+  yarn run build
+  # Build linux binary
+  yarn pkg:linux
 }
 
-package() {
-  $SHELL -c "source ./commands.sh && package_commands"
+package_command() {
+  check-for-asdf
+
+  cd "$srcdir/$pkgname"
+
+  # Install the tool-versions file
+  install -Dm755 "$srcdir/.tool-versions" "$pkgdir/usr/share/dashlane-cli-git/.tool-versions"
+
+  # Install the binary
+  install -Dm755 "$srcdir/$pkgname/bundle/dcli-linux" "$pkgdir/usr/share/dashlane-cli-git/dcli"
+
+  # Create a symlink to the binary
+  mkdir -p "$pkgdir/usr/bin"
+  cd "$pkgdir/usr/bin"
+  ln -sf "./../share/dashlane-cli-git/dcli" "dcli"
 }
