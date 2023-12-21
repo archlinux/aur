70c383ee5ac1d32d735a3ac1cb73c3b40dc4396a
Anti Räis <antirais@gmail.com>
2023-12-21 16:42:17 +0200

    add pkcs11 opens flag for java security reasons

--- cdoc4j-util.sh
+++ cdoc4j-util.sh
@@ -5,4 +5,4 @@ do
 	CP=$CP:$name
 done
 
-exec /usr/bin/java -cp "$CP" -jar /usr/share/java/cdoc4j-util/cdoc4j-util.jar "$@"
+exec /usr/bin/java -cp "$CP" --add-opens=jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED -jar /usr/share/java/cdoc4j-util/cdoc4j-util.jar "$@"
