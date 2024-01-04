da678d86c3d9c5a3487edd16f7f25e28ecf1527a
thynkon <thynkon@protonmail.com>
2024-01-04 21:46:22 +0100

    Update to v. 2.13.12-1

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -x
 
 ROOT_DIR="${HOME}/packaging/AUR/joplin-appimage"
 
@@ -8,7 +9,7 @@ fi
 
 cd "${ROOT_DIR}"
 
-package_version=$(grep "^pkgver" PKGBUILD | cut -d '=' -f 2)
-package_release=$(grep "^pkgrel" PKGBUILD | cut -d '=' -f 2)
+package_version=$(grep "^pkgver" ./PKGBUILD | cut -d '=' -f 2)
+package_release=$(grep "^pkgrel" ./PKGBUILD | cut -d '=' -f 2)
 
-makepkg --printsrcinfo > .SRCINFO && git commit -am "Update to v. ${package_version}-${package_release}" && git push origin master
+makepkg --printsrcinfo > .SRCINFO && git commit -am "Update to v. ${package_version}-${package_release}" # && git push origin master
