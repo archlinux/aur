ba550b53d2e647c360def6aab1ffbcf0ad101945
Patrick Münch <patrick@mondoo.com>
2024-02-01 01:53:09 +0000

    10.1.3

--- mondoo.sh
+++ mondoo.sh
@@ -2,5 +2,5 @@
 # Copyright (c) Mondoo, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
-/opt/mondoo/bin/mondoo --config /etc/opt/mondoo/mondoo.yml "$@"
+/usr/bin/cnspec --config /etc/opt/mondoo/mondoo.yml "$@"
 
