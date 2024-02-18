c77bcac107657b5ced6a802109c9300151779bd5
neeshy <neeshy@tfwno.gf>
2024-02-18 01:38:57 -0500

    Set OSU_EXTERNAL_UPDATE_PROVIDER

--- osu-lazer.sh
+++ osu-lazer.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOTNET_CLI_TELEMETRY_OPTOUT="1" exec dotnet /opt/osu-lazer/osu!.dll "$@"
+DOTNET_CLI_TELEMETRY_OPTOUT="1" OSU_EXTERNAL_UPDATE_PROVIDER="1" exec dotnet /opt/osu-lazer/osu!.dll "$@"
