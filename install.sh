77a8a109de3464242608d472ec845a102cb21abe
Morbius <archlinux@seichter.de>
2024-02-21 06:51:47 +0100

    Add quotes to satisfy shellcheck

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
+# Create automx2 user account if it does not already exist
+# vim: ts=4 sw=4 noet
 post_install() {
-	u="automx2"
+	local u="automx2"
 	grep -q "^${u}:" /etc/passwd && return
-	echo Creating user account ${u}
-	d="/var/lib/${u}"
-	useradd -mrd ${d} ${u}
-	rm -fr ${d}/.*
+	local d="/var/lib/${u}"
+	echo "Creating user account ${u} with home directory ${d}"
+	useradd -mrd "${d}" "${u}"
+	rm -fr "${d}"/.*
 }
