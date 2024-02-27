c0ed02bc88ed8002a2492531c4036dae7bbec771
Edmundo Sanchez <zomundo@gmail.com>
2024-02-27 11:50:16 -0600

    update find_release script

--- find_release.sh
+++ find_release.sh
@@ -2,12 +2,23 @@
 
 curl -s https://snapcraft.io/taskade > taskade.html
 
+current_version=$(grep -Po "^pkgver=\K.*" PKGBUILD)
 version=$(sed -n '/<script type="application\/ld+json">/,/<\/script>/p' taskade.html | \
 sed 's/<script type="application\/ld+json">//g' | \
 sed 's/<\/script>//g' | \
 jq -r '. as $json | if $json["@type"] == "SoftwareApplication" then $json.softwareVersion else empty end')
 
+echo "current version is $current_version"
 echo "found version $version"
+rm taskade.html
 
-# replace with the actual new version
-sed -i "s/^pkgver=.*/pkgver=${version}/" PKGBUILD
+# check if version is different
+if [ "$current_version" != "$version" ]; then
+    echo "Build the thing"
+    echo "Build it" > build.env
+    # replace with the actual new version
+    sed -i "s/^pkgver=.*/pkgver=${version}/" PKGBUILD
+else 
+    echo "No new version"
+fi
+
