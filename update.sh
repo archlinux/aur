d8ef6d852bdf28a63bdc8f680b47d237fb013c6c
myself600 <myself600@tuta.io>
2024-02-28 20:18:43 +0100

    Fix pkgrel not being updated

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
-pkgver=`curl -s https://api.github.com/repos/07th-mod/ponscripter-fork/releases/latest|grep tag_name|cut -d\" -f4`
-sed -i /^pkgver=/s/=.*/=${pkgver/v}/ PKGBUILD
+. ./PKGBUILD
+_pkgver=`git ls-remote https://github.com/07th-mod/ponscripter-fork|sed '$!d;s,.*/,,'`
+sed -i /^pkgver=/s/=.*/=${_pkgver/v}/`[ $pkgver = ${_pkgver/v} ] || echo \;/^pkgrel=/s/=.*/=1/` PKGBUILD
