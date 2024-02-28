03b2a2b22c2a27b4cacc970198605ebddf80a430
myself600 <myself600@tuta.io>
2024-02-28 20:17:26 +0100

    Fix pkgrel not being updated

--- gen-srcinfo.sh
+++ gen-srcinfo.sh
@@ -1,4 +1,3 @@
-export CARCH=x86_64
-. PKGBUILD
-sed -i /^pkgver=/s/=.*/=`pkgver`/ PKGBUILD
-makepkg --printsrcinfo>.SRCINFO
+. <(grep -e^{,_}pkgver= PKGBUILD|tr - _)
+sed -i /^pkgver=/s/=.*/=$_pkgver/`[ $pkgver = $_pkgver ] || echo \;/^pkgrel=/s/=.*/=1/` PKGBUILD
+CARCH=x86_64 makepkg --printsrcinfo>.SRCINFO
