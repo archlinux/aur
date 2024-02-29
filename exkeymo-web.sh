fa1d01597a98264ca58c9bdd7eba349e332a78ff
Wu Zhenyu <wuzhenyu@ustc.edu>
2024-03-01 00:33:39 +0800

    :children_crossing: Add exec

--- exkeymo-web.sh
+++ exkeymo-web.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-java -Dserver.port=8080 "$@" -jar /usr/share/java/exkeymo-web-*-jar-with-dependencies.jar
+exec java -Dserver.port=8080 "$@" -jar /usr/share/java/exkeymo-web-*-jar-with-dependencies.jar
