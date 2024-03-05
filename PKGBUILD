99f4c0aa2241a8dce1ee8c400caa6de4ddaceff1
Kimiblock Moe <kimiblock@icloud.com>
2024-03-05 23:37:45 +0800

    Initial Commit

---
+++ PKGBUILD
@@ -0,0 +1,41 @@
+# Maintainer: Kimiblock Moe
+pkgname=wechat-uos-bwrap
+pkgver=1
+pkgrel=1
+epoch=
+pkgdesc="Testing"
+arch=('x86_64')
+url=""
+license=('proprietary')
+groups=()
+depends=('nss' 'lsb-release' 'xdg-utils' 'libxss' "wechat-uos" 'bc' 'libnotify' 'bubblewrap' 'xdg-desktop-portal-gtk')
+makedepends=()
+checkdepends=()
+optdepends=(
+	'xdg-desktop-portal-kde: KDE Portal'
+	'xdg-desktop-portal-lxqt: lxqt Portal'
+	'xdg-desktop-portal-gnome: Gnome Portal'
+)
+provides=()
+conflicts=()
+replaces=()
+source=(
+	wechat.sh
+	wechat-uos-beta.desktop
+	wechat-uos-beta.svg
+	wechat-${pkgver}.deb::"https://cdn4.cnxclm.com/uploads/2024/03/05/3VDyAc0x_wechat-beta_1.0.0.145_amd64.deb?attname=wechat-beta_1.0.0.145_amd64.deb"
+)
+noextract=()
+md5sums=('0cb570b59d13fac76ec4e29cc0ccb293'
+         '0b654857592a9713c65a58bd061811ba'
+         '600e74549ce2258c045d5c2f7689ea63'
+         '1da072bd774d1b5c08b9545b409e3fcb')
+validpgpkeys=()
+function package() {
+	tar -xf data.tar.xz ./opt/wechat-beta
+	mkdir -p "${pkgdir}"/opt
+	cp opt/wechat-beta "${pkgdir}"/opt/wechat-beta -r
+	install -Dm644 wechat-uos-beta.desktop "${pkgdir}/usr/share/applications/wechat-uos-beta.desktop"
+	install -Dm755 wechat.sh "${pkgdir}/usr/bin/wechat-uos-beta"
+	install -Dm644 wechat-uos-beta.svg "${pkgdir}/usr/share/icons/hicolor/scalable/apps/wechat-uos-beta.svg"
+}
---
+++ wechat.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#https://cdn4.cnxclm.com/uploads/2024/03/05/3VDyAc0x_wechat-beta_1.0.0.145_amd64.deb?attname=wechat-beta_1.0.0.145_amd64.deb
+
+function moeDect() {
+	if [[ -f /usr/share/moeOS-Docs/os-release ]]; then
+		osRel="/usr/share/moeOS-Docs/os-release"
+	else
+		osRel="/usr/lib/os-release"
+	fi
+
+}
+
+function gamescopeLaunch() {
+	gamescope -F fsr --sharpness 0 -- launch
+}
+
+function launch() {
+	bwrap \
+		--dir "$XDG_RUNTIME_DIR" \
+		--dir /tmp \
+		--symlink usr/lib /lib \
+		--symlink usr/lib64 /lib64 \
+		--symlink usr/bin /bin \
+		--symlink usr/bin /sbin \
+		--ro-bind /home/kimiblockmoe/Downloads/TrashCan/opt /opt \
+		--bind "$XDG_RUNTIME_DIR/bus" "$XDG_RUNTIME_DIR/bus" \
+		--bind "$XDG_RUNTIME_DIR/pulse" "$XDG_RUNTIME_DIR/pulse" \
+		--dev /dev \
+		--dev-bind /dev/dri /dev/dri \
+		--dev-bind /dev/shm /dev/shm \
+		--ro-bind /sys/dev/char /sys/dev/char \
+		--proc /proc \
+		--ro-bind /usr /usr \
+		--bind "${HOME}/Documents/TrashBox" "$HOME" \
+		--bind "${XAUTHORITY}" "${XAUTHORITY}" \
+		--ro-bind /etc /etc \
+		--hostname trash \
+		--unshare-uts \
+		--bind /usr/share/wechat-uos/var/ /var/ \
+		--bind /usr/share/wechat-uos/etc/os-release /usr/share/moeOS-Docs/os-release \
+		--bind /usr/share/wechat-uos/etc/lsb-release /etc/lsb-release \
+		--bind /usr/lib/wechat-uos/license/ /usr/lib/license/ \
+		env QT_QPA_PLATFORM=xcb \
+			GTK_USE_PORTAL=1 \
+			LD_LIBRARY_PATH=/opt/wechat-beta \
+			LIBVA_DRIVER_NAME=iHD \
+			LD_PRELOAD=/usr/lib/wechat-uos/license/libuosdevicea.so \
+			/opt/wechat-beta/wechat
+}
+
+launch
