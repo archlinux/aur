9c348d40d5f7f94b7747b22a2f54b77b59ab58fd
Vryali <vryali@gmail.com>
2024-03-14 09:28:55 -0600

    Changed helper script to find latest electron

--- stream-overlay.sh
+++ stream-overlay.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+# Because electron doesn't auto-symlink for latest, simply use the latest here.
+# sed finds the last match for an executable file (ends in *) and then removes the *
+ELECTRON_PATH="`ls -1Ftr /usr/bin/electron* | sed -n '/.*\*$/h; ${x;p;}' | sed 's/.$//'`"
+
 cd /usr/share/stream-overlay 
-nohup /usr/bin/electron app 1>/tmp/stream-overlay.out 2>/tmp/stream-overlay.err &
+nohup ${ELECTRON_PATH} app 1>/tmp/stream-overlay.out 2>/tmp/stream-overlay.err &
