72918026773da51bec62c979ca590dc113d50964
holybaechu <holyb@holyb.xyz>
2024-03-18 16:18:08 +0900

    Fix .SRCINFO (again)

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,7 @@
+# Generate .SRCINFO
+makepkg --printsrcinfo > .SRCINFO
+
+# Clean-up
 find . -maxdepth 1 -name labymod-appimage-*-x86_64.pkg.tar.zst -delete
 find . -maxdepth 1 -name LabyMod%20Launcher-*.AppImage -delete
 rm -rf src
