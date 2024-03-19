6dd575af340834a962b083024c54e636eb08fba2
sukanka <su975853527@gmail.com>
2024-03-19 22:45:45 +0800

    add QT_AUTO_SCREEN_SCALE_FACTOR=1

--- dingtalk.sh
+++ dingtalk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 # try wayland and fallback to xcb in case that it may support xcb some day.
 export QT_QPA_PLATFORM="wayland;xcb"
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
 cd /opt/dingtalk/release
 LD_PRELOAD=./libcef.so ./com.alibabainc.dingtalk
