b1de3b9f44f3bfa7540ec5f38ffecdf5a3a1e063
ghostbuster <ghost_buster+aur@posteo.de>
2024-04-07 00:56:02 +0200

    Initial commit

---
+++ PKGBUILD
@@ -0,0 +1,48 @@
+# Maintainer: ghostbuster <ghost_buster+aur@posteo.de>
+pkgname="manage-kernel-backups"
+pkgver="1.0"
+pkgrel=1
+pkgdesc="Script collection to create and manage backups of updated kernels and their modules"
+arch=('i686' 'x86_64')
+url="https://support.brother.com/g/b/downloadlist.aspx?c=de&lang=de&prod=dcp7065dn_all&os=128&flang=English"
+license=('custom:brother commercial license')
+
+depends=('bash')
+optdepends=('dkms')
+conflicts=(
+  'kernel-modules-hook'
+  'linux-rt'
+  'linux-rt-lts'
+  'linux-hardened'
+  'linux-rt'
+)
+source=(
+  '10-linux-kernel-pre-install.hook'
+  '10-linux-modules-post-install.hook'
+  '10-linux-modules-pre-install.hook'
+  '91-linux-kernel-post-install.hook'
+  '91-mkinitcpio-post-install.hook'
+  '99-linux-kernel-post-install.hook'
+  'manage_kernel_backup.sh'
+)
+
+sha512sums=(
+  '5d9ff14a70cb1cddaa2810d2afe5ef3fb209eaadc35cc99b2c32f1dd38d46673d831410b75c0a0e7e391153313fe3d9bd04ccc75189f7541682878ed411b9bf5'
+  'a46851cbaccc909b1e380db33c609a79de68a15dad33ba762c1e946c94d897aad9497200acbcb8371b8e69f0919e3aefa3f2887681b82e4fc888b340aa0ec09a'
+  '2858db394a5fbfc2d44ed523bbfa9446bb6c1b0b433b005ccf06bea0e14ed9d6ec12a8bc44287e0490820bff27def8e218a9dcfb22e433454b9ca03754954a60'
+  'b50c6e797f7ede53d9789d4f1ed67d50740ecce2fbce6b03c16fe1ca368bc2b433a7009f683486c05def22d2e5fe94ef006ce15b804ffad19f74d183ebed5ca9'
+  'a9d2dfebf224669d91d8b66724d489292c26b00630b496905c5a78ea318a304bc725abff3831a01468f7115db2a612c3688d6c687cdf90851e95e303268b2293'
+  'd838944672432bb01c1e5e8746fc016b8b13eab80a1fc266cc67afff19001a8e8fc0ee67b5ca990041ccca662223bcb7ee13f737bd8adcbeccdcb3cfa609c973'
+  '12b3095e120a541cd640cbb72df9be182d97fa4fbe5fcd5d2fea6aced08f022ea87f47a6a83831c641bad7445a592cd64e534f65dbc5f1fc5e73b124ed7804a4'
+)
+
+package() {
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "10-linux-kernel-pre-install.hook"
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "10-linux-modules-post-install.hook"
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "10-linux-modules-pre-install.hook"
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "91-linux-kernel-post-install.hook"
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "91-mkinitcpio-post-install.hook"
+  install -m 644 -D -t "$pkgdir/usr/share/libalpm/hooks/" "99-linux-kernel-post-install.hook"
+  install -m 755 -D -t "$pkgdir/usr/share/libalpm/scripts/" "manage_kernel_backup.sh"
+}
+
---
+++ manage_kernel_backup.sh
@@ -0,0 +1,382 @@
+#!/usr/bin/env bash
+
+set -eu
+
+if [ -n "${KERNEL_HOOK_DEBUG:-""}" ]; then
+  set -x
+fi
+
+declare -a SUPPORTED_KERNEL_PACKAGES
+SUPPORTED_KERNEL_PACKAGES=("custom" "linux" "linux-lts")
+
+declare -A KERNEL_IMAGES
+KERNEL_IMAGES["custom"]="vmlinuz" # Fake entry, custom build scripts do not create an unversioned kernel image
+KERNEL_IMAGES["linux"]="vmlinuz-linux"
+KERNEL_IMAGES["linux-lts"]="vmlinuz-linux-lts"
+
+declare -A INITRAMFS_IMAGES
+INITRAMFS_IMAGES["custom"]="initramfs.img" # Fake entry, custom build scripts do not create an unversioned initramfs image
+INITRAMFS_IMAGES["linux"]="initramfs-linux.img"
+INITRAMFS_IMAGES["linux-lts"]="initramfs-linux-lts.img"
+
+KERNEL_DIRECTORY="/boot"
+MODULES_DIRECTORY="/usr/lib/modules"
+
+_update_grub_cfg() {
+  grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+_validate_version_string() {
+  kernel_version_raw="${1}"
+  if [[ "${kernel_version_raw}" =~ ^[^0-9] ]]; then
+    return 1
+  fi
+  kernel_version="$(echo "${kernel_version_raw}" | tr -cd '[:alnum:]._-')"
+  if [ "${kernel_version_raw}" != "${kernel_version}" ]; then
+    return 1
+  fi
+  return 0
+}
+
+_kernel_version_from_image() {
+  package="${1}"
+  file="${2}"
+  kernel_version="$(file -bL "${file}" | head -n1 | sed -e 's/.*version //' -e 's/ (.*//')"
+  if _validate_version_string "${kernel_version}"; then
+    echo "${kernel_version}"
+  else
+    echo "Invalid version string ${kernel_version} for ${kernel_image}" >&2
+  fi
+}
+
+_kernel_version_from_kernel_file_name() {
+  package="${1}"
+  file="${2}"
+  base="${KERNEL_IMAGES[$package]}"
+  kernel_version="${file/#"${base}-"/}"
+  if _validate_version_string "${kernel_version}"; then
+    if [ -f "${file}" ]; then
+      kernel_version_from_image="$(_kernel_version_from_image "${package}" "${file}")"
+      if [ "${kernel_version_from_image}" != "${kernel_version}" ]; then
+        echo "Version string mismatch for kernel image ${file}, got ${kernel_version} from file name and ${kernel_version_from_image} from image" >&2
+        return 1
+      fi
+    fi
+    echo "${kernel_version}"
+  else
+    echo "Invalid version string ${kernel_version} from file name ${file}" >&2
+    return 1
+  fi
+}
+
+_kernel_version_from_initramfs_file_name() {
+  package="${1}"
+  file="${2}"
+  base="${INITRAMFS_IMAGES[$package]}"
+  base="${base/%.img/}"
+  file="${file/%.img/}"
+  kernel_version="${file/#"${base}-"/}"
+  if _validate_version_string "${kernel_version}"; then
+    echo "${kernel_version}"
+  else
+    echo "Invalid version string ${kernel_version} from file name ${file}" >&2
+    return 1
+  fi
+}
+
+_versioned_kernel_file_name() {
+  package="${1}"
+  version="${2}"
+  base="${KERNEL_IMAGES[$package]}"
+  versioned_file_name="${base}-${version}"
+  version_from_file="$(_kernel_version_from_kernel_file_name "${package}" "${versioned_file_name}")"
+  if [ "${version_from_file}" != "${version}" ]; then
+    echo "Unable to extract version from generated file name, used ${version} got ${version_from_file}" >&2
+    return 1
+  fi
+  echo "${versioned_file_name}"
+}
+
+_versioned_initramfs_file_name() {
+  package="${1}"
+  version="${2}"
+  base="${INITRAMFS_IMAGES[$package]}"
+  base="${base/%.img/}"
+  versioned_file_name="${base}-${version}.img"
+  version_from_file="$(_kernel_version_from_initramfs_file_name "${package}" "${versioned_file_name}")"
+  if [ "${version_from_file}" != "${version}" ]; then
+    echo "Unable to extract version from generated file name, used ${version} got ${version_from_file}" >&2
+    return 1
+  fi
+  echo "${versioned_file_name}"
+}
+
+_delete_kernel_version() {
+  package="${1}"
+  file="${2}"
+  version="${3}"
+  full_removal="${4:-0}"
+  initramfs_image="${INITRAMFS_IMAGES[$package]}"
+  [ -f "${file}" ] && rm "${file}"
+  versioned_initramfs_file="$(_versioned_initramfs_file_name "${package}" "${version}")"
+  [ -f "${versioned_initramfs_file}" ] && rm "${versioned_initramfs_file}"
+  if [[ ${full_removal} -ne 0 ]]; then
+    modules_dir="${MODULES_DIRECTORY}/${version}"
+    if [ -d "${modules_dir}" ]; then
+      rm -R "${modules_dir}"
+    fi
+    if which dkms &> /dev/null; then
+      dkms status -k "${version}" | grep -v ": added$" | sed -e 's/, .*//' | while read -r mod; do
+        echo "dkms remove ${mod} -k ${version}"
+        dkms remove "${mod}" -k "${version}" || echo "DKMS returned an error"
+      done
+    fi
+  fi
+}
+
+create_kernel_backup() {
+  package="${1}"
+  kernel_image="${KERNEL_IMAGES[$package]}"
+  initramfs_image="${INITRAMFS_IMAGES[$package]}"
+  cd "${KERNEL_DIRECTORY}"
+  if [ ! -e "${kernel_image}" ]; then
+    echo "Kernel image ${kernel_image} for kernel package ${package} does not exist"
+    return 1
+  fi
+  kernel_version="$(_kernel_version_from_image "${package}" "${kernel_image}")"
+  versioned_kernel_file_name="$(_versioned_kernel_file_name "${package}" "${kernel_version}")"
+  if [ ! -e "${versioned_kernel_file_name}" ]; then
+    cp "${kernel_image}" "${versioned_kernel_file_name}"
+  fi
+  versioned_initramfs_file_name="$(_versioned_initramfs_file_name "${package}" "${kernel_version}")"
+  cp "${initramfs_image}" "${versioned_initramfs_file_name}"
+  echo "Created backup for kernel package ${package} with version ${kernel_version}" >&2
+}
+
+create_initramfs_backup() {
+  package="${1}"
+  kernel_image="${KERNEL_IMAGES[$package]}"
+  initramfs_image="${INITRAMFS_IMAGES[$package]}"
+  cd "${KERNEL_DIRECTORY}"
+  if [ ! -e "${kernel_image}" ]; then
+    return 0
+  fi
+  kernel_version="$(_kernel_version_from_image "${package}" "${kernel_image}")"
+  versioned_initramfs_file_name="$(_versioned_initramfs_file_name "${package}" "${kernel_version}")"
+  if [ -f "${initramfs_image}" ]; then
+    cp "${initramfs_image}" "${versioned_initramfs_file_name}"
+    echo "Created backup for initramfs iamge for kernel ${package} with version ${kernel_version}" >&2
+  fi
+}
+
+clean_backups() {
+  package="${1}"
+  kernel_image="${KERNEL_IMAGES[$package]}"
+  running_version="$(uname -r)"
+  installed_version="_"
+  cd "${KERNEL_DIRECTORY}"
+  if [ -e "${kernel_image}" ]; then
+    installed_version="$(_kernel_version_from_image "${package}" "${kernel_image}")"
+  fi
+  declare -i keep=0
+  declare -a clean=()
+
+  declare -a kernel=()
+  # shellcheck disable=SC2012
+  mapfile -t kernel < <(ls -1 "${kernel_image}"-[0-9]* | sort -r)
+  for k in "${kernel[@]}"; do
+    v="$(_kernel_version_from_kernel_file_name "${package}" "${k}")"
+    if [ "${v}" == "${installed_version}" ]; then
+      keep+=1
+      continue
+    fi
+    if [ "${v}" == "${running_version}" ]; then
+      keep+=1
+      continue
+    fi
+  done
+  for k in "${kernel[@]}"; do
+    if [ "${v}" == "${installed_version}" ]; then
+      continue
+    fi
+    if [ "${v}" == "${running_version}" ]; then
+      continue
+    fi
+    if [ ${keep} -lt 2 ]; then
+      keep+=1
+      continue
+    fi
+    clean+=("${k}")
+  done
+
+  if [ ${#clean[@]} -gt 0 ]; then
+    for k in "${clean[@]}"; do
+      v="$(_kernel_version_from_kernel_file_name "${package}" "${k}")"
+      _delete_kernel_version "${package}" "${k}" "${v}" 1
+    done
+  fi
+}
+
+remove_backups() {
+  package="${1}"
+  kernel_image="${KERNEL_IMAGES[$package]}"
+  installed_version="_"
+  cd "${KERNEL_DIRECTORY}"
+  if [ -e "${kernel_image}" ]; then
+    installed_version="$(_kernel_version_from_image "${package}" "${kernel_image}")"
+  fi
+  # shellcheck disable=SC2012
+  for k in $(ls -1 "${kernel_image}"-[0-9]* | sort -r); do
+    v="$(_kernel_version_from_kernel_file_name "${package}" "${k}")"
+    if [ "${v}" == "${installed_version}" ]; then
+      _delete_kernel_version "${package}" "${k}" "${v}" 0
+    else
+      _delete_kernel_version "${package}" "${k}" "${v}" 1
+    fi
+  done
+}
+
+create_modules_backup() {
+  package="${1}"
+  kernel_image="${KERNEL_IMAGES[$package]}"
+  cd "${KERNEL_DIRECTORY}"
+  kernel_version="$(_kernel_version_from_image "${package}" "${kernel_image}")"
+  cd "${MODULES_DIRECTORY}"
+  mkdir -p backup
+  if [ -d "${kernel_version}" ]; then
+    if [ -d "backup/${kernel_version}" ]; then
+      echo "Backup for modules for kernel ${kernel_version} found. Cleaning up" >&2
+      rm -R "backup/${kernel_version}"
+    fi
+    cp -al "${kernel_version}" backup/
+  else
+    echo "No modules for kernel ${kernel_version} found." >&2
+  fi
+  create_dkms_backup "${p}"
+}
+
+create_dkms_backup() {
+  # No DKMS backup creation, relying on rebuild instead of restore
+  return 0
+}
+
+restore_modules_backup() {
+  package="${1}"
+  cd "${MODULES_DIRECTORY}"
+  if [ -d "backup" ]; then
+    cd backup
+    declare -a versions
+    # shellcheck disable=SC2012 disable=SC2010
+    mapfile -t versions < <(ls -1 | grep "[0-9]*" | sort -r)
+    for v in "${versions[@]}"; do
+      if [ -d "${v}" ]; then
+        if [ -d "../${v}" ]; then
+          echo "Modules for kernel ${v} already restored but still backed up. Trying again" >&2
+          rm -R "../${v}"
+        fi
+        cp -al "${v}" ../
+        rm -R "${v}"
+        restore_dkms_backup "${p}" "${v}"
+      else
+        echo "Module backup for kernel ${v} is not a directory. Ignoring" >&2
+      fi
+    done
+  fi
+}
+
+restore_dkms_backup() {
+  kernel_version="${2}"
+  dkms autoinstall -k "${kernel_version}"
+}
+
+
+# check for root to avoid endless error messages
+if (( EUID )); then
+  echo 'You must be root to use this hook' >&2
+  exit 1
+fi
+
+action="${1:-""}"
+declare -a packages=()
+
+while read -r package; do
+  for p in "${SUPPORTED_KERNEL_PACKAGES[@]}"; do
+    if [[ "${p}" == "${package}" ]]; then
+      if [ -n "${KERNEL_IMAGES[$package]}" ] && [ -n "${INITRAMFS_IMAGES[$package]}" ]; then
+        packages+=("${package}")
+        break
+      fi
+    fi
+  done
+done
+
+case "${action}" in
+  create)
+    for p in "${packages[@]}"; do
+      if [ "${p}" == "custom" ]; then
+        continue
+      fi
+      create_kernel_backup "${p}"
+      _update_grub_cfg
+    done
+    ;;
+  remove)
+    for p in "${packages[@]}"; do
+      if [ "${p}" == "custom" ]; then
+        continue
+      fi
+      remove_backups "${p}"
+    done
+    ;;
+  modules_backup)
+    for p in "${packages[@]}"; do
+      if [ "${p}" == "custom" ]; then
+        continue
+      fi
+      create_modules_backup "${p}"
+    done
+    ;;
+  modules_restore)
+    for p in "${packages[@]}"; do
+      if [ "${p}" == "custom" ]; then
+        continue
+      fi
+      restore_modules_backup "${p}"
+    done
+    ;;
+  mkinitcpio)
+    if [ ${#packages[@]} -eq 0 ]; then
+      for p in "${SUPPORTED_KERNEL_PACKAGES[@]}"; do
+        if [ "${p}" == "custom" ]; then
+          continue
+        fi
+        create_initramfs_backup "${p}"
+      done
+    else
+      for p in "${packages[@]}"; do
+        if [ "${p}" == "custom" ]; then
+          continue
+        fi
+        create_initramfs_backup "${p}"
+      done
+    fi
+    _update_grub_cfg
+    ;;
+  clean)
+    if [ ${#packages[@]} -eq 0 ]; then
+      for p in "${SUPPORTED_KERNEL_PACKAGES[@]}"; do
+        if [ "${p}" == "custom" ]; then
+          continue
+        fi
+        clean_backups "${p}"
+      done
+    else
+      for p in "${packages[@]}"; do
+        if [ "${p}" == "custom" ]; then
+          continue
+        fi
+        clean_backups "${p}"
+      done
+    fi
+    ;;
+esac
