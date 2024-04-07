7da740128989e636c843012edaef608505c690af
matthew55 <78285385+matthew55@users.noreply.github.com>
2024-04-07 13:12:35 -0400

    Update Java JFX path & fix typo

--- chunky-launcher.sh
+++ chunky-launcher.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 JAVA_HOME=/usr/lib/jvm/java-17-openjdk
-JFX_PATH=$JAVA_HOME/lib
+# Different JFX providers install to different locations
+if [ -d "/usr/lib/jvm/java-17-openjfx" ]; then
+    JFX_PATH="/usr/lib/jvm/java-17-openjfx/lib"
+else
+    JFX_PATH=$JAVA_HOME/lib
+fi
 
 # Solve incompatibilites with jrt.fs
 "$JAVA_HOME/bin/java" --module-path "$JFX_PATH/javafx.base.jar:$JFX_PATH/javafx.controls.jar:$JFX_PATH/javafx.graphics.jar:$JFX_PATH/javafx.fxml.jar" --add-modules=javafx.base,javafx.graphics,javafx.controls,javafx.fxml -jar /usr/share/java/chunky/ChunkyLauncher.jar --launcher --noRetryJavafx --javaOptions "--module-path $JFX_PATH/javafx.base.jar:$JFX_PATH/javafx.controls.jar:$JFX_PATH/javafx.graphics.jar:$JFX_PATH/javafx.fxml.jar --add-modules=javafx.base,javafx.graphics,javafx.controls,javafx.fxml" "$@"
