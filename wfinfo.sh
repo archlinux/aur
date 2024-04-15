427d8d733aac118a557541ddbcf63a2f182297dd
gigamicro <gammamicro@gmail.com>
2024-04-15 14:54:25 -0400

    wrapper: rename CACHELOC to CACHEDIR

--- wfinfo.sh
+++ wfinfo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-CACHELOC=~/.cache/wfinfo-ng
-mkdir -pv "$CACHELOC"
-cd "$CACHELOC" || { echo Could not enter cache directory, exiting...; exit 1; }
+CACHEDIR=~/.cache/wfinfo-ng
+mkdir -pv "$CACHEDIR"
+cd "$CACHEDIR" || { echo Could not enter cache directory, exiting...; exit 1; }
 wfinfo-update ||
-if [ -f "$CACHELOC/filtered_items.json" ] && [ -f "$CACHELOC/prices.json" ]; then
+if [ -f "$CACHEDIR/filtered_items.json" ] && [ -f "$CACHEDIR/prices.json" ]; then
 	echo Update failed, continuing...
 else
 	echo Update failed and no cached price info, exiting...
