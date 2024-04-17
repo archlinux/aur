4c10d4b52e48ac642676941f27679511a368a8c8
August Wikerfors <git@augustwikerfors.se>
2024-04-17 21:49:01 +0200

    Fix Electron app path

--- arduino-ide.sh
+++ arduino-ide.sh
@@ -4,6 +4,6 @@ if [ $XDG_SESSION_TYPE = "wayland" ] && [ -c /dev/nvidia0 ]; then
   FLAGS="--disable-gpu-sandbox"
 fi
 
-exec /usr/lib/electron27/electron /usr/lib/arduino-ide/arduino-ide-electron-main.js $FLAGS "$@"
+exec /usr/lib/electron27/electron /usr/lib/arduino-ide $FLAGS "$@"
 
 # vim: ts=2 sw=2 et:
