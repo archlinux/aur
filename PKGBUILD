2a3d7330dcfbdae12f42f5007cc5591c8bdb446d
Karl L. Brennan <karlludwigbrennan@outlook.com>
2024-04-23 20:44:52 +1000

    Fork to LLVM-MinGW (add fix for armv7 and aarch64)

--- PKGBUILD
+++ PKGBUILD
@@ -1,10 +1,12 @@
-pkgname=mingw-w64-environment
+pkgname=llvm-mingw-w64-environment
 pkgver=1
 pkgrel=5
 arch=('any')
-pkgdesc="Script providing common environment variables and functions for MinGW (mingw-w64)"
+pkgdesc="Script providing common environment variables and functions for MinGW (mingw-w64) (fixed for armv7 and aarch64)"
 license=("BSD")
 url="http://fedoraproject.org/wiki/MinGW"
+conflicts=("mingw-w64-environment")
+provides=("mingw-w64-environment")
 source=("mingw-env.sh")
 sha256sums=('SKIP')
 
--- mingw-env.sh
+++ mingw-env.sh
@@ -1,10 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 _arch=$1
 
 default_mingw_pp_flags="-D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS"
 default_mingw_compiler_flags="$default_mingw_pp_flags -O2 -pipe -fno-plt -fexceptions --param=ssp-buffer-size=4 -Wformat -Werror=format-security -fcf-protection"
 default_mingw_linker_flags="-Wl,-O1,--sort-common,--as-needed -fstack-protector"
+
+# -fcf-protection is not supported on armv7 and aarch64
+if [ "$_arch" = "armv7-w64-mingw32" ] || [ "$_arch" = "aarch64-w64-mingw32" ]
+then
+default_mingw_compiler_flags=${default_mingw_compiler_flags//"-fcf-protection"}
+fi
+
 
 export CPPFLAGS="${MINGW_CPPFLAGS:-$default_mingw_pp_flags $CPPFLAGS}"
 export CFLAGS="${MINGW_CFLAGS:-$default_mingw_compiler_flags $CFLAGS}"
