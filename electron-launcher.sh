814f5295983b022d5cc2fe925177d80b60922768
Caleb Maclennan <caleb@alerque.com>
2024-04-24 10:40:28 +0300

    upgpkg: electron24 24.8.8-4
    Rebuild with support for fallback flags in the launcher

--- electron-launcher.sh
+++ electron-launcher.sh
@@ -4,14 +4,17 @@ set -euo pipefail
 
 name=@ELECTRON@
 flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+fallback_file="${XDG_CONFIG_HOME:-$HOME/.config}/electron-flags.conf"
 
-declare -a flags
-
+lines=()
 if [[ -f "${flags_file}" ]]; then
-    mapfile -t < "${flags_file}"
+    mapfile -t lines < "${flags_file}"
+elif [[ -f "${fallback_file}" ]]; then
+    mapfile -t lines < "${fallback_file}"
 fi
 
-for line in "${MAPFILE[@]}"; do
+flags=()
+for line in "${lines[@]}"; do
     if [[ ! "${line}" =~ ^[[:space:]]*#.* ]] && [[ -n "${line}" ]]; then
         flags+=("${line}")
     fi
