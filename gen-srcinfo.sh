d235b20a94f672ea5a0e42a8489b984e121b0bb8
myself600 <myself600@tuta.io>
2024-04-25 13:04:23 +0200

    Undo gen-srcinfo.sh

--- gen-srcinfo.sh
+++ gen-srcinfo.sh
@@ -1,1 +1,3 @@
+. <(grep -e^{,_}pkgver= PKGBUILD|tr - _)
+sed -i /^pkgver=/s/=.*/=$_pkgver/`[ $pkgver = $_pkgver ] || echo \;/^pkgrel=/s/=.*/=1/` PKGBUILD
 CARCH=x86_64 makepkg --printsrcinfo>.SRCINFO
--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 _curl(){ curl -LsS --retry 3 https://packages.ubuntu.com/$1;}
 dist=`_curl gcc-9\&exact=1|grep resultlink|cut -d\" -f2|tail -1`
-_pkgver=`_curl $dist/gcc-9|grep Package:|sed 's/.*(//;s/)//'`
+pkgver=`_curl $dist/gcc-9|grep Package:|sed 's/.*(//;s/)//'`
 sha256sums=`for i in cpp-9 gcc-9 libgcc-9-dev g++-9 libstdc++-9-dev gfortran-9;do _curl $dist/amd64/$i/download|grep SHA256;done|sed 's/.*<tt>/\\\n\t/;s/<.*//'|tr -d '\n'`
-. <(grep ^pkgver= PKGBUILD)
-sed -i "/^pkgver=/s/=.*/=${_pkgver/-/_}/`[ $pkgver = ${_pkgver/-/_} ] || echo \;/^pkgrel=/s/=.*/=1/`;/^_dist=/s/=.*/=$dist/;/^_pkgver=/s/=.*/=$_pkgver/
-/^sha256sums_x86_64=(/,/)/{s/(.*/($sha256sums)/p;d}" PKGBUILD
+sed -i "/^_dist=/s/=.*/=$dist/;/^_pkgver=/s/=.*/=$pkgver/;/^sha256sums_x86_64=(/,/)/{s/(.*/($sha256sums)/p;d}" PKGBUILD
