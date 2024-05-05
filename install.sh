3768945a78e63523d6bc0f22491963e92cde54ee
Karaoke Mugen CD <karaokemugen@shelter.moe>
2024-05-05 17:19:59 +0000

    :hammer: update install script to match docs

--- install.sh
+++ install.sh
@@ -42,8 +42,10 @@ setup_postgres() {
     fi
     # Creating the database
     sudo -u postgres -g postgres -H -- psql -c "CREATE DATABASE karaokemugen_app ENCODING 'UTF8';"
-    sudo -u postgres -g postgres -H -- psql -c "CREATE USER karaokemugen_app WITH ENCRYPTED PASSWORD 'musubi'; GRANT ALL PRIVILEGES ON DATABASE karaokemugen_app TO karaokemugen_app;"
+    sudo -u postgres -g postgres -H -- psql -c "CREATE USER karaokemugen_app WITH ENCRYPTED PASSWORD 'musubi';"
+    sudo -u postgres -g postgres -H -- psql -c "GRANT ALL PRIVILEGES ON DATABASE karaokemugen_app TO karaokemugen_app;"
     sudo -u postgres -g postgres -H -- psql -d karaokemugen_app -c "CREATE EXTENSION unaccent;"
+    sudo -u postgres -g postgres -H -- psql -d karaokemugen_app -c "GRANT CREATE ON SCHEMA public TO public;"
     echo -e "${_COL_GREEN_}karaokemugen_app database created!"
 }
 
