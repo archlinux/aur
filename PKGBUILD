5b0239e59cbcf5d77a406286707447def52e12dc
Patrick Northon <northon_patrick3@yahoo.ca>
2024-05-14 12:37:17 -0400

    - Use python 3.11 for version less than 3.14. - Use `flutter/tools/gn` for compiling.

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Patrick Northon <northon_patrick3@yahoo.ca>
 
 pkgname=flutter-engine
-pkgver=1.0.3
+pkgver=1.0.4
 pkgrel=1
 epoch=1
 pkgdesc='Engine for flutter applications.'
@@ -13,9 +13,9 @@ source=(
 	'flutter-engine-prepare.sh'
 	'flutter-engine-build.sh'
 	'flutter-engine-base.incl')
-sha256sums=('d276fe833a189e911693791df048dbf803c9a242676bf1e62ef27f4311ed4ac2'
+sha256sums=('3351b5125fa5f1384ae22f5b221e544526a20df7d8938c8e6b78edd044337531'
             '0c3f02c89162eec5d0c8ee6840dd82132f13a3f6375c7d64ef50518a7f3495e8'
-            '7459e07cbba59611d4c52a1a7237339fbec5afe919fc818274e545464ab24d3a')
+            'bc310053f1e61ca2ab2a34dce2309f11eb9627066f46fa3b45d40d303d776231')
 
 package() {
 	depends+=(
@@ -33,7 +33,7 @@ package() {
 		'python-httplib2'
 		'python-six')
 	install -dm755 "${pkgdir}/opt/flutter-engine"
-	
+
 	install -Dm755 'flutter-engine-prepare.sh' "${pkgdir}/opt/flutter-engine/pkgbuild-prepare.sh"
 	install -Dm755 'flutter-engine-build.sh' "${pkgdir}/opt/flutter-engine/pkgbuild-build.sh"
 	install -Dm644 'flutter-engine-base.incl' "${pkgdir}/opt/flutter-engine/base.incl"
--- flutter-engine-prepare.sh
+++ flutter-engine-prepare.sh
@@ -2,10 +2,11 @@
 
 source '/opt/flutter-engine/base.incl'
 
+FLUTTER_TARGET_CPU="${FLUTTER_TARGET_CPU:-x64}"
+readonly _flutter_outdir="linux_release_$FLUTTER_TARGET_CPU"
 readonly _cache_home=${XDG_CACHE_HOME:-$HOME/.cache}
 readonly _engine_path="${srcdir}/flutter-engine"
-readonly _dart_sdk="$_engine_path/out/host_arch_release/dart-sdk"
-FLUTTER_TARGET_CPU="${FLUTTER_TARGET_CPU:-x64}"
+readonly _dart_sdk="$_engine_path/out/arch_release/dart-sdk"
 
 if [[ ! -v _engine_version ]]; then
 	_engine_version="$(yq -er .environment.flutter 'pubspec.yaml')"
@@ -26,36 +27,51 @@ fi
 readonly _cached_flutter_dir="${_engine_version+"$_cache_home/flutter-engine/$FLUTTER_TARGET_CPU/$_engine_version"}"
 
 pushd "${srcdir}"
-	sed -i 's|"$FLUTTER_ROOT/bin/internal/update_dart_sdk.sh"|#|' "${srcdir}/flutter/bin/internal/shared.sh"
-	sed -i -E 's|_wait_for_lock$|#_wait_for_lock|' "${srcdir}/flutter/bin/internal/shared.sh"
+	readonly _real_engine_version="$(git -C 'flutter-engine' describe --tags)"
+
+	if [[ $(vercmp "$_real_engine_version" '3.14.0') == -1 ]]; then
+		readonly _python_exec="python3.11"
+	else
+		readonly _python_exec='python3'
+	fi
+
+	if ! "$_python_exec" --version &>/dev/null; then
+		>&2 echo "Engine version $_real_engine_version requires $_python_exec."
+		return 1
+	fi
+
+	sed -i 's|"$FLUTTER_ROOT/bin/internal/update_dart_sdk.sh"|#|' "flutter/bin/internal/shared.sh"
+	sed -i -E 's|_wait_for_lock$|#_wait_for_lock|' "flutter/bin/internal/shared.sh"
 	sed -i 's|--no-enable-mirrors "$SCRIPT_PATH"|--no-enable-mirrors "$SCRIPT_PATH" $ARCH_FLUTTER_OPTS|' \
-		"${srcdir}/flutter/bin/internal/shared.sh"
+		"flutter/bin/internal/shared.sh"
 	sed -i 's|exec "$DART" --disable-dart-dev --packages="$FLUTTER_TOOLS_DIR/.dart_tool/package_config.json" $FLUTTER_TOOL_ARGS "$SNAPSHOT_PATH" "$@"|exec "$DART" --disable-dart-dev --packages="$FLUTTER_TOOLS_DIR/.dart_tool/package_config.json" $FLUTTER_TOOL_ARGS "$SNAPSHOT_PATH" $ARCH_FLUTTER_OPTS "$@"|' \
-		"${srcdir}/flutter/bin/internal/shared.sh"
-	rm -rf "${srcdir}/flutter/bin/cache/dart-sdk" || true
-	mkdir -p "${srcdir}/flutter/bin/cache"
-	_ln "$_dart_sdk" "${srcdir}/flutter/bin/cache/dart-sdk"
+		"flutter/bin/internal/shared.sh"
+	rm -rf "flutter/bin/cache/dart-sdk" || true
+	mkdir -p "flutter/bin/cache"
+	_ln "$_dart_sdk" "flutter/bin/cache/dart-sdk"
+
+	sed -i "s/exec \"python3\"/exec \"$_python_exec\"/" 'depot_tools/vpython3'
 
 	install -Dm755 <(cat << EOF
 #!/usr/bin/env sh
 
 $_dart_sdk/bin/dart "\$@"
 EOF
-	) "${srcdir}/flutter/bin/dart"
+	) "flutter/bin/dart"
 
-	if [ ! -d "${srcdir}/depot_tools" ]; then
-		git clone --depth=1 --single-branch 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' "${srcdir}/depot_tools"
+	if [ ! -d "depot_tools" ]; then
+		git clone --depth=1 --single-branch 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' "depot_tools"
 	fi
 popd
 
 export \
-	PATH+=":${srcdir}/depot_tools" \
+	PATH="${srcdir}/depot_tools:$PATH" \
 	DEPOT_TOOLS_UPDATE=0 \
 	VPYTHON_BYPASS='manually managed python not supported by chrome operations'
 
 _prepare_flutter_engine() {
 	pushd "${srcdir}"
-	
+
 	cat >.gclient <<EOF
 solutions = [
 	{
@@ -67,29 +83,35 @@ solutions = [
 	}
 ]
 EOF
-	
-	#--nohooks \
-	gclient.py sync -D -R \
+
+	gclient.py sync -n -D -R \
 		--with_branch_heads \
 		--with_tags \
 		--output-json='gclient-sync.json' \
-		--shallow
-	
+		--shallow \
+		--no-bootstrap
+
+	sed -i "s/'python3'/'$_python_exec'/" 'src/flutter/DEPS'
+
+	gclient.py runhooks
+
 	cd 'src'
-	
+
 	sed -i 's|prefix = rebased_clang_dir|prefix= ""|g' 'build/toolchain/linux/BUILD.gn' # use system clang
 	sed -i 's|}/|}|g' 'build/toolchain/linux/BUILD.gn' # use system clang
 	sed -i 's|rebase|#|g' 'build/toolchain/linux/BUILD.gn'
-	
-	sed -i 's|$wayland_dir|//third_party/angle/third_party/wayland|' \
-		'third_party/angle/BUILD.gn' \
-		'third_party/angle/src/common/vulkan/BUILD.gn' \
-		'third_party/angle/src/third_party/volk/BUILD.gn'
-	
-	sed -i 's|import("//build/config/chromecast_build.gni")||' 'third_party/angle/src/tests/BUILD.gn'
+
+	if [[ -e 'third_party/angle/BUILD.gn' ]]; then
+		sed -i 's|$wayland_dir|//third_party/angle/third_party/wayland|' \
+			'third_party/angle/BUILD.gn' \
+			'third_party/angle/src/common/vulkan/BUILD.gn' \
+			'third_party/angle/src/third_party/volk/BUILD.gn'
+		sed -i 's|import("//build/config/chromecast_build.gni")||' 'third_party/angle/src/tests/BUILD.gn'
+	fi
+
 	sed -i '/-Wno-deprecated-literal-operator/d' 'build/config/compiler/BUILD.gn'
 	sed -i '/G_DEFINE_AUTOPTR_CLEANUP_FUNC(PangoLayout, g_object_unref)/d' 'flutter/shell/platform/linux/fl_accessible_text_field.cc'
-	
+
 	cat > 'third_party/dart/build/dart/prebuilt_dart_sdk.gni' <<-EOF
 		import("../executable_suffix.gni")
 		_dart_root = rebase_path("../..")
@@ -97,75 +119,60 @@ EOF
 		#_prebuilt_dart_exe_trial = ""
 		prebuilt_dart_exe_works = true
 EOF
-	
+
 	#_ln "${srcdir}/emsdk" 'buildtools/emsdk'
 	popd
 }
 
 _build_flutter_engine() {
 	pushd "${srcdir}"
-	
-	gclient.py sync -D -R \
-		--with_branch_heads \
-		--with_tags \
-		--output-json='gclient-sync.json' \
-		--shallow
-	
+
 	cd 'src'
-	
+
 	local outdir
 	if [[ -z "$_cached_flutter_dir" ]]; then
-		outdir="${srcdir}/flutter-out/host_arch_release"
+		outdir="${srcdir}"
 	else
-		outdir="${_cached_flutter_dir}/out/host_arch_release"
-		_ln "${_cached_flutter_dir}/out" "${srcdir}/flutter-out"
+		outdir="${_cached_flutter_dir}"
+		_ln "${_cached_flutter_dir}/out" "${srcdir}/out"
 	fi
-	
-	gn gen --no-prebuilt-dart-sdk -qv "$outdir" --args="$(cat <<-EOF
-		target_os = "linux"
-		target_cpu = "$FLUTTER_TARGET_CPU"
-		dart_use_crashpad = false
-		dart_use_fallback_root_certificates = true
-		dart_use_compressed_pointers = true
-		dart_vm_code_coverage = false
-		dart_debug = false
-		dart_runtime_mode = "release"
-		is_debug = false
-		exclude_kernel_service = false
-		is_clang = true
-		skia_use_piex = false
-		target_sysroot = "/"
-		verify_sdk_hash = false
-		enable_unittests = false
-		flutter_runtime_mode = "release"
-	EOF
-	)" || { >&2 echo '`gn gen` command failed.'; return 1; }
-	
-	sed -i 's|ldflags}|ldflags} -fuse-ld=lld|g' "$outdir/toolchain.ninja" # use system linker
-	ninja -v -C "$outdir" || { >&2 echo 'Flutter engine build failed.'; return 1; }
-	
+
+	"./flutter/tools/gn" --no-prebuilt-dart-sdk --verbose --out-dir="$outdir" \
+		--linux \
+		--linux-cpu="$FLUTTER_TARGET_CPU" \
+		--no-goma \
+		$(check_option 'lto' 'y' && echo '--lto') \
+		--enable-vulkan \
+		--target-sysroot='/' \
+		--runtime-mode='release' \
+		--no-enable-unittests \
+	|| { >&2 echo '`gn gen` command failed.'; return 1; }
+
+	sed -i 's|ldflags}|ldflags} -fuse-ld=lld|g' "$outdir/out/$_flutter_outdir/toolchain.ninja" # use system linker
+	ninja -v -C "$outdir/out/$_flutter_outdir" || { >&2 echo 'Flutter engine build failed.'; return 1; }
+
 	#export PATH+=":${srcdir}/src/flutter/lib/web_ui/dev"
 	#felt build
-	
+
 	popd
 }
 
 _install_flutter_engine() {
 	pushd "${srcdir}"
-	
-	rm -rf 'flutter-out/host_arch_release/'{gen,obj,lib.unstripped,exe.unstripped}
-	ln -s 'host_arch_release' "flutter-out/arch_release"
-	ln -s 'host_arch_release' "flutter-out/host_release"
-	
+
+	rm -rf "out/$_flutter_outdir/"{gen,obj,lib.unstripped,exe.unstripped}
+	ln -s "$_flutter_outdir" 'out/arch_release'
+	ln -s "$_flutter_outdir" 'out/host_release'
+
 	popd
 }
 
 if [[ -n "$_cached_flutter_dir" ]]; then
 	mkdir -p "$_cached_flutter_dir"
-	
+
 	{
 		flock -s 4
-		
+
 		if [[ ! -L "$_cached_flutter_dir/out/arch_release" ]]; then
 			_prepare_flutter_engine && \
 			_build_flutter_engine && \
@@ -173,7 +180,7 @@ if [[ -n "$_cached_flutter_dir" ]]; then
 				{ return 1; }
 		fi
 	} 4> "$_cached_flutter_dir/.lock"
-	
+
 	_ln "$_cached_flutter_dir/out" "${srcdir}/flutter-engine/out"
 else
 	_prepare_flutter_engine && \
