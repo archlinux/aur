3722856c061fc3d9910cca6cb98d17c9ec4d0837
0xgingi <0xgingi@0xgingi.com>
2024-05-22 11:03:28 -0400

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 makepkg
 makepkg --printsrcinfo > .SRCINFO
-git add PKGBUILD .SRCINFO update.sh
-git commit -m "update"
-git push
 rm -rf pkg src jellyfin-rpc
 rm -rf *.zst jellyfin-rpc-x86_64-linux jellyfin-rpc.service
 rm example.json
+git add .
+git add PKGBUILD .SRCINFO update.sh
+git commit -m "update"
+git push
