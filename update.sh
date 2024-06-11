65366e72c69d10a44b4f774978153ddd2faa55f4
Alexandre Bury <alexandre.bury@gmail.com>
2024-06-11 08:15:04 -0400

    Ignore pre-releases

--- update.sh
+++ update.sh
@@ -12,10 +12,11 @@ LAST=$(jq -r <<<$RELEASES '
 [ keys[]
     | select(contains("beta") == false)
     | select(contains("rc") == false)
+    | select(contains("a") == false)
     | [split(".")[] | tonumber] ]
   | sort[-1]
   | join(".")
-')
+' || (echo $RELEASES > resp.err && exit 1))
 
 if [ "$pkgver" == "$LAST" ]; then
     echo "Already up to date" >&2
