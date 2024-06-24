213c0d05f87755d341ce68d4548e80717deb5b27
Gr3q <floyd0122@gmail.com>
2024-06-24 18:49:54 +0100

    fix incorrect variable check in dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -4,8 +4,8 @@
 # Add dotnet directory to PATH, according to docs it must be added, plus VSCode C# Dev Kit doesn't work without this.
 # See https://learn.microsoft.com/en-us/dotnet/core/install/linux-scripted-manual#set-environment-variables-system-wide
 case "$PATH" in
-    *"$$DOTNET_ROOT"* ) true ;;
-    * ) PATH="$PATH:$$DOTNET_ROOT" ;;
+    *"$DOTNET_ROOT"* ) true ;;
+    * ) PATH="$PATH:$DOTNET_ROOT" ;;
 esac
 
 # Add dotnet tools directory to PATH
