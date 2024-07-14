a3e7f0d33e1bcd9bea2d7892b736c8ae3d3d559e
Michel Zou <xantares09@hotmail.com>
2024-07-14 09:07:26 +0200

    _arch

--- mingw-env.sh
+++ mingw-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_arch=$1
+_mingw_arch=$1
 
 default_mingw_pp_flags="-D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS"
 default_mingw_compiler_flags="$default_mingw_pp_flags -O2 -pipe -fno-plt -fexceptions --param=ssp-buffer-size=4 -Wformat -Werror=format-security -fcf-protection"
@@ -11,9 +11,9 @@ export CFLAGS="${MINGW_CFLAGS:-$default_mingw_compiler_flags $CFLAGS}"
 export CXXFLAGS="${MINGW_CXXFLAGS:-$default_mingw_compiler_flags $CXXFLAGS}"
 export LDFLAGS="${MINGW_LDFLAGS:-$default_mingw_linker_flags $LDFLAGS}"
 
-export CC="${MINGW_CC:-$_arch-gcc}"
-export CXX="${MINGW_CXX:-$_arch-g++}"
+export CC="${MINGW_CC:-$_mingw_arch-gcc}"
+export CXX="${MINGW_CXX:-$_mingw_arch-g++}"
 
-mingw_prefix=/usr/${_arch}
+mingw_prefix=/usr/${_mingw_arch}
 export PKG_CONFIG_SYSROOT_DIR="${mingw_prefix}"
 export PKG_CONFIG_LIBDIR="${mingw_prefix}/lib/pkgconfig:${mingw_prefix}/share/pkgconfig"
