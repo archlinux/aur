1fb8a47a30ee01854485ed0487ae1253cb61362a
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2024-08-07 16:28:00 +0530

    fix update script to first cd into directory

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@
 decodeURL() { printf "%b\n" "$(sed 's/+/ /g; s/%\([0-9a-f][0-9a-f]\)/\\x\1/gi;')"; }
 # find every hex-encoded character, and decode to ascii
 
+set -eu
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 newver=$(decodeURL <<< $(curl -sI 'https://www.zotero.org/download/client/dl?platform=linux-x86_64&channel=beta' | grep "^location" | sed -E "s/.*beta\/(.*)\/.*/\1/g"))
 # decodeURL <<< curl -onlyheaders latest | get redirect location | pick the version
 
