9c518e211a984281cda67bd959cab74021c76e70
Rubin Simons <me@rubin55.org>
2024-08-16 15:49:59 +0200

    Removed CLANGD_FLAGS setting, as it is messy when other compilers are involved. Renamed source script.

--- marsdev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if [[ -x /opt/marsdev/m68k-elf/bin/m68k-elf-gcc && -z "$CLANGD_FLAGS" ]]; then
-    # See: https://clangd.llvm.org/guides/system-headers#query-driver
-    export CLANGD_FLAGS='--query-driver="/opt/marsdev/*-elf/bin/*"'
-fi
