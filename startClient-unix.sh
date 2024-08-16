c8836635f7742f232250127eba6c2e38ef0141a1
Aidan Coward <aidan.coward@gmail.com>
2024-08-16 18:01:30 -0400

    updated to version 1.4.53

--- startClient-unix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /usr/share/xmage/mage-client
-
-java -Xms256m -Xmx2048m -Dfile.encoding=UTF-8 -jar /usr/share/xmage/mage-client/lib/mage-client-1.4.50.jar &
--- startServer-unix.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /usr/share/xmage/mage-server
-
-java -Xms256M -Xmx512M -Dfile.encoding=UTF-8 -Djava.security.policy=./config/security.policy -Dlog4j.configuration=file:./config/log4j.properties -jar /usr/share/xmage/mage-server/lib/mage-server-1.4.50.jar
