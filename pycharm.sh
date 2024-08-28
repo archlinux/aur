9c59828bfe5c3ee9f7e28091f2528fccac3466b0
Mark Blakeney <mark.blakeney@bullet-systems.net>
2024-08-28 17:51:32 +1000

    Fix startup directory error in prev commit

--- pycharm.sh
+++ pycharm.sh
@@ -2,7 +2,6 @@
 if [ -z "$PYCHARM_JDK" ] ; then
   PYCHARM_JDK="/usr/share/pycharm/jbr/"
 fi
-cd /usr/share/pycharm/bin
-exec env PYCHARM_JDK="$PYCHARM_JDK" ./pycharm "$@"
+exec env PYCHARM_JDK="$PYCHARM_JDK" /usr/share/pycharm/bin/pycharm "$@"
 
 # vim: ts=2 sw=2 et:
