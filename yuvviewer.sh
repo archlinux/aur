c233f8eb071b0de0bd3f6e76b70182b2f1519a2d
zxp19821005 <zxp19821005@163.com>
2024-08-30 11:16:52 +0800

    fix errors

--- yuvviewer.sh
+++ yuvviewer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-_APPDIR=/opt/@appname@
+set -o pipefail
+_APPDIR="/usr/lib/@appname@"
 _RUNNAME="${_APPDIR}/@runname@"
 export PATH="${_APPDIR}:${PATH}"
 export QT_PLUGIN_PATH="${_APPDIR}/plugins:${PATH}"
