2fe0568e77856ba6b6c348576345541e079e9c69
fam007e <faisalmoshiur@gmail.com>
2024-09-25 21:32:49 +0600

    script and PKGBUILD updated for new release

--- PKGBUILD
+++ PKGBUILD
@@ -2,16 +2,15 @@
 pkgname=nerdfonts-installer-bin
 pkgver=2024.09.25.1
 pkgrel=1
-pkgdesc="A script to detect the OS and install Nerd Fonts"
+pkgdesc="Executable binary to install Nerd Fonts"
 arch=('x86_64')
 url="https://github.com/fam007e/nerd_fonts_installer"
 license=('MIT')
 depends=('curl' 'unzip' 'fontconfig')
-source=("https://github.com/fam007e/nerd_fonts_installer/archive/refs/tags/v${pkgver}.tar.gz")
+source=("https://github.com/fam007e/nerd_fonts_installer/releases/download/v${pkgver}/nerdfonts-installer-${pkgver}.tar.gz")
 sha256sums=('10446880c5f7e7c30a16886dc975dc597c4e88605acb0b7247e26218913ac6cd')
 
 package() {
     cd "$srcdir/nerd_fonts_installer-${pkgver}"
-    install -Dm755 "release/nerdfonts-installer" "$pkgdir/usr/bin/nerdfonts-installer"
-    install -Dm644 LICENSE "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
+    install -Dm755 "nerdfonts-installer" "$pkgdir/usr/bin/nerdfonts-installer"
 }
--- nerdfonts_installer.sh
+++ nerdfonts_installer.sh
@@ -18,12 +18,12 @@ detect_os_and_set_package_manager() {
                 PKG_MANAGER="sudo pacman -Syu --noconfirm"
                 ;;
             *)
-                printf "%b\n" '\033[0;31mUnsupported OS: '"$ID"'\033[0m'  
+                printf "%b\n" '\033[0;31mUnsupported OS: '"$ID"'\033[0m'
                 exit 1
                 ;;
         esac
     else
-        printf "%b\n" '\033[0;31mOS detection failed. Please install curl, tar, and fontconfig manually.\033[0m'  
+        printf "%b\n" '\033[0;31mOS detection failed. Please install curl, tar, and fontconfig manually.\033[0m'
         exit 1
     fi
 }
@@ -31,17 +31,17 @@ detect_os_and_set_package_manager() {
 # Function to check and install dependencies
 install_dependencies() {
     if ! command -v curl >/dev/null 2>&1; then
-        printf "%b\n" '\033[0;33mcurl not found. Installing curl...\033[0m'  
+        printf "%b\n" '\033[0;33mcurl not found. Installing curl...\033[0m'
         $PKG_MANAGER curl
     fi
 
-    if ! command -v tar >/dev/null 2>&1; then
-        printf "%b\n" '\033[0;33mtar not found. Installing tar...\033[0m'  
-        $PKG_MANAGER tar
+    if ! command -v unzip >/dev/null 2>&1; then
+        printf "%b\n" '\033[0;33munzip not found. Installing unzip...\033[0m'
+        $PKG_MANAGER unzip
     fi
 
     if ! command -v fc-cache >/dev/null 2>&1; then
-        printf "%b\n" '\033[0;33mfontconfig (fc-cache) not found. Installing fontconfig...\033[0m'  
+        printf "%b\n" '\033[0;33mfontconfig (fc-cache) not found. Installing fontconfig...\033[0m'
         $PKG_MANAGER fontconfig
     fi
 }
@@ -58,111 +58,115 @@ mkdir -p "$HOME/.local/share/fonts"
 # Create tmp directory if it doesn't exist
 mkdir -p "$HOME/tmp"
 
-# List of available fonts
+# List of available fonts using release page names
 fonts="
-0xProto Nerd Font
-3270 Nerd Font
-Agave Nerd Font
-AnonymicePro Nerd Font
-Arimo Nerd Font
-AurulentSansMono Nerd Font
-BigBlueTerminal Nerd Font
-BitstromWera Nerd Font
-BlexMono Nerd Font
-CaskaydiaCove Nerd Font
-CaskaydiaMono Nerd Font
-CodeNewRoman Nerd Font
-ComicShannsMono Nerd Font
-CommitMono Nerd Font
-Cousine Nerd Font
-D2Coding Nerd Font
-DaddyTimeMono Nerd Font
-DejaVuSansMono Nerd Font
-DroidSansMono Nerd Font
-EnvyCodeR Nerd Font
-FantasqueSansMono Nerd Font
-FiraCode Nerd Font
-FiraMono Nerd Font
-GeistMono Nerd Font
-GoMono Nerd Font
-Gohu Nerd Font
-Hack Nerd Font
-Hasklug Nerd Font
-HeavyDataMono Nerd Font
-Hurmit Nerd Font
-iM-Writing Nerd Font
-Inconsolata Nerd Font
-InconsolataGo Nerd Font
-InconsolataLGC Nerd Font
-IntoneMono Nerd Font
-Iosevka Nerd Font
-IosevkaTerm Nerd Font
-IosevkaTermSlab Nerd Font
-JetBrainsMono Nerd Font
-Lekton Nerd Font
-Literation Nerd Font
-Lilex Nerd Font
-MartianMono Nerd Font
-Meslo Nerd Font
-Monaspice Nerd Font
-Monofur Nerd Font
-Monoid Nerd Font
-Mononoki Nerd Font
-M+ Nerd Font
-Noto Nerd Font
-OpenDyslexic Nerd Font
-Overpass Nerd Font
-ProFont Nerd Font
-ProggyClean Nerd Font
-RecMono Nerd Font
-RobotoMono Nerd Font
-SauceCodePro Nerd Font
-ShureTechMono Nerd Font
-SpaceMono Nerd Font
-Terminess Nerd Font
-Tinos Nerd Font
-Ubuntu Nerd Font
-UbuntuMono Nerd Font
-VictorMono Nerd Font
-ZedMono Nerd Font
+0xProto
+3270
+Agave
+AnonymousPro
+Arimo
+AurulentSansMono
+BigBlueTerminal
+BitstreamVeraSansMono
+CascadiaCode
+CascadiaMono
+CodeNewRoman
+ComicShannsMono
+CommitMono
+Cousine
+D2Coding
+DaddyTimeMono
+DejaVuSansMono
+DroidSansMono
+EnvyCodeR
+FantasqueSansMono
+FiraCode
+FiraMono
+GeistMono
+Go-Mono
+Gohu
+Hack
+Hasklig
+HeavyData
+Hermit
+iA-Writer
+IBMPlexMono
+Inconsolata
+InconsolataGo
+InconsolataLGC
+IntelOneMono
+Iosevka
+IosevkaTerm
+IosevkaTermSlab
+JetBrainsMono
+Lekton
+LiberationMono
+Lilex
+MartianMono
+Meslo
+Monaspace
+Monofur
+Monoid
+Mononoki
+MPlus
+NerdFontsSymbolsOnly
+Noto
+OpenDyslexic
+Overpass
+ProFont
+ProggyClean
+Recursive
+RobotoMono
+ShareTechMono
+SourceCodePro
+SpaceMono
+Terminus
+Tinos
+Ubuntu
+UbuntuMono
+UbuntuSans
+VictorMono
+ZedMono
 "
 
-# Format the font list into three columns
+# Format the font list into three columns, adjusting for variable lengths
 format_fonts_into_columns() {
-    printf "%b\n" "$fonts" | nl | awk '{printf "%-40s", $0; if (NR%3==0) printf "\n"} END {if (NR%3!=0) printf "\n"}'
+    printf "%b\n" "$fonts" | nl -w 2 -s '. ' | awk '{printf "%-35s", $0; if(NR % 3 == 0) print "";}'
 }
 
-# Display menu of available fonts in three columns with 'more'
-printf "%b\n" '\033[0;32mSelect fonts to install (separate with spaces):\033[0m'  
+# Display menu of available fonts in three columns
+printf "%b\n" '\033[0;32mSelect fonts to install (separate with spaces):\033[0m'
 printf "%b\n" "---------------------------------------------"
-format_fonts_into_columns | more  # Use 'more' to paginate font list in three columns
+format_fonts_into_columns
 printf "%b\n" "---------------------------------------------"
 
 # Prompt user to select fonts and validate input
 while true; do
-    printf "%b\n" '\033[0;36mEnter the numbers of the fonts to install (e.g., "1 2 3"): \033[0m'  
+    printf "%b\n" '\033[0;36mEnter the numbers of the fonts to install (e.g., "1 2 3"): \033[0m'
     read -r font_selection
 
     # Check if user has made a selection
     if [ -n "$font_selection" ]; then
         break  # Exit loop if input is not empty
     else
-        printf "%b\n" '\033[0;31mPlease select at least one font.\033[0m'  
+        printf "%b\n" '\033[0;31mPlease select at least one font.\033[0m'
     fi
 done
 
 # Download and install selected fonts
 for selection in $font_selection; do
-    font=$(printf "%b\n" "$fonts" | sed -n "${selection}p")  # Extract font based on user selection
+    font_index=$((selection))  # Get the user input number
+    font_index=$((font_index + 1))  # Increment by 1 for the correct index
+    font=$(echo "$fonts" | sed -n "${font_index}p")  # Get the font based on adjusted index
     if [ -z "$font" ]; then
-        printf "%b\n" '\033[0;31mInvalid selection: '"$selection"'\033[0m'  
+        printf "%b\n" '\033[0;31mInvalid selection: '"$selection"'\033[0m'
         continue
     fi
-    printf "%b\n" '\033[0;34mDownloading and installing '"$font"'\033[0m'  
+    printf "%b\n" '\033[0;34mDownloading and installing '"$font"'\033[0m'
     font_name=$(printf "%b\n" "$font" | awk '{print $1}')
-    curl -sSLo "$HOME/tmp/$font_name.tar.xz" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.tar.xz"
-    tar -xf "$HOME/tmp/$font_name.tar.xz" -C "$HOME/.local/share/fonts"
-    rm "$HOME/tmp/$font_name.tar.xz"
+    curl -sSLo "$HOME/tmp/$font_name.zip" "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$font_name.zip"
+    unzip -o "$HOME/tmp/$font_name.zip" -d "$HOME/.local/share/fonts"
+    rm "$HOME/tmp/$font_name.zip"
 done
 
 # Update font cache
