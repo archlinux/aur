c196d4d6f5e3dcfe84531565d1196a1a7e4148ba
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-10-04 10:36:01 +0900

    update.sh

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ORIGINAL_REPO=$(grep "^source" PKGBUILD |grep "git+https"|sed -e 's|.*com/\([^#]*\)#.*$|\1|' -e 's|.git$||')
+LATEST_COMMIT=$(curl -s https://api.github.com/repos/$ORIGINAL_REPO/commits/HEAD|jq -r ".sha")
+echo $ORIGINAL_REPO
+echo $LATEST_COMMIT
+
+# PKGBUILD
+COMMIT=$(grep "^_commit" PKGBUILD|cut -f2 -d"=")
+echo $COMMIT
+
+if [[ "$COMMIT" != "$LATEST_COMMIT" ]]; then
+    echo "change detected."
+    git checkout master
+    sed -i 's|^_commit=.*$|_commit='"${LATEST_COMMIT}"'|' PKGBUILD
+    updpkgsums
+    mksrcinfo
+    git diff
+    git commit -a -m "Update: _commit=${LATEST_COMMIT}"
+    git push
+    git checkout makepkg
+else
+    echo "No change detected."
+fi
