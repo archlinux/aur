9661d9f21b77ac752a9abb7596ee63db28a20356
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-10-05 22:06:16 +0900

    Squashed commit of the following:
    commit 5dca97496a824dff1b1e9355843b28d6d35bd3a8
    Author: Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
    Date:   Sat Oct 5 21:30:51 2024 +0900
    
        update.sh update
    
    commit a3a127c6dbfbd63831afe4acc8030f641a6f620b
    Author: Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
    Date:   Sat Oct 5 21:21:23 2024 +0900
    
        update
    
        _mozc_commit=e7fbedee076e8428abb72de77236e312073f8846
    
    commit 72e98bfa304b65818d04fb6510c599cc086c62fb
    Author: Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
    Date:   Sat Oct 5 20:18:55 2024 +0900
    
        Add bcr(bazel central registry) to sources
    
    commit 3182b79c8055caa6c30de72d528775cb979cdb83
    Author: Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
    Date:   Thu Oct 3 23:35:48 2024 +0900
    
        update.sh

---
+++ update.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#SudahiDict
+SudachiDict_DATE=$(curl -s 'http://sudachi.s3-website-ap-northeast-1.amazonaws.com/sudachidict-raw/' | grep -o '<td>[0-9]*</td>' | grep -o '[0-9]*' | sort -n | tail -n 1)
+
+#ORIGINAL_REPO=fcitx/mozc
+ORIGINAL_REPO=$(grep "^url" PKGBUILD | tr -d '"' | sed -e 's|.*com/\(.*\)$|\1|' -e 's|.git$||')
+BCR_REPO=bazelbuild/bazel-central-registry
+FCITX5_MOZC_COMMIT=$(curl -s https://api.github.com/repos/$ORIGINAL_REPO/commits/fcitx|jq -r ".sha")
+BCR_COMMIT=$(curl -s https://api.github.com/repos/$BCR_REPO/commits/main|jq -r ".sha")
+
+# PKGBUILD
+COMMIT=$(grep "^_mozc_commit" PKGBUILD|cut -f2 -d"=")
+SUDACHI_DATE=$(grep "^_sudachidict_date" PKGBUILD|cut -f2 -d"=")
+
+UPDATED_FLAG=0
+if [[ "$COMMIT" != "$FCITX5_MOZC_COMMIT" ]]; then
+    echo "Mozc Updated."
+    echo "$COMMIT"
+    echo "$FCITX5_MOZC_COMMIT"
+    UPDATED_FLAG=1
+fi
+if [[ "$SudachiDict_DATE" != "$SUDACHI_DATE" ]];then
+    echo "SudachiDict Updated."
+    echo "${SUDACHI_DATE}"
+    echo "${SudachiDict_DATE}"
+    UPDATED_FLAG=1
+fi
+if [[ "$UPDATE_FLAG" == "1" ]]; then
+    git checkout makepkg
+fi
+if [[ "$COMMIT" != "$FCITX5_MOZC_COMMIT" ]]; then
+    sed -i 's|^_mozc_commit=.*$|_mozc_commit='"${FCITX5_MOZC_COMMIT}"'|' PKGBUILD*
+    sed -i 's|^_bcr_commit=.*$|_bcr_commit='"${BCR_COMMIT}"'|' PKGBUILD*
+    git diff
+    git commit -a -m "Update: _mozc_commit=$FCITX5_MOZC_COMMIT"
+fi
+if [[ "$SudachiDict_DATE" != "$SUDACHI_DATE" ]];then
+    sed -i 's|^_sudachidict_date=.*$|_sudachidict_date='"${SudachiDict_DATE}"'|' PKGBUILD*
+    git diff
+    git commit -a -m "Update: SudachiDict=$SudachiDict_DATE"
+fi
+if [[ "$UPDATE_FLAG" == "1" ]]; then
+    echo "Change Detected."
+    updpkgsums
+    mksrcinfo
+    git commit -a -m "Update CheckSum."
+    #git push
+    git checkout master
+else
+    echo "No change Detected."
+fi
