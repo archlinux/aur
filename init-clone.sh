5b575b0f280358501fc3864a81eaa1807ff4fa64
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-10-23 23:56:10 +0900

    init-clone.sh update

--- init-clone.sh
+++ init-clone.sh
@@ -3,13 +3,20 @@
 source $PWD/PKGBUILD
 for url in $(echo ${source[@]}|xargs -n1|grep "git+")
 do
+    echo $url
     DIR=${url##*/}
+    if [[ -z $DIR ]];then
+        DIR=${url%/}
+        DIR=${DIR##*/}
+    fi
     COMMIT=${DIR#*commit=}
+    COMMIT=${COMMIT%\?*}
     DIR=${DIR%.git*}
     URL=${url%.git*}.git
     if [[ ! $url =~ ^git\+ ]];then
         URL=${URL#*::}
         DIR=${url%::*}
+        DIR=${DIR%.git*}
     fi
     URL=${URL#git+}
     echo $URL
