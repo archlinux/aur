157afbad2a8e3fef111015039f63100e6dcb5aff
gigamicro <gammamicro@gmail.com>
2024-10-26 17:59:20 -0400

    update for upstream's new automatic tmpdir & in-binary downloading

--- PKGBUILD
+++ PKGBUILD
@@ -1,20 +1,19 @@
 # Maintainer: gigamicro <gammamicro@gmail.com>
-pkgname='wfinfo-ng-git'
 _pkgname="wfinfo-ng"
+pkgname="$_pkgname-git"
 pkgdesc="Analyze Warframe relic reward screen to determine platinum value of items."
-pkgver=0.1.0.113.g87d033a
-pkgrel=2
-#epoch=1
+pkgver=0.1.0.114.gc440b6b
+pkgrel=1
 arch=('any') #TODO: verify this
 url="https://github.com/knoellle/wfinfo-ng"
 license=('GPL-3.0')
 depends=(curl '''''tesseract-data-eng''''' libxcb gcc-libs)
 makedepends=(git rust\>=1.74 cmake clang libxtst fontconfig)
-optdepends=('mlocate: to autodetect EE.log location')
+optdepends=('plocate: to autodetect non-default EE.log location')
 provides=('wfinfo')
 #conflicts=('wfinfo')
 source=("$_pkgname::git+$url.git" 'wfinfo.sh')
-sha256sums=('SKIP' '9493ca86314d36f50ee71831188cef8ed202a422f1924395ac7a8d3bccf2fbb0')
+sha256sums=('SKIP' '127b76052941393f822df669405053f914dd713f573b703a3c879718848ad0af')
 
 pkgver() {
 	cd "$srcdir/$_pkgname"
@@ -27,28 +26,25 @@ pkgver() {
 
 build(){
 	cd "$srcdir/$_pkgname"
-	env CFLAGS= cargo build --bin wfinfo --release --locked
-	sed 's/ | jq .//' -i "update.sh"
+	CFLAGS= cargo build --bin wfinfo --release --locked
 }
 
 check(){
 	cd "$srcdir/$_pkgname"
 	echo Updating database for check...
-	./update.sh
+	cat ./update.sh | sed 's/| jq \.//' | sh -
 	echo Grabbing test-images/1.png... # (bypassing git-lfs)
 	curl https://media.githubusercontent.com/media/knoellle/wfinfo-ng/master/test-images/1.png > test-images/1.png
-	env CFLAGS= cargo test --bin wfinfo --release --locked -- --skip wfi_images_99_percent
+	CFLAGS= cargo test --bin wfinfo --release --locked -- --skip wfi_images_99_percent
 }
 
 package() {
 	# wrapper script
 	install -D -m755 "$srcdir/wfinfo.sh" "$pkgdir/usr/bin/wfinfo"
-	# (edited from upstream) update script
-	install -D -m755 "$srcdir/$_pkgname/update.sh" "$pkgdir/usr/bin/wfinfo-update"
 	# main binary
 	install -D -m755 "$srcdir/$_pkgname/target/release/wfinfo" "$pkgdir/usr/bin/wfinfo-ng"
 	# Following can be added by 1. removing '--bin wfinfo' from the cargo build/test above and 2. uncommenting them here
-	# Sanctuary Onslaught indicator helper, watches key $1 (only 1-4) and turns red for $2 seconds
+	# Sanctuary Onslaught ability use helper, watches key $1 (only 1-4) and turns red for $2 seconds (for reference: 1 2/5, 2 10/3, 3 10/3, 4 10/1)
 	#install -D -m755 "$srcdir/$_pkgname/target/release/ability-timer" "$pkgdir/usr/bin/wfinfo-ability-timer"
 	# same as main binary (dumps into $PWD! needs databases in $PWD!), but takes filenames of images as args instead of its own screenshots
 	#install -D -m755 "$srcdir/$_pkgname/target/release/image" "$pkgdir/usr/bin/wfinfo-image"
--- wfinfo.sh
+++ wfinfo.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
-# technically this could be as simple as 'cd /tmp;wfinfo-update;wfinfo-ng' if your warframe compat data is at the default path
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+	cat <<-EOF
+		##### Wrapper #####
+		- expects \$1 to be the log location (or -h/--help),
+		   so options must be passed after that
+		 - will find a log location with \`locate\` if it's
+		    not at \$1 or the default location
+		- creates a cache directory under ~/.cache/
+		   instead of the binary's default of /tmp/
+		 - configurable via environment variable TMPDIR
+		- btw WFINFO_LOG={off, error, warn, info, debug,
+		   trace} changes the loglevel of the binary
+		##### Binary #####
+	EOF
+	exec wfinfo-ng "$1"
+fi
 
 ##### cache setup #####
-: ${WFINFO_CACHEDIR:=~/.cache/wfinfo-ng}
-mkdir -pv "$WFINFO_CACHEDIR" >&2
-cd "$WFINFO_CACHEDIR" || { echo Could not enter cache directory, exiting... >&2; exit 1; }
+: ${TMPDIR:=~/.cache/wfinfo-ng}
+mkdir -pv "$TMPDIR" >&2
 #######################
 
 ### EE.log location ###
@@ -12,35 +27,26 @@ cd "$WFINFO_CACHEDIR" || { echo Could not enter cache directory, exiting... >&2;
 logloc="$1"; [ "$#" -ge 1 ] && shift
 logloc="${logloc:-$(
 # set in cachedir?
-cat "$WFINFO_CACHEDIR/EElogloc" 2>&- ||
+cat -- "$TMPDIR/EElogloc" 2>&- ||
 # locatable?
 { locate --version >&- &&
 echo finding EE.log with most recent modification time... >&2 &&
-locate \*/Warframe/EE.log |
-xargs -rd \\n stat -c '%Y %n' |
-sort -rn | head -n 1 | cut -d ' ' -f 2-
+locate \*/Warframe/EE.log -0 |
+xargs -r0 stat --printf '%Y %n\0' |
+sort -rnz | head -zn 1 | cut -zd ' ' -f 2- | tr \\0 \\n
 }
 )}"
 if ! [ "$logloc" ]; then
-	echo No EE.log location '($1)', using default...
+	echo No EE.log location '($1)', using default... >&2
 	logloc="$HOME/.local/share/Steam/steamapps/compatdata/230410/pfx/drive_c/users/steamuser/AppData/Local/Warframe/EE.log"
 fi
 if ! [ -r "$logloc" ]; then
 	echo Invalid or unreadable EE.log location \""$logloc"\", exiting... >&2
 	exit 4
 fi
-printf %s\\n "$logloc" > "$WFINFO_CACHEDIR/EElogloc" && echo Storing EE.log location... >&2
-#######################
-
-###### json data ######
-wfinfo-update ||
-if [ -f "$WFINFO_CACHEDIR/filtered_items.json" ] && [ -f "$WFINFO_CACHEDIR/prices.json" ]; then
-	echo Update failed, continuing... >&2
-else
-	echo Update failed and no cached price info, exiting... >&2
-	exit 2
-fi
+printf %s\  Storing EE.log location = >&2 &&
+printf %s\\n "$logloc" | tee "$TMPDIR/EElogloc"
 #######################
 
 # valid WFINFO_LOG values are: error, warn, info, debug, trace, off
-WFINFO_LOG="${WFINFO_LOG:-warn}" exec wfinfo-ng "$logloc" "$@"
+WFINFO_LOG="${WFINFO_LOG:-warn}" TMPDIR="$TMPDIR" exec wfinfo-ng "$logloc" "$@"
