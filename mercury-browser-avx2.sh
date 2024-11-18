7b1322eaf37262a9b6c811e75a21c2cf1a60373c
saxophone_man <itsarchit@keemail.me>
2024-11-18 12:35:38 +0530

    remove goofy ahh popup

--- mercury-browser-avx2.sh
+++ mercury-browser-avx2.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # check microprocessor architecture level
-if grep -qE '\bavx2\b' /proc/cpuinfo ; then
-  _message=''
-  _message+=$'Your processor does support AVX2 instructions.'
-else
-  _message=''
-  _message+=$'Your processor does not support AVX2 instructions.\n'
-  _message+=$'mercury-browser may not work on your computer.'
+if ! grep -qE '\bavx2\b' /proc/cpuinfo ; then
+  _message=$'Your processor does not support AVX2 instructions.\nmercury-browser may not work on your computer.'
+  
+  # display processor support message
+  if tty -s ; then
+    echo "$_message"
+  else
+    [ ! -e "$HOME/.mercury" ] && notify-send -a "mercury-browser" -t 7500 "$_message"
+  fi
 fi
 
 # Allow users to override command-line options
@@ -15,13 +17,6 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
 _FLAGFILE="$XDG_CONFIG_HOME/mercury-flags.conf"
 if [[ -f "$_FLAGFILE" ]]; then
   _USER_FLAGS=$(cat "$_FLAGFILE")
-fi
-
-# display processor support message
-if tty -s ; then
-  echo "$_message"
-else
-  [ ! -e "$HOME/.mercury" ] && notify-send -a "mercury-browser" -t 7500 "$_message"
 fi
 
 # Launch
