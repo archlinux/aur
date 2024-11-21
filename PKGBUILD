91f7bdf39dcd76848a43f04872b0091969109f65
devome <evinedeng@hotmail.com>
2024-11-21 15:29:20 +0800

    python-bluetooth-auto-recovery: 1.4.2-1

---
+++ PKGBUILD
@@ -0,0 +1,27 @@
+# Maintainer: devome <evinedeng@hotmail.com>
+
+_pkgname=bluetooth-auto-recovery
+_pipname="${_pkgname//-/_}"
+pkgname="python-${_pkgname}"
+pkgver=1.4.2
+pkgrel=1
+pkgdesc="Recover bluetooth adapters that are in an stuck state"
+arch=('any')
+url="https://github.com/bluetooth-devices/bluetooth-auto-recovery"
+license=("MIT")
+depends=(python-bluetooth-adapters python-btsocket python-pyric python-usb-devices)
+makedepends=('python-build' 'python-installer' 'python-poetry-core' 'python-wheel')
+optdepends=(python-myst-parser python-sphinx python-sphinx-rtd-theme)
+source=("${_pkgname}-${pkgver}.tar.gz::https://files.pythonhosted.org/packages/source/${_pkgname::1}/${_pkgname}/${_pipname}-${pkgver}.tar.gz")
+sha256sums=('7146bacf1864b07c6ef0ed989b8e7784aa5597426a80c05735558f72b255b7d8')
+
+build() {
+    cd "${_pipname}-${pkgver}"
+    python -m build --wheel --no-isolation
+}
+
+package() {
+    cd "${_pipname}-${pkgver}"
+    python -m installer --destdir="${pkgdir}" dist/*.whl
+    install -Dm644 LICENSE "${pkgdir}/usr/share/licenses/${pkgname}/LICENSE"
+}
---
+++ geninfo.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+## run './geninfo.sh' after run 'updpkgsums', require install yq and jq.
+
+_pkgname=$(awk -F= '/_pkgname=/{print $2}' PKGBUILD)
+_pipname="${_pkgname//-/_}"
+pkgver=$(awk -F= '/pkgver=/{print $2}' PKGBUILD)
+pytoml="src/${_pipname}-${pkgver}/pyproject.toml"
+
+makepkg -do
+pkgdesc=$(yq eval -o=json "$pytoml" | jq -r '.tool.poetry.description')
+url=$(yq eval -o=json "$pytoml" | jq -r '.tool.poetry.repository')
+optdepends=$(yq eval -o=json "$pytoml" | jq -r '.tool.poetry.extras.docs.[]' | tr 'A-Z' 'a-z' | grep -oP '^[a-z0-9_\-]+' | sed 's|^|python-|' | sed 's|python-python-|python-|' | sed '/^python-python$/d' | sed 's|_|-|' | sort -u | tr '\n' ' ' | sed 's| $||')
+depends=$(yq eval -o=json "$pytoml" | jq '.tool.poetry.dependencies | keys' | jq -r '.[]' | tr 'A-Z' 'a-z' | grep -v 'async-timeout' | grep -oP '^[a-z0-9_\-]+' | sed 's|^|python-|' | sed 's|python-python-|python-|' | sed '/^python-python$/d' | sed 's|_|-|' | grep -vP "^(${optdepends// /|})$" | sort -u | tr '\n' ' ' | sed 's| $||')
+
+sed -e "s|^pkgdesc=.*|pkgdesc=\"$pkgdesc\"|" \
+    -e "s|^url=.*|url=\"${url}\"|" \
+    -e "s|^depends=.*|depends=(${depends})|" \
+    -e "s|^optdepends=.*|optdepends=(${optdepends})|" \
+    -i PKGBUILD
