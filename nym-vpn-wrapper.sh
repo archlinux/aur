9c335c520b547d40518889ce221dc3a1948e6d61
pierre <dommerc.pierre@gmail.com>
2024-11-29 10:33:55 +0100

    remove dead wrapper script

--- nym-vpn-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# fix an issue with NVIDIA gpu
-# https://github.com/nymtech/nym-vpn-client/issues/305
-export WEBKIT_DISABLE_DMABUF_RENDERER=1
-
-LOG_FILE=1 RUST_LOG=info,nym_vpn_app=trace /usr/bin/nym-vpn-app
