78ebdbe4fd2cf939c2b4227f7bfa492548364d4e
Nikos Toutountzoglou <nikos.toutou@protonmail.com>
2024-12-08 01:40:59 +0100

    Update

--- PKGBUILD
+++ PKGBUILD
@@ -1,46 +1,40 @@
-# Maintainer: Nikos Toutountzoglou <nikos dot toutou at protonmail dot com>
-# Contributor: Erikas Rudinskas <erikmnkl at gmail dot com>
+# Maintainer: Nikos Toutountzoglou <nikos.toutou@protonmail.com>
+# Contributor: Erikas Rudinskas <erikmnkl@gmail.com>
 
 pkgname=wg++
 pkgver=5.3
-pkgrel=1
-pkgdesc='WebGrab+Plus is a Freeware, closed-source multi-site incremental XMLTV EPG grabber'
+pkgrel=2
+pkgdesc="A Freeware, closed-source multi-site incremental XMLTV EPG grabber"
 arch=('x86_64' 'armv7h' 'aarch64')
-url='http://webgrabplus.com/'
-license=('custom:donator')
-depends=('dotnet-runtime-8.0' 'unzip' 'libxml2' 'curl' 'wget')
-optdepends=('dotnet-runtime-bin: binary dotnet core runtime alternative')
-source=("http://webgrabplus.com/sites/default/files/download/SW/V5.3.0/WebGrabPlus_V5.3_install.tar.gz"
-        'wgpp.sh')
+url="http://webgrabplus.com"
+license=('LicenseRef-donator')
+depends=('bash' 'dotnet-runtime-8.0' 'unzip' 'libxml2' 'wget')
+optdepends=('dotnet-runtime-8.0-bin: binary dotnet core runtime alternative')
+source=("${url}/sites/default/files/download/SW/V${pkgver}.0/WebGrabPlus_V${pkgver}_install.tar.gz"
+        "wgpp.sh")
 sha256sums=('d619430b8099070eabe1f871f72aa946afb876e80fd8f2d58f5bd647cbb0dad7'
-            '8cfd3acd0e7fe8d78990de1243e4f3dd6a0f18d4c59128a4de8b0f61ee6fa086')
+            '2c968010115a492874aaaae396757e7e6a9ce628d2d1a9ae148cabb4aced5831')
 
 prepare() {
   # Rename folder
   mv ".${pkgname}" "${pkgname}"
-  # Check for latest version of SiteIniPack from 'http://webgrabplus.com/epg-channels'
-  curl -sL http://webgrabplus.com/epg-channels | grep 'SiteIni\.Pack_' |
-    sed -e 's/.*btn"><a href="//' -e 's/".*//' -e 's/[^0-9][^0-9][^0-9]*//g' >siteini_ver
-  # Download latest version of SiteIniPack
-  if [ -s siteini_ver ]; then
-    _siteini_ver=$(cat siteini_ver)
-    msg2 "Found latest SiteIniPack_V${_siteini_ver}"
-    curl -LO http://webgrabplus.com/sites/default/files/download/ini/SiteIni.Pack_${_siteini_ver}.zip
-    bsdtar -xf SiteIni.Pack_${_siteini_ver}.zip
-    rm -r "${pkgname}/siteini.pack.update"/*
-    cp -r siteini.pack/* "${pkgname}/siteini.pack.update"
-  fi
-  # Run 'install.sh' script
+
+  # Run install script
   cd "${pkgname}"
   ./install.sh
-  # Adaptation for XML pretty formatting
+
+  # Adapt XML formatting
   sed -i 's/guide\.xml/latest\.xml/g' WebGrab++.config.xml
+
+  # Download latest version of SiteIniPack
+  cd bin.net
+  ./SiteIni.Pack.Update.sh
 }
 
 package() {
-  install -d "${pkgdir}/usr/share/${pkgname}"
-  cp -a --no-preserve='mode,ownership' "${srcdir}/${pkgname}" "${pkgdir}/usr/share"
-  find "${pkgdir}" -type f -name '*.sh' -exec chmod u=rwx,go=rx {} \;
+  install -dm755 "${pkgdir}/usr/share/${pkgname}"
+  cp -Pr --no-preserve='mode,ownership' "${srcdir}/${pkgname}" "${pkgdir}/usr/share"
+  find "${pkgdir}" -type f -name "*.sh" -exec chmod 755 {} \;
   install -Dm755 "${srcdir}/wgpp.sh" "${pkgdir}/usr/bin/${pkgname}"
 }
 
--- wgpp.sh
+++ wgpp.sh
@@ -1,42 +1,38 @@
 #!/usr/bin/env bash
 # script: wg++ (WebGrab+Plus)
 # author: Nikos Toutountzoglou, nikos.toutou@protonmail.com
-# rev.date: 17/08/2024
+# rev.date: 08/12/2024
 
 # Variables
 WGPP_USR=$(whoami)
-WGPP_EXE=$(basename $0)
+WGPP_EXE=$(basename "$0")
 WGPP_USR_HOME=$(getent passwd "$WGPP_USR" | cut -d: -f6)
-WGPP_CFGDIR=$WGPP_USR_HOME/wg++
-WGPP_SYS=/usr/share/wg++
+WGPP_CFGDIR="$WGPP_USR_HOME/wg++"
+WGPP_SYS="/usr/share/wg++"
 
 # Functions
 checkReq() {
 	local packages=(dotnet libxml2 curl unzip sudo)
-	for p in ${packages[@]}; do
-		if ! pacman -Qs ${p} >/dev/null; then
-			printf "'${p}' package is not installed. Exiting.\n"
+	for p in "${packages[@]}"; do
+		if ! pacman -Qs "$p" >/dev/null; then
+			printf "'%s' package is not installed. Exiting.\n" "$p"
 			exit 1
 		fi
 	done
 }
 
 missingSysFiles() {
-	if [ ! -e "$WGPP_CFGDIR/install.sh" -o ! -e "$WGPP_CFGDIR/run.net.sh" ]; then
-		cp -r -u $WGPP_SYS/* "$WGPP_CFGDIR" 2>/dev/null
+	if [[ ! -e "$WGPP_CFGDIR/install.sh" || ! -e "$WGPP_CFGDIR/run.net.sh" ]]; then
+		cp -r -u "$WGPP_SYS"/* "$WGPP_CFGDIR" 2>/dev/null
 		printf ":: Restored missing script files 'install.sh' and/or 'run.net.sh'.\n"
 	fi
 }
 
 updateSiteIni() {
-	SITEINI_URL="http://webgrabplus.com/sites/default/files/download/ini/SiteIniPack_current.zip"
-
-	if [ -d "$WGPP_CFGDIR/siteini.pack" ]; then
-		printf ":: Starting update of '$WGPP_CFGDIR/siteini.pack' to latest release.\n"
-		cd "$WGPP_CFGDIR"
-		curl -LO $SITEINI_URL
-		unzip -o "SiteIniPack_current.zip" "siteini.pack/*"
-		rm -f "SiteIniPack_current.zip"
+	if [[ -d "$WGPP_CFGDIR/siteini.pack" ]]; then
+		printf ":: Starting update of '%s/siteini.pack' to latest release.\n" "$WGPP_CFGDIR"
+		cd "$WGPP_CFGDIR/bin.net" || exit 1
+		./SiteIni.Pack.Update.sh
 	else
 		printf ":: Error, cannot find folder 'siteini.pack'. Recreating folder.\n:: Please re-run 'update' of 'siteini.pack' now.\n"
 		mkdir -v -p "$WGPP_CFGDIR/siteini.pack"
@@ -45,8 +41,8 @@ updateSiteIni() {
 }
 
 missingWGPPDir() {
-	if [ ! -d "$WGPP_CFGDIR" ]; then
-		printf ":: No '$WGPP_EXE' working directory found for user '$WGPP_USR', exiting.\n"
+	if [[ ! -d "$WGPP_CFGDIR" ]]; then
+		printf ":: No '%s' working directory found for user '%s', exiting.\n" "$WGPP_EXE" "$WGPP_USR"
 		exit 1
 	fi
 }
@@ -60,23 +56,23 @@ custFolder() {
 }
 
 genFolder() {
-	if [ ! -d "$WGPP_CFGDIR" ]; then
-		cp -r $WGPP_SYS "$WGPP_CFGDIR"
-		cd "$WGPP_CFGDIR" && sudo -u $WGPP_USR ./install.sh
-		printf ":: Configuration folder '$WGPP_CFGDIR' created.\n:: It's now possible to configure 'WebGrab++.config.xml' and re-run '$WGPP_EXE' to generate EPG data.\n"
+	if [[ ! -d "$WGPP_CFGDIR" ]]; then
+		cp -r "$WGPP_SYS" "$WGPP_CFGDIR"
+		cd "$WGPP_CFGDIR" && sudo -u "$WGPP_USR" ./install.sh
+		printf ":: Configuration folder '%s' created.\n" "$WGPP_CFGDIR"
+		printf ":: It's now possible to configure 'WebGrab++.config.xml' and re-run '%s' to generate EPG data.\n" "$WGPP_EXE"
 		exit 0
 	else
-		printf ":: Configuration folder '$WGPP_CFGDIR' already exists, exiting.\n"
+		printf ":: Configuration folder '%s' already exists, exiting.\n" "$WGPP_CFGDIR"
 		exit 1
 	fi
 }
 
 runScript() {
-	cd "$WGPP_CFGDIR"
-	# For 'No internet' issue use 'dotnet-runtime-bin' or run as 'root' user
-	sudo -u $WGPP_USR ./run.net.sh
+	cd "$WGPP_CFGDIR" || exit 1
+	sudo -u "$WGPP_USR" ./run.net.sh
 
-	if [ ! -e "$WGPP_CFGDIR/latest.xml" ]; then
+	if [[ ! -e "$WGPP_CFGDIR/latest.xml" ]]; then
 		printf ":: Error, cannot find latest EPG XML data file named 'latest.xml'. Exiting.\n"
 		exit 1
 	fi
@@ -86,8 +82,8 @@ runScript() {
 
 helpMsg() {
 	printf "WebGrab+Plus EPG/XMLTV grabber
-Usage: $WGPP_EXE [-d|--dir <CUSTOM_DIR>] [-g|--generate] [-d <CUSTOM_DIR> -g] [-u|--update] [-d <CUSTOM_DIR> -u]
-       $WGPP_EXE -h|--help
+Usage: %s [-d|--dir <CUSTOM_DIR>] [-g|--generate] [-d <CUSTOM_DIR> -g] [-u|--update] [-d <CUSTOM_DIR> -u]
+       %s -h|--help
 
 Options:
   -d <CUSTOM_DIR>         Run from custom configuration folder <CUSTOM_DIR>.
@@ -95,15 +91,15 @@ Options:
   -d <CUSTOM_DIR> -g      Create new custom configuration folder <CUSTOM_DIR>
   -u                      Update to latest 'siteini.pack' and channel list files
   -d <CUSTOM_DIR> -u      Update custom configuration folder to latest 'siteini.pack' and channel list files.
-  -h, --help              Show help\n"
+  -h, --help              Show help\n" "$WGPP_EXE" "$WGPP_EXE"
 
 	exit
 }
 
-# While loop
+# Argument parsing
 args=("$@")
 
-while [ $# -ne 0 ]; do
+while [[ $# -ne 0 ]]; do
 	case "$1" in
 	-d | --dir)
 		shift
@@ -122,7 +118,7 @@ while [ $# -ne 0 ]; do
 		exit 1
 		;;
 	*)
-		printf "Unknown argument '$1'\n"
+		printf "Unknown argument '%s'\n" "$1"
 		exit 1
 		;;
 	esac
