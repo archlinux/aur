d98f76ac2cba6fa8959580cf41fb62870a991f98
Aleksy Grabowski <hurufu@gmail.com>
2024-12-08 13:44:56 +0100

    Reorganize package dependencies + improve backend guessing

--- PKGBUILD
+++ PKGBUILD
@@ -1,8 +1,8 @@
 # Maintainer: Alex Grabowski <hurufu+aur@gmail.com>
 pkgbase=dogelog
-pkgname=(dogelog-node dogelog-py dogelog-java)
+pkgname=(dogelog-node dogelog-py dogelog-java dogelog-license dogelog)
 pkgver=1.2.5
-pkgrel=3
+pkgrel=4
 arch=(any)
 url='https://www.dogelog.ch/'
 pkgdesc='The avant-garde of logic programming'
@@ -12,24 +12,39 @@ source=(https://www.dogelog.ch/typtab/rscsrv/docs/50_archive/archive_1_2/dogelog
 sha256sums=('ac929e3662f3bb539d47d12c81004b1d48b6e7cf800f93353d04905854a25a61')
 
 package_any() {
-    install -D --mode=644 -t "$pkgdir/usr/share/licenses/$pkgbase/$1" ../LICENSE
     install -D --mode=655 -t "$pkgdir/usr/share/$pkgbase" "$2"
 }
 
 package_dogelog-py() {
     pkgdesc='The avant-garde of logic programming (Python backend)'
+    depends=(dogelog-license)
     optdepends=('python3>=3.10.14' pypy3)
+    provides=(dogelog-package)
     package_any py playerpy/canned/dogelog.py
 }
 
 package_dogelog-node() {
     pkgdesc='The avant-garde of logic programming (NodeJS backend)'
+    depends=(dogelog-license)
     optdepends=('nodejs>=22.11.0' bun)
+    provides=(dogelog-package)
     package_any node player/canned/dogelog.mjs
 }
 
 package_dogelog-java() {
     pkgdesc='The avant-garde of logic programming (Java backend)'
-    depends=('java-runtime>=21')
+    depends=('java-runtime>=21' dogelog-license)
+    provides=(dogelog-package)
     package_any java playerj/canned/dogelog.zip
+}
+
+package_dogelog() {
+    depends=(dogelog-package)
+    license=(GPL-2.0-or-later)
+    install -m755 -D ../dogelog.sh "$pkgdir/usr/bin/$pkgbase"
+}
+
+package_dogelog-license() {
+    license=()
+    install -m644 -Dt "$pkgdir/usr/share/licenses/$pkgbase" ../LICENSE
 }
--- dogelog.sh
+++ dogelog.sh
@@ -4,33 +4,51 @@ set -eu ${TRACE+-x}
 
 PREFIX=/usr/share/dogelog
 
+guess_backend() {
+    if [ -f  "$PREFIX/dogelog.mjs" ]; then
+        if command -v node >/dev/null 2>&1; then
+            echo node; return
+        elif command -v bun >/dev/null 2>&1; then
+            echo bun; return
+        fi
+    fi
+    if [ -f "$PREFIX/dogelog.py" ]; then
+        if command -v pypy3 >/dev/null 2>&1; then
+            echo pypy3; return
+        elif command -v python3 >/dev/null 2>&1; then
+            echo python3
+        fi
+    fi
+    if [ -f "$PREFIX/dogelog.zip" ]; then
+        if command -v java >/dev/null 2>&1; then
+            echo java; return
+        fi
+    fi
+    echo Please install dogelog >&2
+    return 1
+}
+
 # If backend isn't specified try to guess it, based on what's installed
-if [ "${DOGELOG_BACKEND-}" = '' ]
-then
-    case "$(ls $PREFIX/dogelog.*)" in
-        *.mjs) DOGELOG_BACKEND=node ;;
-        *.py)  DOGELOG_BACKEND=python ;;
-        *.zip) DOGELOG_BACKEND=java ;;
-        *)
-            echo Please install dogelog >&2
-            exit 1 ;;
-    esac
+if [ "${DOGELOG_BACKEND-}" = '' ]; then
+    DOGELOG_BACKEND="$(guess_backend)"
 fi
 
 # Select command line appropriate to the backend
 case "${DOGELOG_BACKEND}" in
-    node*|js|javascript)
-        CMDLINE="node $PREFIX/dogelog.mjs"
-        ;;
-    py*)
-        CMDLINE="python3 $PREFIX/dogelog.py"
-        ;;
+    node*)
+        CMDLINE="node $PREFIX/dogelog.mjs" ;;
+    bun*)
+        CMDLINE="bun $PREFIX/dogelog.mjs" ;;
+    python*)
+        CMDLINE="python3 $PREFIX/dogelog.py" ;;
+    pypy*)
+        CMDLINE="pypy3 $PREFIX/dogelog.py" ;;
     java*)
         CMDLINE="java -cp $PREFIX/dogelog.zip:playerj/canned Index"
         ;;
     *)
         cat <<-EOT >&2
-			DOGELOG_BACKEND must be undefined or be one of: node, python or java
+			DOGELOG_BACKEND must be undefined or be one of: node, bun, python, pypy or java
 			Current value is: $DOGELOG_BACKEND
 		EOT
         exit 1 ;;
