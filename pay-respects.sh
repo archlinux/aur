e1c8340398676542de6c5f1da189081c4cc2825e
iff <iff@ik.me>
2024-12-11 17:56:02 +0100

    less nesting

--- pay-respects.sh
+++ pay-respects.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-if [ "$#" -gt 1 ]; then
-	if [ -z "$_PR_LIB" ]; then
-		SHELL=$(basename $SHELL)
-		LIB="/usr/lib/pay-respects"
-		if [ "$SHELL" = "nu" ]; then
-			echo "env:_PR_LIB=$LIB"
-		elif [[ "$SHELL" = "pwsh" ]]; then
-			echo "\$env:_PR_LIB=\"$LIB\""
-		else
-			echo "export _PR_LIB=$LIB"
-		fi
+if [ "$#" -gt 1 ] && [ -z "$_PR_LIB" ]; then
+	SHELL=$(basename $SHELL)
+	LIB="/usr/lib/pay-respects"
+	if [ "$SHELL" = "nu" ]; then
+		echo "env:_PR_LIB=$LIB"
+	elif [[ "$SHELL" = "pwsh" ]]; then
+		echo "\$env:_PR_LIB=\"$LIB\""
+	else
+		echo "export _PR_LIB=$LIB"
 	fi
 fi
 /opt/pay-respects/bin/pay-respects "$@"
