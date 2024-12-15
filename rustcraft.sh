7bdf7fff442f2107bef8894113226119aa40392c
Adam Perkowski <adas1per@protonmail.com>
2024-12-15 18:03:59 +0100

    rustcraft-bin: fix exec script

--- rustcraft.sh
+++ rustcraft.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 mkdir -p ~/.local/share/rustcraft
+
+if [ ! -d ~/.local/share/rustcraft/data ]; then
+    echo "Copying assets..."
+    cp -R /opt/rustcraft/data ~/.local/share/rustcraft/data
+fi
+
 /opt/rustcraft/bin/rustcraft --game-folder-path ~/.local/share/rustcraft --assets-folder-path ../data "$@"
