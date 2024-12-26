647cfd73fa18064be29485821c0813b134b7e65c
Kimiblock Moe <kimiblock@icloud.com>
2024-12-26 10:23:45 +0800

    Warn users if portable is installed but its config is missing

--- wechat.sh
+++ wechat.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
-if [ -f /usr/bin/portable ]; then
+if [ -f /usr/bin/portable ] && [ -f /usr/lib/wechat/portable-config ]; then
 	export _portalConfig=/usr/lib/wechat/portable-config
 	portable $@
+elif [ -f /usr/bin/portable ]; then
+	echo "You'll need to install wechat-bwrap for sandboxing to work!"
+	notify-send "You'll need to install wechat-bwrap for sandboxing to work!"
+	zenity --error --title "Sandbox non-functional" --icon=security-low-symbolic --text "Please install aur/wechat-bwrap"
 else
 	/opt/wechat/wechat $@
 fi
