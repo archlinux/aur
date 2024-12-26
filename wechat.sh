1c2793f6c1ef685f961f5b15c6209823850a8967
Kimiblock Moe <kimiblock@icloud.com>
2024-12-26 19:04:33 +0800

    Remove unused code

--- wechat.sh
+++ wechat.sh
@@ -10,8 +10,10 @@ if [ -f /usr/bin/portable ] && [ ! -f /usr/lib/wechat/portable-config ]; then # 
 	echo "You'll need to install wechat-bwrap for sandboxing to work!"
 	notify-send "You'll need to install wechat-bwrap for sandboxing to work!"
 	zenity --error --title "Sandbox non-functional" --icon=security-low-symbolic --text "Please install aur/wechat-bwrap"
-elif [ -f /usr/bin/wechat-sandbox-provider ]; then
-	/usr/bin/wechat-sandbox-provider "$@"
+fi
+
+if [ -f /usr/bin/wechat-sandbox ]; then
+	/usr/bin/wechat-sandbox "$@"
 else
 	/opt/wechat/wechat $@
 fi
