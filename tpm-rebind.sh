bbc0e0ad8239def93cfe97dc4313dcca5e0c0dba
Doridian <git@doridian.net>
2024-12-28 20:50:04 -0800

    Auto-detect slot and rootdev

--- tpm-rebind.sh
+++ tpm-rebind.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 set -euo pipefail
 
-SLOT=1
-ROOTDEV=/dev/nvme0n1p2
+ROOTDEV_UUID="$(grep -oP 'cryptdevice=UUID=[0-9a-f-]+' /proc/cmdline | cut -d= -f3)"
+ROOTDEV="/dev/disk/by-uuid/${ROOTDEV_UUID}"
 
-MEASURED_PIN="$(dmesg | grep -F 'Clevis pin:' | cut -d: -f2- | tr -d '=\r\n\t ')"
+MEASURED_PIN="$(dmesg | grep -F 'Clevis pin:' | head -1 | cut -d: -f2- | tr -d '=\r\n\t ')"
 
-if ! clevis luks list -d "${ROOTDEV}" -s "${SLOT}" | grep -qF 'tpm2'; then
-	clevis luks unbind -d "${ROOTDEV}" -s "${SLOT}" || true
-	clevis luks bind -d "${ROOTDEV}" -s "${SLOT}" tpm2 "${MEASURED_PIN}"
+TPM2_PIN="$(clevis luks list -d "${ROOTDEV}" | grep -F 'tpm2' || true)"
+
+if [ -z "$TPM2_PIN" ]; then
+	echo 'No binding found, creating a new one!'
+	clevis luks bind -d "${ROOTDEV}" tpm2 "${MEASURED_PIN}"
 else
+	SLOT="$(echo "${TPM2_PIN}" | cut -d: -f1)"
+	echo "Binding already exists in slot ${SLOT}, rebinding!"
 	clevis luks edit -d "${ROOTDEV}" -s "${SLOT}" -c "${MEASURED_PIN}"
 fi
