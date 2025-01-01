82711a656043d358c60ae921c00a4fd350a4b870
Jonas Leder <jonas@jonasled.de>
2025-01-01 23:20:34 +0000

    Initial commit
    initial commit
    
    add gitignore file
    
    add srcinfo
    
    implement package step
    
    add ci pipeline and update script
    
    remove readme

---
+++ PKGBUILD
@@ -0,0 +1,26 @@
+# Maintainer: Jonas Leder <info@jonasled.de>
+_pkgname=aptakube
+
+pkgname="${_pkgname}"
+pkgver=1.9.1
+pkgrel=1
+pkgdesc="A modern and lightweight Kubernetes desktop client to help you operate workloads on multiple clusters. (free to use during the public preview)"
+arch=('x86_64')
+url="https://aptakube.com/"
+license=('Proprietary')
+depends=('zlib' 'hicolor-icon-theme')
+makedepends=('binutils')
+options=(!strip)
+_debfile="${pkgname}-${pkgver}.deb"
+source_x86_64=("${_debfile}::https://releases.aptakube.com/${_pkgname}_${pkgver}_amd64.deb")
+noextract=("${_debfile}")
+sha256sums_x86_64=('c53c25fdc173020306a11ef5b6727ad94a4b7ebdc39e666b51f24d49c536d0ba')
+
+package() {
+    cd "$srcdir"
+
+    # Extract .deb file
+    ar x "${_debfile}"
+    tar -xvf data.tar.* -C "$pkgdir/"
+
+}
---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source PKGBUILD
+
+if [ "${pkgver}" == "${1}" ]; then
+    echo "Same version as PKGBUILD; nothing to do"
+    exit 0
+fi
+
+echo "Updating PKGBUILD file with latest upstream release"
+
+sed -i "s/pkgver=${pkgver}/pkgver=${1}/g" PKGBUILD
+sed -i "s/${pkgver}/${1}/g" .SRCINFO
+source PKGBUILD 
+
+for type in source sha256sums; do
+    for architecture in ${arch[@]}; do
+        if [ "$type" == "source" ]; then
+            domain=${type}_${architecture}
+            curl -o ${architecture}.tmp https://releases.aptakube.com/aptakube_${pkgver}_amd64.deb
+            shasum=$(sha256sum ${architecture}.tmp)
+            echo "sha256sums_${architecture}=('${shasum%%[[:space:]]*}')"
+            sed -i "s/sha256sums_${architecture}=.*/sha256sums_${architecture}=('${shasum%%[[:space:]]*}')/g" PKGBUILD
+            sed -i "s/sha256sums_${architecture} = .*/sha256sums_${architecture} = ${shasum%%[[:space:]]*}/g" .SRCINFO
+            rm ${architecture}.tmp
+        fi
+    done
+done
+sudo -u nobody makepkg --printsrcinfo > .SRCINFO
