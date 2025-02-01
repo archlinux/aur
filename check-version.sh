c5065c17a5ce40643231a747dc96c33ecf9fdc97
witt <1989161762@qq.com>
2025-02-01 23:05:36 +0800

    update check-version

--- check-version.sh
+++ check-version.sh
@@ -4,28 +4,40 @@ current_version=$(grep 'pkgver=' PKGBUILD | awk -F '=' '{ print $2 }');
 
 echo "Current version: $current_version";
 
-tmp_dir=$(mktemp -d);
+current_file="apifox-v${current_version}.zip"
+
+# [ -e "${current_file}" ] && rm "${current_file}";
+
+updpkgsums;
+
+# 使用 file 命令检查文件类型
+current_file_mime_type=$(file --mime-type -b "${current_file}")
+
+# 判断文件类型是否为 zip
+if [ "${current_file_mime_type}" != "application/zip" ] && [ "${current_file_mime_type}" != "application/octet-stream" ] ; then
+    rm "${current_file}";
+    git checkout PKGBUILD;
+    echo "Fetch ${current_file} not zip , restricted !";
+    exit 2;
+fi
 
 source ./PKGBUILD;
 
-major_url=$(echo "${source[0]}" | awk -F '::' '{ print $2 }');
-
-echo "fetching apifox-latest.zip from $major_url";
-wget -q -O "$tmp_dir/apifox-latest.zip" "${major_url}";
-
-cd "$tmp_dir" || exit;
-
 echo "Parse version ...";
 
-unzip -q apifox-latest.zip;
+temp_dir=$(mktemp -d);
+
+unzip -q "${current_file}" -d "${temp_dir}";
+
+cd "${temp_dir}" || exit 1;
 
 prepare;
 
 next_version=$(pkgver);
 
-cd - || exit;
+cd - || exit 1;
 
-echo "Next version: $next_version";
+echo "Next version: ${next_version}";
 
 
 if [[ "${current_version}" == "${next_version}" ]]; then
@@ -38,4 +50,7 @@ echo "Version is outdated";
 sed -i "s/pkgver=${current_version}/pkgver=${next_version}/" PKGBUILD;
 
 echo "Update version to $next_version";
-updpkgsums;
+
+mv "${current_file}" "apifox-v${next_version}.zip";
+
+makepkg --printsrcinfo > .SRCINFO
