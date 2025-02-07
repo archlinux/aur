cc50ede72cdb2a48670275a585459aa3ff79fda4
devome <evinedeng@hotmail.com>
2025-02-07 12:16:59 +0800

    python-langchain-core: 0.3.34-2

--- PKGBUILD
+++ PKGBUILD
@@ -5,13 +5,13 @@ _pkgname=langchain-core
 _pipname="${_pkgname//-/_}"
 pkgname="python-${_pkgname}"
 pkgver=0.3.34
-pkgrel=1
-pkgdesc="null"
+pkgrel=2
+pkgdesc="Building applications with LLMs through composability"
 arch=('any')
 url='https://github.com/langchain-ai/langchain/tree/master/libs/core'
 license=('MIT')
-depends=()
-makedepends=('python-build' 'python-installer' 'python-poetry-core' 'python-wheel')
+depends=(python-jsonpatch python-langsmith python-packaging python-pydantic python-pyyaml python-tenacity python-typing_extensions)
+makedepends=('python-build' 'python-installer' 'python-pdm-backend' 'python-wheel')
 source=("${_pkgname}-${pkgver}.tar.gz::https://files.pythonhosted.org/packages/source/${_pipname::1}/${_pipname}/${_pipname}-${pkgver}.tar.gz")
 sha256sums=('26504cf1e8e6c310adad907b890d4e3c147581cfa7434114f6dc1134fe4bc6d3')
 
--- geninfo.sh
+++ geninfo.sh
@@ -8,8 +8,19 @@ pkgver=$(awk -F= '/pkgver=/{print $2}' PKGBUILD)
 pytoml="src/${_pipname}-${pkgver}/pyproject.toml"
 
 makepkg -do
-pkgdesc=$(yq eval -o=json "$pytoml" | jq -r '.tool.poetry.description')
-depends=$(yq eval -o=json "$pytoml" | jq '.tool.poetry.dependencies | keys' | jq -r '.[]' | tr 'A-Z' 'a-z' | sort -u | sed 's|^|python-|' | sed 's|python-python-|python-|' | sed 's|typing-extensions|typing_extensions|' | sed '/^python-python$/d' | tr '\n' ' ' | sed 's| $||')
+pkgdesc=$(yq eval -o=json "$pytoml" | jq -r '.project.description')
+depends=$(yq eval -o=json "$pytoml" |
+    jq -r '.project.dependencies[]' |
+    tr 'A-Z' 'a-z' |
+    grep -oP '^[a-z0-9_-]+' |
+    sed 's|^|python-|' |
+    sed 's|python-python-|python-|' |
+    sed 's|typing-extensions|typing_extensions|' |
+    sed '/^python-python$/d' |
+    sort -u |
+    tr '\n' ' ' |
+    sed 's| $||'
+)
 
 sed -e "s|^pkgdesc=.*|pkgdesc=\"$pkgdesc\"|" \
     -e "s|^depends=.*|depends=(${depends})|" \
