e40469080b30005fc2aa3998522b74cc5a3a7d4e
Jove Yu <yushijun110@126.com>
2025-02-11 11:37:21 +0800

    use printsrcinfo

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 # Get latest version
 VER=$(curl "https://dl.lazycat.cloud/client/desktop/lzc-client-desktop.tar.zst.metadata.json" --silent | awk -F '"' '/buildVersion/ {print $4}')
+MD5=$(curl "https://dl.lazycat.cloud/client/desktop/stable/lzc-client-desktop_${VER}.tar.zst.md5" --silent)
 
 # Insert latest version into PKGBUILD and update hashes
-sed -i \
-	-e "s/^_pkgver=.*/_pkgver=${VER}/" \
-	PKGBUILD
+sed -i -e "s/^_pkgver=.*/_pkgver=${VER}/" PKGBUILD
+sed -i -e "s/^md5sums=.*/md5sums=('${MD5}'/" PKGBUILD
 
 # Check whether this changed anything
 if (git diff --exit-code PKGBUILD); then
@@ -19,10 +19,12 @@ sed -i \
 	-e 's/pkgrel=.*/pkgrel=1/' \
 	PKGBUILD
 
-updpkgsums
-
 # Update .SRCINFO
-makepkg --printsrcinfo >.SRCINFO
+if command -v printsrcinfo > /dev/null; then
+  printsrcinfo > .SRCINFO
+else
+  makepkg --printsrcinfo > .SRCINFO
+fi
 
 # Commit changes
 git add PKGBUILD .SRCINFO
