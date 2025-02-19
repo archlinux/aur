cf93d94f2d163dd66450b806d94fd75461d1917c
Horange <horange321@163.com>
2025-02-19 13:10:31 +0800

    <Add>build.fish

---
+++ build.fish
@@ -0,0 +1,33 @@
+#!/bin/fish
+# 获取更新
+set ver (curl 'https://hmcl.huangyuhui.net/api/update_link?channel=dev' | jq -r '.["version"]')
+echo 最新版 $ver
+
+# 获取当前版本
+set pkgver (cat .SRCINFO | grep pkgver | cut -d ' ' -f 3)
+set pkgrel (cat .SRCINFO | grep pkgrel | cut -d ' ' -f 3)
+echo 当前版 $pkgver.$pkgrel
+
+if [ $ver = $pkgver.$pkgrel ]
+    echo 已是最新版
+    exit
+end
+
+# 生成 PKGBUILD
+set vers (string split . $ver)
+set pkgver $vers[1].$vers[2].$vers[3]
+set pkgrel $vers[4]
+curl "http://mirrors.cloud.tencent.com/nexus/repository/maven-public/org/glavo/hmcl/hmcl-dev/$ver/hmcl-dev-$ver.jar" -O
+echo -n pkgver= >PKGBUILD
+echo $pkgver >>PKGBUILD
+echo -n pkgrel= >>PKGBUILD
+echo $pkgrel >>PKGBUILD
+echo -n 'sha256sums=("' >>PKGBUILD
+sha256sum hmcl-dev-$pkgver.$pkgrel.jar | cut -d ' ' -f 1 | tr -d '\n' >>PKGBUILD
+echo '"' >>PKGBUILD
+cat PKGBUILD.part1 >>PKGBUILD
+
+# build
+makepkg -f
+makepkg --printsrcinfo >.SRCINFO
+makepkg -i
