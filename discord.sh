5d031d8b39a3cd3756f391dd7fb6c3e2666233fa
Kimiblock Moe <kimiblock@icloud.com>
2025-03-03 12:41:05 +0800

    Discord fucks up

--- discord.sh
+++ discord.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 if [ ${XDG_SESSION_TYPE} = wayland ]; then
-	export launchTarget="/opt/discord/Discord --disable-setuid-sandbox --no-sandbox $@ --ozone-platform-hint=auto" --wayland-text-input-version=3
+	export launchTarget="/opt/discord/Discord --disable-setuid-sandbox --no-sandbox $@ --ozone-platform-hint=auto --wayland-text-input-version=3"
 else
 	export launchTarget="/opt/discord/Discord --disable-setuid-sandbox --no-sandbox $@"
 fi
@@ -9,11 +9,11 @@ fi
 export _portalConfig=/usr/lib/portable/info/com.discord.app/config
 
 if [[ "$@" = "--actions opendir" ]]; then
-	portable --actions opendir
+	exec portable --actions opendir
 elif [[ "$@" = "--actions share-files" ]]; then
-	portable --actions share-files
+	exec portable --actions share-files
 elif [[ "$@" = "--actions quit" ]]; then
-	portable --actions quit
+	exec portable --actions quit
 else
-	portable "$@"
+	exec portable "$@"
 fi
