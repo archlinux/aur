37ebf18c6096e6b8eb55d4dde77500204a4a9639
Boris Barbulovski <bbarbulovski@gmail.com>
2025-03-05 04:09:08 +0100

    Dependant on JRE>=17 only. And choose closes one installed on the system

--- PKGBUILD
+++ PKGBUILD
@@ -5,13 +5,13 @@
 _pkgname=jd-gui
 pkgname=${_pkgname}-bin
 pkgver=1.7.1
-pkgrel=7
+pkgrel=8
 pkgdesc='A standalone Java decompiler GUI'
 arch=('x86_64' 'i686' 'pentium4' 'armv7h' 'aarch64')
 url="https://github.com/QuentiumYT/${_pkgname}"
 _urlraw="https://raw.githubusercontent.com/QuentiumYT/${_pkgname}/v${pkgver}"
 license=('GPL-3.0-only')
-depends=('jdk21-openjdk' 'java-runtime' 'java-runtime-common' 'java-environment-common' 'glibc')
+depends=('java-runtime>=17' 'glibc')
 provides=("${_pkgname}")
 conflicts=("${_pkgname}")
 source=(
@@ -27,24 +27,17 @@ sha512sums=('c34b6091cac06962df72aa163c4cb3a15c654262eecd281cb8504d3d02f635e9c6a
             '767c54d4147e5d223ffa453a3094d8f7f8a33832d1c92cd8a189fd8fbaa7dec00767a5ff8e29caf295de65780b0745e062f796c4aa2d8ab368b1fe20e03ff3b2'
             'd224dd23bcb057c65eb1470dc9f83eefa67f41cec0193099902059e86882bf0525bb660d323d2f62743d3cd3e9aec468e5a7686eb2fd33774f89bc519ef7b49c'
             'd300ac330c141b57e5b62f57b5d0462d5618fd67660bfb3bb2c89723686ed76c1e5b34fc670ab9f026e23b6645854a2ee90596a9b496d0b91d46ecf2013f4e17'
-            '6aa22310adff87a4385b4780501a1e691fd24eb0cb182e63817677fce8a45f0d90cf12cc322a00e18bc2b7c20667e4c56a2a4e3323d63cb8fc06cafde5e83ec0'
+            '41e99f1884d7c9e17353b44afea846bb4ab639518a83188fcbafcf4c49924fc8bd8cc6c87de931f537aadde85fcec0ca876c28c9eaa39b6c136e3a3b911f8659'
             'b2328bed9c9191f1ee1292bb453f2f66a9db4990934d816d001ce7cbac3104da93e45aa37ec9e9bd3dac8fa4eb2f5b61e51a05c6126b837ae1634adfa6820af9')
 
 build() {
-  export DFLAGS='-L-zrelro -L-znow'
-
   gcc \
     -O2 \
-    -D ARCH_PKGVER=\"${pkgver}\" \
-    -D ARCH_PKGREL=\"${pkgrel}\" \
+    -DARCH_PKGVER=\"${pkgver}\" \
+    -DARCH_PKGREL=\"${pkgrel}\" \
     -DNDEBUG \
-    -I /usr/lib/jvm/java-21-openjdk/include \
-    -I /usr/lib/jvm/java-21-openjdk/include/linux \
-    -L /usr/lib/jvm/java-21-openjdk/lib \
-    -L /usr/lib/jvm/java-21-openjdk/lib/jli \
-    -Wl,-rpath,/usr/lib/jvm/java-21-openjdk/lib \
-    -Wl,-z,relro,-z,now,-z,shstk \
-    -l jli \
+    -fcf-protection \
+    -Wl,-z,relro,-z,now \
     -o "${srcdir}/jd-gui" \
     "${srcdir}/jd-gui.c"
 }
--- jd-gui.c
+++ jd-gui.c
@@ -1,23 +1,96 @@
+#include <sys/types.h>
+#include <stdbool.h>
 #include <stdlib.h>
-#include <jni.h>
+#include <dirent.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <stdio.h>
 
-int JLI_Launch(int argc, char** argv,
+const int MIN_JVM_VERSION = 17;
+
+int (*JLI_Launch)(int argc, char** argv,
                int jargc, const char** jargv,
                int appclassc, const char** appclassv,
                const char* fullversion,
                const char* dotversion,
                const char* pname,
                const char* lname,
-               jboolean javaargs,
-               jboolean cpwildcard,
-               jboolean javaw,
-               jint ergo);
+               unsigned char javaargs,
+               unsigned char cpwildcard,
+               unsigned char javaw,
+               int ergo);
+
+char *find_openjdk_jli_pathname()
+{
+    const struct dirent* entry = NULL;
+    char pathname[PATH_MAX];
+    int jvm_version = 0;
+    DIR* dir = NULL;
+
+    dir = opendir("/usr/lib/jvm");
+    if (!dir) {
+        return NULL;
+    }
+
+    while((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            int value = 0;
+            sscanf(entry->d_name, "java-%d-openjdk", &value);
+            if ((value >= MIN_JVM_VERSION)&&((value < jvm_version)||(jvm_version == 0))) {
+                jvm_version = value;
+            }
+        }
+    }
+
+    closedir(dir);
+
+    if (jvm_version > 0) {
+        snprintf(pathname, sizeof(pathname), "/usr/lib/jvm/java-%d-openjdk/lib/libjli.so", jvm_version);
+        return strdup(pathname);
+    }
+
+    return NULL;
+}
 
 int main(int argc, char* argv[])
 {
-    int jli_argc = argc + 3;
-    int size = jli_argc * sizeof(void*);
-    char** jli_argv = malloc(size);
+    int ret = 0;
+
+    char *openjdk_jli_so_pathname = NULL;
+    char **jli_argv = NULL;
+    void *libjli_so = NULL;
+    int jli_argc = 0;
+    int size = 0;
+
+    openjdk_jli_so_pathname = find_openjdk_jli_pathname();
+    if (!openjdk_jli_so_pathname) {
+        fprintf(stderr, "No compatible(openjdk>=%d) Java runtime found!\n", MIN_JVM_VERSION);
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+    libjli_so = dlopen(openjdk_jli_so_pathname, RTLD_LAZY);
+    if (!libjli_so) {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+    jli_argc = argc + 3;
+    size = jli_argc * sizeof(void*);
+    jli_argv = malloc(size);
+    if (jli_argv == NULL) {
+        fprintf(stderr, "Failed to allocate memory!\n");
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
+
+    JLI_Launch = dlsym(libjli_so, "JLI_Launch");
+    if (!JLI_Launch) {
+        fprintf(stderr, "Error: %s\n", dlerror());
+        ret = EXIT_FAILURE;
+        goto exit;
+    }
 
     jli_argv[0] = argv[0];
     jli_argv[1] = "-jar";
@@ -27,19 +100,25 @@ int main(int argc, char* argv[])
     for(int c = 1; c < argc; c++)
         jli_argv[c + 2] = argv[c];
 
-    int ret = JLI_Launch(jli_argc, jli_argv,
+    ret = JLI_Launch(jli_argc, jli_argv,
                      0, NULL,
                      0, NULL,
                      ARCH_PKGVER "-" ARCH_PKGREL,
                      ARCH_PKGVER,
                      "jd-gui",
                      "jd-gui",
-                     JNI_FALSE,
-                     JNI_FALSE,
-                     JNI_FALSE,
+                     false,
+                     false,
+                     false,
                      0);
 
-    free(jli_argv);
+exit:
+    if (openjdk_jli_so_pathname)
+        free(openjdk_jli_so_pathname);
+    if (jli_argv)
+        free(jli_argv);
+    if (libjli_so)
+        dlclose(libjli_so);
 
     return ret;
 }
