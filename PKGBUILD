ad11560dd142454637debe3e8698109e18bdd1c1
Oliver Wegner <oliver.wegner@csr-informatik.de>
2025-03-25 10:44:06 +0100

    pushing version to 2.7.1,
    and adjusting start script, as apparently, the latest-linux.yaml file
    is not always up-to-date, so we also need to look at the latest.yaml,
    just to be sure we see the actual latest version.

--- PKGBUILD
+++ PKGBUILD
@@ -2,7 +2,7 @@
 
 pkgname=sipgate-app-clinq
 pkgdesc="Telephony desktop application for sipgate Clinq customers"
-pkgver=2.7.0
+pkgver=2.7.1
 _distfile="sipgate%20CLINQ-${pkgver}-amd64.deb"
 pkgrel=1
 arch=('x86_64')
@@ -18,8 +18,8 @@ source=(
 )
 noextract=("${_distfile}")
 b2sums=(
-	'db8ff13f6c007d2725bcdba7939f7f0ead66d908d4c1c00b72bc0f9dbf75eae9eb7bf6c30df4940f390e9b3687e570306db2df90d5ff54363da525b9d777b459'
-	'fa1fe9461f76a9c24a6fb53c6333d39dd68d5bcec572766565464ab27114e4ad9ff3019624ce042fb9bcccb5f08874ddc4fbccde4b8c1fc5112da785700d5944'
+	'a59d4fc894688f9b4b30d9f6ae37e4f5ad34f8d4c0ec430b1bd96b4877e4f084c11cd4a5a1968628dd27ceb103c9e0b81c82b003f175d4bc9013144b89a6b13e'
+	'3dbd3a9f84f41eac4a5d0b5db42a45db10a6b3655c07a303fe7de3f2c1d5efa4217d33ff7d7e610c7329d475d951f07aa6cfc421a0f090a62f8b0b0b5b8c05be'
 )
 
 pkgver() {
--- run-sipgate-desktop.sh
+++ run-sipgate-desktop.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
 INSTALLED_VERSION=$(pacman -Qi sipgate-app-clinq | sed -r -n 's/^Version\s*:\s*([0-9\.]+).*$/\1/p')
-LATEST_VERSION=$(wget --quiet -O - https://desktop.download.sipgate.com/latest-linux.yml | yq -r '.version')
+LATEST_VERSION=$(wget --quiet -O - https://desktop.download.sipgate.com/latest.yml | yq -r '.version')
+LATEST_VERSION_LINUX=$(wget --quiet -O - https://desktop.download.sipgate.com/latest-linux.yml | yq -r '.version')
 
 echo "installed version: ${INSTALLED_VERSION}"
 echo "latest version: ${LATEST_VERSION}"
+echo "latest linux version: ${LATEST_VERSION_LINUX}"
 echo
 
-if [ "x$LATEST_VERSION" != "x$INSTALLED_VERSION" ]; then
+if [ "x$LATEST_VERSION" != "x$INSTALLED_VERSION" -a "x$LATEST_VERSION_LINUX" != "x$INSTALLED_VERSION" ]; then
   MSG="Please install the latest version of the sipgate-app-clinq package (${LATEST_VERSION}). If there is no AUR package available yet for this version, you may notify the maintainers at https://aur.archlinux.org/packages/sipgate-app-clinq"
   notify-send "$MSG"
   echo "$MSG" >&2
