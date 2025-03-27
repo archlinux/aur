48d355285a8411f6f10b7a25d4da74ff28bfc1ac
Damglador <vse.stopchanskyi@gmail.com>
2025-03-27 21:20:02 +0100

    Auto enabling Wayland with checks, change renderer to GDI and move prefix to ~/.local/share/undertalemodtool

--- undertalemodtool.sh
+++ undertalemodtool.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-WINE_D3D_CONFIG="renderer=vulkan" wine /opt/undertalemodtool/UndertaleModTool.exe "$1"
+# Set Wine prefix to custom location, for better compatability
+export WINEPREFIX="$HOME/.local/share/undertalemodtool"
+
+# Make directory for Wine prefix if doesn't exist
+if [ ! -d "$WINEPREFIX" ]; then
+    mkdir -p "$WINEPREFIX"
+fi
+
+# If session is Wayland and file "$WINEPREFIX/forceX" doesn't exit, force Wayland mode
+if [ echo $XDG_SESSION_TYPE == "wayland" ] && [ ! -f "$WINEPREFIX/forceX" ]; then
+    export DISPLAY= 
+fi
+
+# Launch
+WINE_D3D_CONFIG="renderer=gdi" wine /opt/undertalemodtool/UndertaleModTool.exe "$1"
