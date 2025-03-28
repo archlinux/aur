059699f1677ea247959cd7b390c3bce76049c5de
Phillip Schichtel <phillip@schich.tel>
2025-03-28 01:51:18 +0100

    update 0.98a-RC5

--- PKGBUILD
+++ PKGBUILD
@@ -1,21 +1,24 @@
 # Maintainer: Phillip Schichtel <phillip@schich.tel>
 
 pkgname=starsector
-pkgver=0.97a_RC11
+pkgver=0.98a_RC5
 pkgrel=1
 pkgdesc='Starsector (formerly "Starfarer") is an in-development open-world single-player space-combat, roleplaying, exploration, and economic game.'
 arch=('i686' 'x86_64')
 url="http://fractalsoftworks.com/"
 license=('custom')
-depends=('openal' 'bash' 'desktop-file-utils')
+depends=('openal'
+         'bash'
+         'desktop-file-utils'
+         'java-environment=17')
 replaces=('starfarer')
 source=(https://f005.backblazeb2.com/file/fractalsoftworks/release/starsector_linux-${pkgver//_/-}.zip
         ${pkgname}.pdf::http://www.fractalsoftworks.com/starfarer/docs/StarfarerManual.pdf
         starsector.sh
         starsector.desktop)
-sha256sums=('eed58226c2d9bcc558673c70176ee70c4c54562e1b009aff529fb4afc901ad96'
+sha256sums=('2c90233d7b2d831a54292428f0b8f2ff55d40b5e3a4c0fed9a5d92bfb57ddd43'
             '0d9a2382e1c15e3d471b88eb3770c68f5dd93edc81710e511fd892def9f2ab16'
-            '878bd3b6a84d60a7e264435cad268d6cb36530f92b255d5ce07172f7ef14d4eb'
+            '975a4c39989b58298b7052c51491c677471f83ba56e6ea4e204fbca527e4f557'
             'becc92483d9599672b75d8b00216a64dd8fda38cfcfccd8e735238aa0ce6d8b6')
 package() {
   install -d "$pkgdir/usr/share/$pkgname" \
@@ -31,12 +34,9 @@ package() {
 
   cd "$srcdir/$pkgname"
 
-  cd jre_linux
-  find -type f -exec install -D "{}" "$pkgdir/usr/lib/$pkgname/jre/{}" \;
-  cd ..
-
   install -Dm644 *.jar "$pkgdir/usr/share/java/$pkgname"
   install -Dm644 LICENSE.txt "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
+  install -Dm644 compiler_directives.txt "$pkgdir/usr/share/$pkgname/"
 
   if [[ "$CARCH" == x86_64 ]]; then
     install -Dm644 native/linux/*64.so "$pkgdir/usr/lib/$pkgname/"
--- starsector.sh
+++ starsector.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-# fix for users of special IM modules
-unset XMODIFIERS GTK_IM_MODULE QT_IM_MODULE
-
 state_dir="${XDG_DATA_HOME:-"$HOME/.local/share"}/starsector"
 saves_path="${state_dir}/saves"
 mods_path="${state_dir}/mods"
@@ -21,27 +18,115 @@ classpath="$(find '/usr/share/java/starsector' -type f -name '*.jar' | paste -sd
 config_path="${XDG_CONFIG_HOME:-"$HOME/.config"}/starsector"
 startup_config="${config_path}/startup.sh"
 
-jvm_args=(-XX:+UseG1GC -Xms1536m -Xmx1536m -Xss2048k -XX:PermSize=192m -XX:MaxPermSize=192m)
+jvm_args=(-Xms2048m -Xmx2048m -Xss4m)
 program_args=()
 
 if [ -r "$startup_config" ]
 then
-    . "$startup_config"
+  . "$startup_config"
 fi
 
-export JAVA_HOME='/usr/lib/starsector/jre'
 cd /usr/share/starsector
-exec "$JAVA_HOME/bin/java" -server \
-  -XX:CompilerThreadPriority=1 \
-  -XX:+CompilerThreadHintNoPreempt \
+exec java \
+  -Dfile.encoding=UTF-8 \
+  -noverify \
+  -XX:+UnlockDiagnosticVMOptions \
+  -XX:+UnlockExperimentalVMOptions \
+  -XX:+ShowCodeDetailsInExceptionMessages \
+  -XX:+ExtensiveErrorReports \
+  -XX:+ShowMessageBoxOnError \
+  -XX:+PrintCommandLineFlags \
+  -XX:+TieredCompilation \
+  -XX:-UseCondCardMark \
+  -Xlog:async \
+  -XX:+UseFastStosb \
+  -XX:+AlwaysCompileLoopMethods \
+  -XX:TieredStopAtLevel=4 \
+  -XX:TrimNativeHeapInterval=60000 \
+  -XX:+UseCriticalCompilerThreadPriority \
+  -XX:+UseCriticalJavaThreadPriority \
+  -XX:UseAVX=3 \
+  -XX:AVX3Threshold=0 \
+  -XX:UseSSE=4 \
+  -XX:+UseSSE42Intrinsics \
+  -XX:+UseCompressedOops \
+  -XX:+UseCompressedClassPointers \
+  -XX:+UseFMA \
+  -XX:+UseBMI1Instructions \
+  -XX:+UseBMI2Instructions \
+  -XX:+UseCLMUL \
+  -XX:+UseFastUnorderedTimeStamps \
+  -XX:+UseUnalignedLoadStores \
+  -XX:+UseXMMForArrayCopy \
+  -XX:+UseXMMForObjInit \
+  -XX:+UseXmmI2D \
+  -XX:+UseXmmI2F \
+  -XX:+DisableExplicitGC \
+  -XX:+AlwaysPreTouch \
+  -XX:+ParallelRefProcEnabled \
+  -XX:+UseShenandoahGC \
+  -XX:ShenandoahGCMode=iu \
+  -XX:ShenandoahGCHeuristics=compact \
+  -XX:ShenandoahAllocationThreshold=85 \
+  -XX:ShenandoahGuaranteedGCInterval=0 \
+  -XX:+ZeroTLAB \
+  -XX:+SegmentedCodeCache \
+  -XX:+AlwaysAtomicAccesses \
+  -XX:+UseStringDeduplication \
+  -XX:-DontCompileHugeMethods \
+  -XX:Tier0Delay=1 \
+  -XX:+UseFPUForSpilling \
+  -XX:+EnableVectorAggressiveReboxing \
+  -XX:+EnableVectorReboxing \
+  -XX:+EnableVectorSupport \
+  -XX:-AlignVector \
+  -XX:TieredOldPercentage=10000 \
+  -XX:Tier0ProfilingStartPercentage=2000 \
+  -XX:IncreaseFirstTierCompileThresholdAt=99 \
+  -XX:InterpreterProfilePercentage=99 \
+  -XX:+UseVectorCmov \
+  -XX:+UseVectorStubs \
+  -XX:ReferencesPerThread=0 \
+  -XX:MaxGCPauseMillis=10 \
+  -XX:-EnableThreadSMRStatistics \
+  -XX:+UseUnalignedAccesses \
+  -XX:PerMethodRecompilationCutoff=100000 \
+  -XX:ProfileMaturityPercentage=100 \
+  -XX:NmethodSweepActivity=1 \
+  -XX:ThreadPriorityPolicy=1 \
+  -XX:ReservedCodeCacheSize=256m \
+  -XX:+PrintCodeCache \
+  -XX:-UseDynamicNumberOfCompilerThreads \
+  -XX:CompilerDirectivesFile=./compiler_directives.txt \
   -Djava.library.path=/usr/lib/starsector \
-  -classpath "${classpath}" \
+  -XX:-BytecodeVerificationLocal \
+  -XX:-BytecodeVerificationRemote \
+  -Djava.util.Arrays.useLegacyMergeSort=true \
+  --enable-preview \
+  --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+  --add-opens=java.base/java.nio=ALL-UNNAMED \
+  --add-opens=java.base/java.nio.Buffer.UNSAFE=ALL-UNNAMED \
+  --add-opens=java.base/java.util=ALL-UNNAMED \
+  --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+  --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED \
+  --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.ref=ALL-UNNAMED \
+  --add-opens=java.base/java.text=ALL-UNNAMED \
+  --add-opens=java.desktop/java.awt.font=ALL-UNNAMED \
+  --add-opens=java.desktop/java.awt.Rectangle=ALL-UNNAMED \
+  --add-opens=java.desktop/java.awt=ALL-UNNAMED \
+  --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED \
+  --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED \
+  --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
   -Dcom.fs.starfarer.settings.paths.saves="${saves_path}" \
   -Dcom.fs.starfarer.settings.paths.screenshots="${screenshots_path}" \
   -Dcom.fs.starfarer.settings.paths.mods="${mods_path}" \
   -Dcom.fs.starfarer.settings.paths.logs="${log_path}" \
   -Dcom.fs.starfarer.settings.linux=true \
+  -classpath "${classpath}" \
   "${jvm_args[@]}" \
   com.fs.starfarer.StarfarerLauncher \
-  "${program_args[@]}"
+  "${program_args[@]}" \
+  "$@"
 
