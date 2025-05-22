d2ebb16af0bb6bd0d64b5c00ab33b1ebe6231dac
Blaadick <lablaadick@gmail.com>
2025-05-23 01:04:48 +0300

    Fix package updating

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 lastVer=$(curl -s "https://api.github.com/repos/Blaadick/BlaadPapers/releases/latest" | jq -r '.tag_name' | sed 's/^v//')
 description=$(curl -s "https://api.github.com/repos/Blaadick/BlaadPapers" | jq -r '.description')
 
-curl -s -L "https://api.github.com/repos/Blaadick/BlaadPapers/tarball/v$lastVer" -o "blaadpapers-$lastVer.tar.gz"
+curl -s -L "https://github.com/Blaadick/BlaadPapers/archive/refs/tags/v$lastVer.tar.gz" -o "blaadpapers-$lastVer.tar.gz"
 sha256sum=$(sha256sum "blaadpapers-$lastVer.tar.gz" | cut -d ' ' -f1)
 rm "blaadpapers-$lastVer.tar.gz"
 
