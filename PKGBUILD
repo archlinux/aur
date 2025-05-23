8c9ae018d173f2ab10b4272c596809818dd4cdb7
brian <173brian@gmail.com>
2025-05-23 15:52:54 -0500

    Initial commit

---
+++ PKGBUILD
@@ -0,0 +1,26 @@
+# Contributor: 173br <173brian at gmail dot com>
+pkgname=android-aarch64-meson
+pkgver=1
+pkgrel=1
+arch=('any')
+pkgdesc="Meson wrapper for arm64 (aarch64)"
+depends=('meson' 'aarch64-linux-gnu-gcc' 'android-pkg-config')
+conflicts=('aarch64-linux-gnu-meson')
+license=("GPL")
+url=""
+source=("toolchain_generator.py"
+        "android-aarch64-meson")
+sha256sums=('91fbae127a8dfc1be2054944fba5d6e942bedba9eecfd116e5525261cb7fe1ec'
+            'bdcd08416190863e6f21f35b629c7f5e766e510085695e714487b7d31a5df143')
+
+build() {
+  python toolchain_generator.py --output-file toolchain-android-aarch64.meson 
+}
+
+package() {
+  install -d "${pkgdir}"/usr/bin
+  install -d "${pkgdir}"/usr/share/aarch64
+  install -m 755 "${srcdir}/toolchain_generator.py" "$pkgdir/usr/bin/meson-cross-android-aarch64-file-generator"
+  install -m 755 "${srcdir}/android-aarch64-meson" "$pkgdir/usr/bin/android-aarch64-linux-gnu-meson"
+  install -m 644 toolchain-android-aarch64.meson "${pkgdir}"/usr/share/aarch64/
+}
---
+++ toolchain_generator.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import argparse
+import configparser
+
+
+class CrossFileGenerator:
+    def __init__(self, output_file):
+        self.arch = "aarch64-linux-gnu"
+        self.output_file = output_file
+        self.cflags = "-D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -O2 -pipe -fexceptions --param=ssp-buffer-size=4 -Wformat -Werror=format-security"
+        self.cxxflags = "-D_FORTIFY_SOURCE=3 -D_GLIBCXX_ASSERTIONS -O2 -pipe -fexceptions --param=ssp-buffer-size=4 -Wformat -Werror=format-security"
+        self.ldflags = "-Wl,-O1,--sort-common,--as-needed -fstack-protector"
+
+        self.cpu_family = "aarch64"
+        self.processor = "aarch64"
+
+    def generate(self):
+        config = configparser.ConfigParser()
+        config["binaries"] = self.get_binaries_section()
+        config["properties"] = self.get_properties_section()
+        config["built-in options"] = self.get_builtin_options_section()
+        config["host_machine"] = self.get_host_machine_section()
+        with open(self.output_file, "w") as configfile:
+            config.write(configfile)
+
+    def get_binaries_section(self):
+        binaries = {
+            "c": "'{}-gcc'".format(self.arch),
+            "cpp": "'{}-g++'".format(self.arch),
+            "fortran": "'{}-gfortran'".format(self.arch),
+            "ar": "'{}-gcc-ar'".format(self.arch),
+            "pkgconfig": "'android-aarch64-pkg-config'",
+            "ranlib": "'{}-gcc-ranlib'".format(self.arch),
+            "strip": "'{}-strip'".format(self.arch),
+            "objcopy": "'{}-objcopy'".format(self.arch),
+            "objdump": "'{}-objdump'".format(self.arch),
+            "readelf": "'{}-readelf'".format(self.arch),
+            "ld": "'{}-ld'".format(self.arch),
+            "cmake": "'{}-cmake'".format(self.arch),
+        }
+        return binaries
+
+    def get_properties_section(self):
+        return {
+            "root": "'{}'".format(self.arch),
+            "sys_root": "'/usr/{}'".format(self.arch),
+        }
+
+    def get_builtin_options_section(self):
+        return {
+            "c_args": [f for f in self.cflags.split(" ") if f],
+            "cpp_args": [f for f in self.cxxflags.split(" ") if f],
+            "c_link_args": [f for f in self.ldflags.split(" ") if f],
+            "cpp_link_args": [f for f in self.ldflags.split(" ") if f],
+        }
+
+    def get_host_machine_section(self):
+        return {
+            "system": "'linux'",
+            "cpu_family": "'{}'".format(self.cpu_family),
+            "cpu": "'{}'".format(self.processor),
+            "endian": "'little'",
+        }
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate a generic Meson cross file for arm64 with android's pkgconfig.",
+        add_help=False,
+    )
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+    required.add_argument(
+        "--output-file",
+        type=str,
+        required=True,
+        help="Write the generated cross file to this path",
+    )
+    optional.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="show this help message and exit",
+    )
+    args = parser.parse_args()
+    generator = CrossFileGenerator(args.output_file)    
+    generator.generate()
