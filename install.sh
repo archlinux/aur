c4363dc6db7eafcbe484e976748a59006ac40dda
Ayaan Hassan <Ayaanhassan511@gmail.com>
2025-07-10 15:56:07 -0400

    fix all errors

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ for rcfile in ~/.bashrc ~/.zshrc; do
   if [ -f "$rcfile" ]; then
     if ! grep -q 'ayaanfetch' "$rcfile"; then
       echo '# >>> AYAAN UNSTABLE ZONE >>>' >> "$rcfile"
-      echo 'alias ayaanfetch="wine /usr/bin/unstable/ayaanfetch/AyaanFetch.exe"' >> "$rcfile"
+      alias ayaanfetch="wine /usr/bin/unstable/ayaanfetch/AyaanFetch.exe 2>/dev/null"
       echo 'export EPATH=/usr/bin/unstable/ayaanfetch' >> "$rcfile"
       echo 'echo \"AYAANFETCH by AYAAN511 (github.com/ayaan511) – WELCOME TO THE UNSTABLE ZONE!\"' >> "$rcfile"
       echo '# <<< AYAAN UNSTABLE ZONE <<<' >> "$rcfile"
