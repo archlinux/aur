d757ef344804368aef23c632eff82ae4a6ee653c
gri573 <david.dg.grimm@gmail.com>
2025-07-14 19:35:20 +0200

    complain when update versioning is inconsistent

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ "$(ls ov-client)" == "" ]
+if ! [ -d ov-client ]
 then
 	git clone "https://github.com/gisogrimm/ov-client.git"
 fi
@@ -7,11 +7,21 @@ cd ov-client
 git checkout master
 git pull
 make gitupdate
-git checkout $(git describe --tags --candidates 1)
+TAG=$(git describe --tags --candidates 1)
+git checkout $TAG
 
 VER=$(make -s ver | sed 's/\([^-]*\)-.*/\1/')
+TAGVER=$(echo $TAG | sed 's/^v//;s/-.*$//')
+
+if [ "$TAGVER" != "$VER" ]
+then
+	echo "Version mismatch! ($TAGVER vs $VER)"
+	printf "please enter correct version: "
+	read VER
+fi
+
 COMMIT=$(git show | grep -e "^commit" | sed 's/commit //' | sed 's/ .*$//')
-echo "$COMMIT"
+echo "Moved to version $VER (tag $TAG) on commit $COMMIT"
 cd ..
 cat PKGBUILD | sed -e "s/pkgver=.*/pkgver=\"$VER\"/" | sed "s/git checkout -q .*/git checkout -q $COMMIT/"> PKGBUILD2
 mv PKGBUILD2 PKGBUILD
