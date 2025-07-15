101b4ba554b55aba11e4a1ced3eaf9f332d865ad
rainbownx <nohramkissoon1@gmail.com>
2025-07-15 16:30:06 -0400

    Initial commit of yak AUR helper

---
+++ PKGBUILD
@@ -0,0 +1,16 @@
+# Maintainer: Noah Ramkissoon nohramkissoon1@gmail.com
+pkgname=yak
+pkgver=1.0.0
+pkgrel=1
+pkgdesc="A simple AUR helper written in Bash using Pacman-like flags"
+arch=('any')
+url="https://github.com/rainbownx/yak"
+license=('MIT')
+depends=('bash' 'git' 'pacman' 'jq' 'curl')
+source=("yak" "install.sh")
+md5sums=('SKIP' 'SKIP')
+
+package() {
+    install -Dm755 "$srcdir/yak" "$pkgdir/usr/bin/yak"
+    install -Dm755 "$srcdir/install.sh" "$pkgdir/usr/share/yak/install.sh"
+}
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Welcome to the yak installer"
+echo "Step 1: Update system package lists"
+sudo pacman -Syu --noconfirm
+
+echo "Step 2: Install build tools (git + base-devel)"
+sudo pacman -S --needed --noconfirm git base-devel
+
+echo "Step 3: Clone the yak repository"
+git clone https://github.com/rainbownx/yak.git
+cd yak
+
+echo "Step 4: Build and install yak with makepkg"
+makepkg -si --noconfirm
+
+echo "Installation finished!"
+echo "You can now use yak by typing: yak"
