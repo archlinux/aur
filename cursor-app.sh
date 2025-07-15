f6dffd26aee2074cf4f1e6ffd92ec38738dd3417
mazix <mazix@bk.ru>
2025-07-15 12:05:34 +0000

    Update to version 1.2.4

--- cursor-app.sh
+++ cursor-app.sh
@@ -8,7 +8,13 @@ XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 CURSOR_FLAGS_FILE="$XDG_CONFIG_HOME/cursor-flags.conf"
 
 # Default flags
-DEFAULT_FLAGS="--no-sandbox"
+DEFAULT_FLAGS=""
+
+# Создать файл с дефолтным содержимым, если он отсутствует
+if [[ ! -f "$CURSOR_FLAGS_FILE" ]]; then
+    mkdir -p "$(dirname "$CURSOR_FLAGS_FILE")"
+    echo "# Пользовательские флаги для Cursor" > "$CURSOR_FLAGS_FILE"
+fi
 
 # Read user flags if file exists
 USER_FLAGS=""
@@ -21,3 +27,6 @@ ALL_FLAGS="$DEFAULT_FLAGS $USER_FLAGS"
 
 # Execute Cursor AppImage with flags and passed arguments
 exec /opt/cursor-app/cursor-app.AppImage $ALL_FLAGS "$@"
+
+# Launch with AppImageLauncher disabled only for this process (author panikinator https://github.com/Gunther-Schulz/aur-cursor-bin-updater/pull/15/commits/1d44207003a7c1ebf16719135c8d03856e109e5f)
+APPIMAGELAUNCHER_DISABLE=TRUE exec /opt/cursor-app/cursor-app.AppImage"$@" $CURSOR_USER_FLAGS
