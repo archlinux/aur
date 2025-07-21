e9ea567dadec342a7c405aceea293c18ce8fb727
Szerwigi1410 <szerwigi14@gmail.com>
2025-07-21 22:39:10 +0200

    Initial AUR release

---
+++ PKGBUILD
@@ -0,0 +1,16 @@
+# Maintainer: Oliwier <your_email@domain.com>
+pkgname=brokefetch
+pkgver=1.0
+pkgrel=1
+pkgdesc="A sad, funny alternative to neofetch"
+arch=('any')
+url="https://github.com/Szerwigi1410/brokefetch"
+license=('MIT')
+depends=('bash')
+source=("brokefetch.sh::https://raw.githubusercontent.com/Szerwigi1410/brokefetch/main/brokefetch.sh")
+md5sums=('SKIP')
+
+package() {
+  install -Dm755 "$srcdir/brokefetch.sh" "$pkgdir/usr/bin/brokefetch"
+}
+
---
+++ brokefetch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cat << "EOF"
+                -`                    oliwier7@brokelaptop 
+               .o+`                   --------------------- 
+              `ooo/                   OS: Arch Linux (Unpaid Edition)
+             `+oooo:                  Host: Bedroom Floor
+            `+oooooo:                 Kernel: 0.00/hr
+            -+oooooo+:                Uptime: 14h (sleep not included)
+          `/:-:++oooo+:               Packages: 1337 (none legal)
+         `/++++/+++++++:              Shell: brokeBash 0.01
+        `/++++++++++++++:             Resolution: CRT 640x480
+       `/+++ooooooooooooo/`           DE: Crying
+      ./ooosssso++osssssso+`          WM: HopiumWM
+     .oossssso-````/ossssss+`         Terminal: Terminal of Regret
+    -osssssso.      :ssssssso.        CPU: Imaginary (thinking hard...)
+   :osssssss/        osssso+++.       GPU: Integrated Depression
+  /ossssssss/        +ssssooo/-       Memory: 512MB (half sadness)
+ `/ossssso+/:-        -:/+osssso+-    
+`+sso+:-`                 `.-/+oso:    
+`++:.                           `-/+/
+`.`                                `
+
+BROKEFETCH 🥀
+EOF
