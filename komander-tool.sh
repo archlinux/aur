82e8a70edd9231ba9ab47fc610e1b3e8ef1ce12d
stikkot <@email.com>
2025-08-03 12:26:05 +0000

    update to komander-tool.sh

---
+++ komander-tool.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+path=$(dirname $(realpath "$0"))
+param="$1"
+
+case $param in
+	setup)
+		read -p "Install zsh-komander theme? (y/n) " cfm
+		if [ $cfm == "y" ]; then
+			:
+		else
+			exit 0
+		fi
+		echo "Autoinstall zsh-komander theme..."
+		chsh -s /bin/zsh
+		sudo mv "${path}/zsh-komander.zsh" "$HOME/.zshrc"
+		echo "End installing..."
+		;;
+	help)
+		echo -e "\n\e[32mHelp menu zsh-komander...\n-------------------------\e[0m\n- Alt+Z\t\t- Show user name.\n- Alt+X\t\t- Show computer name.\n- Alt+C\t\t- Show pwd.\n"
+		echo -e "\e[32mHelp to komander-tool...\n-------------------------\e[0m\n- setup\t\t- Autosetup script zsh-komander.\n- help\t\t- Show this is menu.\n- version\t- Show to zsh-komander version.\n"	
+		;;
+	version)
+		echo "zsh-komander 1.3"
+		;;
+	*)
+		echo "Not found command: $param..."
+		;;
+esac
