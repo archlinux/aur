93c8934bf8fd92c5483ad40e406cd3547fe59df2
oech3 <>
2025-08-03 18:06:09 +0900

    cleanup so fetcher

--- fetch-soname-by-chromium.sh
+++ fetch-soname-by-chromium.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-url='https://chromium.googlesource.com/chromium/third_party/ffmpeg'
 _commit=$(curl -sL https://raw.githubusercontent.com/chromium/chromium/refs/tags/${1}/DEPS | grep -oP "'ffmpeg_revision': '\K[0-9a-f]{40}'" | tr -d \')
 for _h in libav{codec,format}/version_major.h libavutil/version.h; do
  curl -sL https://chromium.googlesource.com/chromium/third_party/ffmpeg/+/${_commit}/${_h}?format=TEXT|base64 -d|grep -E 'VERSION_MAJOR +[0-9]'
