baf8f17dbfd58d993b47e4fd31f5770612e89701
Tristan Kuhn <tristanjkuhn007@gmail.com>
2025-08-06 16:37:24 +0200

    Fixed HyprHideDev. added cmd line args, added version.txt. other minor changes

--- HyprHideGui.py
+++ HyprHideGui.py
@@ -8,6 +8,7 @@ import sys
 import signal
 import configparser
 import commentjson
+import argparse
 from PyQt6.QtGui import QFont, QPixmap, QIcon, QCursor
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QVBoxLayout, QLabel,
@@ -18,18 +19,16 @@ from PyQt6.QtCore import (
 )
 from PyQt6.QtWidgets import QGraphicsOpacityEffect
 from PyQt6.QtWidgets import QLineEdit, QGridLayout
-VERSION = "1.1.1"
+VERSION = "1.9.8"
 
 config = configparser.ConfigParser()
 user_config_path = os.path.expanduser("~/.config/hyprhide/config.cfg")
-default_config_path = "/usr/share/hyprhide/config.cfg"
+# default_config_path = "/usr/share/hyprhide/config.cfg"
 
 if os.path.exists(user_config_path):
     config.read(user_config_path)
-elif os.path.exists(default_config_path):
-    config.read(default_config_path)
-elif os.path.exists("config.cfg"):
-    config.read("config.cfg")
+# elif os.path.exists(default_config_path):
+#     config.read(default_config_path)
 else:
     print("Please create a config.cfg file, or install this properly ")
 
@@ -582,6 +581,30 @@ class HyprHideAppInitWindow(QWidget):
 
         main_layout.addWidget(integration_group)
 
+        # --- Developer Settings (collapsed by default) ---
+        self.dev_group = QGroupBox("Developer Settings")
+        self.dev_group.setCheckable(True)
+        self.dev_group.setChecked(False)
+        self.dev_group.setStyleSheet("QGroupBox { font-weight: bold; }")
+
+        dev_layout = QVBoxLayout()
+        self.dev_group.setLayout(dev_layout)
+
+        # Developer mode checkbox
+        self.cb_developer_mode = QCheckBox("Enable Developer Mode")
+        self.cb_developer_mode.setToolTip("Runs local development Python script instead of installed binary")
+        self.cb_developer_mode.stateChanged.connect(self.toggle_dev_path_option)
+        dev_layout.addWidget(self.cb_developer_mode)
+
+        # Developer script path input
+        self.dev_path_input = QLineEdit()
+        self.dev_path_input.setPlaceholderText("Path to local Python script (e.g. ./dev/main.py)")
+        self.dev_path_input.setToolTip("Dev runner will expand local paths")
+        self.dev_path_input.setEnabled(False)  # Initially disabled
+        dev_layout.addWidget(self.dev_path_input)
+
+        main_layout.addWidget(self.dev_group)
+        
         # Spacer to push buttons to bottom
         main_layout.addStretch()
 
@@ -600,6 +623,8 @@ class HyprHideAppInitWindow(QWidget):
         info_label = QLabel(info_text)
         info_label.setStyleSheet("font-weight: normal;")
         # main_layout.addWidget(info_label)
+    def toggle_dev_path_option(self,state):
+        self.dev_path_input.setEnabled(state == 2)
     def toggle_keybind_input(self, state):
         print("HELLI",state)
         self.keybind_input.setEnabled(state == 2)
@@ -686,20 +711,42 @@ class HyprHideAppInitWindow(QWidget):
         }
         config["WAYBAR"] = {"ENABLED": str(self.cb_waybar.isChecked())}
 
-        with open(user_config_path, "w") as cfgfile:
-            config.write(cfgfile)
+        
         if(self.cb_waybar.isChecked() == True):
             print("Installing")
             self.install_into_waybar()
         if(self.cb_hyprland.isChecked() ==  True):
             self.install_into_hyprland()
+        if self.cb_developer_mode.isChecked() == True:
+            if not config.has_section("DEV"):
+                config.add_section("DEV")
+            print("Setting this")
+            config.set("DEV","devmode",'True')
+            deb_path = self.dev_path_input.text()
+            config.set("DEV","hyprhide_src",deb_path)
+            print(config.get("DEV","devmode",fallback="Know it"))
+        with open(user_config_path, "w") as cfgfile:
+            config.write(cfgfile)
         self.close()
         self.main_app = HyprHideApp()
         self.main_app.position_near_mouse()
         self.main_app.show()
 
 if __name__ == "__main__":
-    first_run = config.get("INIT","first",fallback=True)
+   
+    parser = argparse.ArgumentParser(description="HyprHide Application")
+    parser.add_argument("--reset", action="store_true", help="Reset initial setup")
+    parser.add_argument("--launched", action="store_true", help="Used internally")
+    parser.add_argument("--set-version", type=str, help="Specify the version")
+    
+    args = parser.parse_args()
+    VERSION = args.set_version
+    if(args.launched != True):
+        exit()
+    if args.reset:
+         
+         config.set("INIT", "first", True)
+    first_run = config.getboolean("INIT","first",fallback=True)
     if(first_run == True):
         app = QApplication(sys.argv)
         signal.signal(signal.SIGINT, signal.SIG_DFL)
--- PKGBUILD
+++ PKGBUILD
@@ -1,5 +1,5 @@
 pkgname=hyprhide
-pkgver=1.8.4
+pkgver=$(< version.txt)
 pkgrel=1
 pkgdesc="A tool for hiding and showing windows in Hyprland using a PyQt6 GUI"
 arch=('any')
@@ -7,14 +7,18 @@ url="https://github.com/KingVentrix007/hyprlandhide"
 license=('MIT') 
 depends=('python' 'python-pyqt6' 'hyprland' 'python-commentjson')
 makedepends=()
-source=('min.sh' 'HyprHideGui.py' 'config.cfg' 'min.py')
-sha256sums=('SKIP' 'SKIP' 'SKIP' 'SKIP')  # You can replace SKIP with actual checksums later
+source=('min.sh' 'HyprHideGui.py' 'config.cfg' 'min.py' 'HyprHideDev.py' 'version.txt')
+sha256sums=('SKIP' 'SKIP' 'SKIP' 'SKIP' 'SKIP' 'SKIP')  # You can replace SKIP with actual checksums later
 
 package() {
     install -Dm755 "$srcdir/min.sh" "$pkgdir/usr/bin/hyprhide-min"
-    install -Dm755 "$srcdir/HyprHideGui.py" "$pkgdir/usr/bin/hyprhide-gui"
+    install -Dm755 "$srcdir/HyprHideDev.py" "$pkgdir/usr/bin/hyprhide-gui"
+    install -Dm755 "$srcdir/HyprHideGui.py" "$pkgdir/usr/bin/hyprhide-gui-main"
+
     # install -Dm644 "$srcdir/hyprhide.desktop" "$pkgdir/usr/share/applications/hyprhide.desktop"
-    install -Dm644 "$srcdir/config.cfg" "$pkgdir/usr/share/hyprhide/config.cfg"
+    # install -Dm644 "$srcdir/config.cfg" "$pkgdir/usr/share/hyprhide/config.cfg"
     install -Dm644 "$srcdir/min.py" "$pkgdir/usr/share/hyprhide/min.py"
+    install -Dm644 "$srcdir/version.txt" "$pkgdir/usr/share/hyprhide/version.txt"
+
 
 }
