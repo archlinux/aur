dc5522571dfeb3183640a248ad44e606537f994a
Damglador <vse.stopchanskyi@gmail.com>
2025-08-08 05:42:34 +0200

    Update product.json list

--- vscodium-xdg-dir-patch.sh
+++ vscodium-xdg-dir-patch.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 list=(
-    /usr/share/vscodium/resources/app/product.json # vscodium
-    /usr/lib/vscodium-electron/product.json # vscodium-electron-bin
-    /usr/lib/vscodium/product.json # vscodium-electron
-    /usr/share/vscodium-git/resources/app/product.json # vscodium-git
+    /usr/share/vscodium/resources/app/product.json      # vscodium
+    /opt/vscodium-bin/resources/app/product.json        # vscodium-bin
+    /usr/share/vscodium-git/resources/app/product.json  # vscodium-git
+    /usr/lib/vscodium/product.json                      # vscodium-electron
+    /usr/lib/vscodium-electron/product.json             # vscodium-electron-bin
 )
 
 for f in ${list[@]}; do
