4351719a566a4c65984a080175ab4c6333312e8d
WitheringAway <124115470+notwithering@users.noreply.github.com>
2025-08-20 20:40:08 -0400

    stuff
    very good commit message

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: Jim Philip <jimphilip394 at gmail dot com>
 pkgname=vencord-hook
 pkgver=1
-pkgrel=11
+pkgrel=12
 pkgdesc="Pacman hook to automatically install Vencord after Discord updates"
 arch=('any')
 url="https://github.com/Vencord/Installer.git"
@@ -12,14 +12,14 @@ source=("vencord-hook.hook"
 		"LICENSE-VENCORD"
 		"LICENSE-VENCORD-HOOK")
 sha256sums=('3b43547e2b3956403b64d16730907552564d442987a10c0d0e12b359e1484b0d'
-			'b81de32212e58c6245743a24d2b3e28d6b10e3f11a01030b8ec752a0a204f828'
+			'4a2f556832438dfa5b1ddf54f863db1b35637dde8b2ee1017a06e65483a71476'
 			'SKIP'
 			'SKIP')
 install="vencord-hook.install"
 
 package() {
-	install -Dm644 "$srcdir/vencord-hook.hook" "${pkgdir}/etc/pacman.d/hooks/vencord-hook.hook"
-	install -Dm755 "$srcdir/vencord-hook.sh" "${pkgdir}/etc/pacman.d/hookbin/vencord-hook.sh"
+	install -Dm644 "$srcdir/vencord-hook.hook" "$pkgdir/etc/pacman.d/hooks/vencord-hook.hook"
+	install -Dm755 "$srcdir/vencord-hook.sh" "$pkgdir/etc/pacman.d/hookbin/vencord-hook.sh"
 
 	install -Dm644 "$srcdir/LICENSE-VENCORD-HOOK" "$pkgdir/usr/share/licenses/$pkgname/LICENSE-VENCORD-HOOK"
 	install -Dm644 "$srcdir/LICENSE-VENCORD" "$pkgdir/usr/share/licenses/$pkgname/LICENSE-VENCORD"
--- vencord-hook.sh
+++ vencord-hook.sh
@@ -2,31 +2,42 @@
 # Copyright (c) 2024-2025 Jim Philip, with Reserved Package Name "vencord-hook"
 # Copyright (c) 2022-2025 Vendicated and Vencord contributors, with Reserved Project Name "Vencord"
 
-set -e
+set -euo pipefail
+
+echo_hook() {
+	# echo -ne "\e[35mHOOK  \e[39m"
+	echo -n "HOOK  "
+	echo $@
+}
 
 err_handler() {
-	echo "an error occurred, please contact the package maintainer: https://aur.archlinux.org/packages/vencord-hook"
+	echo_hook "An error occurred. If unresolvable, contact the package maintainer: https://aur.archlinux.org/packages/vencord-hook"
 }
 trap err_handler ERR
 
-installer=$(mktemp /tmp/vencord.XXXXXX)
-trap 'rm -f "$installer"' EXIT
+installer=$(mktemp /tmp/vencord-hook.XXXXXX)
+cleanup() {
+	rm -f "$installer"
+}
+trap cleanup EXIT
 
-curl -sSL https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
-	--output "$installer"
+curl -fsSLo "$installer" \
+	https://github.com/Vendicated/VencordInstaller/releases/latest/download/VencordInstallerCli-Linux \
+|| {
+	err_handler
+	exit 1
+}
 
 chmod +x "$installer"
 
 while IFS= read -r package || [ -n "$package" ]; do
-	branch=$(echo "$package" | sed 's/^discord-*//')
+	branch=${package#discord}
+	branch=${branch#-}
+	branch=${branch:-stable}
 
-	if [ -z "$branch" ]; then
-		branch="stable"
-	fi
-
-	echo "Installing Vencord for $branch branch..."
+	echo_hook "Installing Vencord for $branch branch..."
 	"$installer" -install -branch "$branch" || {
-		echo "Falling back to auto branch..."
-		"$installer" -install -branch auto
+		err_handler
+		exit 1
 	}
 done
