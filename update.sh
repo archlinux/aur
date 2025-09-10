a13e93baa861273707a5986554154d3e66b923db
Yu Shijun <yushijun@linakesi.com>
2025-09-10 10:58:44 +0800

    add update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+printsrcinfo() {
+  if command -v /usr/local/bin/printsrcinfo > /dev/null; then
+    /usr/local/bin/printsrcinfo > .SRCINFO
+  else
+    makepkg --printsrcinfo > .SRCINFO
+  fi
+}
+
+
 pkgver=$(awk -F= '/^pkgver=/{print $2}' PKGBUILD)
 repo=$(awk -F'/' '/^url=/{print $4"/"$5}' PKGBUILD)
 latest=$(curl -s https://api.github.com/repos/$repo/releases/latest | awk -F\" '/tag_name/{print $4}')
@@ -13,13 +22,11 @@ fi
 
 sed -i -e 's/pkgrel=.*/pkgrel=1/' PKGBUILD
 
-updpkgsums
-
-if command -v /usr/local/bin/printsrcinfo > /dev/null; then
-  /usr/local/bin/printsrcinfo > .SRCINFO
-else
-  makepkg --printsrcinfo > .SRCINFO
-fi
+printsrcinfo
+source=$(awk -F' = ' '/source/{print $2}' .SRCINFO)
+hash=$(curl -sL $source | md5sum | awk '{print $1}')
+sed -i "s/^md5sums=.*/md5sums=('$hash')/" PKGBUILD
+printsrcinfo
 
 git add PKGBUILD .SRCINFO
 git commit -m "update ${latest}"
