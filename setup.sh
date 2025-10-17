685d53ee5ea32ec824d4d5a9c97892c929c3a1dc
Nate <secondarynate@icloud.com>
2025-10-17 11:49:28 -0400

    Version 1.2.4

--- setup.sh
+++ setup.sh
@@ -5,12 +5,11 @@ BIN_NAME="auditorium-v${VERSION}-linux-amd64"
 CHECKSUM=$(curl -Ls "https://github.com/nate-craft/auditorium/releases/download/${VERSION}/${BIN_NAME}" | sha256sum | cut -d ' ' -f1)
 
 sed -i "s/^pkgver=.*/pkgver=${VERSION}/" PKGBUILD
-sed -i "s/^sha256sums=.*/sha256sums=${CHECKSUM}/" PKGBUILD
+sed -i "s/^sha256sums=.*/sha256sums=(\"${CHECKSUM}\")/" PKGBUILD
 
 makepkg --printsrcinfo > .SRCINFO
 git add .
 git commit -m "Version $VERSION"
-git remote add aur ssh://aur@aur.archlinux.org/auditorium.git
 git fetch aur
 git push --set-upstream aur main
 
