7b909ab45c7985452b2972572c46ac9aa78a87ae
Émile Nemo <emile.nemo@protonmail.com>
2025-10-20 14:42:59 +0200

    6.17.3.arch2-1 (no changes, auto-updated)
    Signed-off-by: Émile Nemo <emile.nemo@protonmail.com>

--- bump.sh
+++ bump.sh
@@ -6,11 +6,11 @@
 # version. Very mutch a work in progress, DO NOT TRUST.
 #
 
-validated_for="6.17.3.arch1-1"
-forceupd="y"
+validated_for="6.17.3.arch2-1"
+forceupd="n"
 
 # Getting current versions
-  curl -s -o .bump-pn https://gitlab.archlinux.org/api/v4/projects/42594/repository/files/PKGBUILD/raw/\?ref\=6.17.3.arch1-1
+  curl -s -o .bump-pn https://gitlab.archlinux.org/api/v4/projects/42594/repository/files/PKGBUILD/raw/\?ref\=6.17.3.arch2-1
   live_ver="$(grep pkgver= .bump-pn | cut -d'=' -f2)"
   live_rel="$(grep pkgrel= .bump-pn | cut -d'=' -f2)"
   local_ver="$(grep pkgver= PKGBUILD | cut -d'=' -f2)"
