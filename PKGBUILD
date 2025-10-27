4eb0c0e301885326741920e5ed052cff392cea47
Patrick Northon <northon_patrick3@yahoo.ca>
2025-10-27 06:02:22 -0400

    Use openjfx variables for launcher.

--- PKGBUILD
+++ PKGBUILD
@@ -4,7 +4,7 @@
 pkgname=maptool
 _pkgname=MapTool
 pkgver=1.18.5
-pkgrel=1
+pkgrel=2
 pkgdesc="An open source virtual tabletop program"
 arch=('any')
 url='https://rptools.net/tools/maptool'
@@ -16,7 +16,7 @@ source=(
 	"git+https://github.com/RPTools/${pkgname}.git#tag=${pkgver}"
 	"${pkgname}.sh")
 sha256sums=('ef89f6a5d629f3ce3c6f789e527fd8aca1e607d4e2a9f2512265245e287615ca'
-            '16720b6f986f79eed2a9517433a6c0880c2b80f0d7c8611fb1359c85d1b372bc')
+            '57750a73c9eaaeae19fb68a459bd02b16fd2dfbba2dbdc7edda9f08112f1fa07')
 install="${pkgname}.install"
 
 _prefix="opt/$pkgname"
--- maptool.sh
+++ maptool.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
 
 MAP_TOOL_JAVA=
-JAVAFX_PATH="${JAVAFX_PATH:-/usr/share/java/java-openjfx}"
-export LD_LIBRARY_PATH="/usr/lib/java-openjfx:$LD_LIBRARY_PATH"
 
-_javajx_classes="$(ls -U1 "$JAVAFX_PATH/javafx"*.jar | tr '\n' ':')"
-_classpath="/usr/share/java/maptool/*:$_javajx_classes"
+_classpath='/usr/share/java/maptool/*'
 _default_jvm_opts='-Xmx768m -Xms32m -Xss8M "-Dsun.java2d.d3d=false" "-Djava.util.Arrays.useLegacyMergeSort=true" "-Dsentry.environment=production" "-Dfile.encoding=UTF-8" "-Dpolyglot.engine.WarnInterpreterOnly=false" "-DMAPTOOL_DATADIR=.maptool-rptools" "-XX:+ShowCodeDetailsInExceptionMessages" "--add-opens=java.desktop/java.awt=ALL-UNNAMED" "--add-opens=java.desktop/java.awt.geom=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt.geom=ALL-UNNAMED" "--add-opens=java.base/java.util=ALL-UNNAMED" "--add-opens=javafx.web/javafx.scene.web=ALL-UNNAMED" "--add-opens=javafx.web/com.sun.webkit=ALL-UNNAMED" "--add-opens=javafx.web/com.sun.webkit.dom=ALL-UNNAMED" "--add-opens=java.desktop/javax.swing=ALL-UNNAMED" "--add-opens=java.desktop/sun.awt.shell=ALL-UNNAMED" "--add-opens=java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED"'
 
 set -- \
@@ -14,7 +11,7 @@ set -- \
         "$@"
 
 eval "set -- $(
-        printf '%s\n' "$_default_jvm_opts $JAVA_OPTS $MAP_TOOL_OPTS" |
+        printf '%s\n' "$_default_jvm_opts $OPENJFX_JAVA_OPTIONS --add-modules javafx.swing,javafx.media,javafx.web,javafx.graphics $JAVA_OPTS $MAP_TOOL_OPTS" |
         xargs -n1 |
         sed ' s~[^-[:alnum:]+,./:=@_]~\\&~g; ' |
         tr '\n' ' '
