d05053418b6c6a014e8cc122bd615f0d00b8f9dd
Horange <horange321@163.com>
2025-10-29 17:47:46 +0800

    3.8.0

--- PKGBUILD
+++ PKGBUILD
@@ -1,10 +1,10 @@
 pkgver=3.8.0
-pkgrel=305
-sha256sums=("d288c8d9772f7402dd4f7eb5a555267d03dea6c34511b99c8092f29a104e90e3"
+sha256sums=(""
             "b5c9aadd1b2172158f0b0ff07fdaf84721c002bb5cd1394c9f9fe4557533964d"
             "65370e5aa8edb5cb8a3c66b385a782370cc5d5c578f7e4073f66e17fbc98ff5d")
 _pkgname=hmcl-dev
 pkgname=hmcl-dev-bin
+pkgrel=1
 pkgdesc="A Minecraft Launcher which is multi-functional, cross-platform and popular"
 url="https://github.com/huanghongxun/HMCL"
 license=("GPL3")
--- build.fish
+++ build.fish
@@ -5,27 +5,22 @@ echo 最新版 $ver
 
 # 获取当前版本
 set pkgver (cat .SRCINFO | grep pkgver | cut -d ' ' -f 3)
-set pkgrel (cat .SRCINFO | grep pkgrel | cut -d ' ' -f 3)
-echo 当前版 $pkgver.$pkgrel
+echo 当前版 $pkgver
 
-if [ $ver = $pkgver.$pkgrel ]
+if [ $ver = $pkgver ]
     echo 已是最新版
     exit
 end
 
 # 生成 PKGBUILD
-set vers (string split . $ver)
-set pkgver $vers[1].$vers[2].$vers[3]
-set pkgrel $vers[4]
 curl "http://mirrors.cloud.tencent.com/nexus/repository/maven-public/org/glavo/hmcl/hmcl-dev/$ver/hmcl-dev-$ver.jar" -O
-echo pkgver=$pkgver >PKGBUILD
-echo pkgrel=$pkgrel >>PKGBUILD
-set sha (sha256sum hmcl-dev-$pkgver.$pkgrel.jar | cut -d ' ' -f 1 | tr -d '\n')
+echo pkgver=$ver >PKGBUILD
+set sha (sha256sum hmcl-dev-$ver.jar | cut -d ' ' -f 1 | tr -d '\n')
 echo "sha256sums=(\"$sha\"" >>PKGBUILD
 cat PKGBUILD.part1 >>PKGBUILD
 
 # build
 makepkg -f
 makepkg --printsrcinfo >.SRCINFO
-git commit -a -m "$pkgver.$pkgrel"
+git commit -a -m "$ver"
 makepkg -i
