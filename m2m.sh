01d1db4afa3c2e371340914a15551ede77060912
Saffron <qwertysaffron@tutamai.com>
2025-11-03 13:05:40 +0530

    Added a flag to save the users file(s) to the current working directory if that's what they wanna do

--- m2m.sh
+++ m2m.sh
@@ -18,43 +18,58 @@ DATE=$(date +'%a_%b_%d_%H_%M_%S')
 LINK=$1
 file_name=$2
 multiple_switch=false
+cwd_switch=false
 
 case $1 in
 	-m)	multiple_switch=true;multiple_switch_counter=$2;;
 esac
 
-if [[ $# -ne 2 ]]; then
-	if [[ $multiple_switch == false ]];then
-		echo -e "$red[!] Usage:m2m <universal_resource_locater> <filename.ext>$norm"
+case $3 in 
+	--cwd) cwd_switch=true ; cwd_dir=$(pwd) ;;
+esac
+
+if [[ true ]]; then
+	if [[ $multiple_switch == false && $# -lt 2 ]];then
+		echo -e "$red[!] Usage:m2m <universal_resource_locater> <filename.ext> [--cwd]$norm"
 		exit 1
-	else
-		echo -e "$red[!] Usage: m2m -m <download_count>$norm"
+	elif [[ $multiple_switch == true && $# -lt 2 ]];then
+		echo -e "$red[!] Usage: m2m -m <download_count> [--cwd]$norm"
 		echo "For infinite downloads"
-		echo -e "$red[!] Usage: m2m -m n$norm"
+		echo -e "$red[!] Usage: m2m -m n [--cwd]$norm"
 		exit 1
 	fi
 fi
 
 multi_dnc(){
+	dest_dir=$1
 	counter=1
 	for file in "${!multiple_download_dict[@]}";do
 		url="${multiple_download_dict[$file]}"
 
 		echo -e "$blue[*] Initiating download for stream $counter $norm"
-		yt-dlp -f best "$url" -o $MULTI_DIR/ytvideo.webm 1>/dev/null 2>$ERROR_LOG/$DATE-yt-dlp-multi-download.log
+		if [[ $file == "*wav*" ]] || [[ $file == "*mp3*" ]];then
+			yt-dlp -f bestaudio $url -o "$dest_dir/ytvideo.webm" 1>/dev/null 2>$ERROR_LOG/$DATE-yt-dlp-multi-download.log
+		else
+			yt-dlp -f best $url -o "$dest_dir/ytvideo.webm" 1>/dev/null 2>$ERROR_LOG/$DATE-yt-dlp-multi-download.log
+		fi
 		
 		if [[ $? -eq 0 ]];then
 	
 			echo -e "$green[✓] Stream $counter downloaded $norm"
 			echo -e "$blue[*] Converting stream $counter $norm"
-			ffmpeg -i "$MULTI_DIR/ytvideo.webm" -map 0:a:0 -map 0:v:0 -c copy "$MULTI_DIR/$file" 1> /dev/null 2>"$ERROR_LOG/$DATE-ffmpeg-multi-download.log"
+			
+			if [[ $file == *"wav"* ]] || [[ $file == *"mp3"* ]];then
+				ffmpeg -i "$dest_dir/ytvideo.webm" "$dest_dir/$file" 1> /dev/null 2>"$ERROR_LOG/$DATE-ffmpeg-multi-download.log"
+			else
+				ffmpeg -i "$dest_dir/ytvideo.webm" -map 0:a:0 -map 0:v:0 -c copy "$dest_dir/$file" 1> /dev/null 2>"$ERROR_LOG/$DATE-ffmpeg-multi-download.log"
+			fi
 
 			if [[ $? -eq 0  ]];then
 				echo -e "$green[✓] Stream $counter saved to filesystem$norm \n"
-				rm $MULTI_DIR/ytvideo.*
+				rm "$dest_dir"/ytvideo.*
 			else
 				echo -e "$red[!] An error occured $norm"
-				echo -e "$red[!] Error saved at $ERROR_LOG"
+				echo -e "$red[!] Error saved at $ERROR_LOG $norm"
 				continue
 			fi
 
@@ -70,18 +85,28 @@ multi_dnc(){
 
 if [[ $multiple_switch != true ]];then
 
+	if [[ $cwd_switch == true ]];then
+		dest_dir=$cwd_dir
+	else
+		dest_dir=$YTDIR
+	fi
 	echo -e  "$blue[*] Downloading stream from youtube $norm"
 
-	yt-dlp -f best $LINK -o "$YTDIR/ytvideo.webm" 1> /dev/null 2>$ERROR_LOG/$DATE-yt-dlp.log
+	yt-dlp -f best $LINK -o "$dest_dir/ytvideo.webm" 1> /dev/null 2>$ERROR_LOG/$DATE-yt-dlp.log
 
 	#Block telling the user whether the stream was downloaded or not
 
 	if [[ $? -eq 0 ]];then
 	
 		echo -e "$blue[*] Stream downloaded $norm"
-
-		echo -e "These are the directories in $YTDIR \n$(ls $YTDIR )\nWhere do you want to save this one?"
-		read -p ">> " file_save_location
+		
+		if [[ $cwd_switch == true ]];then
+			file_save_location=$cwd_dir
+		else
+			echo -e "These are the directories in $dest_dir \n$(ls $dest_dir)\nWhere do you want to save this one?"
+			read -p ">> " file_save_location
+			file_save_location="$dest_dir/$file_save_location"
+		fi
 	else
 		echo -e "$red[!] Stream not downloaded $norm"
 		echo -e "$red[!] Error saved at $ERROR_LOG $norm"
@@ -89,12 +114,12 @@ if [[ $multiple_switch != true ]];then
 	fi
 	
 	echo -e "$blue[*] Converting the stream...$norm"
-	ffmpeg -i "$YTDIR/ytvideo.webm" -map 0:a:0 -map 0:v:0 -c copy "$YTDIR/$file_save_location/$file_name" 1> /dev/null 2>"$ERROR_LOG/$DATE-ffmpeg.log"
+	ffmpeg -i "$dest_dir/ytvideo.webm" -map 0:a:0 -map 0:v:0 -c copy "$file_save_location/$file_name" 1> /dev/null 2>"$ERROR_LOG/$DATE-ffmpeg.log"
 
 	#Block telling the user the final action and removing the ghost file.
 
 	if [[ $? -eq 0  ]];then
-		rm $YTDIR/ytvideo.web*
+		rm "$dest_dir"/ytvideo.web*
 		echo -e "$green[*] Streams Saved to filesystem at $YTDIR !! $norm" 
 	else
 		echo -e "$red[!] An error occured $norm"
@@ -103,7 +128,7 @@ if [[ $multiple_switch != true ]];then
 
 fi
 
-if [[ $multiple_switch = true && $multiple_switch_counter != "n" ]];then
+if [[ $multiple_switch == true && $multiple_switch_counter != "n" ]];then
 	#Taking the multiple links and file names from the user
 	declare -A multiple_download_dict
 
@@ -113,7 +138,11 @@ if [[ $multiple_switch = true && $multiple_switch_counter != "n" ]];then
 	multiple_download_dict["$save_file"]="$url"
 	done
 	
-	multi_dnc
+	if [[ $cwd_switch != true ]];then
+		multi_dnc "$MULTI_DIR"
+	else
+		multi_dnc "$cwd_dir"
+	fi
 fi
 
 if [[ $multiple_switch_counter == "n" ]];then
@@ -127,5 +156,10 @@ if [[ $multiple_switch_counter == "n" ]];then
 		multiple_download_dict["$save_file"]="$url"
 	done
 
-	multi_dnc
+	if [[ $cwd_switch != true ]];then
+		multi_dnc "$MULTI_DIR"
+	else
+		multi_dnc "$cwd_dir"
+	fi
+
 fi
