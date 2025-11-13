bbcc82c6fb08ab9e428707a8b38e3279574e9ae4
xtexecute <mirzaakinbeyazay@gmail.com>
2025-11-13 17:14:22 +0300

    Initial commit: fleshfetch 1.0

---
+++ PKGBUILD
@@ -0,0 +1,15 @@
+# Maintainer: Mirza (xtexecute) <none>
+
+pkgname=fleshfetch
+pkgver=1.0
+pkgrel=1
+pkgdesc="Enlightenment."
+arch=('any')
+license=('MIT')
+depends=()
+source=("fleshfetch.sh")
+sha256sums=('SKIP')
+
+package() {
+    install -Dm755 "$srcdir/fleshfetch.sh" "$pkgdir/usr/bin/fleshfetch"
+}
---
+++ fleshfetch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+cat << 'EOF'
+ _____  _     _____ ____   _   _
+/  __/ / \   /  __// ___\ / \ / \
+|  __\ | |   | _\  |    \ | |_| |
+| |    | |_/\|  /_ \___ | | |-| |
+\_/    \____/\____\\____/ \_/ \_/
+
+
+OS: flesh
+Host: flesh
+Kernel: flesh
+Uptime: flesh
+Packages: flesh
+Shell: flesh
+DE: flesh
+WM: flesh
+Theme: flesh
+CPU: flesh
+GPU 1: flesh
+GPU 2: give me flesh
+Memory: flesh
+Swap: flesh
+Disk (flesh): flesh
+Local IP: flesh
+Battery: flesh
+Locale: flesh
+EOF
