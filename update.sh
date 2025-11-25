0a108d04198556831999b3a6e2273f58f58de335
Patrick vd Glind <patrick_de_glind@hotmail.com>
2025-11-25 22:21:07 +0100

    updated update.sh check for new Plexamp version and download/process if found

--- update.sh
+++ update.sh
@@ -1,21 +1,27 @@
 cd $(dirname $0)
 
-NEWVER=$1
+PKGBUILD_VERSION=$(cat PKGBUILD | grep 'pkgver=' | cut -d'=' -f2)
+echo "Current PKGBUILD version:"
+echo $PKGBUILD_VERSION
 
-if [ "$NEWVER" = "" ]; then
-	echo "enter version name"
-	exit 1
+VERSION=$(curl --silent https://plexamp.plex.tv/plexamp.plex.tv/desktop/latest-linux.yml | grep version | cut -d' ' -f2)
+echo "Latest Plexamp Linux version:"
+echo $VERSION
+
+if [ $PKGBUILD_VERSION != $VERSION ]; then
+	echo "Newer version found, starting download"
+	curl -O https://plexamp.plex.tv/plexamp.plex.tv/desktop/Plexamp-$VERSION.AppImage
+
+	sed -i "s/^pkgver=.*/pkgver=$VERSION/" PKGBUILD
+	makepkg
+	CHKSUM="$(sha512sum Plexamp-$VERSION.AppImage | cut -d' ' -f1)"
+	sed -i "s/^sha512sums=.*/sha512sums=('$CHKSUM')/;s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+	makepkg
+
+	echo "Press enter to continue"
+	read
+	makepkg --printsrcinfo > .SRCINFO
+	git add .SRCINFO PKGBUILD
+	git commit -m "Updated to $VERSION"
+	git push
 fi
-
-sed -i "s/^pkgver=.*/pkgver=$NEWVER/" PKGBUILD
-makepkg
-CHKSUM="$(sha512sum Plexamp-$NEWVER.AppImage | cut -d' ' -f1)"
-sed -i "s/^sha512sums=.*/sha512sums=('$CHKSUM')/;s/^pkgrel=.*/pkgrel=1/" PKGBUILD
-makepkg
-
-echo "Press enter to continue"
-read
-makepkg --printsrcinfo > .SRCINFO
-git add .SRCINFO PKGBUILD
-git commit -m "Updated to $NEWVER"
-git push
