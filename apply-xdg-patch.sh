4be6ab2fd8d3c4d679fb3322662c3e71e6611dc2
Tiago de Paula <tiagodepalves@gmail.com>
2025-11-29 03:12:59 -0300

    feat: remove fix for #129953
    Has been fixed upstream for some time now.

--- apply-xdg-patch.sh
+++ apply-xdg-patch.sh
@@ -11,11 +11,6 @@ fix_15741() {
   add_mime_type 'inode/directory' "$1"
 }
 
-# see https://github.com/microsoft/vscode/issues/129953#issuecomment-1323778127
-fix_129953() {
-  sed -i -E 's/"desktopName":\s*"(.+)-url-handler\.desktop"/"desktopName": "\1.desktop"/' "$1"
-}
-
 # see https://github.com/microsoft/vscode/issues/214741
 fix_214741() {
   add_mime_type 'text/plain' "$1"
@@ -26,10 +21,6 @@ while read -r file; do
     *.desktop)
       fix_214741 "${file}"
       fix_15741 "${file}"
-      echo "patched ${file}"
-      ;;
-    */package.json)
-      fix_129953 "${file}"
       echo "patched ${file}"
       ;;
     *)
