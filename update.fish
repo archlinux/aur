77b0fb490d2655142e71b38f5e988b098f385aef
Mike Lei <7297-mikelei@users.noreply.gitlab.archlinux.org>
2025-12-09 00:19:57 +0800

    build: update update script

--- update.fish
+++ update.fish
@@ -12,5 +12,7 @@ string replace -r 'sha256sums_x86_64=\("\K\w+' $x86_64 < PKGBUILD\
 | string replace -r 'sha256sums_aarch64=\("\K\w+' $aarch64 > PKGBUILD.new
 mv PKGBUILD.new PKGBUILD
 makepkg --printsrcinfo > .SRCINFO
-git add PKGBUILD .SRCINFO
-git commit -m "bump: $_pkgver"
+if not git diff --quiet
+    git add PKGBUILD .SRCINFO
+    git commit -m "bump: $_pkgver"
+end
