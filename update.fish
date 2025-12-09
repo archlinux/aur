31c64afd07ac6ff88cd5e36b5afe2d71d55bb095
Mike Lei <7297-mikelei@users.noreply.gitlab.archlinux.org>
2025-12-09 16:02:29 +0800

    build: add update script

---
+++ update.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+
+set -l pkgver (curl -sLf "https://product-details.mozilla.org/1.0/firefox_versions.json" | jq -r ".FIREFOX_DEVEDITION")
+sed -i -r "/^pkgver/{s/=.+\$/=$pkgver/}" PKGBUILD
+if not git diff --quiet
+    makepkg --printsrcinfo > .SRCINFO
+    git add PKGBUILD .SRCINFO
+    git commit -m "bump: $_pkgver"
+end
