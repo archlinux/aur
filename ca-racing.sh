0b7c615ec649c3a4c9addb05d9b260019bcdb029
piotrek2713 <karasinskipiotrek13@gmail.com>
2026-01-05 23:14:10 +0000

    Update to v

--- ca-racing.sh
+++ ca-racing.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Add vendor directory to PYTHONPATH
 export PYTHONPATH="/opt/ca-racing/vendor${PYTHONPATH:+:}${PYTHONPATH}"
+# Change to the application directory
+cd /opt/ca-racing || exit
 # Execute the main script
-exec python /opt/ca-racing/main.py "$@"
+exec python main.py "$@"
