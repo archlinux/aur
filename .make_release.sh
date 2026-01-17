3a706c034a6aa1a73ea3a0eebce53a007fa6394f
mvtab <mvtabilitas@protonmail.com>
2026-01-17 15:48:11 +0100

    1.0.7-4: Added git-connection option and improved error messages.

--- .make_release.sh
+++ .make_release.sh
@@ -26,10 +26,10 @@ make_release() {
     makepkg --printsrcinfo > .SRCINFO
 
     git add -A
-    git commit -m "${TARGET_VERSION}: ${RELEASE_MESSAGE}" 
+    git commit -m "${TARGET_VERSION}: ${RELEASE_MESSAGE}"
+    git push
     #git tag -a "${TARGET_VERSION}" -m "${TARGET_VERSION}: ${RELEASE_MESSAGE}"
     #git push --tags
-    git push
 }
 
 make_release "${@}"
