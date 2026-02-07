b38334f7b9fe4b25d26a85749c0aa320c4e067fc
Rune580 <runemaster580@gmail.com>
2026-02-07 10:35:53 -0500

    release 1.12.0

--- PKGBUILD
+++ PKGBUILD
@@ -1,16 +1,16 @@
 pkgname=gale-bin
-pkgver=1.11.1
-pkgrel=2
+pkgver=1.12.0
+pkgrel=1
 pkgdesc="Lightweight Thunderstore client"
 arch=('x86_64')
 url="https://github.com/Kesomannen/gale"
 license=('GPL-3.0-or-later')
 depends=('cairo' 'desktop-file-utils' 'gdk-pixbuf2' 'glib2' 'gtk3' 'hicolor-icon-theme' 'libsoup' 'openssl' 'pango' 'webkit2gtk-4.1' 'xdg-utils')
 options=('!strip' '!emptydirs')
-source_x86_64=("https://github.com/Kesomannen/gale/releases/download/$pkgver/gale_"$pkgver"_amd64_linux.deb"
+source_x86_64=("https://github.com/Kesomannen/gale/releases/download/$pkgver/gale_"$pkgver"_amd64.deb"
     "webkit-disable-compositing.patch"
 )
-sha256sums_x86_64=('b2f063bd2d8bbdfb55089a3957050a4ca05053676184bb899e6528733ed702f4'
+sha256sums_x86_64=('6b759cebf304dd2f3b963a23cf65d2b3494eecb8d7e66856b5610fd02cb130aa'
     SKIP
 )
 
--- update-pkgbuild.sh
+++ update-pkgbuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+LINUX_DEB_END_PATTERN="amd64.deb"
+
 if [[ -n "$1" ]]; then
     RELEASE_DATA=$(curl -s https://api.github.com/repos/Kesomannen/gale/releases/tags/$1)
 else
@@ -7,12 +9,12 @@ else
 fi
 
 VERSION=$(echo "$RELEASE_DATA" | jq '.name' | sed 's/\"//g')
-DOWNLOAD_URL=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.browser_download_url | endswith("amd64_linux.deb")) | .browser_download_url')
-CHECKSUM=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.browser_download_url | endswith("amd64_linux.deb")) | .digest | sub("^sha256:"; "")')
+DOWNLOAD_URL=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.browser_download_url | endswith("'$LINUX_DEB_END_PATTERN'")) | .browser_download_url')
+CHECKSUM=$(echo "$RELEASE_DATA" | jq -r '.assets[] | select(.browser_download_url | endswith("'$LINUX_DEB_END_PATTERN'")) | .digest | sub("^sha256:"; "")')
 
 wget "$DOWNLOAD_URL"
 
-DEB_FILE="Gale_${VERSION}_amd64_linux.deb"
+DEB_FILE="Gale_${VERSION}_${LINUX_DEB_END_PATTERN}"
 
 sed -ri "s/pkgver=[0-9.]+/pkgver=$VERSION/" PKGBUILD
 sed -ri "s/(sha256sums_x86_64=\(\s*?)'[0-9A-Za-z]+'/\1'$CHECKSUM'/g" PKGBUILD
