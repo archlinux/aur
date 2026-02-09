522f82a9cd348846eebda3f3c706746b2629044d
soracat <125882337+s0racat@users.noreply.github.com>
2026-02-09 16:02:03 +0900

    add Update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+pkgver=$(curl -s "https://api.github.com/repos/linux-jm/manual/releases/latest"|jq -r '.tag_name | sub("^v"; "")')
+sed -i "s/pkgver=.*/pkgver=${pkgver}/" PKGBUILD
+updpkgsums
+makepkg --printsrcinfo > .SRCINFO
+echo "Updated to $pkgver"
