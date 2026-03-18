408efecf5e10c943ee6e66fc27db6190085eb23a
FlyXOwl <FlyXOwl@users.noreply.github.com>
2026-03-18 15:24:55 +0000

    Update AUR package

--- PKGBUILD
+++ PKGBUILD
@@ -1,48 +1,17 @@
 pkgname=bebz-essentials
-pkgver=0.2.0
+pkgver=1.0.0
 pkgrel=1
-pkgdesc="La Bebzerie : Setup complet Gaming & Système (Nvidia, Steam, Lutris, Wine)"
+pkgdesc="L'installeur interactif de la Bebzerie (Gaming, Drivers)"
 arch=('any')
 url="https://github.com/FlyXOwl/aur-bebz-essentials"
 license=('MIT')
+install=bebz.install
 
-# Dépendances
-depends=(
-    'nvidia-dkms'
-    'nvidia-utils'
-    'lib32-nvidia-utils'
-    'steam'
-    'discord'
-    'networkmanager'
-    'nm-connection-editor'
-    'bluez'
-    'bluez-utils'
-    'git'
-    'base-devel'
-    'lutris'
-    'heroic-games-launcher-bin'
-    'wine-staging'
-    'winetricks'
-    'gamemode'
-    'lib32-gamemode'
-    'mangohud'
-    'lib32-mangohud'
-    'giflib' 'lib32-giflib'
-    'libpng' 'lib32-libpng'
-    'libldap' 'lib32-libldap'
-    'vulkan-icd-loader'
-    'lib32-vulkan-icd-loader'
-)
+depends=('gum' 'base-devel' 'git')
 
-optdepends=(
-    'proton-ge-custom-bin: Version de Proton optimisée par GE'
-    'gamescope: Micro-compositeur pour optimiser le focus des jeux'
-    'goverlay: Interface graphique pour configurer MangoHud'
-)
+source=("bebzmenu.sh")
+sha256sums=('12f6b66cf8de7f0cb7d68fd3d8536565f4a19094d44cd4ac57626172a8ba99db')
 
 package() {
-    install -d "$pkgdir/usr/share/licenses/$pkgname"
-
-    echo "Copyright (c) 2026 FlyXOwl - MIT License" > "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
-    echo "Metapackage personnel pour l'installation automatisée de la Bebzerie." >> "$pkgdir/usr/share/licenses/$pkgname/LICENSE"
+    install -Dm755 "${srcdir}/bebzmenu.sh" "${pkgdir}/usr/bin/bebzmenu"
 }
---
+++ bebzmenu.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export GUM_CHOOSE_SELECTED_FOREGROUND="212"
+export GUM_CHOOSE_HEADER_FOREGROUND="57"
+
+if [ ! -f /etc/arch-release ]; then
+    echo "Erreur : Ce script est conçu pour Arch Linux ou ses distro dérivées" | gum format
+    exit 1
+fi
+
+echo "--- BIENVENUE DANS LE DEPENDENCIES SETUP DE LA BEBZERIE ---" | gum format
+
+MODE=$(gum choose "Sélection manuelle" "All-In (Tout installer)" "Quitter")
+
+if [ "$MODE" == "Quitter" ] || [ -z "$MODE" ]; then exit 0; fi
+
+LISTE=""
+
+if [ "$MODE" == "All-In (Tout installer)" ]; then
+    LISTE="nvidia-dkms nvidia-utils lib32-nvidia-utils steam discord networkmanager nm-connection-editor bluez bluez-utils git base-devel lutris heroic-games-launcher-bin wine-staging winetricks gamemode lib32-gamemode mangohud lib32-mangohud giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap vulkan-icd-loader lib32-vulkan-icd-loader"
+else
+    CAT=$(gum choose --no-limit --header "Catégories (Espace pour cocher) :" "Gaming" "Système")
+
+    if [[ $CAT == *"Gaming"* ]]; then
+        GAMING_CHOICE=$(gum choose --no-limit --header "Jeux :" "steam" "lutris" "heroic-games-launcher-bin" "wine-staging")
+        LISTE="$LISTE $GAMING_CHOICE"
+    fi
+
+    if [[ $CAT == *"Système"* ]]; then
+        SYSTEM_CHOICE=$(gum choose --no-limit --header "Système :" "nvidia-dkms" "discord" "bluez" "lib32-nvidia-utils" "networkmanager" "nm-connection-editor" "bluez-utils" "git" "base-devel" "giflib" "lib32-giflib" "libpng" "lib32-libpng" "libldap" "lib32-libldap" "vulkan-icd-loader" "lib32-vulkan-icd-loader")
+        LISTE="$LISTE $SYSTEM_CHOICE"
+    fi
+fi
+
+FINAL_LIST=$(echo "$LISTE" | tr '\n' ' ' | xargs)
+
+if [ -n "$FINAL_LIST" ]; then
+    echo "## Préparation de l'installation de :" | gum format
+    echo "$FINAL_LIST"
+
+    if gum confirm "Lancer l'installation maintenant ?"; then
+        sudo pacman -S --needed $FINAL_LIST
+    else
+        echo "Installation annulée par l'utilisateur."
+    fi
+else
+    echo "Rien à installer (Aucune option cochée)" | gum format
+fi
