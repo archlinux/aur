a36720fb97e16f34355f857701cba92a2349d18b
Laura Hausmann <laura@hausmann.dev>
2026-03-19 15:55:40 +0100

    Update apply-upstream-patch.sh

--- apply-upstream-patch.sh
+++ apply-upstream-patch.sh
@@ -6,12 +6,12 @@ cp "$1" "$1.new"
 # PKGBUILD
 sed -i '' 's/pkgbase=ffmpeg/pkgbase=ffmpeg-libfdk_aac/g' "$1.new"
 sed -i '' 's/pkgname=ffmpeg/pkgname=ffmpeg-libfdk_aac/g' "$1.new"
-sed -i '' "s/pkgdesc='Complete solution to record, convert and stream audio and video'/pkgdesc='Complete solution to record, convert and stream audio and video (Same as official package except with libfdk-aac support)'/g" "$1"
+sed -i '' "s/pkgdesc='Complete solution to record, convert and stream audio and video'/pkgdesc='Complete solution to record, convert and stream audio and video (Same as official package except with libfdk-aac support)'/g" "$1.new"
 
 # .SRCINFO
 sed -i '' 's/pkgbase = ffmpeg/pkgbase = ffmpeg-libfdk_aac/g' "$1.new"
 sed -i '' 's/pkgname = ffmpeg/pkgname = ffmpeg-libfdk_aac/g' "$1.new"
-sed -i '' 's/pkgdesc = Complete solution to record, convert and stream audio and video/pkgdesc = Complete solution to record, convert and stream audio and video (Same as official package except with libfdk-aac support)/g' "$1"
+sed -i '' 's/pkgdesc = Complete solution to record, convert and stream audio and video/pkgdesc = Complete solution to record, convert and stream audio and video (Same as official package except with libfdk-aac support)/g' "$1.new"
 
 # apply patch
 git am "$1.new" || git am --abort
