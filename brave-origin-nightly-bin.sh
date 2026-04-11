51338b0761dd2accd3e9224ed34cbca112ae163c
brave-prerelease <aur-prerelease@brave.com>
2026-04-11 16:55:57 +0000

    1.91.33

--- brave-origin-nightly-bin.sh
+++ brave-origin-nightly-bin.sh
@@ -2,7 +2,7 @@
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
 # Allow users to override command-line options
-USER_FLAGS_FILE="$XDG_CONFIG_HOME/brave-nightly-flags.conf"
+USER_FLAGS_FILE="$XDG_CONFIG_HOME/brave-origin-nightly-flags.conf"
 if [[ -f $USER_FLAGS_FILE ]]; then
    USER_FLAGS="$(cat $USER_FLAGS_FILE | sed 's/#.*//')"
 fi
