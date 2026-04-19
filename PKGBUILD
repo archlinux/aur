309c8660693c929a2e2bd2ea028e137e81fe0e22
AT10GHz <sebastian_franz@mba42.de>
2026-04-19 21:21:40 +0200

    Fix: Wineprefix initialization added to kabx.sh

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer: AT10GHz <sebastian_franz@mba42.de>
 pkgname=dayton-kabx
 pkgver=1.0.2
-pkgrel=2
+pkgrel=3
 pkgdesc="Software für Dayton Audio KAB Bluetooth Verstärker (Wine). Inkl. USB-Auto-Config."
 arch=('any')
 url="https://www.daytonaudio.com"
@@ -13,7 +13,7 @@ source=("kabx.zip::https://www.daytonaudio.com/images/resources/dayton-audio-kab
         "99-dayton-kabx.rules")
 sha256sums=('fb6d86fd4d501f56299d61188373a687f5e81fb32b63697a700f410c2d1f4e24'
             '8af5565ce0c2a667f46a980727f73362df3fda5ca1b0ad57a78b556b989a3cc5'
-            'e10c11c06a65c8a056fe88133f7f2651d69d37384af699ef2cd50c791d13aed0'
+            'fb912fa0bfc8a594712a3f63e8a163e40aa7c9af584255f94ed3d780336ebef5'
             '5cdf92855e180f0ca431965368f05816f35e363863e9516bd5834c7dd267dcee')
 
 package() {
--- kabx.sh
+++ kabx.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 export WINEPREFIX="$HOME/.local/share/wineprefixes/dayton-kabx"
-mkdir -p "$WINEPREFIX/dosdevices"
 
-# 1. Alten Link löschen und neuen Link auf den SYMLINK setzen
+# 1. Falls der Präfix neu ist, initialisiere ihn sauber
+if [ ! -d "$WINEPREFIX" ]; then
+    echo "Erstelle neue Wine-Umgebung... bitte warten..."
+    wineboot -u
+fi
+
+# 2. Hardware-Links setzen
+mkdir -p "$WINEPREFIX/dosdevices"
 rm -f "$WINEPREFIX/dosdevices/com1"
 ln -s /dev/dayton_kabx "$WINEPREFIX/dosdevices/com1"
 
-# 2. Wine die Hardware in die Registry schreiben (beendet den Zufall)
-wine reg add "HKEY_LOCAL_MACHINE\Software\Wine\Ports" /v "COM1" /t REG_SZ /d "/dev/dayton_kabx" /f > /dev/null 2>&1
+# Registry-Eintrag für COM1 (Wichtig: Backslashes müssen für Bash escaped werden)
+wine reg add "HKEY_LOCAL_MACHINE\\Software\\Wine\\Ports" /v "COM1" /t REG_SZ /d "/dev/dayton_kabx" /f > /dev/null 2>&1
 
 FINAL_EXE="$WINEPREFIX/drive_c/Program Files/KPX DSP Control/KPX DSP Control.exe"
 
+# 3. Installer oder App starten
 if [ ! -f "$FINAL_EXE" ]; then
-    wine /opt/dayton-kabx/installer.exe
+    echo "Installer wird gestartet..."
+    cd /opt/dayton-kabx/
+    wine installer.exe
 else
-    # Eine Sekunde warten, damit die Hardware-Initialisierung fertig ist
     sleep 1
     wine "$FINAL_EXE" "$@"
 fi
