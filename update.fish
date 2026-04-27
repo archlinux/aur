66414ce8c7840b8fa4856019ee1a78eab09097fc
Mike Lei <7297-mikelei@users.noreply.gitlab.archlinux.org>
2026-04-28 03:05:09 +0800

    fix(update): handle network and API failures

--- update.fish
+++ update.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/fish
 
-set -l release (curl -sLf https://api.github.com/repos/SagerNet/sing-box/releases?per_page=3 | jq -rc 'map(select(.prerelease)) | first')
+set -l _data (curl -sLf https://api.github.com/repos/SagerNet/sing-box/releases?per_page=3)
+or echo "Network request failed" && return 1
+set -l release (echo $_data | jq -rc 'map(select(.prerelease)) | first')
+test "$release" = null; and echo "Cannot fetch release data from GitHub API"\n"Raw data:"\n"$_data"; and return 1
 set -l _pkgver (echo $release | jq -r .name)
 set -l assets (echo $release | jq -rc '.assets | map(select(.name | endswith(".pkg.tar.zst")))')
 for asset in (echo $assets | jq -r '.[] | "\(.name) \(.digest)"' | string replace -r '.+_linux_(x86_64|armv7h|aarch64)l?\.pkg\.tar\.zst sha256:' '$1 ')
@@ -12,8 +15,10 @@ string replace -r '^_pkgver="\K[^"]+' $_pkgver < PKGBUILD\
 | string replace -r '^sha256sums_armv7h=\("\K\w+' $armv7h\
 | string replace -r '^sha256sums_aarch64=\("\K\w+' $aarch64 > PKGBUILD.new
 mv PKGBUILD.new PKGBUILD
-if not git diff --quiet
+if not git diff --quiet PKGBUILD
     makepkg --printsrcinfo > .SRCINFO
     git add PKGBUILD .SRCINFO
     git commit -m "bump: $_pkgver"
+else
+    echo "No update"
 end
