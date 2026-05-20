71d1f2e5c6e247206f8a80a5ec390ea4b34b6b99
Mike Lei <7297-mikelei@users.noreply.gitlab.archlinux.org>
2026-05-20 23:27:56 +0800

    feat(update): show message when no update is available

--- update.fish
+++ update.fish
@@ -2,8 +2,10 @@
 
 set -l pkgver (curl -sLf "https://product-details.mozilla.org/1.0/firefox_versions.json" | jq -r ".FIREFOX_DEVEDITION")
 sed -i -r "/^pkgver/{s/=.+\$/=$pkgver/}" PKGBUILD
-if not git diff --quiet
+if not git diff --quiet PKGBUILD
     makepkg --printsrcinfo > .SRCINFO
     git add PKGBUILD .SRCINFO
     git commit -m "bump: $pkgver"
+else
+    echo "No update"
 end
