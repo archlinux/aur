e9726e51ca72e88eb0eb4ecdaaf2ae213304cf53
somnus-cachyos-desktop <somnus0917chen@hotmail.com>
2026-06-10 00:18:09 +0800

    fix linux webkit rendering

--- superlabel.sh
+++ superlabel.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+export WEBKIT_DISABLE_DMABUF_RENDERER="${WEBKIT_DISABLE_DMABUF_RENDERER:-1}"
+
+if [ -n "${SUPERLABEL_FORCE_X11:-}" ]; then
+  export GDK_BACKEND="${GDK_BACKEND:-x11}"
+fi
+
 exec /opt/superlabel/superlabel.AppImage "$@"
