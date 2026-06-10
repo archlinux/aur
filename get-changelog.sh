1f5ed8d93244e96e8bc4bab390c75abacc5d5dbd
Luis Martinez <luis.martinez@disroot.org>
2026-06-10 17:30:43 -0500

    add .nvchecker.toml and REUSE files

--- get-changelog.sh
+++ get-changelog.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# SPDX-FileCopyrightText: 2026 Arch Linux contributors
+#
+# SPDX-License-Identifier: 0BSD
+
 curl -O --location "https://github.com/sourcegraph/src-cli/raw/$1/CHANGELOG.md"
