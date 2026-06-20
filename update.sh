262b47c57e4e8453e9a72508e7880792e5a6d57f
Sadie K <zulc22.db@gmail.com>
2026-06-20 01:38:19 -0700

    make updater script

--- update.sh
+++ update.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
-sed -i "s/\$sha256sums=/$(makepkg --geninteg)/" PKGBUILD
+set -eo pipefail
+
+printf "Finding latest version... " 
+NEWVER="$(pip index versions neostab --only-final latest | cat | head -n 1 | cut -d' ' -f2 | cut -d'(' -f2 | cut -d')' -f1)" || {
+    printf "failed\n"
+    exit 1
+}
+printf "%s\n" "$NEWVER"
+
+echo "Generating integrity..."
+INTEG="$(makepkg --geninteg)"
+
+echo Replacing version: "$NEWVER"
+sed -i "s/^pkgver=.*/pkgver=$NEWVER/" PKGBUILD
+echo Replacing integrity
+sed -i "s/^sha256sums=.*/$INTEG/" PKGBUILD
+echo Updating .SRCINFO...
 makepkg --printsrcinfo > .SRCINFO
 
 if [[ "$1" == "and_upload" ]]; then
