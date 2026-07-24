edbbc24f42b8e2584650cd27e1653c183a2e9dd0
Mazhar Hussain <realmazharhussain@gmail.com>
2026-07-24 16:39:47 +0500

    Fix notification when no internet

--- notify-gnome-shell-update.sh
+++ notify-gnome-shell-update.sh
@@ -6,8 +6,13 @@ echo "Installed: $installed_version"
 
 nvchecker_config=/usr/share/notify-gnome-shell-update/gnome-shell-archpkg.toml
 released_version=$(nvchecker --file="$nvchecker_config" --logger=json | jq '.version' | sed -E 's/^"(.*)"$/\1/')
+
+if [[ $released_version == null$'\n'null ]]; then
+  released_version="Unknown"
+fi
+
 echo "Released: $released_version"
 
-if [[ $released_version != $installed_version ]]; then
+if [[ $released_version != $installed_version ]] && [[ $released_version != "Unknown" ]]; then
   notify-send "New GNOME Version" "GNOME $released_version is now available on Arch"
 fi
