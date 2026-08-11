d6ec7f5d3da7e6066218933c77fcbd8549bb008f
tuxnix <tuxnix@gmx.com>
2026-08-11 22:42:03 +0200

    chown

--- remove-mpvcut.sh
+++ remove-mpvcut.sh
@@ -2,18 +2,13 @@
 
 # For mpvcut.hook: Remove mpvcut for all user.
 
-echo "Remove mpvcut for all user..."
-
 for user_home in /home/*; do
     if [ -d "$user_home" ]; then
         username=$(basename "$user_home")
         if id "$username" >/dev/null 2>&1; then
             if [ -d "$user_home/.config/mpv/scripts/mpvcut" ]; then
                 rm -rf "$user_home/.config/mpv/scripts/mpvcut"
-                echo "  Entfernt: $user_home/.config/mpv/scripts/mpvcut"
             fi
         fi
     fi
 done
-
-echo "Done!"
--- update-mpvcut.sh
+++ update-mpvcut.sh
@@ -8,6 +8,7 @@ for user_home in /home/*; do
         if id "$username" >/dev/null 2>&1; then
             mkdir -p "$user_home/.config/mpv/scripts/"
             cp -r /etc/skel/.config/mpv/scripts/mpvcut "$user_home/.config/mpv/scripts/"
+            chown --reference=$user_home -R $user_home/.config/mpv/scripts/mpvcut
         fi
     fi
 done
