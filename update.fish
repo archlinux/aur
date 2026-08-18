6b6f5189f883a337d4714ee316e86fc76d2c547c
Mike Lei <7297-mikelei@users.noreply.gitlab.archlinux.org>
2026-08-18 17:36:25 +0800

    feat(update): add cleanup function and run it on errors

--- update.fish
+++ update.fish
@@ -1,11 +1,18 @@
 #!/usr/bin/fish
 
+function cleanup
+    echo $argv
+    rm -f SHA512SUMS SHA512SUMS.asc
+    git restore PKGBUILD .SRCINFO
+    exit 1
+end
+
 set -l pkgver (curl -sLf "https://product-details.mozilla.org/1.0/firefox_versions.json" | jq -r ".FIREFOX_DEVEDITION")
 sed -i -r "/^pkgver/{s/=.+\$/=$pkgver/}" PKGBUILD
 if not git diff --quiet PKGBUILD
-    makepkg --printsrcinfo > .SRCINFO
+    makepkg --printsrcinfo > .SRCINFO || cleanup "Failed to generate .SRCINFO"
     git add PKGBUILD .SRCINFO
     git commit -m "bump: $pkgver"
 else
-    echo "No update"
+    cleanup "No update"
 end
