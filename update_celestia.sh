2e140d8a24d5ff0a7095e8454ded8a50f2615dec
Rafael Baboni Dominiquini <rafaeldominiquini@gmail.com>
2026-08-21 17:55:15 -0300

    REFACTOR

--- update_celestia.sh
+++ update_celestia.sh
@@ -15,21 +15,32 @@ NEW_SUBVERSION_REMOTE_DATA=$(grep -o ">celestia-data-${VERSION_APP}~.*-any\.pkg\
 
 NEW_VERSION_CSPICE=$(grep -o ">cspice-.*-x86_64\.pkg\.tar\.zst<" <<< "${URL_DATA}" | head -n 1 | sed -E 's/>cspice-(.*)-x86_64\.pkg\.tar\.zst</\1/')
 
+CURRENT_PKGVER=$(grep -E '^\s*pkgver=' ./PKGBUILD | sed -E 's/pkgver=["'\'' ]*([^"'\'' ]*).*/\1/')
+CURRENT_PKGREL=$(grep -E '^\s*pkgrel=' ./PKGBUILD | sed -E 's/pkgrel=["'\'' ]*([^"'\'' ]*).*/\1/')
+
 if [ "${CURRENT_SUBVERSION_REMOTE_APP}" != "${NEW_SUBVERSION_REMOTE_APP}" ] || [ "${CURRENT_SUBVERSION_REMOTE_DATA}" != "${NEW_SUBVERSION_REMOTE_DATA}" ] || [ "${CURRENT_VERSION_CSPICE}" != "${NEW_VERSION_CSPICE}" ]; then
-    CURRENT_REL=$(grep -E '^\s*pkgrel=' ./PKGBUILD | sed -E 's/pkgrel=["'\'' ]*([^"'\'' ]*).*/\1/')
+    NEW_PKGVER=${CURRENT_PKGVER}
+    NEW_PKGREL=$((CURRENT_PKGREL + 1))
 
-    NEW_REL=$((CURRENT_REL + 1))
+    if [ "${CURRENT_PKGVER}" == "${VERSION_APP}" ]; then
+        sed -i "s/^[[:space:]]*_version_celestia_app=.*/_version_celestia_app=\"${NEW_SUBVERSION_REMOTE_APP}\"/" ./PKGBUILD
+        sed -i "s/^[[:space:]]*_version_celestia_data=.*/_version_celestia_data=\"${NEW_SUBVERSION_REMOTE_DATA}\"/" ./PKGBUILD
 
-    sed -i "s/^[[:space:]]*_version_celestia_app=.*/_version_celestia_app=\"${NEW_SUBVERSION_REMOTE_APP}\"/" ./PKGBUILD
-    sed -i "s/^[[:space:]]*_version_celestia_data=.*/_version_celestia_data=\"${NEW_SUBVERSION_REMOTE_DATA}\"/" ./PKGBUILD
+        sed -i "s/^[[:space:]]*_version_cspice=.*/_version_cspice=\"${NEW_VERSION_CSPICE}\"/" ./PKGBUILD
 
-    sed -i "s/^[[:space:]]*_version_cspice=.*/_version_cspice=\"${NEW_VERSION_CSPICE}\"/" ./PKGBUILD
+        sed -i "s/^[[:space:]]*pkgver=.*/pkgver=${NEW_PKGVER}/" ./PKGBUILD
+        sed -i "s/^[[:space:]]*pkgrel=.*/pkgrel=${NEW_PKGREL}/" ./PKGBUILD
 
-    sed -i "s/^[[:space:]]*pkgrel=.*/pkgrel=${NEW_REL}/" ./PKGBUILD
+        updpkgsums && makepkg -o && makepkg --printsrcinfo > .SRCINFO
 
-    updpkgsums && makepkg -o && makepkg --printsrcinfo > .SRCINFO
-
-    echo "New version: ${VERSION_APP}-${NEW_REL}"
+        echo "New version: ${NEW_PKGVER}-${NEW_PKGREL}!"
+    else
+        echo "Unable to update!"
+    fi
 else
-    echo "The already updated!"
+    if [ "${CURRENT_PKGVER}" == "${VERSION_APP}" ]; then
+        echo "Already updated!"
+    else
+        echo "Unable to update!"
+    fi
 fi
