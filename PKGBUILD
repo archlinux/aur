a49fb00ba6462b51d704581b1d4383dd890c80d6
Patrick Glind <patrick_de_glind@hotmail.com>
2026-09-08 10:34:42 +0200

    Initial release with 4.50.6 added

---
+++ PKGBUILD
@@ -0,0 +1,41 @@
+# Maintainer: Naxels <naxels at gmail dot com>
+
+pkgname=plexamp-beta-appimage
+pkgver=4.50.6
+pkgrel=1
+pkgdesc="Modern music client for Plex BETA"
+provides=('plexamp')
+conflicts=('plexamp' 'plexamp-appimage')
+arch=('x86_64' 'aarch64')
+depends=('fuse2')
+url="https://plexamp.com"
+options=(!strip)
+_desktop_name=Plexamp.desktop
+_filename=Plexamp-${pkgver}-${arch}.AppImage
+source_x86_64=(
+  https://plexamp.plex.tv/desktop/${_filename}
+)
+source_aarch64=(
+  https://plexamp.plex.tv/desktop/${_filename}
+)
+sha256sums_x86_64=('5e6dba0dc8742b050442393b7810ef3ac13bdbbe660a6de185432fd132a9ed11')
+sha256sums_aarch64=('6c740c32e51a56ee731f6bbf84aca3d98727910bf600119cf326481e773b079d')
+
+
+prepare() {
+	cd "$srcdir"
+	rm -rf squashfs-root
+	chmod +x $_filename
+	./$_filename --appimage-extract
+}
+
+package() {
+	echo "Starting install"
+	install -Dm755 $_filename "$pkgdir"/usr/bin/Plexamp.AppImage
+	echo "Installing desktop launch file to $pkgdir/usr/bin/Plexamp.AppImage"
+	install -Dm755 squashfs-root/${_desktop_name} "$pkgdir"/usr/share/applications/${_desktop_name}
+	echo "Installing icons"
+	for res in "32x32" "128x128" "256x256@2"; do
+		install -Dm644 squashfs-root/usr/share/icons/hicolor/$res/apps/Plexamp.png $pkgdir/usr/share/icons/hicolor/$res/apps/Plexamp.png
+	done
+}
---
+++ update.sh
@@ -0,0 +1,35 @@
+cd $(dirname $0)
+
+PKGBUILD_VERSION=$(cat PKGBUILD | grep 'pkgver=' | cut -d'=' -f2)
+echo "Current PKGBUILD version:"
+echo $PKGBUILD_VERSION
+
+echo "Downloading latest update json"
+curl -O https://plexamp.plex.tv/desktop/tauri-update.json
+
+VERSION=$(jq -r '.version' tauri-update.json)
+echo "Latest Plexamp Beta Linux version:"
+echo $VERSION
+
+if [ $PKGBUILD_VERSION != $VERSION ]; then
+	echo "Newer version found, starting download"
+	LATEST_PACKAGE_X86_64=$(jq -r '.platforms."linux-x86_64".url' tauri-update.json)
+	LATEST_PACKAGE_AARCH64=$(jq -r '.platforms."linux-aarch64".url' tauri-update.json)
+	curl -O "$LATEST_PACKAGE_X86_64"
+	curl -O "$LATEST_PACKAGE_AARCH64"
+
+	sed -i "s/^pkgver=.*/pkgver=$VERSION/" PKGBUILD
+	makepkg
+	CHKSUM_X86_64="$(sha256sum Plexamp-$VERSION-x86_64.AppImage | cut -d' ' -f1)"
+        sed -i "s/^sha256sums_x86_64=.*/sha256sums_x86_64=('$CHKSUM_X86_64')/;s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+	CHKSUM_AARCH64="$(sha256sum Plexamp-$VERSION-aarch64.AppImage | cut -d' ' -f1)"
+        sed -i "s/^sha256sums_aarch64=.*/sha256sums_aarch64=('$CHKSUM_AARCH_64')/;s/^pkgrel=.*/pkgrel=1/" PKGBUILD
+        makepkg
+
+	echo "Press enter to continue"
+        read
+        makepkg --printsrcinfo > .SRCINFO
+        git add .SRCINFO PKGBUILD
+        git commit -m "Updated to $VERSION"
+        git push
+fi
