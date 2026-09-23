be9aadbc65d0f7d67585f35e87db9499b1b2e13f
Laura Hausmann <laura@hausmann.dev>
2026-09-24 01:27:00 +0200

    Update apply-upstream-patch.sh

--- apply-upstream-patch.sh
+++ apply-upstream-patch.sh
@@ -13,5 +13,7 @@ sed -i '' 's/pkgbase = ffmpeg/pkgbase = ffmpeg-libfdk_aac/g' "$1.new"
 sed -i '' 's/pkgname = ffmpeg/pkgname = ffmpeg-libfdk_aac/g' "$1.new"
 sed -i '' 's/pkgdesc = Complete solution to record, convert and stream audio and video/pkgdesc = Complete solution to record, convert and stream audio and video (Same as official package except with libfdk-aac support)/g' "$1.new"
 
+perl -0777 -i'' -pe 's/@@ pkgbase = ffmpeg-libfdk_aac\n \tprovides = libavutil.so\n \tprovides = libswresample.so\n \tprovides = libswscale.so\n/@@ pkgbase = ffmpeg-libfdk_aac\n \tprovides = libswresample.so\n \tprovides = libswscale.so\n \tconflicts = ffmpeg\n/g' "$1.new"
+
 # apply patch
 git am "$1.new" || git am --abort
